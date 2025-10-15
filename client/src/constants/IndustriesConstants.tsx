621d11fc42bb871a146424ab7ecb1f1f39199143
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-10-15 16:11:17 +0300

    FEAT: Defined industry constants

---
+++ client/src/constants/IndustriesConstants.tsx
@@ -0,0 +1,10 @@
+
+import manfk from "../assets/manfk.jpg"
+import corp from "../assets/corp.jpg"
+import creat from "../assets/creat.jpg"
+
+export const IndustriesConsts = [
+    { header: "Manufacturing & Industrial Operations", text: "Supports production-oriented businesses in managing employee schedules, tracking performance, handling leave requests, and encouraging continuous improvement through innovation and feedback modules.", image: manfk },
+    { header: "Corporate & Enterprise Organizations", text: "Ideal for medium to large-scale companies seeking to centralize HR, finance, and innovation processes. Castra helps streamline internal workflows, improve communication, and enhance employee engagement across departments.", image: corp },
+    { header: "Technology & Creative Agencies", text: "Perfect for fast-paced, project-driven teams that value collaboration, transparency, and recognition. The Suite enables efficient task coordination, idea sharing, and performance acknowledgment, fostering a culture of innovation and appreciation.", image: creat },
+];
