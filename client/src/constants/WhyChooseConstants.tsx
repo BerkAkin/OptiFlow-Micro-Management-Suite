eabadc2eb0c61ac0f7d30a54f6232a803b37fd65
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-10-15 16:11:26 +0300

    FEAT: Defined whychoose  constants

---
+++ client/src/constants/WhyChooseConstants.tsx
@@ -0,0 +1,6 @@
+export const WhyChooseItems = [
+    { id: 1, header: "Integrated Modules", txt: "Finance, human resources, innovation, leave management, and recognition systems are all unified in one platform — no need to juggle between multiple tools." },
+    { id: 2, header: "Data-Driven Decisions", txt: "With interactive charts, reports, and automated insights, management can make informed decisions quickly and accurately." },
+    { id: 3, header: "User-Friendly Interface", txt: "Each module is designed with simplicity and efficiency in mind, ensuring an optimized experience for both employees and managers." },
+    { id: 4, header: "Human-Centered Approach", txt: "Features like silent alerts, support requests, and recognition systems keep employee satisfaction and engagement at the heart of your operations." },
+]
