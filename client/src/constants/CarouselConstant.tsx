37c6b8e2d5e77d6a96f7e7479b8ae525404f4667
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-10-16 19:58:19 +0300

    STYLE: Renamed project constants

--- client/src/constants/CarouselConstant.tsx
+++ client/src/constants/CarouselConstant.tsx
@@ -1,6 +1,6 @@
 
 const CarouselConstant = [
-    { id: 1, title: "1", text: "Castra Micromanagement Suite unites all these modules into one cohesive environment — helping organizations increase operational efficiency, strengthen communication, and build a culture of trust and recognition." },
+    { id: 1, title: "1", text: "OptiFlow Management Suite unites all these modules into one cohesive environment — helping organizations increase operational efficiency, strengthen communication, and build a culture of trust and recognition." },
     { id: 2, title: "2", text: "Collect valuable insights from employees through fully customizable surveys. Analyze results with graphical reports, identify emerging trends, and automatically generate insights such as “High salary expectation among staff.”Empowers management to make informed, people-centered decisions." },
     { id: 3, title: "3", text: "Foster a culture of creativity by allowing employees to submit, comment on, and vote for new ideas or process improvements. Managers can review and approve submissions, assign recognition, and track engagement statistics such as “Most Active Contributor” or “Idea of the Year.”" },
     { id: 4, title: "4", text: "A discreet and secure channel for employees to report sensitive situations such as harassment, bullying, or emotional distress.Accessed via a hidden shortcut reports remain confidential and visible only to HR. Includes anonymous support requests and awareness statistics for organizational well-being." },
