98d9de6c50bf13a7510833558dbc209bd10e0270
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-10-16 19:58:40 +0300

    FEAT: Footer constants defined

---
+++ client/src/constants/FooterContactsConstants.tsx
@@ -0,0 +1,10 @@
+import phone from '../assets/phone.png'
+import location from '../assets/location.png'
+import mail from '../assets/mail.png'
+
+
+export const FooterContactsConstants = [
+    { icon: phone, header: "Phone Us", text: "Monday – Friday : 10am to 6pm +123456789" },
+    { icon: mail, header: "Mail Us", text: "OptiFlowManagement@info.com" },
+    { icon: location, header: "Visit Us", text: "41 City Road, Izmit, Kocaeli, Turkey, 28/4" }
+]
