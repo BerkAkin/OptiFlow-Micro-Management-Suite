0050b27613e0ff9ed2013dfc8d4e750ee43bd3dd
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-10-16 19:58:06 +0300

    FIX: fixed script

--- client/src/components/PageTopBtn/PageTopBtn.tsx
+++ client/src/components/PageTopBtn/PageTopBtn.tsx
@@ -1,14 +1,10 @@
 export default function PageTopBtn() {
 
     const scrollTop = () => {
-
         window.scrollTo({ top: 0, behavior: 'smooth' })
-
     }
-
-
     return (
-        <div className='fixed rounded-3xl w-[50px] h-[50px] bg-indigo-400 border border-indigo-500 shadow-xl bottom-10 right-10 z-50'>
+        <div className='fixed rounded-3xl w-[50px] h-[50px] bg-indigo-500  shadow-xl bottom-10 right-10 z-50'>
             <button className='w-full h-full text-3xl text-white cursor-pointer' onClick={scrollTop} > ↑ </button>
         </div>
     )
