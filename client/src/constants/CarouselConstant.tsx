8aaf60bc964b01afabe8e72a470188d821f11aaa
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-10-17 22:52:19 +0300

    FEAT: Added images and titles

--- client/src/constants/CarouselConstant.tsx
+++ client/src/constants/CarouselConstant.tsx
@@ -1,11 +1,18 @@
+import tempA from '../assets/tempA.jpg'
+import tempB from '../assets/tempB.jpg'
+import tempC from '../assets/tempC.jpg'
+import tempD from '../assets/tempD.jpg'
+import tempE from '../assets/tempE.jpg'
+import tempF from '../assets/tempF.jpg'
+
 
 const CarouselConstant = [
-    { id: 1, title: "1", text: "OptiFlow Management Suite unites all these modules into one cohesive environment — helping organizations increase operational efficiency, strengthen communication, and build a culture of trust and recognition." },
-    { id: 2, title: "2", text: "Collect valuable insights from employees through fully customizable surveys. Analyze results with graphical reports, identify emerging trends, and automatically generate insights such as “High salary expectation among staff.”Empowers management to make informed, people-centered decisions." },
-    { id: 3, title: "3", text: "Foster a culture of creativity by allowing employees to submit, comment on, and vote for new ideas or process improvements. Managers can review and approve submissions, assign recognition, and track engagement statistics such as “Most Active Contributor” or “Idea of the Year.”" },
-    { id: 4, title: "4", text: "A discreet and secure channel for employees to report sensitive situations such as harassment, bullying, or emotional distress.Accessed via a hidden shortcut reports remain confidential and visible only to HR. Includes anonymous support requests and awareness statistics for organizational well-being." },
-    { id: 5, title: "5", text: "Encourages a positive workplace culture where employees can thank and recognize each other’s contributions. Features include a “Thank You” system, “Employee of the Month” algorithm, and a digital “Wall of Appreciation” that spreads positivity and builds engagement." },
-    { id: 6, title: "6", text: "Designed for small and medium-sized businesses, this module enables effortless tracking of income and expenses, invoice creation, and payment management. Generate professional PDF invoices, import and export Excel reports, and visualize your financial health with monthly balance charts — all in one place." },
+    { id: 1, image: tempA, title: "Financial Tracking and Invoicing", text: "OptiFlow Management Suite unites all these modules into one cohesive environment — helping organizations increase operational efficiency, strengthen communication, and build a culture of trust and recognition." },
+    { id: 2, image: tempB, title: "Corporate Survey and Insight", text: "Collect valuable insights from employees through fully customizable surveys. Analyze results with graphical reports, identify emerging trends, and automatically generate insights such as “High salary expectation among staff.”Empowers management to make informed, people-centered decisions." },
+    { id: 3, image: tempC, title: "Corporate Suggestion and Innovation", text: "Foster a culture of creativity by allowing employees to submit, comment on, and vote for new ideas or process improvements. Managers can review and approve submissions, assign recognition, and track engagement statistics such as “Most Active Contributor” or “Idea of the Year.”" },
+    { id: 4, image: tempD, title: "Permission Tracking and Approval System", text: "A discreet and secure channel for employees to report sensitive situations such as harassment, bullying, or emotional distress.Accessed via a hidden shortcut reports remain confidential and visible only to HR. Includes anonymous support requests and awareness statistics for organizational well-being." },
+    { id: 5, image: tempE, title: "Employee Support", text: "Encourages a positive workplace culture where employees can thank and recognize each other’s contributions. Features include a “Thank You” system, “Employee of the Month” algorithm, and a digital “Wall of Appreciation” that spreads positivity and builds engagement." },
+    { id: 6, image: tempF, title: "Appreciation System", text: "Designed for small and medium-sized businesses, this module enables effortless tracking of income and expenses, invoice creation, and payment management. Generate professional PDF invoices, import and export Excel reports, and visualize your financial health with monthly balance charts — all in one place." },
 ]
 
 export default CarouselConstant
