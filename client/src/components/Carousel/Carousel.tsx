ecab81f3d7513106cd256450c4073d8f57588f6e
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-10-17 22:52:04 +0300

    STYLE: Carousel is now animated on click

--- client/src/components/Carousel/Carousel.tsx
+++ client/src/components/Carousel/Carousel.tsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
+import { motion, AnimatePresence, scale, easeIn, animate } from "framer-motion";
 
 interface CarouselProps {
-    items: { id: number, title: string, text: string }[]
+    items: { id: number, image: string, title: string, text: string }[]
 
 }
 
@@ -23,16 +24,33 @@ function Carousel({ items }: CarouselProps) {
     return (
         <>
 
+
+
             <div className='relative flex items-center justify-center text-gray-700'>
                 {carouselItems.map((item, index) => (
-                    <div key={item.id} onClick={rotateCarousel} className={`absolute shadow-md bg-white cursor-pointer rounded-sm w-[700px] h-[400px] border`} style={{ zIndex: carouselItems.length - index, marginLeft: index * 35, marginBottom: index * 25 }}>
-                        <div className='grid grid-cols-2 w-[100%] h-[100%]'>
-                            <h2 className="text-2xl font-bold">{item.title}</h2>
-                            <p className="mt-2" >{item.text}</p>
+                    <motion.div
+                        whileHover={{ y: index === 0 ? 20 : 0, transition: { duration: 0.1 } }}
+                        animate={{ y: index === 5 ? [-20, -10, 0] : 0, transition: { duration: 0.5 } }}
+                        key={item.id} onClick={rotateCarousel}
+                        className={`grid grid-cols-2 absolute shadow-md cursor-pointer rounded-sm w-[700px] h-[400px] border`}
+                        style={{ backgroundImage: `url(${item.image})`, backgroundSize: "cover", backgroundPosition: "center", backgroundRepeat: "no-repeat", zIndex: carouselItems.length - index, marginLeft: index * 35, marginBottom: index * 25 }}
+                    >
+
+
+                        <div>
                         </div>
-                    </div>
+                        <div>
+                            <div className='bg-white h-[280px] rounded-b-xs border'>
+                                <h2 className=" font-bold text-center p-5 text-xl" style={{ fontFamily: "roobert" }}>{item.title}</h2>
+                                <p className="text-sm p-5 text-center" >{item.text}</p>
+                            </div>
+
+                        </div>
+                    </motion.div>
                 ))}
             </div >
+
+
         </>
     )
 }
