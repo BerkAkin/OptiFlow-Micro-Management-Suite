21922a13693a35cee0c7e3ea34ce2112333c10c0
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-10-27 16:49:51 +0300

    STYLE: Main page is now rendering conditionally

---
+++ client/MainClient/src/pages/MainPage.tsx
@@ -0,0 +1,57 @@
+import { useState } from 'react'
+import ModuleSelection from '../components/ModuleSelection/ModuleSelection'
+import Navbar from '../components/Navbar/Navbar'
+import LoginModal from '../components/LoginContainer/LoginContainer'
+import Sidebar from '../components/Sidebar/Sidebar'
+import { useSelectedModuleContext } from '../context/SelectedModuleContext'
+import { useAuthContext } from '../context/AuthContext'
+import { Outlet } from 'react-router-dom'
+
+function MainPage() {
+    const { selectedModule } = useSelectedModuleContext();
+    const { isAuth, setIsAuth } = useAuthContext();
+
+
+    return (
+        <div>
+
+            <Navbar />
+
+
+            <div>
+                {
+                    !isAuth ?
+                        (
+                            <div className='flex items-center justify-center h-[90dvh]'>
+                                <LoginModal />
+                            </div>
+                        )
+                        : selectedModule === null ?
+
+                            (
+                                <div className='flex items-center justify-center h-[90dvh]'>
+                                    <ModuleSelection />
+                                </div>
+                            )
+                            :
+
+                            (
+                                <div className=" grid grid-cols-[15%_85%] h-[90dvh]">
+                                    <div className='border-e h-[100%]'>
+                                        <Sidebar />
+                                    </div>
+                                    <div className='h-[100%] overflow-scroll'>
+                                        <Outlet />
+                                    </div>
+
+                                </div>
+                            )
+                }
+            </div>
+
+
+        </div>
+    )
+}
+
+export default MainPage
