cdb6e57a483f7689d58bc6cbd70346e5221e18c9
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-10-27 16:49:03 +0300

    FEAT: Created sidebar to load outlet from looped sub headings of module

---
+++ client/MainClient/src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,27 @@
+import { Link } from 'react-router-dom'
+import { useSelectedModuleContext } from '../../context/SelectedModuleContext'
+
+
+function Sidebar() {
+    const { selectedModule, moduleParts } = useSelectedModuleContext()
+    return (
+        <div>
+            <div className="flex flex-col w-full bg-white">
+                <div className=" px-8 py-4 flex flex-row items-center justify-between">
+                    <a className="text-xl text-indigo-500 font-semibold text-gray-800 " style={{ fontFamily: "roobert" }}>{selectedModule}</a>
+
+                </div>
+                <nav className="px-4 ">
+                    {moduleParts.map((item: { url: string, module: string }, index: number) => (
+                        <>
+                            <Link to={`${item.url}`} className="block px-4 py-2 mt-2 text-sm text-gray-900 bg-transparent rounded-sm hover:bg-gray-200 focus:bg-indigo-200">{item.module}</Link>
+                        </>
+                    ))}
+
+                </nav>
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
