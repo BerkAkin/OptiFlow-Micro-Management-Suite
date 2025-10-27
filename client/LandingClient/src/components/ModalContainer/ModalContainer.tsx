534dbbb6ca018d7cf53ef049e1b9b601e4837a85
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-10-27 16:44:04 +0300

    REFACTOR: Removed Login modal checks

--- client/LandingClient/src/components/ModalContainer/ModalContainer.tsx
+++ client/LandingClient/src/components/ModalContainer/ModalContainer.tsx
@@ -1,17 +1,13 @@
-import React, { useContext } from 'react'
-import { ModalContext } from '../../context/ModalContext';
-import LoginModal from '../Auth/LoginModal';
 import RegisterModal from '../Auth/RegisterModal';
 
 
 function ModalContainer() {
 
-    const { modalType } = useContext(ModalContext);
 
     return (
         <>
             <div className=' w-[100%] h-full bg-black/40 absolute z-10 flex items-center justify-center backdrop-blur-sm '>
-                {modalType == "login" ? <LoginModal /> : modalType == "register" ? <RegisterModal /> : ""}
+                <RegisterModal />
             </div>
         </>
     )
