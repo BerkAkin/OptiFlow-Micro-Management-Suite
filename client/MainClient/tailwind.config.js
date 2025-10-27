c7652ce3d8493d0bf5ca49d0ae1590436ee7e914
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-10-27 16:45:41 +0300

    FIX: Tailwind certain styling register

---
+++ client/MainClient/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+  extend: {
+  },
+},
+   safelist: [
+    { pattern: /bg-(red|cyan|indigo|violet|purple|fuchsia|emerald|lime|teal|amber|rose)-(100|200|300|400|500|600|700|800|900)/ },
+     { pattern: /border-(red|cyan|indigo|violet|purple|fuchsia|emerald|lime|teal|amber|rose)-(100|200|300|400|500|600|700|800|900)/ }
+  ],
+  plugins: [],
+}
+
