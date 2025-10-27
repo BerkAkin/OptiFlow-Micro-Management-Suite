b4219b376c7df374f540d89b00bb223ee5f834ac
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-10-27 16:48:13 +0300

    FEAT: Module selection is now looping modules through constants

---
+++ client/MainClient/src/components/ModuleSelection/ModuleSelection.tsx
@@ -0,0 +1,25 @@
+import { useSelectedModuleContext } from "../../context/SelectedModuleContext";
+import { modulePaths } from "../../config/modulePaths";
+
+function ModuleSelection() {
+    const { setSelectedModule, setModuleParts } = useSelectedModuleContext();
+
+    return (
+        <div className="mx-auto w-[950px] h-[600px] grid grid-cols-3 gap-8">
+            {Object.entries(modulePaths).map(([moduleName, info]) => (
+                <div key={moduleName} className={`${info.color.split(" ")[0]} text-center flex items-center justify-center rounded-sm`}>
+                    <button
+                        onClick={() => {
+                            setSelectedModule(moduleName);
+                            setModuleParts(info.parts);
+                        }}
+                        className={`text-white text-3xl p-5 h-full w-full transition duration-400 hover:scale-105 ${info.color}`} style={{ fontFamily: "roobert" }}>
+                        {moduleName}
+                    </button>
+                </div>
+            ))}
+        </div>
+    );
+}
+
+export default ModuleSelection;
