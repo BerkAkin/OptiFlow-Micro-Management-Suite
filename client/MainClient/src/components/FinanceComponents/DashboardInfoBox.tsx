4df7704a318c20338e3e62c08fe8c8d082cb9979
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-10-28 18:09:32 +0300

    FEAT: Created info box to show on dashboardhcarts component to inform user about certain datas

---
+++ client/MainClient/src/components/FinanceComponents/DashboardInfoBox.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+interface DashboardInfoBoxProps {
+    Header: string,
+    Text: string,
+    Color: string
+}
+
+function DashboardInfoBox({ Header, Text, Color }: DashboardInfoBoxProps) {
+    return (
+        <div className={`border bg-white justify-center rounded-sm flex shadow-md grid grid-cols-[60%_40%] `}>
+            <div className={`h-[50px] flex items-center justify-center`} style={{ fontFamily: "roobert" }}>
+                <p className={`text-lg ps-1 text-gray-700 text-${Color}-600`}>
+                    {Header}
+                </p>
+            </div>
+            <div className={`border-s flex items-center justify-center`} style={{ fontFamily: "roobert" }}>
+                <p className={`text-xl text-gray-700 ps-1 `}>
+                    {Text}$
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default DashboardInfoBox
