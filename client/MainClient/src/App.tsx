48152f64f1d0778e74a39d42b3ce4294742254c3
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-10-29 14:17:47 +0300

    REFACTOR: Renamed outgoing variables as expenses

--- client/MainClient/src/App.tsx
+++ client/MainClient/src/App.tsx
@@ -3,7 +3,8 @@ import './App.css';
 import MainPage from './pages/MainPage';
 import FinanceDashboardPage from './pages/FinancePages/FinanceDashboardPage';
 import FinancePaymentsPage from './pages/FinancePages/FinancePaymentsPage';
-import FinanceIncomeExpensePage from './pages/FinancePages/FinanceIncomeExpensePage';
+import FinanceIncomePage from './pages/FinancePages/FinanceIncomePage';
+import FinanceExpensePage from './pages/FinancePages/FinanceExpensePage';
 import FinanceBillsPage from './pages/FinancePages/FinanceBillsPage';
 
 function App() {
@@ -14,7 +15,8 @@ function App() {
           <Route path='/' element={<MainPage />} >
             <Route path='finance/financeDashboard' element={<FinanceDashboardPage />} />
             <Route path='finance/financeBills' element={<FinanceBillsPage />} />
-            <Route path='finance/financeIncomeExpensePage' element={<FinanceIncomeExpensePage />} />
+            <Route path='finance/financeIncomePage' element={<FinanceIncomePage />} />
+            <Route path='finance/financeExpensePage' element={<FinanceExpensePage />} />
             <Route path='finance/financePayments' element={<FinancePaymentsPage />} />
           </Route>
         </Routes>
