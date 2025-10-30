12a3d8b2147ee8220d1ef4cb290ecdd75db34ca3
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-10-30 20:53:00 +0300

    FEAT: Created calendar component as visually

---
+++ client/MainClient/src/components/FinanceComponents/Calendar/Calendar.tsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react'
+import DateSelectArg from '@fullcalendar/react'
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from '@fullcalendar/interaction'
+
+import './styles.css'
+
+
+
+interface EventType {
+    id: string
+    title: string
+    start: string
+    end?: string
+}
+
+function Calendar() {
+    const [events, setEvents] = useState<EventType[]>([])
+
+    // buraya backend olayları eklenecek 
+
+    const handleDateSelect = (selectInfo: any) => {
+        const title = prompt('Etkinlik adı girin:')
+        if (title) {
+            const newEvent: EventType = {
+                id: String(events.length + 1),
+                title,
+                start: selectInfo.startStr,
+                end: selectInfo.endStr
+            }
+            setEvents((prev) => [...prev, newEvent])
+        }
+    }
+
+    return (
+        <div className="fc-toolbar fc-header-toolbar">
+            <FullCalendar
+                plugins={[dayGridPlugin, interactionPlugin]}
+                initialView="dayGridMonth"
+                selectable={true}
+                events={events}
+                select={handleDateSelect}
+                editable={true}
+                themeSystem=''
+                headerToolbar={{
+                    left: 'prev,next today',
+                    center: 'title',
+                    right: ''
+                }}
+                buttonText={{
+                    today: 'Today',
+                }}
+
+            />
+        </div>
+    )
+}
+
+export default Calendar
