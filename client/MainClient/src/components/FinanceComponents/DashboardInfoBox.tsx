25f99a903e86b7b8fedacea8d8e2d39021fa49d0
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-10-30 20:52:43 +0300

    STYLE: Minor improvements

--- client/MainClient/src/components/FinanceComponents/DashboardInfoBox.tsx
+++ client/MainClient/src/components/FinanceComponents/DashboardInfoBox.tsx
@@ -8,7 +8,7 @@ interface DashboardInfoBoxProps {
 
 function DashboardInfoBox({ Header, Text, Color }: DashboardInfoBoxProps) {
     return (
-        <div className={`border bg-white justify-center rounded-sm flex shadow-md grid grid-cols-[55%_45%] `}>
+        <div className={`border bg-white justify-center rounded-sm flex shadow-sm grid grid-cols-[55%_45%] `}>
             <div className={`h-[50px] flex items-center justify-center`} style={{ fontFamily: "roobert" }}>
                 <p className={`text-lg ps-1 text-gray-700 text-${Color}-600`}>
                     {Header}
