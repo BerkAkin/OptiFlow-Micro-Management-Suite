7afac62ba793f84c6981e2e430da877d3957a3aa
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-11-13 17:32:48 +0300

    STYLE: Activity tab took the place of  income and expense tabs

--- client/MainClient/src/pages/FinancePages/FinanceDashboardPage.tsx
+++ client/MainClient/src/pages/FinancePages/FinanceDashboardPage.tsx
@@ -5,9 +5,9 @@ import DoughChart from '../../components/FinanceComponents/Charts/DoughChart';
 import VerticalChart from '../../components/FinanceComponents/Charts/VerticalChart';
 import InstallmentPaymentsCard from '../../components/FinanceComponents/InstallmentPaymentsCard/InstallmentPaymentsCard';
 import RemainingPaymentsCard from '../../components/FinanceComponents/RemainingPaymentsCard/RemainingPaymentsCard';
-import AddIncomeOrExpenseForm from '../../components/FinanceComponents/AddIncomeOrExpenseForm/AddIncomeOrExpenseForm';
-import IncomeExpenseTable from '../../components/FinanceComponents/IncomeExpenseTable/IncomeExpenseTable';
+import AddActivityForm from '../../components/FinanceComponents/AddActivityForm/AddActivityForm';
 import AddPaymentForm from '../../components/FinanceComponents/AddPaymentForm/AddPaymentForm';
+import ActivityTable from '../../components/FinanceComponents/ActivityTable/ActivityTable';
 
 ChartJS.register(ArcElement, CategoryScale, Tooltip, Legend, LinearScale, BarElement, PointElement, LineElement)
 
@@ -30,43 +30,38 @@ veritabanından sadece label ve data kısmı gelcek
 function FinanceDashboardPage() {
     return (
         <div className='container mx-auto mt-10'>
-            <div className='grid grid-cols-[20%_20%_20%_40%] gap-2'>
-                <div className='bg-white shadow-md border '>
+            <div className='grid flex justify-center grid-cols-[20%_20%_20%_30%] gap-10'>
+                <div className='bg-white shadow-lg rounded-lg border '>
                     <BarChart />
                 </div>
-                <div className='bg-white shadow-md border'>
+                <div className='bg-white shadow-lg rounded-lg border'>
                     <DoughChart />
                 </div>
-                <div className='bg-white shadow-md border'>
+                <div className='bg-white shadow-lg rounded-lg border'>
 
                     <VerticalChart />
                 </div>
-                <div className='bg-white shadow-md border'>
+                <div className='bg-white shadow-lg rounded-lg border'>
                     <Calendar />
                 </div>
             </div>
 
-            <div className='grid grid-cols-[40%_40%_20%] gap-2 my-5'>
-
-                <div className='bg-white shadow-md border '>
-                    <IncomeExpenseTable colorScheme='green' filterFn={() => { }} />
+            <div className='flex justify-center mx-auto grid grid-cols-[71%_25%] gap-5 my-10'>
+                <div className='bg-white shadow-lg border rounded-lg'>
+                    <ActivityTable />
                 </div>
-                <div className='bg-white shadow-md border '>
-                    <IncomeExpenseTable colorScheme='red' filterFn={() => { }} />
-                </div>
-
-                <div className='bg-white shadow-md border'>
-                    <AddIncomeOrExpenseForm />
+                <div className='bg-white shadow-lg border rounded-lg'>
+                    <AddActivityForm />
                 </div>
             </div>
 
-            <div className='grid grid-cols-[40%_40%_20%] gap-2'>
-                <div className='bg-white shadow-md border'>
+            <div className='flex justify-center grid grid-cols-[35%_35%_25%] gap-5 my-10'>
+                <div className='bg-white shadow-lg border rounded-lg'>
                     <InstallmentPaymentsCard /></div>
-                <div className='bg-white shadow-sm border'>
+                <div className='bg-white shadow-lg border rounded-lg'>
                     <RemainingPaymentsCard />
                 </div>
-                <div className='bg-white shadow-md border'>
+                <div className='bg-white shadow-lg border rounded-lg'>
                     <AddPaymentForm />
                 </div>
 
