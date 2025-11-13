1f4d6e413e4c1137ba48367232f14cac6192cb30
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-11-13 17:29:35 +0300

    STYLE: Updated location of button to swap between incomes and expenses

--- client/MainClient/src/components/FinanceComponents/Charts/VerticalChart.tsx
+++ client/MainClient/src/components/FinanceComponents/Charts/VerticalChart.tsx
@@ -8,7 +8,7 @@ function VerticalChart() {
     const ChartExample = [[65, 81], [36, 24]];
 
     const [chartData, setChartData] = useState<number[][]>(ChartExample);
-    const Colors = ['#16a34a', '#ef4444']
+    const Colors = ['#22c55e', '#f87171']
     const [color, setColor] = useState<string[]>(Colors)
 
     const ChartData = {
@@ -33,19 +33,22 @@ function VerticalChart() {
 
     return (
         <div className="h-[420px]">
-            <div className='flex justify-center h-[10%]'>
-                <p className={`text-2xl text-center w-64 border-b border-x text-indigo-400 border-indigo-400 bg-white rounded-b-md `} style={{ fontFamily: "roobert" }}>Most</p>
+            <div className='flex justify-center h-[8%]'>
+                <p className={`text-xl text-center w-64 rounded-b-sm text-white`} style={{ backgroundColor: `${color[0]}`, fontFamily: "roobert" }}>
+                    Most
+                    <span>
+                        <button className={`h-[20px] text-white text-2xl ps-1`} onClick={handleNext}> ⇄</button>
+                    </span>
+                </p>
             </div>
-            <div className='h-[60%]'>
-                <Bar data={ChartData} className='p- h-96' options={{ maintainAspectRatio: false, indexAxis: 'y' as const, responsive: true, plugins: { legend: { display: false, } }, scales: { x: { reverse: true }, y: { position: "right" } } }} />
+            <div className='h-[70%] pt-5'>
+                <Bar data={ChartData} className='h-96' options={{ maintainAspectRatio: false, indexAxis: 'y' as const, responsive: true, plugins: { legend: { display: false, } }, scales: { x: { reverse: true }, y: { position: "right" } } }} />
             </div>
             <div className=' h-[10%]'>
 
 
             </div>
-            <div className='h-[20%] flex justify-center items-center '>
-                <button className={`h-[50px] bg-indigo-400 text-white text-4xl text-md  w-[80%] rounded-sm hover:bg-indigo-500`} onClick={handleNext}>⇄</button>
-            </div>
+
         </div>
     )
 }
