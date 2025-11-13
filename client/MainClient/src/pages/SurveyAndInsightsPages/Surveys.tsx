ccd1ddfe148d9930575b29cd47e1cfa867d412b6
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-11-13 17:33:37 +0300

    FEAT & STYLE: Surveys are now having a button that leads to show results and thier visuals are now smaller

--- client/MainClient/src/pages/SurveyAndInsightsPages/Surveys.tsx
+++ client/MainClient/src/pages/SurveyAndInsightsPages/Surveys.tsx
@@ -28,57 +28,63 @@ function SurveyResults() {
     }
 
     return (
-        <div className='m-10 bg-white shadow-md h-[800px]'>
+        <div className='m-10'>
             <div className='w-[95%] mt-10 mx-auto grid grid-cols-2 gap-4' >
                 {initialTempSurvey.surveys.map((item, index) => (
-                    <div key={index} className={`my-4 shadow-sm h-[400px] grid grid-cols-[40%_60%] border`}>
+                    <div key={index} className={`my-4 bg-white shadow-md h-[200px] grid grid-cols-[40%_60%] border rounded-lg`}>
                         <div>
                             <Link to={`${item.status === "Active" ? `/survey/surveys/${item.slug}` : ""} `} className={`${item.status === "Timeout" ? "cursor-default" : " cursor-pointer"}`}>
                                 <div className={`${item.status === "Timeout" ? "bg-gray-100" : "hover:bg-indigo-50"} w-full h-full border-e`}>
-                                    <div className='h-[33%] flex items-end justify-center'>
-                                        <p style={{ fontFamily: "roobert" }} className='text-center text-3xl  text-gray-600 mx-4'>{item.text}</p>
+                                    <div className='h-[50%] flex items-center justify-center'>
+                                        <p style={{ fontFamily: "roobert" }} className='text-center text-3xl text-gray-600 mx-4'>
+                                            {item.text}
+                                            <span className='text-sm'>
+                                                {item.status === "Timeout" ?
+                                                    (
+                                                        <span className='h-[10%] flex justify-end text-sky-500'>
+                                                            <Link className='pe-2' to={`${item.slug}/result`}>Go to Result {">"}</Link>
+                                                        </span>
+                                                    ) :
+                                                    ""}
+                                            </span>
+                                        </p>
+
                                     </div>
-                                    <div className='h-[33%] flex items-end justify-center'>
+                                    <div className='h-[25%] flex items-end justify-center'>
                                         <p style={{ fontFamily: "roobert" }} className={item.status === "Active" ? "text-green-700 p-2 text-xl" : "text-red-600 text-xl"}>{item.status}</p>
                                     </div>
-                                    <div className='h-[33%] flex items-center justify-center'>
+                                    <div className='h-[25%] flex items-center justify-center'>
                                         <p style={{ fontFamily: "roobert" }} className='text-gray-600 '>{item.date}</p>
-
                                     </div>
-
                                 </div>
                             </Link>
                         </div>
                         <div>
 
-                            <div className='h-[60%] flex w-[100%] p-10 justify-center items-center'>
-                                <Doughnut
-                                    data={{
-                                        labels: ['Participators', 'Nonparticipators'],
-                                        datasets: [{
-                                            data: [item.participationCount, item.totalEmployee - item.participationCount],
-                                            backgroundColor: ['#34d399', '#22c55e',],
-                                        }]
-                                    }}
-                                    options={{ maintainAspectRatio: false, plugins: { legend: { display: true, position: "right" } } }}
-                                />
+                            <div className='grid  h-full grid-cols-3'>
+                                <div className='my-auto'>
+                                    <Doughnut
+                                        data={{
+                                            labels: ['Participators', 'Nonparticipators'],
+                                            datasets: [{
+                                                data: [item.participationCount, item.totalEmployee - item.participationCount],
+                                                backgroundColor: ['#34d399', '#22c55e',],
+                                            }]
+                                        }}
+                                        options={{ plugins: { legend: { display: true, position: "bottom" } } }} />
+                                </div>
+
+                                <div className='bg-orange-400 text-white rounded-sm '>
+                                    <div className='h-[50%] flex items-center justify-center'> <p className=' text-8xl'>☺</p></div>
+                                    <div className='h-[50%] flex items-center justify-center'><p className=' text-4xl'>{item.satisfactionCount} </p></div>
+                                </div>
+                                <div className='bg-sky-400 text-white rounded-sm '>
+                                    <div className='h-[50%] flex items-center justify-center'> <p className=' text-6xl pt-5'>☹</p></div>
+                                    <div className='h-[50%] flex items-center justify-center'><p className=' text-4xl'>{item.totalEmployee - item.satisfactionCount} </p></div>
+                                </div>
+
                             </div>
 
-                            <div className='h-[40%]  grid grid-cols-2'>
-                                <div>
-                                    <div className='w-[70%] bg-orange-400 rounded-sm mx-auto h-full'>
-                                        <div className='h-[50%] flex items-center justify-center'> <p className='text-white text-8xl'>☺</p></div>
-                                        <div className='h-[50%] flex items-center justify-center'><p className='text-white text-4xl'>{item.satisfactionCount} </p></div>
-                                    </div>
-                                </div>
-                                <div>
-                                    <div className='w-[70%] bg-sky-400 rounded-sm mx-auto  h-full'>
-                                        <div className='h-[50%] flex items-center justify-center'> <p className='text-white text-6xl pt-5'>☹</p></div>
-                                        <div className='h-[50%] flex items-center justify-center'><p className='text-white text-4xl'>{item.totalEmployee - item.satisfactionCount} </p></div>
-                                    </div>
-
-                                </div>
-                            </div>
                         </div>
 
                     </div>
@@ -87,7 +93,7 @@ function SurveyResults() {
             </div>
 
 
-        </div>
+        </div >
     )
 }
 
