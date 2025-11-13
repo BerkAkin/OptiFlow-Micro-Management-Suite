deff546d1cd2cd872befb884c06ef14c43922ace
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-11-13 17:30:19 +0300

    STYLE: Minor improvements

--- client/MainClient/src/components/FinanceComponents/RemainingPaymentsCard/RemainingPaymentsCard.tsx
+++ client/MainClient/src/components/FinanceComponents/RemainingPaymentsCard/RemainingPaymentsCard.tsx
@@ -10,20 +10,20 @@ function TodaysPaymentsCard() {
     return (
         <div>
             <div className='w-full h-[50px] text-center flex justify-center items-start'>
-                <p className={`text-2xl text-center w-64 border-b border-x text-orange-400 border-orange-400 bg-white rounded-b-md `} style={{ fontFamily: "roobert" }}>Remaining Payments</p>
+                <p className={`text-2xl text-center w-64 text-white bg-orange-400 rounded-b-sm `} style={{ fontFamily: "roobert" }}>Remainings</p>
             </div>
-            <div className='border-b h-5 w-full grid grid-cols-[65%_10%_10%_10%] gap-2 flex justify-center uppercase'>
+            <div className='border-b h-5 w-full grid grid-cols-[65%_10%_10%_10%] gap-2 text-orange-400 flex justify-center'>
                 <div>
-                    <p className={`text-sm font-bold text-start ps-5 w-full text-gray-700 `} style={{ fontFamily: "roobert" }}>Description</p>
+                    <p className={`text-sm font-bold text-start ps-5 w-full `} style={{ fontFamily: "roobert" }}>Description</p>
                 </div>
                 <div>
-                    <p className={`text-sm font-bold text-center w-full text-gray-700 `} style={{ fontFamily: "roobert" }}>To</p>
+                    <p className={`text-sm font-bold text-center w-full `} style={{ fontFamily: "roobert" }}>To</p>
                 </div>
                 <div>
-                    <p className={`text-sm font-bold text-center w-full text-gray-700 `} style={{ fontFamily: "roobert" }}>Price</p>
+                    <p className={`text-sm font-bold text-center w-full `} style={{ fontFamily: "roobert" }}>Price</p>
                 </div>
                 <div>
-                    <p className={`text-sm font-bold text-center w-full text-gray-700 `} style={{ fontFamily: "roobert" }}></p>
+                    <p className={`text-sm font-bold text-center w-full `} style={{ fontFamily: "roobert" }}></p>
                 </div>
             </div>
 
@@ -38,7 +38,7 @@ function TodaysPaymentsCard() {
                     <p>{TodaysPayments.Price}</p>
                 </div>
                 <div className='text-gray-600 flex items-center justify-center'>
-                    <button className='bg-indigo-400 hover:bg-indigo-500 rounded-sm text-white p-1 me-1 w-full '>Pay</button>
+                    <button className='bg-gray-400 hover:bg-gray-500 rounded-sm text-white p-1 me-1 w-full '>$</button>
                 </div>
             </div>
         </div>
