3077e16efdea1f24ac866a5f0a83db8365b70bd7
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-11-13 17:31:01 +0300

    STYLE:Minor improvements

--- client/MainClient/src/pages/SurveyAndInsightsPages/SurveyBuilder.tsx
+++ client/MainClient/src/pages/SurveyAndInsightsPages/SurveyBuilder.tsx
@@ -89,7 +89,7 @@ function SurveyBuilder() {
 
     /* Value kısmı eklenecek iç answer inputunda */
     return (
-        <div className='m-10 bg-white container  mx-auto shadow-md  border'>
+        <div className='m-10 bg-white container mx-auto border shadow-md rounded-lg'>
             <div className='text-center'>
                 <p className='text-5xl text-gray-600 p-5' style={{ fontFamily: "roobert" }}>Build New Survey</p>
             </div>
@@ -98,9 +98,9 @@ function SurveyBuilder() {
                 <button onClick={() => handleAddNewQuestion(newQuestionHeader)} className=' bg-green-600 hover:bg-green-700 rounded-sm text-white p-1 text-lg'>+ Add New Question</button>
 
             </div>
-            <div >
+            <div>
                 {survey.questions.map((question) => (
-                    <div key={question.id} className='rounded-sm p-5 m-5 border shadow-sm'>
+                    <div key={question.id} className='rounded-lg p-5 m-5 border shadow-sm'>
                         <div className='grid grid-cols-[90%_10%]'>
                             <div><p className='text-lg text-gray-800'>{question.id}{`) `}{question.text}</p></div>
                             <div><button onClick={() => deleteQuestion(question.id)} className=' bg-red-400 hover:bg-red-500 rounded-sm text-white p text-lg p-1'>- Delete Question</button></div>
