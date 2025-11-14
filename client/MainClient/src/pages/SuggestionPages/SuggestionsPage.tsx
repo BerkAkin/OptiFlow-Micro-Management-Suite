2a13bbe90e9d985fe75a61129d160410bfa560c8
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-11-14 16:35:46 +0300

    FEAT: Suggestionpage implemented to show related content

---
+++ client/MainClient/src/pages/SuggestionPages/SuggestionsPage.tsx
@@ -0,0 +1,81 @@
+import React from 'react'
+import SuggestionCard from '../../components/SuggestionComponents/SuggestionCard/SuggestionCard'
+import SuggestionList from '../../components/SuggestionComponents/SuggestionList/SuggestionList'
+import SuggestionShowcaseCard from '../../components/SuggestionComponents/SuggestionShowcaseCard/SuggestionShowcaseCard'
+
+//suggest ideas burada çekilecek ve card dönülecek
+
+
+
+const Data = [
+    {
+        Status: "Approved",
+        Header: "Sokak Hayvanları",
+        Description: "Bence soğuk havada hayvanları ofiste ayrılmış bölümün içerisine almalıyız",
+        VoteCount: 1,
+        CommentCount: 2,
+        Comments: [
+            {
+                CommentText: "Alerjisi olan insanlar için kötü bir fikir",
+                Date: "20.10.2025",
+            },
+            {
+                CommentText: "Ayrı bir bölüm olacaksa olabilir",
+                Date: "20.10.2025",
+            },
+        ]
+    },
+    {
+        Status: " ",
+        Header: "Sokak Hayvanları",
+        Description: "Ofis camları günde 5 saat kadar açılmalı ve öyle kalmalı",
+        VoteCount: 1,
+        CommentCount: 2,
+        Comments: [
+            {
+                CommentText: "Havalar soğuk olmaz",
+                Date: "20.10.2025",
+            },
+            {
+                CommentText: "Mola saatlerinde olabilir",
+                Date: "20.10.2025",
+            },
+        ]
+    },
+]
+
+
+//suggestion verileri burada çekilip componentlere dağıtılsın
+
+function SuggestIdeas() {
+    return (
+        <div className='container mx-auto my-10 grid grid-cols-[20%_75%] flex justify-center gap-10'>
+
+            <div className='bg-white h-[800px] rounded-lg shadow-md border'>
+                <div className='h-[5%] w-full text-center flex justify-center items-start'>
+                    <p className={`text-2xl text-center w-64 text-white bg-sky-400 rounded-b-sm font-roobert`}>Current Suggestions</p>
+                </div>
+                <div className='h-[95%] overflow-y-auto'>
+                    {Data.map((item, index) => (
+                        <div key={index}>
+                            <SuggestionCard {...item} />
+                        </div>
+                    ))}
+                </div>
+            </div>
+            <div>
+                <div className='grid grid-cols-2 gap-10 mb-5'>
+                    <div>
+                        <SuggestionShowcaseCard isStar={true} Description='Bence soğuk havada hayvanları ofiste ayrılmış bölümün içerisine almalıyız' VoteCount={753} />
+                    </div>
+                    <div>
+                        <SuggestionShowcaseCard isStar={false} Description='Ofis camları günde 5 saat kadar açılmalı ve öyle kalmalı' VoteCount={242} />
+                    </div>
+                </div>
+                <SuggestionList />
+            </div>
+        </div >
+    )
+}
+
+export default SuggestIdeas
