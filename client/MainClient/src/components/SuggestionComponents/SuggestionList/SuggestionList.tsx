2b5b7997a5511c39fadad6361759db4ede276e71
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-11-18 16:59:28 +0300

    STYLE: Revamped ailgnment of containers

--- client/MainClient/src/components/SuggestionComponents/SuggestionList/SuggestionList.tsx
+++ client/MainClient/src/components/SuggestionComponents/SuggestionList/SuggestionList.tsx
@@ -26,81 +26,75 @@ function SuggestionList() {
 
         <div className='h-[680px] bg-white rounded-lg shadow-lg border'>
             <div className='w-full h-[10%] text-center flex justify-center items-start'>
-                <p className={`text-2xl text-center w-64 text-white bg-indigo-400 rounded-b-sm font-roobert`}>All Suggestions</p>
+                <p className={`text-2xl text-center px-2 text-white bg-indigo-400 rounded-b-sm font-roobert`}>All Suggestions</p>
             </div>
 
-            <div className={`h-[5%] p-2 text-indigo-400 font-bold border-b text-md grid grid-cols-[10%_60%_10%_10%_10%]`}>
-                <div className="text-start">
+            <div className={`p-2 h-[5%] flex items-end text-indigo-400 font-bold border-b text-md grid grid-cols-10`}>
+                <div className="col-span-2 text-start">
                     Header
                 </div>
-                <div className="text-start px-4">
+                <div className="col-span-5 text-start px-4">
                     Description
                 </div>
-                <div className="text-center ">
+                <div className="col-span-1 text-center ">
                     Votes
                 </div>
-                <div className="text-center">
+                <div className="col-span-1 text-center">
                     Status
                 </div>
-                <div className="text-center">
+                <div className="col-span-1 text-center">
                     Date
                 </div>
             </div>
             <div className="h-[75%] border-b overflow-y-auto ">
-                <div className="text-gray-700 p-2 bg-gray-50 hover:bg-gray-200 text-md grid grid-cols-[10%_60%_10%_10%_10%]">
-                    <div className="text-center">
+                <div className="text-gray-700 p-2 bg-gray-50 hover:bg-gray-200 text-md grid grid-cols-10">
+                    <div className="col-span-2 text-start">
                         Deneme Header
                     </div>
-                    <div className="text-start px-4">
+                    <div className="col-span-5 text-start px-4">
                         Deneme Description
                     </div>
-                    <div className="text-center ">
+                    <div className="col-span-1 text-center ">
                         12
                     </div>
-                    <div className="text-center">
+                    <div className="col-span-1 text-center">
                         Approved
                     </div>
-                    <div className="text-center">
+                    <div className="col-span-1 text-center">
                         12.02.2025
                     </div>
                 </div>
             </div>
 
-            <div className='h-[10%] grid grid-cols-[80%_20%]'>
+            <div className='h-[10%] flex items-center pt-1'>
                 <Formik onSubmit={handleFilter} initialValues={filterInitialValues}>
                     <Form>
-                        <div className='grid grid-cols-[25%_25%_25%_25%]'>
-                            <div className='ms-2'>
+                        <div className='grid grid-cols-12'>
+                            <div className='col-span-3'>
                                 <Field className="text-gray-700 focus:outline-none w-full px-3 h-full bg-transparent focus:bg-transparent" placeholder="Month" name="month" ></Field>
                             </div>
-                            <div>
+                            <div className='col-span-3'>
                                 <Field className="text-gray-700 focus:outline-none w-full px-3 h-full bg-transparent focus:bg-transparent" placeholder="Year" name="year" ></Field>
                             </div>
-                            <div>
+                            <div className='col-span-3'>
                                 <select name='category' id='category' className='text-gray-700 focus:outline-none w-full px-3 h-full bg-transparent focus:bg-transparent'>
                                     <option className='border-none' selected>Select Filter</option>
                                     <option className='border-none' value={"expenses"}>Approved</option>
                                     <option className='border-none' value={"incomes"}>Refused</option>
                                 </select>
                             </div>
-                            <div className='mt-4 flex justify-end'>
-                                <button className='rounded-sm bg-gray-400 hover:bg-gray-500 text-3xl text-white w-[50%]'>{"⌕"}</button>
+                            <div className='col-span-2 flex justify-start'>
+                                <button className='rounded-sm bg-gray-400 hover:bg-gray-500 text-3xl w-[50px] text-white w-full mx-2'>{"⌕"}</button>
+                            </div>
+                            <div className='flex col-span-1 items-center'>
+                                <button className='rounded-sm bg-gray-400 hover:bg-gray-500 h-[30px] w-[50px] text-white w-[50%] ms-2'>{"🡸"}</button>
+                                <button className='rounded-sm bg-gray-400 hover:bg-gray-500 h-[30px] w-[50px] text-white w-[50%] mx-2'>{"🡺"}</button>
                             </div>
                         </div>
-
-
                     </Form>
                 </Formik>
-                <div className='grid grid-cols-2'>
-                    <div className='flex items-center justify-end'>
-                        <button className='rounded-sm bg-gray-400 hover:bg-gray-500  text-white w-[50%] p-2'>{"🡸"}</button>
-                    </div>
-                    <div className='flex items-center justify-start'>
-                        <button className='rounded-sm bg-gray-400 hover:bg-gray-500  text-white w-[50%] mx-2 p-2'>{"🡺"}</button>
-                    </div>
-                </div>
-
             </div>
+
 
         </div>
     )
