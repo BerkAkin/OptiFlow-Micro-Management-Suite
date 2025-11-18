20e56f1419b0c49a283d699f7d0550959cbd6dfe
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-11-18 16:58:30 +0300

    STYLE: Revamped ailgnment of containers

--- client/MainClient/src/components/LoginContainer/LoginContainer.tsx
+++ client/MainClient/src/components/LoginContainer/LoginContainer.tsx
@@ -41,43 +41,48 @@ function LoginModal() {
 
 
     return (
-        <>
-
-            <div className='size-96 h-[500px] flex justify-center items-center pe-2 bg-white border rounded-lg shadow-md '>
-                <Formik initialValues={{ email: "", password: "" }} onSubmit={(values) => loginMutation.mutate(values)}>
-                    {({ isSubmitting }) => (
-                        <Form>
-                            <div className="mb-9">
-                                <h1 className="text-3xl text-center text-gray-700 text-semibold">Login</h1>
-                            </div>
-                            <div className="grid grid-cols-1 h-48">
-                                <div className="mb-1">
-                                    <Field type="text" name="email" placeholder="E-Mail" className="text-gray-700 border-b focus:outline-none p-2 mt-1 w-full bg-transparent focus:bg-transparent" />
-                                    <ErrorMessage name="email" component="div" className="text-red-500 text-sm mt-1" />
+        <div>
+            <div>
+                <div className="border shadow-lg my-2 rounded-lg bg-white w-96">
+                    <p className="my-10 text-gray-700 text-3xl  text-center">Optiflow Management Suite </p>
+                </div>
+                <div className='w-96 h-[500px] flex justify-center items-center pe-2 bg-white border rounded-lg shadow-md '>
+                    <Formik initialValues={{ email: "", password: "" }} onSubmit={(values) => loginMutation.mutate(values)}>
+                        {({ isSubmitting }) => (
+                            <Form>
+                                <div className="mb-9">
+                                    <h1 className="text-3xl text-center text-gray-700 text-semibold">Login</h1>
                                 </div>
-                                <div>
-                                    <Field type={showPassword ? "text" : "password"} name="password" placeholder="Password" className="text-gray-700 border-b focus:outline-none p-2 mt-1 w-full bg-transparent" />
-                                    <ErrorMessage name="password" component="div" className="text-red-500 text-sm mt-1" />
+                                <div className="grid grid-cols-1 h-48">
+                                    <div className="mb-1">
+                                        <Field type="text" name="email" placeholder="E-Mail" className="text-gray-700 border-b focus:outline-none p-2 mt-1 w-full bg-transparent focus:bg-transparent" />
+                                        <ErrorMessage name="email" component="div" className="text-red-500 text-sm mt-1" />
+                                    </div>
+                                    <div>
+                                        <Field type={showPassword ? "text" : "password"} name="password" placeholder="Password" className="text-gray-700 border-b focus:outline-none p-2 mt-1 w-full bg-transparent" />
+                                        <ErrorMessage name="password" component="div" className="text-red-500 text-sm mt-1" />
+                                    </div>
+                                    <label className="flex items-center gap-2 mb-2 text-sm text-gray-700">
+                                        <Field type="checkbox" checked={showPassword} onChange={(e: any) => setShowPassword(e.target.checked)} className="accent-black" />
+                                        Show Password
+                                    </label>
                                 </div>
-                                <label className="flex items-center gap-2 mb-2 text-sm text-gray-700">
-                                    <Field type="checkbox" checked={showPassword} onChange={(e: any) => setShowPassword(e.target.checked)} className="accent-black" />
-                                    Show Password
-                                </label>
-                            </div>
-                            <div className="grid grid-cols-1 mt-5">
-                                <div className='flex justify-center'>
-                                    <button onClick={() => setIsAuth(true)} className='bg-black text-white w-48 h-10' type="submit">Login</button>
+                                <div className="grid grid-cols-1 mt-5">
+                                    <div className='flex justify-center'>
+                                        <button onClick={() => setIsAuth(true)} className='bg-black text-white w-48 h-10' type="submit">Login</button>
 
+                                    </div>
                                 </div>
-                            </div>
 
 
-                        </Form>
-                    )}
-                </Formik>
+                            </Form>
+                        )}
+                    </Formik>
+                </div>
+
             </div>
 
-        </>
+        </div>
     )
 }
 
