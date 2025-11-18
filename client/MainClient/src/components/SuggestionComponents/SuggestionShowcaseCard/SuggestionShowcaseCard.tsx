c8d7d20af14370320646d388692656c4fd237ac3
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-11-18 16:59:24 +0300

    STYLE: Revamped ailgnment of containers

--- client/MainClient/src/components/SuggestionComponents/SuggestionShowcaseCard/SuggestionShowcaseCard.tsx
+++ client/MainClient/src/components/SuggestionComponents/SuggestionShowcaseCard/SuggestionShowcaseCard.tsx
@@ -8,14 +8,14 @@ interface suggestionShowcaseProps {
 
 function SuggestionShowcaseCard({ isStar, Description, VoteCount }: suggestionShowcaseProps) {
     return (
-        <div className='bg-white rounded-lg h-[100px] shadow-lg border grid grid-cols-[20%_60%_20%]'>
-            <div className='flex items-center justify-center'>
+        <div className='bg-white rounded-lg h-[100px] shadow-lg border grid grid-cols-10'>
+            <div className='col-span-2 flex items-center justify-center'>
                 <p className={`${isStar == true ? "text-6xl" : "text-7xl pb-2"} text-amber-500`}>{isStar == true ? "★" : "☺︎"}</p>
             </div>
-            <div >
+            <div className='col-span-6'>
                 <p className='text-gray-700 text-lg h-full w-full pt-6 px-5'>{Description}</p>
             </div>
-            <div className='flex justify-center items-center'>
+            <div className='col-span-2 flex justify-center items-center'>
                 <p className='text-4xl text-gray-600'>{VoteCount}</p>
             </div>
         </div>
