cb617566ca69b45e82dd6f598b616116f2e91643
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-11-18 16:59:20 +0300

    STYLE: Revamped ailgnment of containers

--- client/MainClient/src/components/SuggestionComponents/MakeSuggestionCard/MakeSuggestionCard.tsx
+++ client/MainClient/src/components/SuggestionComponents/MakeSuggestionCard/MakeSuggestionCard.tsx
@@ -13,18 +13,18 @@ function MakeSuggestionCard() {
     return (
         <div className='absolute h-48 w-64 bg-white rounded-lg border shadow-lg z-20 left-1/2 transform -translate-x-1/2 top-16'>
             <div className='w-full h-[50px] flex justify-center items-start'>
-                <p className={`text-center w-48 text-white bg-indigo-400 rounded-b-sm font-roobert`} >Make Suggestion</p>
+                <p className={`text-center px-2 text-white bg-indigo-400 rounded-b-sm font-roobert`} >Make Suggestion</p>
             </div>
             <div>
                 <Formik onSubmit={handleSubmit} initialValues={initialValues}>
                     {({ isSubmitting }) => (
                         <Form>
-                            <div className='grid grid-cols-[40%_60%]'>
-                                <div>
+                            <div className='grid grid-cols-10 '>
+                                <div className='col-span-4'>
                                     <div className='justify-end flex '><label className=' text-lg mx-1 text-gray-700' htmlFor='topic'>Topic:</label></div>
                                     <div className='justify-end flex '><label className=' text-lg mx-1 text-gray-700' htmlFor='description'>Description:</label></div>
                                 </div>
-                                <div >
+                                <div className='col-span-6'>
                                     <div>
                                         <Field className="border-b text-lg focus:outline-none w-[90%] bg-transparent focus:bg-transparent" name="topic" placeholder="Temp Topic" />
                                     </div>
