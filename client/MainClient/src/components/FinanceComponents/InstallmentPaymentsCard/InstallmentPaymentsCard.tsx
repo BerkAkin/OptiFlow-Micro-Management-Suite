07c5a2f383caed1b58d61419ca344667f9fbd657
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-11-18 16:58:24 +0300

    STYLE: Revamped ailgnment of containers

--- client/MainClient/src/components/FinanceComponents/InstallmentPaymentsCard/InstallmentPaymentsCard.tsx
+++ client/MainClient/src/components/FinanceComponents/InstallmentPaymentsCard/InstallmentPaymentsCard.tsx
@@ -14,38 +14,44 @@ const InstallCardProps = {
 function InstallmentPaymentsCard() {
     return (
 
-        <div className='w-full h-96 '>
-            <div className='w-full h-[50px] flex justify-center items-start'>
-                <p className={`text-2xl text-center w-64 text-white bg-orange-400 rounded-b-sm font-roobert`}>Installments</p>
+        <div className='w-full h-full border'>
+
+            <div className='h-[10%] w-full h-[50px] flex justify-center items-start'>
+                <p className={`text-2xl text-center  px-2  text-white bg-orange-400 rounded-b-sm font-roobert`}>Installments</p>
             </div>
-            <div className='text-orange-400 border-b h-5 w-full grid grid-cols-[65%_10%_10%_10%] gap-2 flex justify-center'>
-                <div>
-                    <p className={`text-sm font-bold text-start ps-2 w-full font-roobert`}>Description</p>
+            <div className='h-[10%] flex items-center text-orange-400 text-center font-bold border-b  grid grid-cols-12 gap-2 flex justify-center'>
+                <div className='col-span-8'>
+                    <p className={`text-sm text-start ps-5 font-roobert`}>Description</p>
                 </div>
-                <div>
-                    <p className={`text-sm font-bold text-center w-full font-roobert`}>Total</p>
+                <div className='col-span-2'>
+                    <p className={`text-sm  w-full font-roobert`}>Total</p>
                 </div>
-                <div>
-                    <p className={`text-sm font-bold text-center w-full  font-roobert`}>Part</p>
+                <div className='col-span-1'>
+                    <p className={`text-sm w-full font-roobert`}>Part</p>
                 </div>
-                <div>
-                    <p className={`text-sm font-bold text-center w-full  font-roobert`}></p>
-                </div>
-            </div>
-            <div className='h-17 w-full grid grid-cols-[65%_10%_10%_10%] gap-2 flex justify-center hover:bg-gray-100'>
-                <div>
-                    <p className='ps-2 text-sm text-gray-600 p-3'>{InstallCardProps.Description}</p>
-                </div>
-                <div className='text-gray-600 flex items-center justify-center'>
-                    <p>{InstallCardProps.Price}</p>
-                </div>
-                <div className='text-gray-600 flex items-center justify-center'>
-                    <p>{InstallCardProps.RemainingPart}</p>
-                </div>
-                <div className='text-gray-600 flex items-center justify-center'>
-                    <Link className='bg-gray-400 text-white p-1 px-2 me-1 cursor-pointer hover:bg-gray-500 rounded-sm' to={`/${InstallCardProps.DetailUrl}`}>↗</Link>
+                <div className='col-span-1'>
+                    <p></p>
                 </div>
             </div>
+            <div className='overflow-y-auto h-[75%]'>
+
+                <div className='grid grid-cols-12 gap-2 hover:bg-gray-100'>
+                    <div className='col-span-8'>
+                        <p className='ps-5 text-sm text-gray-600 p-3 text-wrap'>{InstallCardProps.Description}</p>
+                    </div>
+                    <div className='col-span-2 text-gray-600 flex items-center justify-center'>
+                        <p>{InstallCardProps.Price}</p>
+                    </div>
+                    <div className='col-span-1 text-gray-600 flex items-center justify-center'>
+                        <p>{InstallCardProps.RemainingPart}</p>
+                    </div>
+                    <div className='col-span-1 text-gray-600 flex items-center justify-center'>
+                        <Link className='bg-gray-400 text-white p-1 px-2 me-1 cursor-pointer hover:bg-gray-500 rounded-sm' to={`/${InstallCardProps.DetailUrl}`}>↗</Link>
+                    </div>
+                </div>
+
+            </div>
+
 
 
         </div>
