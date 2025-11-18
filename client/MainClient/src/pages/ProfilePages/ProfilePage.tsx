11fd9b526a1ec0e3168112f3c43b6bae8a6d0a29
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-11-18 18:30:16 +0300

    STYLE: User information boxes defined as stylings

--- client/MainClient/src/pages/ProfilePages/ProfilePage.tsx
+++ client/MainClient/src/pages/ProfilePages/ProfilePage.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import MySuggestions from '../../components/ProfileComponents/MySuggestions/MySuggestions'
 import MyDayOffs from '../../components/ProfileComponents/MyDayOffs/MyDayOffs'
+import Settings from '../../components/ProfileComponents/Settings/Settings'
 
 function ProfilePage() {
     const [activeTab, setActiveTab] = useState(0)
@@ -9,10 +10,39 @@ function ProfilePage() {
         <div>
             <div className='container mx-auto mt-10 grid gap-2 grid-cols-10 flex justify-center h-[150px]'>
                 <div className=' col-span-1 bg-white rounded-full shadow-lg flex justify-center items-center border '>
-                    <div className='border rounded-full size-32'></div>
+                    <div className='border rounded-full size-32 flex justify-center items-center'>IMAGE</div>
                 </div>
-                <div className='col-span-8 bg-white rounded-lg shadow-lg border'></div>
-                <div className='col-span-1 bg-white rounded-lg shadow-lg border'></div>
+
+                <div className='col-span-8 bg-white rounded-lg shadow-lg border grid grid-cols-4'>
+                    <div className='col-span-1 flex items-center justify-center'>
+                        <p className='text-gray-600 text-5xl '>BERK AKIN</p>
+                    </div>
+                    <div className='col-span-3 grid grid-cols-2'>
+                        <div className='grid'>
+                            <div className='flex items-center'> <p className='text-gray-600 text-lg'>Company: <span className='text-gray-400'>MicroUsers</span></p></div>
+                            <div className='flex items-center'> <p className='text-gray-600 text-lg'>Department: <span className='text-gray-400'>Administration</span></p></div>
+                            <div className='flex items-center'> <p className='text-gray-600 text-lg'>Role: <span className='text-gray-400'>SuperUser</span></p></div>
+                        </div>
+                        <div className='grid'>
+
+                            <div className='flex items-center'> <p className='text-gray-600 text-lg'>Email: <span className='text-gray-400'>berkakin@mail.com</span></p></div>
+                            <div className='flex items-center'> <p className='text-gray-600 text-lg'>Change Password:<span className='text-gray-400'>*****</span></p></div>
+                            <div className='flex items-center'></div>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div className='col-span-1 bg-white rounded-lg shadow-lg border '>
+                    <div className='h-[20%]'>
+                        <p className={`text-lg rounded-t-lg text-center rounded-b-sm text-white bg-neutral-400 font-roobert`}>Last Day Online</p>
+                    </div>
+
+                    <div className='h-[75%] flex items-center justify-center'>
+                        <p className={`text-2xl text-gray-600 font-roobert`}>25.12.2025</p>
+                    </div>
+
+                </div>
             </div>
 
             <div className='container mx-auto my-3 flex justify-center grid grid-cols-10 gap-2'>
@@ -30,7 +60,7 @@ function ProfilePage() {
                 <div className=' h-[500px] col-span-9'>
                     {activeTab === 0 && <div> <MyDayOffs /></div>}
                     {activeTab === 1 && <div><MySuggestions /></div>}
-                    {activeTab === 2 && <div>Tab 3 İçeriği</div>}
+                    {activeTab === 2 && <div><Settings /></div>}
 
                 </div>
             </div>
