5482a30299fd064fff57e79b1db59d54073c279b
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-11-19 16:45:10 +0300

    FEAT: Created moods page

---
+++ client/MainClient/src/pages/MoodPages/MoodPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MoodRecorder from '../../components/MoodComponents/MoodRecorder/MoodRecorder'
+
+function MoodPage() {
+    return (
+        <div className='my-10 container mx-auto'>
+            <MoodRecorder />
+        </div>
+    )
+}
+
+export default MoodPage
