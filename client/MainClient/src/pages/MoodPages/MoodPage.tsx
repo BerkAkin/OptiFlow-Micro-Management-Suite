bfecf18543ea26338daed453a352947c7e51589a
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-11-20 00:07:43 +0300

    FEAT: Moodpage defined

--- client/MainClient/src/pages/MoodPages/MoodPage.tsx
+++ client/MainClient/src/pages/MoodPages/MoodPage.tsx
@@ -1,10 +1,31 @@
-import React from 'react'
 import MoodRecorder from '../../components/MoodComponents/MoodRecorder/MoodRecorder'
+import PreviousMoods from '../../components/MoodComponents/PreviousMoods/PreviousMoods'
+import GetHelp from '../../components/MoodComponents/GetHelp/GetHelp'
+import MoodTable from '../../components/MoodComponents/MoodTable/MoodTable'
+import RateOthers from '../../components/MoodComponents/RateOthers/RateOthers'
 
 function MoodPage() {
     return (
         <div className='my-10 container mx-auto'>
-            <MoodRecorder />
+            <div>
+                <MoodRecorder />
+            </div>
+            <div className='grid grid-cols-10 my-5 gap-5'>
+                <div className='bg-white col-span-4 shadow-lg border rounded-lg'>
+                    <MoodTable />
+                </div>
+                <div className='bg-white h-[300px] col-span-6 shadow-lg border rounded-lg border'>
+                    <PreviousMoods />
+                </div>
+            </div>
+            <div className='grid grid-cols-10 my-5 gap-5'>
+                <div className='bg-white col-span-3 shadow-lg border rounded-lg'>
+                    <GetHelp />
+                </div>
+                <div className=' col-span-7'>
+                    <RateOthers />
+                </div>
+            </div>
         </div>
     )
 }
