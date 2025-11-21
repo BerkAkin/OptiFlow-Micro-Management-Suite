5e2131182d1a9e416bbe657c3f6d7a4f4d251724
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-11-21 23:07:29 +0300

    FEAT: Component now has filtering options

--- client/MainClient/src/components/MoodComponents/MoodTable/MoodTable.tsx
+++ client/MainClient/src/components/MoodComponents/MoodTable/MoodTable.tsx
@@ -1,5 +1,9 @@
-import React from 'react'
 import DynamicTable from '../../DynamicTable/DynamicTable'
+
+interface filterTypes {
+    mood: string,
+    date: string
+}
 
 function MoodTable() {
 
@@ -7,11 +11,28 @@ function MoodTable() {
     const tempData: any = [
         { "mood": "Happy", "tags": "Work, Friends, Food, Weather", "date": "25.12.2025", },
     ]
+    const handleFilter = (values: filterTypes) => {
+        console.log(values)
+    }
+    const filterFields = [
+        {
+            name: "mood",
+            type: "select" as const,
+            options: [
+                { label: "Very Sad", value: "verySad" },
+                { label: "Sad", value: "sad" },
+                { label: "Neutral", value: "neutral" },
+                { label: "Happy", value: "happy" },
+                { label: "Very Happy", value: "veryHappy" },
+            ]
+        },
+        { name: "date", type: "date" as const }
+    ]
 
 
     return (
         <div className=' w-full h-full '>
-            <DynamicTable textScheme='text-rose-400' colorScheme='bg-rose-400' data={tempData} title='Previous Moods' />
+            <DynamicTable handleFilter={handleFilter} filterFields={filterFields} textScheme='text-rose-400' colorScheme='bg-rose-400' data={tempData} title='Previous Moods' />
         </div>
     )
 }
