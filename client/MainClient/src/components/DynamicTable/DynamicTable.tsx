401fdee566d4af5a52144539ae09d6612e90db3e
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-11-21 23:07:11 +0300

    FEAT: Component has now filtering options

--- client/MainClient/src/components/DynamicTable/DynamicTable.tsx
+++ client/MainClient/src/components/DynamicTable/DynamicTable.tsx
@@ -1,21 +1,35 @@
+import { Field, Form, Formik } from "formik";
+
+interface filterFields {
+    name: string,
+    type: 'text' | 'select' | 'date',
+    placeholder?: string,
+    options?: { label: string, value: string }[]
+}
 
 interface TableProps {
     title: string,
     colorScheme: string,
     textScheme: string,
     data: Record<string, any>[],
+    filterFields?: filterFields[],
+    handleFilter?: (values: any) => void,
+    children?: React.ReactNode
 }
 
 
 
-function DynamicTable({ title, colorScheme, textScheme, data }: TableProps) {
+
+
+function DynamicTable({ title, colorScheme, textScheme, data, children, handleFilter, filterFields }: TableProps) {
 
     const columns = Object.keys(data[0]);
     const gridTemplate = columns.map(col => (col === 'description' ? '4fr' : '1fr')).join(' ')
+    const filterInitials = filterFields ? Object.fromEntries(filterFields.map((item) => [item.name, ""])) : {}
+
 
     return (
-        <div className='h-full '>
-
+        <div className='h-full'>
             <div className='h-[10%] w-full text-center flex justify-center items-start'>
                 <p className={`text-2xl text-center text-white ${colorScheme} rounded-b-sm font-rubik px-6 `}>{title}</p>
             </div>
@@ -29,7 +43,7 @@ function DynamicTable({ title, colorScheme, textScheme, data }: TableProps) {
 
 
             </div>
-            <div className="h-[70%] overflow-y-auto border-b ">
+            <div className="h-[75%] overflow-y-auto border-b ">
                 {data.map((row, index) => (
                     <div key={index} className={`text-gray-700 p-2 bg-gray-50 hover:bg-gray-200 text-md grid`} style={{ gridTemplateColumns: gridTemplate }} >
                         {columns.map((col) => (
@@ -40,37 +54,44 @@ function DynamicTable({ title, colorScheme, textScheme, data }: TableProps) {
                     </div>
                 ))}
             </div>
+            <div className='h-[7%] flex items-center'>
+                {handleFilter && filterFields && (
 
-            <div className='h-[10%] flex items-center'>
+                    <Formik onSubmit={handleFilter} initialValues={filterInitials}>
+                        <Form>
+                            <div className='grid grid-cols-12 px-6'>
+                                <div className="col-span-10 grid grid-cols-12">
+                                    {filterFields.map((item) => (
+                                        <div className='col-span-4'>
 
-                {/*    <Formik onSubmit={filterFunction} initialValues={filterValues}>
-                    <Form>
-                        <div className='grid grid-cols-12'>
-                            <div className='col-span-3'>
-                                <Field className="text-gray-700 focus:outline-none w-full px-3 h-full bg-transparent focus:bg-transparent" placeholder="Month" name="month" ></Field>
+                                            {item.type === 'select' ? (
+                                                <Field className="px-6 py-1.5 w-[90%] border" as={item.type} type={item.type} name={item.name} id={item.name} placeholder={item.placeholder} >
+                                                    <option value="">Select</option>
+                                                    {item.options?.map(opt => (
+                                                        <option value={opt.value}>{opt.label}</option>
+                                                    ))}
+                                                </Field>
+
+                                            ) : (
+                                                <Field className="w-[90%] px-6 py-1 border  focus:outline-none" type={item.type} name={item.name} id={item.name} placeholder={item.placeholder} />
+                                            )}
+                                        </div>
+                                    ))}
+                                </div>
+                                <div className='col-span-1 flex justify-start items-center'>
+                                    <button type="submit" className='rounded-sm bg-gray-400 hover:bg-gray-500 text-3xl w-[50px] text-white w-full mx-2'>{"⌕"}</button>
+                                </div>
+                                <div className='flex col-span-1 items-center'>
+                                    <button className='rounded-sm bg-gray-400 hover:bg-gray-500 h-[30px] w-[50px] text-white w-[50%] ms-2'>{"🡸"}</button>
+                                    <button className='rounded-sm bg-gray-400 hover:bg-gray-500 h-[30px] w-[50px] text-white w-[50%] mx-2'>{"🡺"}</button>
+                                </div>
+
                             </div>
-                            <div className='col-span-3'>
-                                <Field className="text-gray-700 focus:outline-none w-full px-3 h-full bg-transparent focus:bg-transparent" placeholder="Year" name="year" ></Field>
-                            </div>
-                            <div className='col-span-3'>
-                                <select name='category' id='category' className='text-gray-700 focus:outline-none w-full px-3 h-full bg-transparent focus:bg-transparent'>
-                                    <option className='border-none' selected>Select Filter</option>
-                                    <option className='border-none' value={"expenses"}>Expenses</option>
-                                    <option className='border-none' value={"incomes"}>Incomes</option>
-                                </select>
-                            </div>
-                            <div className='col-span-2 flex justify-start'>
-                                <button className='rounded-sm bg-gray-400 hover:bg-gray-500 text-3xl w-[50px] text-white w-full mx-2'>{"⌕"}</button>
-                            </div>
-                            <div className='flex col-span-1 items-center'>
-                                <button className='rounded-sm bg-gray-400 hover:bg-gray-500 h-[30px] w-[50px] text-white w-[50%] ms-2'>{"🡸"}</button>
-                                <button className='rounded-sm bg-gray-400 hover:bg-gray-500 h-[30px] w-[50px] text-white w-[50%] mx-2'>{"🡺"}</button>
-                            </div>
-                        </div>
-                    </Form>
-                </Formik> */}
+                        </Form>
+                    </Formik>
 
 
+                )}
 
             </div>
 
