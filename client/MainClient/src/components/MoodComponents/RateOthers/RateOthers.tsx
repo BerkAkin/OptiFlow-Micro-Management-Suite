8c712794d876b50e4c10ae721efd2a568ac95150
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-11-21 17:42:06 +0300

    REFACTOR: Rate others form is now using dynamic form

--- client/MainClient/src/components/MoodComponents/RateOthers/RateOthers.tsx
+++ client/MainClient/src/components/MoodComponents/RateOthers/RateOthers.tsx
@@ -1,5 +1,6 @@
 import { Field, Form, Formik } from 'formik'
 import { useState } from 'react'
+import DynamicForm from '../../DynamicForm/DynamicForm'
 
 
 interface FormValueTypes {
@@ -23,59 +24,39 @@ function RateOthers() {
     const [selectedIndex, setSelectedIndex] = useState<number | null>(null);
 
     return (
-        <div className='grid grid-cols-10 gap-6'>
-            <div className='col-span-5 shadow-custom border rounded-lg bg-white'>
-                <div className='w-full'>
-                    <p className={`text-center px-4 text-xl py-1 text-white bg-sky-400 font-rubik rounded-t-lg`}>Rate Others</p>
-                </div>
-                <Formik onSubmit={handleSubmit} initialValues={initialValues}>
-                    {({ setFieldValue }) => (
-                        <Form className="p-4 space-y-1">
-                            <div className='flex flex-col space-y-1'>
+        <div>
+            <DynamicForm onSubmit={handleSubmit} fields={null} btnText='Rate Others' colorScheme='bg-rose-400' hoverScheme='hover:bg-rose-500' title='Rate Others' initialValues={initialValues} >
+                {({ setFieldValue, values }) => {
+                    return (
+                        <div>
+                            <label className='text-gray-700' htmlFor='employee'>Employee</label>
+                            <Field as="select" className="border resize-none text-gray-600 w-full rounded-sm px-2 py-1 focus:outline-none" name="employee" id="employee">
+                                <option value="">Select Employee</option>
+                                <option value="berkAkin">Berk Akın</option>
+                            </Field>
 
-                                <label className='text-gray-700' htmlFor='employee'>Employee</label>
-                                <Field as="select" className="border resize-none w-full rounded-sm px-2 py-1 focus:outline-none" name="employee" id="employee">
-                                    <option value="">select employee</option>
-                                    <option value="berkAkin">Berk Akın</option>
-                                </Field>
-
+                            <div className='flex my-5'>
                                 <label className='text-gray-700' htmlFor='stars'>Rate</label>
-                                <div className='flex'>
-                                    {
-                                        Array.from({ length: 5 }).map((item, index) => {
-                                            const isActive = hoverIndex !== null ? index <= hoverIndex : index <= (selectedIndex ?? +1)
-                                            return (
-                                                <>
-                                                    <p className={`${isActive ? "text-orange-400" : ""} cursor-pointer text-lg text-gray-700 ps-1`}
-                                                        onClick={() => { setSelectedIndex(index); setFieldValue("stars", index + 1) }} onMouseEnter={() => setHoverIndex(index)} onMouseLeave={() => setHoverIndex(null)}>★</p>
-                                                </>
-                                            )
+                                {
+                                    Array.from({ length: 5 }).map((item, index) => {
+                                        const isActive = hoverIndex !== null ? index <= hoverIndex : index <= (selectedIndex ?? +1)
+                                        return (
+                                            <p className={`${isActive ? "text-orange-400" : ""} cursor-pointer text-lg text-gray-700 ps-1`}
+                                                onClick={() => { setSelectedIndex(index); setFieldValue("stars", index + 1) }} onMouseEnter={() => setHoverIndex(index)} onMouseLeave={() => setHoverIndex(null)}>★</p>
+                                        )
 
-                                        })}
-
-                                </div>
+                                    })}
                             </div>
 
+                        </div>
+                    )
+                }}
 
 
-                            <div className="flex justify-center pt-2">
-                                <button type='submit' className=' text-white px-6 py-2 rounded-sm bg-sky-400 hover:bg-sky-500  transition'>Rate Others</button>
-                            </div>
-                        </Form >
-                    )}
-                </Formik >
-            </div>
-            <div className='col-span-2 shadow-custom border rounded-lg bg-white'>
-                <div className='w-full h-[15%] flex justify-center items-start'>
-                    <p className={`text-center px-4 text-lg text-white bg-orange-400 rounded-b-sm font-rubik`}>My Score</p>
-                </div>
-                <div className='h-[50%] flex justify-center items-center'>
-                    <p className={`text-7xl pt-4 text-amber-500`}>★</p>
-                </div>
-                <div className='h-[25%] flex justify-center items-start'>
-                    <p className={`text-4xl text-amber-500`}>4.7</p>
-                </div>
-            </div>
+            </DynamicForm>
+
+
+
         </div>
     )
 }
