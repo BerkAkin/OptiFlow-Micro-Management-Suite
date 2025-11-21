8dadc6f1afe30e8d0881c1f791a1f1775c15b1c6
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-11-21 17:42:23 +0300

    REFACTOR: Removed ask for help and refactored styling

--- client/MainClient/src/pages/MoodPages/MoodPage.tsx
+++ client/MainClient/src/pages/MoodPages/MoodPage.tsx
@@ -1,6 +1,5 @@
 import MoodRecorder from '../../components/MoodComponents/MoodRecorder/MoodRecorder'
 import PreviousMoods from '../../components/MoodComponents/PreviousMoods/PreviousMoods'
-import GetHelp from '../../components/MoodComponents/GetHelp/GetHelp'
 import MoodTable from '../../components/MoodComponents/MoodTable/MoodTable'
 import RateOthers from '../../components/MoodComponents/RateOthers/RateOthers'
 
@@ -11,18 +10,13 @@ function MoodPage() {
                 <MoodRecorder />
             </div>
             <div className='grid grid-cols-10 my-6 gap-6'>
-                <div className='bg-white col-span-4 shadow-custom border rounded-lg'>
+                <div className='bg-white h-[500px] col-span-4 shadow-custom border rounded-lg'>
                     <MoodTable />
                 </div>
-                <div className='bg-white h-[300px] col-span-6 shadow-custom border rounded-lg border'>
+                <div className='bg-white h-[500px] col-span-4 shadow-custom border rounded-lg border'>
                     <PreviousMoods />
                 </div>
-            </div>
-            <div className='grid grid-cols-10 my-6 gap-6'>
-                <div className='bg-white col-span-3 shadow-custom border rounded-lg'>
-                    <GetHelp />
-                </div>
-                <div className=' col-span-7'>
+                <div className='bg-white h-[300px] col-span-2 shadow-custom border rounded-lg border'>
                     <RateOthers />
                 </div>
             </div>
