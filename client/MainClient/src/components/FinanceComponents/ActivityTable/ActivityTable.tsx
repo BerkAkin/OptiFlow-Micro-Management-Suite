9b745a02012d7d3d8dcb02ff30b9f8dd58dd6c95
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-11-21 23:07:20 +0300

    FEAT: Component now has filtering options

--- client/MainClient/src/components/FinanceComponents/ActivityTable/ActivityTable.tsx
+++ client/MainClient/src/components/FinanceComponents/ActivityTable/ActivityTable.tsx
@@ -2,31 +2,28 @@ import DynamicTable from '../../DynamicTable/DynamicTable';
 
 
 interface filterValues {
-    month: string,
-    year: string
+    date: string,
+    type: string
 }
 
 
 
 function ActivityTable() {
 
-
-
-
     const tempData: any = [
         { "type": "+", "name": "Happy", "description": "Denemeler", "By": "Work", "Date": "2025-02-12", "Exchange": "$", "quantity": "3", "price": "5000", "invoice": "" },
-        { "type": "+", "name": "Happy", "description": "Denemeler", "By": "Work", "Date": "2025-02-12", "Exchange": "$", "quantity": "3", "price": "5000", "invoice": "" },
-        { "type": "+", "name": "Happy", "description": "Denemeler", "By": "Work", "Date": "2025-02-12", "Exchange": "$", "quantity": "3", "price": "5000", "invoice": "" },
-        { "type": "+", "name": "Happy", "description": "Denemeler", "By": "Work", "Date": "2025-02-12", "Exchange": "$", "quantity": "3", "price": "5000", "invoice": "" },
-        { "type": "+", "name": "Happy", "description": "Denemeler", "By": "Work", "Date": "2025-02-12", "Exchange": "$", "quantity": "3", "price": "5000", "invoice": "" },
-        { "type": "+", "name": "Happy", "description": "Denemeler", "By": "Work", "Date": "2025-02-12", "Exchange": "$", "quantity": "3", "price": "5000", "invoice": "" },
     ]
-
-
+    const filterFields = [
+        { name: "date", type: "date" as const, placeholder: "", },
+        { name: "type", type: "select" as const, placeholder: "Temp type", options: [{ label: "Income", value: "Income" }, { label: "Expense", value: "Expense" }] },
+    ]
+    const filterFunction = (values: filterValues) => {
+        console.log(values);
+    }
     return (
 
         <div className='h-full'>
-            <DynamicTable textScheme='text-sky-400' colorScheme='bg-sky-400' data={tempData} title='Latest Activity' />
+            <DynamicTable textScheme='text-sky-400' colorScheme='bg-sky-400' data={tempData} title='Latest Activity' handleFilter={filterFunction} filterFields={filterFields} />
         </div>
     )
 }
