cd9e51c1e9e5e7a1ec4b74b6fb772234c5bad2b9
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-11-21 23:07:27 +0300

    FEAT: Component now has filtering options

--- client/MainClient/src/components/FinanceComponents/RemainingPaymentsCard/RemainingPaymentsCard.tsx
+++ client/MainClient/src/components/FinanceComponents/RemainingPaymentsCard/RemainingPaymentsCard.tsx
@@ -1,17 +1,26 @@
 import DynamicTable from '../../DynamicTable/DynamicTable'
 
+interface FilterTypes {
+    description: string
+}
 
 const InstallCardProps = [
     { "description": "Deneme", "Price": 123, "To": "Berk", "": <button className='bg-gray-400 text-white p-1 px-2 me-1 cursor-pointer hover:bg-gray-500 rounded-sm'>$</button> },
-    { "description": "Deneme", "Price": 251, "To": "Berk", "": <button className='bg-gray-400 text-white p-1 px-2 me-1 cursor-pointer hover:bg-gray-500 rounded-sm'>$</button> },
-    { "description": "Deneme", "Price": 547, "To": "Berk", "": <button className='bg-gray-400 text-white p-1 px-2 me-1 cursor-pointer hover:bg-gray-500 rounded-sm'>$</button> },
 ]
 
+const handleFilter = (values: FilterTypes) => {
+    console.log(values)
+}
+
+const filterFields = [
+    { name: "description", placeholder: "Temp Description", type: "text" as const },
+    { name: "to", placeholder: "To", type: "text" as const },
+]
 
 function TodaysPaymentsCard() {
     return (
         <div className='w-full h-full'>
-            <DynamicTable textScheme='text-orange-400' colorScheme='bg-orange-400' data={InstallCardProps} title='Installments' />
+            <DynamicTable handleFilter={handleFilter} filterFields={filterFields} textScheme='text-orange-400' colorScheme='bg-orange-400' data={InstallCardProps} title='Installments' />
         </div>
 
     )
