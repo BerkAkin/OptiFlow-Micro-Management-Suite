5c3f06445dcf29c2887292606048bb113a6b251e
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-11-25 23:00:08 +0300

    STYLE: Added icons

--- client/MainClient/src/components/FinanceComponents/RemainingPaymentsCard/RemainingPaymentsCard.tsx
+++ client/MainClient/src/components/FinanceComponents/RemainingPaymentsCard/RemainingPaymentsCard.tsx
@@ -1,11 +1,12 @@
 import DynamicTable from '../../DynamicTable/DynamicTable'
+import dollar from '../../../assets/dollar.svg'
 
 interface FilterTypes {
     description: string
 }
 
 const InstallCardProps = [
-    { "description": "Deneme", "Price": 123, "To": "Berk", "": <button className='bg-gray-400 text-white p-1 px-2 me-1 cursor-pointer hover:bg-gray-500 rounded-sm'>$</button> },
+    { "description": "Deneme", "Price": 123, "To": "Berk", "": <button className='text-white  px-2 me-1 cursor-pointer  rounded-sm'><img src={dollar} width={25} alt="" /></button> },
 ]
 
 const handleFilter = (values: FilterTypes) => {
@@ -20,7 +21,7 @@ const filterFields = [
 function TodaysPaymentsCard() {
     return (
         <div className='w-full h-full'>
-            <DynamicTable handleFilter={handleFilter} filterFields={filterFields} textScheme='text-orange-400' colorScheme='bg-orange-400' data={InstallCardProps} title='Installments' />
+            <DynamicTable handleFilter={handleFilter} filterFields={filterFields} textScheme='text-orange-400' colorScheme='bg-orange-400' data={InstallCardProps} title='Remainings' />
         </div>
 
     )
