cf3474d638b321ec55fe8f2ae63ba249dda3766c
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-11-26 19:37:35 +0300

    FEAT: HelpPage defined

--- client/MainClient/src/pages/HelpPages/HelpPage.tsx
+++ client/MainClient/src/pages/HelpPages/HelpPage.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import HelpChat from '../../components/HelpComponents/HelpChat/HelpChat'
 
 function HelpPage() {
     return (
-        <div className='container mx-auto mt-6'>
+        <div className='container my-10 mx-auto'>
+            <div className='h-[450px]'>
+                <HelpChat />
+            </div>
+            <div className='grid grid-cols-10 my-6 gap-6'>
+                <div className='bg-white h-[400px] col-span-3 shadow-custom border border-gray-200 rounded-lg'>
+                </div>
+                <div className='bg-white h-[400px] col-span-7 shadow-custom border border-gray-200 rounded-lg '>
+                </div>
 
+            </div>
         </div>
     )
 }
