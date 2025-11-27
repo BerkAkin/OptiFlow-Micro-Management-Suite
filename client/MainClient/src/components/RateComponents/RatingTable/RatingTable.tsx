58c094ff7360ee68eae57312ec3c4b5e323c6bc7
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-11-27 19:45:59 +0300

    FEAT: Rating table to show users points defined as stylings

---
+++ client/MainClient/src/components/RateComponents/RatingTable/RatingTable.tsx
@@ -0,0 +1,25 @@
+import DynamicTable from '../../DynamicTable/DynamicTable'
+
+interface RatingTableData {
+    Employee: string,
+    Points: string,
+    Votes: string,
+}
+
+function RatingTable() {
+
+    const tempData: RatingTableData[] = [{ Employee: "Berk Akın", Points: "4.8", Votes: "278" }]
+    const filterFields = [{ name: "employee", id: "employee", type: "select" as const, options: [{ value: "berkakin", label: "Berk Akın" }] }]
+
+    const handleFilter = (values: any) => {
+        console.log(values)
+    }
+
+    return (
+        <div className='h-full'>
+            <DynamicTable colorScheme='bg-red-400' textScheme='text-red-400' data={tempData} title='Employee Ratings' filterFields={filterFields} handleFilter={handleFilter} />
+        </div>
+    )
+}
+
+export default RatingTable
