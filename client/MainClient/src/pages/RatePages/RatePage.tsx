32dc7af0ec5b6b50111ab318be153cf88d6e94cc
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-11-27 19:46:41 +0300

    FEAT: Created Ratings page

--- client/MainClient/src/pages/RatePages/RatePage.tsx
+++ client/MainClient/src/pages/RatePages/RatePage.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import RateOthers from "../../components/RateComponents/RateOthers/RateOthers"
+import RatingTable from "../../components/RateComponents/RatingTable/RatingTable"
+import UserComments from "../../components/RateComponents/UserComments/UserComments"
+
 
 function RatePage() {
     return (
-        <div>
-
+        <div className='my-6 container mx-auto space-y-6'>
+            <div className='grid grid-cols-12 gap-6 h-[380px]'>
+                <div className='col-span-9 shadow-custom rounded-lg border border-gray-200'>
+                    <RatingTable />
+                </div>
+                <div className='col-span-3 shadow-custom rounded-lg border border-gray-200'>
+                    <RateOthers />
+                </div>
+            </div>
+            <div className='h-[400px]'>
+                <UserComments />
+            </div>
         </div>
     )
 }
