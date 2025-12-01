315dc5539879f3f2903cb63fb3c2783420a0bdc8
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-01 16:25:37 +0300

    REFACTOR: Table is now can make filtering and paginating

--- client/MainClient/src/components/FinanceComponents/ActivityTable/ActivityTable.tsx
+++ client/MainClient/src/components/FinanceComponents/ActivityTable/ActivityTable.tsx
@@ -1,29 +1,35 @@
+import { useState } from 'react';
+import { useLatestActivity } from '../../../hooks/FinanceHooks/useFinance';
 import DynamicTable from '../../DynamicTable/DynamicTable';
-
-
-interface filterValues {
-    date: string,
-    type: string
-}
 
 
 
 function ActivityTable() {
+    const [filters, setFilters] = useState({});
+    const [page, setPage] = useState(1);
 
-    const tempData: any = [
-        { "type": "+", "name": "Happy", "description": "Denemeler", "By": "Work", "Date": "2025-02-12", "Exchange": "$", "quantity": "3", "price": "5000", "invoice": "" },
-    ]
-    const filterFields = [
-        { name: "date", type: "date" as const, placeholder: "", },
-        { name: "type", type: "select" as const, placeholder: "", options: [{ label: "Income", value: "Income" }, { label: "Expense", value: "Expense" }] },
-    ]
-    const filterFunction = (values: filterValues) => {
-        console.log(values);
+    const { isLoading, error, data } = useLatestActivity(filters, page);
+    if (isLoading) return (<p>Loading...</p>)
+    if (error || !data) return (<p>Error...</p>)
+    const { values, filterFields, maxPage } = data;
+
+    const filterFunction = (values: any) => {
+        setPage(1);
+        setFilters(values);
+    };
+
+    const nextPage = () => {
+        if (!data) return;
+        if (page < maxPage) setPage(page + 1)
     }
+    const prevPage = () => {
+        if (page > 1) setPage(page - 1);
+    }
+
     return (
 
         <div className='h-full'>
-            <DynamicTable textScheme='text-sky-400' colorScheme='bg-sky-400' data={tempData} title='Latest Activity' handleFilter={filterFunction} filterFields={filterFields} />
+            <DynamicTable onNext={nextPage} onPrev={prevPage} textScheme='text-sky-400' colorScheme='bg-sky-400' data={values} title='Latest Activity' handleFilter={filterFunction} filterFields={filterFields} />
         </div>
     )
 }
