be159285cbc1b44a565e09f0380ffa4adf961b3c
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-01 18:48:04 +0300

    REFACTOR: All tables and forms but charts are now in transactions component

--- client/MainClient/src/pages/FinancePages/FinanceDashboardPage.tsx
+++ client/MainClient/src/pages/FinancePages/FinanceDashboardPage.tsx
@@ -3,29 +3,9 @@ import Calendar from '../../components/FinanceComponents/Calendar/Calendar';
 import BarChart from '../../components/FinanceComponents/Charts/BarChart';
 import DoughChart from '../../components/FinanceComponents/Charts/DoughChart';
 import VerticalChart from '../../components/FinanceComponents/Charts/VerticalChart';
-import InstallmentPaymentsCard from '../../components/FinanceComponents/InstallmentPaymentsCard/InstallmentPaymentsCard';
-import RemainingPaymentsCard from '../../components/FinanceComponents/RemainingPaymentsCard/RemainingPaymentsCard';
-import AddActivityForm from '../../components/FinanceComponents/AddActivityForm/AddActivityForm';
-import AddPaymentForm from '../../components/FinanceComponents/AddPaymentForm/AddPaymentForm';
-import ActivityTable from '../../components/FinanceComponents/ActivityTable/ActivityTable';
+import Transactions from '../../components/FinanceComponents/Transactions/Transactions';
 
 ChartJS.register(ArcElement, CategoryScale, Tooltip, Legend, LinearScale, BarElement, PointElement, LineElement)
-
-/* geçici datalar  */
-
-/*  
-buraya çekilecek datalar: 
-
-en çok  harcama ve gelir olan 2şer kategori 
-toplam harcama ve gelirler
-harcama bütçesi, gelir beklentisi,
-gelir türlerine göre o ayki miktarlar pastada göstermek üzere
-aylara göre harcama ve gelir miktarları
-veritabanından sadece label ve data kısmı gelcek
-*/
-
-
-
 
 function FinanceDashboardPage() {
     return (
@@ -45,28 +25,12 @@ function FinanceDashboardPage() {
                     <Calendar />
                 </div>
             </div>
-
-            <div className='flex justify-center mx-auto grid grid-cols-10 gap-6 my-6'>
-                <div className='col-span-8 bg-white shadow-custom border border-gray-200 rounded-lg'>
-                    <ActivityTable />
-                </div>
-                <div className='col-span-2 bg-white shadow-custom border border-gray-200 rounded-lg'>
-                    <AddActivityForm />
-                </div>
+            <div>
+                <Transactions />
             </div>
 
-            <div className='flex justify-center grid grid-cols-10 gap-6 mb-6'>
-                <div className='col-span-4 bg-white shadow-custom border border-gray-200 rounded-lg'>
-                    <InstallmentPaymentsCard />
-                </div>
-                <div className='col-span-4 bg-white shadow-custom border border-gray-200 rounded-lg'>
-                    <RemainingPaymentsCard />
-                </div>
-                <div className='col-span-2 bg-white shadow-custom border border-gray-200 rounded-lg'>
-                    <AddPaymentForm />
-                </div>
 
-            </div>
+
         </div>
     )
 }
