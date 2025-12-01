0f40df607b06c408e1492ae11a682622c3783412
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-01 15:20:26 +0300

    REMOVE: Unused file

--- client/MainClient/src/domain/repositories/finance/incomeRepository.ts
+++
@@ -1,8 +0,0 @@
-import axiosInstance from "../../../infrastructure/axiosInstance";
-
-export const incomeRepository ={
-    addIncome: async(data:any)=>{
-        const res= await axiosInstance.post("/incomes",data); 
-        return res.data;
-    },
-};
