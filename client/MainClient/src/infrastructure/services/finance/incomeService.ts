6fef5d4a09de9cf70248239e604471f0d838c2d7
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-01 15:21:04 +0300

    REMOVE: Unused file

--- client/MainClient/src/infrastructure/services/finance/incomeService.ts
+++
@@ -1,9 +0,0 @@
-import { incomeRepository } from "../../../domain/repositories/finance/incomeRepository";
-
-export const incomeService = {
-    addIncome: async(data:any)=>{
-        //business logic eklenir veri formatlanır doğrulanır vesaire
-        const formattedData = {...data, createdAt: new Date()};
-        return await incomeRepository.addIncome(formattedData);
-    }
-}
