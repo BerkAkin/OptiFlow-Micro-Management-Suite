91314aa83019c12a59e07dde122772f0abf6683d
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-01 15:19:15 +0300

    REFACTOR: chart is now fetching data from related custom hook

--- client/MainClient/src/components/FinanceComponents/Charts/VerticalChart.tsx
+++ client/MainClient/src/components/FinanceComponents/Charts/VerticalChart.tsx
@@ -1,54 +1,53 @@
-import { useState } from 'react'
 import { Bar } from "react-chartjs-2";
-import swap from '../../../assets/swap.svg'
+import { useMost } from "../../../hooks/FinanceHooks/useFinance";
 
 
 function VerticalChart() {
-
-    const ChartExample = [[65, 81], [36, 24]];
-
-    const [chartData, setChartData] = useState<number[][]>(ChartExample);
-    const Colors = ['#22c55e', '#f87171']
-    const [color, setColor] = useState<string[]>(Colors)
+    const { data, isLoading, error } = useMost();
+    if (isLoading) return (<p>Loading...</p>)
+    if (error || !data) return (<p>Error...</p>)
+    const { categories, values } = data
 
     const ChartData = {
-        labels: ['Ulaşım', 'Temizlik'],
+        labels: categories,
         datasets: [
             {
-                data: chartData[0],
-                backgroundColor: color[0],
+                data: values,
+                backgroundColor: ['#f87171', '#ef4444'],
             }
         ],
-    };
-
-
-
-    const handleNext = () => {
-        const rotated = [...chartData!.slice(1), chartData![0]];
-        setChartData(rotated);
-        const rotatedColor = [...color!.slice(1), color![0]];
-        setColor(rotatedColor);
     };
 
 
     return (
         <div className="h-[420px]">
             <div className='flex justify-center'>
-                <p className={`text-xl text-center  px-6  rounded-b-sm text-white font-rubik`} style={{ backgroundColor: `${color[0]}` }}>
+                <p className={`text-xl text-center  px-6  rounded-b-sm text-white font-rubik bg-red-400`}>
                     Most
-                    <span>
-                        <button className={`h-[20px] text-white text-2xl ps-1 cursor-pointer`} onClick={handleNext}>
-                            <img src={swap} width={25} alt='' />
-                        </button>
-                    </span>
                 </p>
             </div>
             <div className='h-[70%] pt-6'>
-                <Bar data={ChartData} className='h-96' options={{ maintainAspectRatio: false, indexAxis: 'y' as const, responsive: true, plugins: { legend: { display: false, } }, scales: { x: { reverse: true }, y: { position: "right" } } }} />
-            </div>
-            <div className=' h-[10%]'>
-
-
+                <Bar
+                    data={ChartData} className='h-96'
+                    options={
+                        {
+                            maintainAspectRatio: false,
+                            indexAxis: 'y' as const,
+                            responsive: true,
+                            plugins: {
+                                legend: {
+                                    display: false,
+                                }
+                            },
+                            scales: {
+                                x: {
+                                    reverse: true
+                                },
+                                y: {
+                                    position: "right"
+                                }
+                            }
+                        }} />
             </div>
 
         </div>
