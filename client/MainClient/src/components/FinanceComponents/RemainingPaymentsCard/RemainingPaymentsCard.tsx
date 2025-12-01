a64abe92f6db3d03683baa5fb01554e233ab87de
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-01 17:01:21 +0300

    REFACTOR: Remainings page implemented

--- client/MainClient/src/components/FinanceComponents/RemainingPaymentsCard/RemainingPaymentsCard.tsx
+++ client/MainClient/src/components/FinanceComponents/RemainingPaymentsCard/RemainingPaymentsCard.tsx
@@ -1,27 +1,32 @@
+import { useState } from 'react'
 import DynamicTable from '../../DynamicTable/DynamicTable'
-import dollar from '../../../assets/dollar.svg'
+import { useRemainings } from '../../../hooks/FinanceHooks/useFinance';
 
-interface FilterTypes {
-    description: string
-}
 
-const InstallCardProps = [
-    { "description": "Deneme", "Price": 123, "To": "Berk", "": <button className='text-white  px-2 me-1 cursor-pointer  rounded-sm'><img src={dollar} width={25} alt="" /></button> },
-]
-
-const handleFilter = (values: FilterTypes) => {
-    console.log(values)
-}
-
-const filterFields = [
-    { name: "description", placeholder: "Description...", type: "text" as const },
-    { name: "to", placeholder: "To", type: "text" as const },
-]
 
 function TodaysPaymentsCard() {
+    const [page, setPage] = useState(1);
+    const [filters, setFilters] = useState({})
+
+    const onNext = () => {
+        if (!data) return
+        if (page < data.maxPage) setPage(page + 1)
+    }
+    const onPrev = () => {
+        if (page > 1) setPage(page - 1)
+    }
+    const handleFilter = (values: any) => {
+        setPage(1)
+        setFilters(values)
+    }
+
+    const { isLoading, error, data } = useRemainings(filters, page);
+    if (error || !data) return (<p>Error...</p>)
+    if (isLoading) return (<p>Loading...</p>)
+
     return (
         <div className='w-full h-full'>
-            <DynamicTable handleFilter={handleFilter} filterFields={filterFields} textScheme='text-orange-400' colorScheme='bg-orange-400' data={InstallCardProps} title='Remainings' />
+            <DynamicTable onNext={onNext} onPrev={onPrev} handleFilter={handleFilter} filterFields={data.filterFields} textScheme='text-orange-400' colorScheme='bg-orange-400' data={data.values} title='Remainings' />
         </div>
 
     )
