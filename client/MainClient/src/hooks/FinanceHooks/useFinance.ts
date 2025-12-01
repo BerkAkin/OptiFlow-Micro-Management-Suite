66676d000a42254cf362b1aae88deef6517b227f
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-01 16:44:45 +0300

    FEAT: UseInstallements hook implemented

--- client/MainClient/src/hooks/FinanceHooks/useFinance.ts
+++ client/MainClient/src/hooks/FinanceHooks/useFinance.ts
@@ -3,6 +3,7 @@ import { MonthlyService } from "../../services/FinanceServices/MonthlyService";
 import { CategoricalService } from "../../services/FinanceServices/CategoricalService";
 import { MostService } from "../../services/FinanceServices/MostService";
 import { LatestActivityService } from "../../services/FinanceServices/LatestActivityService";
+import { InstallmentService } from "../../services/FinanceServices/InstallmentService";
 
 export const useMonthly = () => {
   return useQuery({
@@ -29,5 +30,12 @@ export const useLatestActivity = (filters: any, page: number) => {
   return useQuery({
     queryKey: ["financeLatestActivity", filters, page],
     queryFn: () => LatestActivityService(filters, page),
+  });
+};
+
+export const useInstallments = (filters: any, page: number) => {
+  return useQuery({
+    queryKey: ["financeInstallment", filters, page],
+    queryFn: () => InstallmentService(filters, page),
   });
 };
