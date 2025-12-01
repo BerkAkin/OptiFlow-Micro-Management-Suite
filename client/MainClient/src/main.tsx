2e1ac2b47f614055cd498ce93bf66d393b219a93
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-01 15:17:33 +0300

    FEAT: Added default queryClient options

--- client/MainClient/src/main.tsx
+++ client/MainClient/src/main.tsx
@@ -5,7 +5,18 @@ import App from './App'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { AuthContextProvider } from './context/AuthContext';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: true,
+      retry: 5,
+      gcTime: 1000 * 60 * 15
+    }
+
+  }
+});
 
 
 createRoot(document.getElementById('root')!).render(
