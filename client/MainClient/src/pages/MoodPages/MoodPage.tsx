06f34d9836ddc83f3483bfabe25b56aabc9693f7
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-01 15:22:28 +0300

    FEAT: Added use moods table

--- client/MainClient/src/pages/MoodPages/MoodPage.tsx
+++ client/MainClient/src/pages/MoodPages/MoodPage.tsx
@@ -1,6 +1,7 @@
 import MoodRecorder from '../../components/MoodComponents/MoodRecorder/MoodRecorder'
 import PreviousMoods from '../../components/MoodComponents/PreviousMoods/PreviousMoods'
 import MoodTable from '../../components/MoodComponents/MoodTable/MoodTable'
+import UserMoodsTable from '../../components/MoodComponents/UserMoodsTable/UserMoodsTable'
 
 function MoodPage() {
     return (
@@ -9,12 +10,19 @@ function MoodPage() {
                 <MoodRecorder />
             </div>
             <div className='grid grid-cols-10 my-6 gap-6'>
-                <div className='bg-white h-[500px] col-span-5 shadow-custom border border-gray-200 rounded-lg'>
+                <div className='bg-white h-[350px] col-span-5 shadow-custom border border-gray-200 rounded-lg'>
                     <MoodTable />
                 </div>
-                <div className='bg-white h-[500px] col-span-5 shadow-custom border border-gray-200 rounded-lg '>
+                <div className='bg-white h-[350px] col-span-5 shadow-custom border border-gray-200 rounded-lg '>
                     <PreviousMoods />
                 </div>
+
+            </div>
+            <div className='grid my-6 gap-6'>
+                <div className='bg-white h-[500px] shadow-custom border border-gray-200 rounded-lg'>
+                    <UserMoodsTable />
+                </div>
+
 
             </div>
         </div>
