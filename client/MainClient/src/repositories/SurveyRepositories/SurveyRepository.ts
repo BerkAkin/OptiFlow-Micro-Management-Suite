7bb41174277fab9076b715fb89b1b4f1b51c5a0a
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-02 17:57:24 +0300

    FEAT: Created surveyResult repo definitions

--- client/MainClient/src/repositories/SurveyRepositories/SurveyRepository.ts
+++ client/MainClient/src/repositories/SurveyRepositories/SurveyRepository.ts
@@ -62,6 +62,31 @@ const tempSurveyDetail = {
   ],
 };
 
+const tempResult = {
+  id: 1,
+  text: "Maintenance Survey Part 1",
+  questions: [
+    {
+      id: 0,
+      text: "Aşağıdaki sistemlerden hangisinin kullanımı sizin için daha yararlı olabilir? Hangisini şirket bünyesinde görmek istersiniz?",
+      answers: [
+        { id: 0, text: "Sistem Analizi", count: 42 },
+        { id: 1, text: "Çalışan Güvenlik Sistemi", count: 58 },
+        { id: 2, text: "Hayvan Denetim Sistemi", count: 32 },
+        { id: 3, text: "Ergonomi Ayıraçları", count: 13 },
+      ],
+    },
+    {
+      id: 1,
+      text: "Aşağıdaki sistemlerden hangisinin denemelerini şirket bünyesinde görmek istersiniz?",
+      answers: [
+        { id: 4, text: "Sistem Denemeleri", count: 69 },
+        { id: 5, text: "Çalışan Güvenlik Denemeleri", count: 72 },
+      ],
+    },
+  ],
+};
+
 export const fetchSurveys = async () => {
   // const res = await api.get("/api/surveys/");
   //return res.data
@@ -77,4 +102,10 @@ export const fetchSurveyDetails = async (slug: string) => {
 export const sendSurveyAnswers = async (payload: any) => {
   const res = await api.post("/api/surveys/sendAnswers", payload);
   return res.data;
+};
+
+export const fetchSurveyResult = async (slug: string) => {
+  /* const res = await api.get(`/api/surveys/result${slug}`)
+  return res.data */
+  return tempResult;
 };
