7e21dcf4fe0f46e8846773591037c1ae5cd3d809
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-02 16:54:28 +0300

    FEAT: Created survey answers sender hook

---
+++ client/MainClient/src/hooks/SurveyHooks/useSurvey.ts
@@ -0,0 +1,31 @@
+import { useQuery, useMutation } from "@tanstack/react-query";
+import { SurveyListService } from "../../services/SurveyServices/SurveyListService";
+import { SurveyDetailsService } from "../../services/SurveyServices/SurveyDetailsService";
+import { sendSurveyAnswersService } from "../../services/SurveyServices/SendSurveyAnswersService";
+
+export const useSurveys = () => {
+  return useQuery({
+    queryKey: ["surveyList"],
+    queryFn: SurveyListService,
+  });
+};
+
+export const useSurveyDetails = (slug: string) => {
+  return useQuery({
+    queryKey: ["surveyDetails", slug],
+    queryFn: () => SurveyDetailsService(slug),
+  });
+};
+
+export const useSubmitSurvey = () => {
+  return useMutation({
+    mutationFn: ({ surveyId, answers }: { surveyId: number; answers: any }) =>
+      sendSurveyAnswersService(surveyId, answers),
+    onSuccess: (data: any) => {
+      console.log("Survey answers submitted successfully!", data);
+    },
+    onError: (error: any) => {
+      console.log("Error", error);
+    },
+  });
+};
