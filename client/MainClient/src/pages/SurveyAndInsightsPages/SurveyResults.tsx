abff006669a609a451d609ad5b0a49830f4e015e
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-02 17:57:12 +0300

    FEAT: Survey results are now fetching from api

--- client/MainClient/src/pages/SurveyAndInsightsPages/SurveyResults.tsx
+++ client/MainClient/src/pages/SurveyAndInsightsPages/SurveyResults.tsx
@@ -1,114 +1,47 @@
 import { useParams } from "react-router"
-
-
-type Surveys = {
-    surveys: Survey[]
-}
-
-type Answer = {
-    text: string,
-    voteCount: number,
-}
-type Question = {
-    text: string,
-    answers: Answer[],
-}
-type Survey = {
-    id: string,
-    header: string,
-    questions: Question[],
-    participateCount: number,
-}
-
-
-const initialSurvey: Survey = {
-    id: "1",
-    header: "Maintenance Survey Part 1",
-    participateCount: 100,
-    questions: [
-        {
-            text: "Aşağıdaki sistemlerden hangisinin kullanımı sizin için daha yararlı olabilir? Hangisini şirket bünyesinde görmek istersiniz?",
-            answers: [
-                { text: "Sistem Analizi", voteCount: 42 },
-                { text: "Çalışan Güvenlik Sistemi", voteCount: 58 },
-            ]
-        },
-        {
-            text: "Aşağıdaki sistemlerden hangisinin kullanımı sizin için daha yararlı olabilir? Hangisini şirket bünyesinde görmek istersiniz?",
-            answers: [
-                { text: "Sistem Analizi", voteCount: 42 },
-                { text: "Çalışan Güvenlik Sistemi", voteCount: 58 },
-            ]
-        },
-        {
-            text: "Aşağıdaki sistemlerden hangisinin kullanımı sizin için daha yararlı olabilir? Hangisini şirket bünyesinde görmek istersiniz?",
-            answers: [
-                { text: "Sistem Analizi", voteCount: 42 },
-                { text: "Çalışan Güvenlik Sistemi", voteCount: 58 },
-            ]
-        },
-        {
-            text: "Aşağıdaki sistemlerden hangisinin kullanımı sizin için daha yararlı olabilir? Hangisini şirket bünyesinde görmek istersiniz?",
-            answers: [
-                { text: "Sistem Analizi", voteCount: 42 },
-                { text: "Çalışan Güvenlik Sistemi", voteCount: 58 },
-            ]
-        },
-        {
-            text: "Aşağıdaki sistemlerden hangisinin kullanımı sizin için daha yararlı olabilir? Hangisini şirket bünyesinde görmek istersiniz?",
-            answers: [
-                { text: "Sistem Analizi", voteCount: 42 },
-                { text: "Çalışan Güvenlik Sistemi", voteCount: 58 },
-            ]
-        },
-        {
-            text: "Aşağıdaki Denemedir",
-            answers: [
-                { text: "Sistem Denemesi", voteCount: 67 },
-                { text: "Çalışan Güvenlik Denemesi", voteCount: 33 },
-            ]
-        },
-    ],
-
-
-}
+import { useSurveyResult } from "../../hooks/SurveyHooks/useSurvey";
 
 
 function SurveyResults() {
     const { slug } = useParams();
+    const { data, isLoading, error } = useSurveyResult(slug!);
+    const name = slug?.split("-").join(" ");
 
-    const initialTempSurvey: Surveys = {
-        surveys: [initialSurvey,]
-    }
+    if (isLoading) return (<p>Loading...</p>)
+    if (error || !data) return (<p>Error...</p>)
+
 
     return (
-        <div className='container mx-auto border border-gray-200 my-10 bg-white rounded-lg shadow-md' >
-
-            {initialTempSurvey.surveys.map((item, index) => (
-                <div key={index} className={`h-[800px] grid grid-cols-10`}>
-                    <div className="col-span-2">
-                        <div className={`w-full h-full flex items-center justify-center border-e border-gray-200`}>
-                            <p className='font-rubik text-center text-3xl text-gray-600 mx-6'>{slug} Results</p>
-                        </div>
-                    </div>
-                    <div className="p-6 col-span-8 overflow-auto">
-                        {item.questions.map((questions, index) => (
-                            <div key={index} className="mb-6">
-                                <p className="text-lg mb-2 text-gray-800">{index + 1}) {questions.text}</p>
-                                {questions.answers.map((answer) => (
-                                    <div>
-                                        <p className="ps-6 text-gray-600 inline">-{answer.text} </p>
-                                        <p className="inline bg-indigo-500 text-white px-2 ms-2  rounded-sm">{answer.voteCount} % </p>
+        <div className='container mx-auto my-10 grid grid-cols-10 gap-6' >
+            <div className="col-span-2 bg-white border border-gray-200 rounded-lg shadow-custom ">
+                <div className={`w-full h-full flex items-center justify-center`}>
+                    <p className='font-rubik text-center text-4xl text-gray-500 mx-6'>{name} Results</p>
+                </div>
+            </div>
+            <div className="col-span-8 bg-white border border-gray-200 rounded-lg shadow-custom h-[650px] p-6">
+                <div className="overflow-y-auto w-full h-full">
+                    {data.questions.map((item, index) => (
+                        <div key={index}>
+                            <div>
+                                <p className="my-3 text-lg text-gray-800">{index + 1})  {item.text}</p>
+                                {item.answers.map((answer, index) => (
+                                    <div key={index}>
+                                        <div className="py-1">
+                                            <p className="ps-6 text-gray-500 inline">-{answer.text} </p>
+                                            <p className="inline bg-indigo-400 text-white px-1 ms-2 rounded-sm">{answer.count} % </p>
+                                        </div>
 
                                     </div>
                                 ))}
-                            </div>
-                        ))}
 
-                    </div>
+                            </div>
+                        </div>
+
+                    ))}
                 </div>
 
-            ))}
+            </div>
+
         </div>
 
 
