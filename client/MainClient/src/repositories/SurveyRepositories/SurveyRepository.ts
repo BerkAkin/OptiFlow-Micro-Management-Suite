9d175ed3f68c4bb8745bce46550d6ecce931ade4
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-02 16:53:53 +0300

    FEAT: Created survey repo and some definitions

---
+++ client/MainClient/src/repositories/SurveyRepositories/SurveyRepository.ts
@@ -0,0 +1,80 @@
+import { api } from "../../lib/api";
+
+const tempSurveys = {
+  data: [
+    {
+      text: "Management Survey Chapter One",
+      slug: "Management-Survey-Chapter-One",
+      date: " 07.11.2025",
+      status: "Active",
+      participationCount: 87,
+      satisfactionCount: 56,
+      totalEmployee: 100,
+    },
+    {
+      text: "Management Survey Chapter Two",
+      slug: "Management-Survey-Chapter-Two",
+      date: "04.11.2025",
+      status: "Timeout",
+      participationCount: 94,
+      satisfactionCount: 82,
+      totalEmployee: 100,
+    },
+  ],
+};
+
+const tempSurveyDetail = {
+  id: 2,
+  text: "Management Survey Chapter One",
+  questions: [
+    {
+      id: 0,
+      text: " Aşağıdaki sistemlerden hangisinin kullanımı sizin için daha yararlı olabilir? Hangisini şirket bünyesinde görmek istersiniz?",
+      answers: [
+        {
+          id: 0,
+          text: "Sistem Analizi",
+          count: 0,
+        },
+        {
+          id: 1,
+          text: "Çalışan Güvenlik Sistemi",
+          count: 0,
+        },
+      ],
+    },
+    {
+      id: 1,
+      text: " Aşağıdakilerden Hangisi Denenmelidir",
+      answers: [
+        {
+          id: 2,
+          text: "Sistem Denemesi",
+          count: 0,
+        },
+        {
+          id: 3,
+          text: "Çalışan Güvenlik Denemesi",
+          count: 0,
+        },
+      ],
+    },
+  ],
+};
+
+export const fetchSurveys = async () => {
+  // const res = await api.get("/api/surveys/");
+  //return res.data
+  return tempSurveys;
+};
+
+export const fetchSurveyDetails = async (slug: string) => {
+  /*  const res = await api.get(`/api/surveys/detail/${slug}`);
+  return res.data; */
+  return tempSurveyDetail;
+};
+
+export const sendSurveyAnswers = async (payload: any) => {
+  const res = await api.post("/api/surveys/sendAnswers", payload);
+  return res.data;
+};
