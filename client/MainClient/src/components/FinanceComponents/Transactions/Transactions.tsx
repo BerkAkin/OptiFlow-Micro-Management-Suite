003deecae9d91e6e30ee122ded03b5dc7006db42
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-02 16:52:08 +0300

    STYLE: Background color changed

--- client/MainClient/src/components/FinanceComponents/Transactions/Transactions.tsx
+++ client/MainClient/src/components/FinanceComponents/Transactions/Transactions.tsx
@@ -1,7 +1,7 @@
 import LatestTransactions from '../LatestTransactions/LatestTransactions'
 import InstallmentPayments from '../InstallmentPayments/InstallmentPayments'
 import AddTransaction from '../AddTransaction/AddTransaction'
-import RemainingPayments from '../RemainingPayments/RemainingPayments'
+import RecurrentPayments from '../RecurrentPayments/RecurrentPayments'
 
 
 
@@ -12,15 +12,15 @@ function Transactions() {
         <div className='my-6'>
             <div className='grid grid-cols-10 gap-6'>
                 <div className='grid col-span-8 gap-6'>
-                    <div className='border rounded-lg shadow-custom border-gray-200 h-[500px]'>
+                    <div className='border rounded-lg bg-white shadow-custom border-gray-200 h-[500px]'>
                         <LatestTransactions />
                     </div>
                     <div className='grid grid-cols-10 gap-6 h-[500px]'>
-                        <div className='col-span-5 border rounded-lg shadow-custom border-gray-200'>
+                        <div className='col-span-5 border bg-white rounded-lg shadow-custom border-gray-200'>
                             <InstallmentPayments />
                         </div>
-                        <div className='col-span-5 border rounded-lg shadow-custom border-gray-200'>
-                            <RemainingPayments />
+                        <div className='col-span-5 border bg-white rounded-lg shadow-custom border-gray-200'>
+                            <RecurrentPayments />
                         </div>
                     </div>
                 </div>
