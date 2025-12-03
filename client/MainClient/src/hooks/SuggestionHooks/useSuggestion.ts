f17a3b1501eb581db32e0843f747fbca6409ae4a
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-03 17:31:05 +0300

    FEAT: UseSuggestions hook implemented

---
+++ client/MainClient/src/hooks/SuggestionHooks/useSuggestion.ts
@@ -0,0 +1,69 @@
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { GetSuggestionsService } from "../../services/SuggestionServices/GetSuggestionsService";
+import { VoteSuggestionService } from "../../services/SuggestionServices/VoteSuggestionService";
+import { CommentSuggestionService } from "../../services/SuggestionServices/CommentSuggestionService";
+import { ApproveOrRejectSuggestionService } from "../../services/SuggestionServices/ApproveOrRejectSuggestionService";
+import { SuggestionShowcaseService } from "../../services/SuggestionServices/SuggestionShowcaseService";
+import { MakeSuggestion } from "../../services/SuggestionServices/MakeSuggestion";
+
+export const useSuggestions = () => {
+  return useQuery({
+    queryKey: ["suggestionList"],
+    queryFn: GetSuggestionsService,
+  });
+};
+
+export const useVote = () => {
+  return useMutation({
+    mutationFn: (values: any) => VoteSuggestionService(values),
+    onSuccess: (data: any) => {
+      console.log("Suggestion vote submitted successfully!", data);
+    },
+    onError: (error: any) => {
+      console.log("Error", error);
+    },
+  });
+};
+
+export const useComment = () => {
+  return useMutation({
+    mutationFn: (values: any) => CommentSuggestionService(values),
+    onSuccess: (data: any) => {
+      console.log("Suggestion comment submitted successfully!", data);
+    },
+    onError: (error: any) => {
+      console.log("Error", error);
+    },
+  });
+};
+
+export const useApprove = () => {
+  return useMutation({
+    mutationFn: (values: any) => ApproveOrRejectSuggestionService(values),
+    onSuccess: (data: any) => {
+      console.log("Suggestion decision submitted successfully!", data);
+    },
+    onError: (error: any) => {
+      console.log("Error", error);
+    },
+  });
+};
+
+export const useShowcase = () => {
+  return useQuery({
+    queryKey: ["suggestionShowcase"],
+    queryFn: SuggestionShowcaseService,
+  });
+};
+
+export const useMakeSuggestion = () => {
+  return useMutation({
+    mutationFn: (values: any) => MakeSuggestion(values),
+    onSuccess: (data: any) => {
+      console.log("Suggestion decision submitted successfully!", data);
+    },
+    onError: (error: any) => {
+      console.log("Error", error);
+    },
+  });
+};
