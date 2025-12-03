91cb6374d9bab64daae9dc0869f8627bddc98a32
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-03 17:30:15 +0300

    REFACTOR: Suggestion card data is now fetching from api

--- client/MainClient/src/components/SuggestionComponents/SuggestionList/SuggestionList.tsx
+++ client/MainClient/src/components/SuggestionComponents/SuggestionList/SuggestionList.tsx
@@ -1,58 +1,12 @@
-import DynamicTable from '../../DynamicTable/DynamicTable';
+import { useSuggestions } from '../../../hooks/SuggestionHooks/useSuggestion';
 import SuggestionCard from '../SuggestionCard/SuggestionCard';
-
-
-interface filterValues {
-    status: string,
-    date: string,
-    description: string
-}
 
 
 function SuggestionList() {
 
-
-    const Data = [
-        {
-            Id: 0,
-            Status: "Approved",
-            Header: "Sokak Hayvanları",
-            Description: "Bence soğuk havada hayvanları ofiste ayrılmış bölümün içerisine almalıyız",
-            VoteCount: 1,
-            Date: "25-12-2025",
-            CommentCount: 2,
-            Comments: [
-                {
-                    CommentText: "Alerjisi olan insanlar için kötü bir fikir",
-                    Date: "20.10.2025",
-                },
-                {
-                    CommentText: "Ayrı bir bölüm olacaksa olabilir",
-                    Date: "20.10.2025",
-                },
-            ]
-        },
-        {
-            Id: 1,
-            Status: "Rejected",
-            Header: "Sokak Hayvanları",
-            Description: "Ofis camları günde 5 saat kadar açılmalı ve öyle kalmalı",
-            VoteCount: 1,
-            Date: "25-12-2025",
-            CommentCount: 2,
-            Comments: [
-                {
-                    CommentText: "Havalar soğuk olmaz",
-                    Date: "20.10.2025",
-                },
-                {
-                    CommentText: "Mola saatlerinde olabilir",
-                    Date: "20.10.2025",
-                },
-            ]
-        },
-    ]
-
+    const { data, isLoading, error } = useSuggestions();
+    if (isLoading) return (<p>Loading...</p>)
+    if (error || !data) return (<p>Error...</p>)
 
     return (
 
@@ -62,7 +16,7 @@ function SuggestionList() {
                 <p className={`text-xl text-center px-6 text-white bg-indigo-400 rounded-b-sm font-rubik`}>Suggestions</p>
             </div>
             <div className='h-[90%] overflow-y-auto my-6 grid gap-6 px-6 grid-cols-12'>
-                {Data.map((item, index) => (
+                {data.suggestions.map((item, index) => (
                     <div className='col-span-3' key={index}>
                         <SuggestionCard {...item} />
                     </div>
