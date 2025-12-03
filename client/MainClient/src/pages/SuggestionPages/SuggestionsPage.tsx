95205e39c7e040addae784094c2106a301adf29a
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-03 15:18:23 +0300

    REFACTOR: Suggestion page is now minimal

--- client/MainClient/src/pages/SuggestionPages/SuggestionsPage.tsx
+++ client/MainClient/src/pages/SuggestionPages/SuggestionsPage.tsx
@@ -1,72 +1,15 @@
-import React from 'react'
-import SuggestionCard from '../../components/SuggestionComponents/SuggestionCard/SuggestionCard'
 import SuggestionList from '../../components/SuggestionComponents/SuggestionList/SuggestionList'
 import SuggestionShowcaseCard from '../../components/SuggestionComponents/SuggestionShowcaseCard/SuggestionShowcaseCard'
 
-//suggest ideas burada çekilecek ve card dönülecek
 
-
-
-const Data = [
-    {
-        Id: 0,
-        Status: "Approved",
-        Header: "Sokak Hayvanları",
-        Description: "Bence soğuk havada hayvanları ofiste ayrılmış bölümün içerisine almalıyız",
-        VoteCount: 1,
-        CommentCount: 2,
-        Comments: [
-            {
-                CommentText: "Alerjisi olan insanlar için kötü bir fikir",
-                Date: "20.10.2025",
-            },
-            {
-                CommentText: "Ayrı bir bölüm olacaksa olabilir",
-                Date: "20.10.2025",
-            },
-        ]
-    },
-    {
-        Id: 1,
-        Status: " ",
-        Header: "Sokak Hayvanları",
-        Description: "Ofis camları günde 5 saat kadar açılmalı ve öyle kalmalı",
-        VoteCount: 1,
-        CommentCount: 2,
-        Comments: [
-            {
-                CommentText: "Havalar soğuk olmaz",
-                Date: "20.10.2025",
-            },
-            {
-                CommentText: "Mola saatlerinde olabilir",
-                Date: "20.10.2025",
-            },
-        ]
-    },
-]
-
-
-//suggestion verileri burada çekilip componentlere dağıtılsın
 
 function SuggestIdeas() {
     return (
         <div className='container mx-auto my-10 grid grid-cols-10 flex justify-center gap-6'>
 
-            <div className='col-span-2 bg-white h-[800px] rounded-lg shadow-custom border border-gray-200'>
-                <div className=' w-full text-center flex justify-center items-start'>
-                    <p className={`text-xl text-center px-5 text-white bg-indigo-400 rounded-b-sm font-rubik`}>Current Suggestions</p>
-                </div>
-                <div className='h-[95%] overflow-y-auto'>
-                    {Data.map((item, index) => (
-                        <div key={index}>
-                            <SuggestionCard {...item} />
-                        </div>
-                    ))}
-                </div>
-            </div>
-            <div className='col-span-8'>
-                <div className='grid grid-cols-10 gap-6 mb-6'>
+
+            <div className='col-span-10'>
+                <div className='grid grid-cols-10 gap-6 '>
                     <div className='col-span-5 '>
                         <SuggestionShowcaseCard isStar={true} Description='Bence soğuk havada hayvanları ofiste ayrılmış bölümün içerisine almalıyız' VoteCount={753} />
                     </div>
@@ -74,6 +17,9 @@ function SuggestIdeas() {
                         <SuggestionShowcaseCard isStar={false} Description='Ofis camları günde 5 saat kadar açılmalı ve öyle kalmalı' VoteCount={242} />
                     </div>
                 </div>
+
+            </div>
+            <div className='col-span-10'>
                 <SuggestionList />
             </div>
         </div >
