34a1d71ff6c0a92089a64c42016adb8bbadb25dc
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-03 15:26:24 +0300

    REFACTOR: Renamed

---
+++ client/MainClient/src/components/MoodComponents/TodaysMoods/TodaysMoods.tsx
@@ -0,0 +1,41 @@
+import DynamicTable from '../../DynamicTable/DynamicTable'
+
+interface filterTypes {
+    mood: string,
+    date: string
+}
+
+function TodaysMoods() {
+
+
+
+    const tempData = [
+        { "user": "Berk Akın", "mood": "Happy", "tags": "Work, Food, Weather", "date": "30.11.2025" },
+    ]
+    const handleFilter = (values: filterTypes) => {
+        console.log(values)
+    }
+    const filterFields = [
+        {
+            name: "mood",
+            type: "select" as const,
+            options: [
+                { label: "Very Sad", value: "verySad" },
+                { label: "Sad", value: "sad" },
+                { label: "Neutral", value: "neutral" },
+                { label: "Happy", value: "happy" },
+                { label: "Very Happy", value: "veryHappy" },
+            ]
+        },
+        { name: "date", type: "date" as const }
+    ]
+
+
+    return (
+        <div className=' w-full h-full '>
+            <DynamicTable handleFilter={handleFilter} filterFields={filterFields} textScheme='text-rose-400' colorScheme='bg-rose-400' data={tempData} title='Todays Moods' />
+        </div>
+    )
+}
+
+export default TodaysMoods
