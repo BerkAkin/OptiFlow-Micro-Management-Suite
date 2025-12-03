07edf1d90541d6dc51513eb703301d5ee1ec6c9b
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-03 17:31:22 +0300

    REFACTOR: Suggestion showcase cards are now in suggestion showcase component

--- client/MainClient/src/pages/SuggestionPages/SuggestionsPage.tsx
+++ client/MainClient/src/pages/SuggestionPages/SuggestionsPage.tsx
@@ -1,23 +1,13 @@
 import SuggestionList from '../../components/SuggestionComponents/SuggestionList/SuggestionList'
-import SuggestionShowcaseCard from '../../components/SuggestionComponents/SuggestionShowcaseCard/SuggestionShowcaseCard'
+import SuggestionShowcase from '../../components/SuggestionComponents/SuggestionShowcase/SuggestionShowcase'
 
 
 
 function SuggestIdeas() {
     return (
         <div className='container mx-auto my-10 grid grid-cols-10 flex justify-center gap-6'>
-
-
             <div className='col-span-10'>
-                <div className='grid grid-cols-10 gap-6 '>
-                    <div className='col-span-5 '>
-                        <SuggestionShowcaseCard isStar={true} Description='Bence soğuk havada hayvanları ofiste ayrılmış bölümün içerisine almalıyız' VoteCount={753} />
-                    </div>
-                    <div className='col-span-5 '>
-                        <SuggestionShowcaseCard isStar={false} Description='Ofis camları günde 5 saat kadar açılmalı ve öyle kalmalı' VoteCount={242} />
-                    </div>
-                </div>
-
+                <SuggestionShowcase />
             </div>
             <div className='col-span-10'>
                 <SuggestionList />
