9801a810a6f7b6ea55510a7fe192026fa83cfabb
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-03 15:18:05 +0300

    REFACTOR: Removed dynamic table component call due to same usage with suggestion history table

--- client/MainClient/src/components/SuggestionComponents/SuggestionList/SuggestionList.tsx
+++ client/MainClient/src/components/SuggestionComponents/SuggestionList/SuggestionList.tsx
@@ -1,4 +1,5 @@
 import DynamicTable from '../../DynamicTable/DynamicTable';
+import SuggestionCard from '../SuggestionCard/SuggestionCard';
 
 
 interface filterValues {
@@ -11,22 +12,63 @@ interface filterValues {
 function SuggestionList() {
 
 
-    const handleFilter = (values: filterValues) => {
-        console.log(values)
-    }
-    const tempData: any = [
-        { "header": "Deneme Header", "description": "Denemeler", "votes": "10", "status": "approved", "date": "2025-02-12" },
+    const Data = [
+        {
+            Id: 0,
+            Status: "Approved",
+            Header: "Sokak Hayvanları",
+            Description: "Bence soğuk havada hayvanları ofiste ayrılmış bölümün içerisine almalıyız",
+            VoteCount: 1,
+            Date: "25-12-2025",
+            CommentCount: 2,
+            Comments: [
+                {
+                    CommentText: "Alerjisi olan insanlar için kötü bir fikir",
+                    Date: "20.10.2025",
+                },
+                {
+                    CommentText: "Ayrı bir bölüm olacaksa olabilir",
+                    Date: "20.10.2025",
+                },
+            ]
+        },
+        {
+            Id: 1,
+            Status: "Rejected",
+            Header: "Sokak Hayvanları",
+            Description: "Ofis camları günde 5 saat kadar açılmalı ve öyle kalmalı",
+            VoteCount: 1,
+            Date: "25-12-2025",
+            CommentCount: 2,
+            Comments: [
+                {
+                    CommentText: "Havalar soğuk olmaz",
+                    Date: "20.10.2025",
+                },
+                {
+                    CommentText: "Mola saatlerinde olabilir",
+                    Date: "20.10.2025",
+                },
+            ]
+        },
     ]
-    const filterFields = [
-        { name: "status", type: "select" as const, options: [{ value: "approved", label: "Approved" }, { value: "rejected", label: "Rejected" }] },
-        { name: "description", type: "text" as const, placeholder: "Description...", },
-        { name: "date", type: "date" as const, },
-    ]
+
 
     return (
 
-        <div className='h-[675px] bg-white rounded-lg shadow-custom border border-gray-200'>
-            <DynamicTable handleFilter={handleFilter} filterFields={filterFields} textScheme='text-indigo-400' colorScheme='bg-indigo-400' data={tempData} title='Suggestions' />
+        <div className='h-[650px] bg-white rounded-lg shadow-custom border border-gray-200'>
+
+            <div className='w-full text-center flex justify-center items-start'>
+                <p className={`text-xl text-center px-6 text-white bg-indigo-400 rounded-b-sm font-rubik`}>Suggestions</p>
+            </div>
+            <div className='h-[90%] overflow-y-auto my-6 grid gap-6 px-6 grid-cols-12'>
+                {Data.map((item, index) => (
+                    <div className='col-span-3' key={index}>
+                        <SuggestionCard {...item} />
+                    </div>
+                ))}
+            </div>
+
         </div>
     )
 
