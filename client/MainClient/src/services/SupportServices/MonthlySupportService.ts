11703ce296e57cbf5d1fed933394b4399b064d40
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-04 18:12:36 +0300

    FEAT:  service impelemented

---
+++ client/MainClient/src/services/SupportServices/MonthlySupportService.ts
@@ -0,0 +1,27 @@
+import { fetchMonthlySupport } from "../../repositories/SupportRepositories/SupportRepository";
+
+export const MonthlySupportService = async () => {
+  const data = await fetchMonthlySupport();
+
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  const newData = months.map((month) => {
+    const item = data.find((x) => x.month === month);
+    return item ? item.value : 0;
+  });
+
+  return { months, newData };
+};
