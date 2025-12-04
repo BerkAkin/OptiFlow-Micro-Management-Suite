3f4b8e51e00c4127392d0125b448025d3a5fff2f
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-04 18:08:52 +0300

    FEAT: Todays moods are now fetching from api

--- client/MainClient/src/components/MoodComponents/TodaysMoods/TodaysMoods.tsx
+++ client/MainClient/src/components/MoodComponents/TodaysMoods/TodaysMoods.tsx
@@ -1,39 +1,38 @@
+import { useState } from 'react';
+import { useTodaysMoods } from '../../../hooks/MoodHooks/UseMood'
 import DynamicTable from '../../DynamicTable/DynamicTable'
-
-interface filterTypes {
-    mood: string,
-    date: string
-}
 
 function TodaysMoods() {
 
+    const userId = 1
+
+    const [page, setPage] = useState(1);
+    const [filters, setFilters] = useState({});
 
 
-    const tempData = [
-        { "employee": "Berk Akın", "mood": "Happy", "tags": "Work, Food, Weather", "date": "30.11.2025" },
-    ]
-    const handleFilter = (values: filterTypes) => {
-        console.log(values)
+    const { data, isLoading, error } = useTodaysMoods(filters, page, userId);
+
+    if (isLoading) return (<p>Loading...</p>)
+    if (error || !data) return (<p>Error...</p>)
+
+
+    const onPrev = () => {
+        if (page > 1) setPage(page - 1)
     }
-    const filterFields = [
-        {
-            name: "mood",
-            type: "select" as const,
-            options: [
-                { label: "Very Sad", value: "verySad" },
-                { label: "Sad", value: "sad" },
-                { label: "Neutral", value: "neutral" },
-                { label: "Happy", value: "happy" },
-                { label: "Very Happy", value: "veryHappy" },
-            ]
-        },
-        { name: "date", type: "date" as const }
-    ]
 
+    const onNext = () => {
+        if (!data) return
+        if (page < data.maxPage) setPage(page + 1)
+    }
+
+    const handleFilter = (values: any) => {
+        setPage(1)
+        setFilters(values)
+    }
 
     return (
         <div className=' w-full h-full '>
-            <DynamicTable handleFilter={handleFilter} filterFields={filterFields} textScheme='text-rose-400' colorScheme='bg-rose-400' data={tempData} title='Todays Moods' />
+            <DynamicTable onPrev={onPrev} onNext={onNext} handleFilter={handleFilter} filterFields={data.filterFields} textScheme='text-rose-400' colorScheme='bg-rose-400' data={data.values} title='Todays Moods' />
         </div>
     )
 }
