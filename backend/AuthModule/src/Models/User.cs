376d6116f7122e69349e834cd2aed8e367f24a92
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-09 20:03:15 +0300

    FEAT: User model is now has adress info fields

--- backend/AuthModule/src/Models/User.cs
+++ backend/AuthModule/src/Models/User.cs
@@ -12,6 +12,13 @@ namespace AuthModule.Models
         public string PasswordHash { get; set; }
         public string PhoneNum { get; set; }
         public int DepartmentId { get; set; }
+        public string Street { get; set; }
+        public string Street2 { get; set; }
+        public string ApartmentNum { get; set; }
+        public string DoorNumber { get; set; }
+        public string Province { get; set; }
+        public string District { get; set; }
+        public string FullAddress { get; set; }
         public Department Department { get; set; }
         public DateTime BirthDate { get; set; }
         public DateTime DateCreate { get; set; }
