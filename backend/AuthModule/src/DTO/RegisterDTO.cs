15bb93df06b3dffacb14b937c2577a20fe3b09f3
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-09 20:03:02 +0300

    FEAT: ReigisterDTO is now has adress info fields

--- backend/AuthModule/src/DTO/RegisterDTO.cs
+++ backend/AuthModule/src/DTO/RegisterDTO.cs
@@ -8,7 +8,15 @@
         public string Password { get; set; }
         public string Tenant { get; set; }
         public string PhoneNum { get; set; }
+        public int DepartmentId { get; set; }
+        public string Street { get; set; }
+        public string Street2 { get; set; }
+        public string ApartmentNum { get; set; }
+        public string DoorNumber { get; set; }
+        public string Province { get; set; }
+        public string District { get; set; }
+        public string FullAddress { get; set; }
         public DateTime BirthDate { get; set; }
-        public List<int> SelectedModuleIds { get; set; } 
+        public List<string> SelectedModuleIds { get; set; } 
     }
 }
