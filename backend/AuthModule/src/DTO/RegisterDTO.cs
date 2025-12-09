dbaf6e231d873b548f64967e48eee675e18be6f0
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-09 18:23:26 +0300

    FEAT: Register imodel now has tenant phonenum birthdate and modules info

--- backend/AuthModule/src/DTO/RegisterDTO.cs
+++ backend/AuthModule/src/DTO/RegisterDTO.cs
@@ -6,6 +6,9 @@
         public string Surname { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-        public int TenantId { get; set; }
+        public string Tenant { get; set; }
+        public string PhoneNum { get; set; }
+        public DateTime BirthDate { get; set; }
+        public List<int> SelectedModuleIds { get; set; } 
     }
 }
