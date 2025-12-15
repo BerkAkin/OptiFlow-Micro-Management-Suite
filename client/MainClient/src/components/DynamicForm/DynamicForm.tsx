71d36d9ec641cc4e1c86e6ad82fe5b9d0d5d4c89
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-15 15:54:38 +0300

    STYLE: Removed labels

--- client/MainClient/src/components/DynamicForm/DynamicForm.tsx
+++ client/MainClient/src/components/DynamicForm/DynamicForm.tsx
@@ -26,27 +26,23 @@ interface FormProps {
 
 function DynamicForm({ title, colorScheme, hoverScheme, fields, initialValues, onSubmit, children, onCancel }: FormProps) {
     return (
-        <div>
-            <div className={`${colorScheme} w-full py-1 rounded-t-lg text-center`}>
-                <p className="text-xl text-white font-rubik">{title}</p>
+        <div className='p-4'>
+            <div>
+                <p className="text-xl px-4 pt-2 font-semibold text-slate-700 font-rubik">{title}</p>
             </div>
 
             <Formik initialValues={initialValues} onSubmit={onSubmit}>
                 {({ isSubmitting, setFieldValue, values }) => (
                     <Form className="p-4 space-y-1">
                         {fields && fields.map((item, index) => (
-                            <div className="flex flex-col space-y-1">
-                                {item.type !== "checkbox" ?
-                                    (
-                                        <label className="text-gray-700" htmlFor={item.id}>{item.label}</label>
-                                    ) : ""
-                                }
+                            <div className="flex flex-col">
+
                                 {
                                     item.type === "file" ? (
-                                        <input hidden id={item.id} name={item.name} type="file" onChange={(e) => { const file = e.target.files?.[0] || null; setFieldValue(item.name, file); }} />
+                                        <input className="cursor-pointer border border-gray-200 resize-none w-full rounded-sm px-2 py-1 focus:outline-none" id={item.id} name={item.name} type="file" onChange={(e) => { const file = e.target.files?.[0] || null; setFieldValue(item.name, file); }} />
                                     ) : item.as === "select" ? (
-                                        <Field as="select" name={item.name} id={item.id} className="cursor-pointer border border-gray-200 resize-none w-full rounded-sm px-2 py-1 focus:outline-none" >
-                                            <option value="">Select</option>
+                                        <Field as="select" name={item.name} id={item.id} className="cursor-pointer border border-gray-200 w-full rounded-sm px-2 py-1 focus:outline-none" >
+                                            <option value="">{item.placeholder}</option>
                                             {item.options?.map(opt => (
                                                 <option key={opt.value} value={opt.value}>{opt.label}</option>
                                             ))}
@@ -59,18 +55,18 @@ function DynamicForm({ title, colorScheme, hoverScheme, fields, initialValues, o
 
                                     )
                                         : (
-                                            <Field as={item.as} name={item.name} id={item.id} type={item.type} placeholder={item.placeholder} className="cursor-pointer border border-gray-200 resize-none w-full rounded-sm px-2 py-1 focus:outline-none" />
+                                            <Field as={item.as} name={item.name} id={item.id} type={item.type} placeholder={item.placeholder} className="cursor-pointer border border-gray-200 resize-none w-full rounded-sm px-4 py-1 focus:outline-none" />
                                         )
                                 }
                             </div>
                         ))}
                         {typeof children === 'function' ? children({ setFieldValue, values }) : children}
                         <div className="flex justify-center pt-2">
-                            <button type="submit" className={`${colorScheme} ${hoverScheme} cursor-pointer text-white w-[50px] py-2 flex items-center  justify-center rounded-sm transition`} >
+                            <button type="submit" className={`${colorScheme} ${hoverScheme} transition-transform duration-150 active:scale-[0.98]  cursor-pointer text-white w-full py-2 flex items-center  justify-center rounded-sm transition`} >
                                 <img src={send} alt="" width={25} />
                             </button>
                             {onCancel &&
-                                <button type='button' onClick={onCancel} className={`bg-red-400 hover:bg-red-500 cursor-pointer text-white w-[50px] py-2 mx-2 flex items-center  justify-center rounded-sm transition`} >
+                                <button type='button' onClick={onCancel} className={`transition-transform duration-150 active:scale-[0.98] bg-red-400 hover:bg-red-500 cursor-pointer text-white w-[50px] py-2 mx-2 flex items-center  justify-center rounded-sm transition`} >
                                     <img src={cancel} alt="" width={25} />
                                 </button>
                             }
