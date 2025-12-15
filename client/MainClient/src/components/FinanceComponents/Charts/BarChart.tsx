7e104972205238fd0e87e41bf91ffc8fb7545ddc
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-15 15:55:47 +0300

    STYLE: header look is now simple

--- client/MainClient/src/components/FinanceComponents/Charts/BarChart.tsx
+++ client/MainClient/src/components/FinanceComponents/Charts/BarChart.tsx
@@ -16,46 +16,47 @@ function BarChart() {
                 type: 'bar' as const,
                 data: incomeData,
                 backgroundColor: '#22c55e',
+                barPercentage: 0.7,
             },
             {
                 type: 'bar' as const,
                 backgroundColor: '#ef4444',
                 data: expenseData,
+                barPercentage: 0.7
             },
         ],
     };
 
     return (
-        <div className="h-[420px]">
-            <div className='flex justify-center'>
-                <p className={`text-xl text-center px-6 rounded-b-sm text-white font-rubik bg-green-500`} >
+        <div className="h-[420px] ">
+            <div className='h-[80%] p-6'>
+                <p className={`text-xl px-1 pb-2 font-semibold font-rubik text-slate-800`} >
                     Monthly
                 </p>
-            </div>
-            <div className='h-[80%] p-6'>
                 <Chart
                     type="bar"
                     data={ChartData}
                     options={{
                         maintainAspectRatio: false,
                         responsive: true,
-                        plugins: { legend: { display: false } }
+                        plugins: { legend: { display: false } },
+                        scales: { x: { grid: { display: false } }, y: { ticks: { stepSize: 100 }, border: { display: false } } }
                     }}
                 />
             </div>
-            <div className='h-[10%] py-6 flex items-center justify-center'>
+            <div className='h-[20%] flex items-center justify-center'>
                 <div className={`bg-white w-full px-6 grid grid-cols-3`}>
-                    <div className={`flex items-center justify-evenly text-white p-2 m-1 font-rubik bg-green-500`} >
-                        <p className='text-xs'>Current:</p>
-                        <p className='text-xs'>2500</p>
+                    <div className={`flex items-center rounded justify-evenly text-white p-2 m-1 font-rubik bg-green-500`} >
+                        <p>Current:</p>
+                        <p>2500</p>
                     </div>
-                    <div className={`flex items-center justify-evenly text-white p-2 m-1 font-rubik bg-green-500`} >
-                        <p className='text-xs'>Previous:</p>
-                        <p className='text-xs'>2500</p>
+                    <div className={`flex items-center rounded justify-evenly text-white p-2 m-1 font-rubik bg-green-500`} >
+                        <p>Previous:</p>
+                        <p>2500</p>
                     </div>
-                    <div className={`flex items-center justify-evenly text-white p-2 m-1 font-rubik bg-green-500`} >
-                        <p className='text-xs'>Difference:</p>
-                        <p className='text-xs'>2500</p>
+                    <div className={`flex items-center rounded justify-evenly text-white p-2 m-1 font-rubik bg-green-500`} >
+                        <p>Difference:</p>
+                        <p>2500</p>
 
                     </div>
                 </div>
