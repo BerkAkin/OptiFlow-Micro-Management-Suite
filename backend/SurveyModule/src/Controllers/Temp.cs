7e433dab28d2230f041c7dc8a6ca85f81a719a8e
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-15 15:54:01 +0300

    Survey Module initial commit

---
+++ backend/SurveyModule/src/Controllers/Temp.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SurveyModule.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Temp : ControllerBase
+    {
+    }
+}
