cc6c8396158362c2eb95a237e05bdf6a0f5679ca
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-15 15:52:50 +0300

    FEAT: Mapping options defined for transactions and related dto's

---
+++ backend/FinanceModule/src/Mappings/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using FinanceModule.DTOs;
+using FinanceModule.Entities;
+
+namespace FinanceModule.Mappings
+{
+    public class MappingProfile: Profile
+    {
+        public MappingProfile() {
+
+            CreateMap<Transaction, TransactionViewModel>();
+            CreateMap<Transaction, InstallmentsViewModel>().ForMember(dest => dest.Parts, opt => opt.MapFrom(src => src.IsPartly ? src.PartCount: 0));
+        } 
+    }
+}
