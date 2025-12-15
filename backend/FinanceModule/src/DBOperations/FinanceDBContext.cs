73c41bcd4d6de07b939485f2b357cab162c81faa
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-15 15:51:36 +0300

    FEAT: DB Context defined for finance

---
+++ backend/FinanceModule/src/DBOperations/FinanceDBContext.cs
@@ -0,0 +1,12 @@
+﻿using FinanceModule.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinanceModule.DBOperations
+{
+    public class FinanceDBContext: DbContext
+    {
+        public FinanceDBContext(DbContextOptions<FinanceDBContext> options): base(options) { }
+
+        public DbSet<Transaction> Transactions { get; set; }
+    }
+}
