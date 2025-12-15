fbc32494832af2b020acb51ac6b92bca33dfc98f
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-15 15:55:20 +0300

    REFACTOR: PartCount and prices ar now string instead of number

--- client/MainClient/src/components/FinanceComponents/AddTransaction/AddTransaction.tsx
+++ client/MainClient/src/components/FinanceComponents/AddTransaction/AddTransaction.tsx
@@ -13,7 +13,7 @@ function AddTransaction() {
     }
 
     const initialValues = {
-        quantity: 0,
+        quantity: "",
         name: "",
         byWho: "",
         exchangeType: "",
@@ -22,29 +22,29 @@ function AddTransaction() {
         type: "",
         isIncome: false,
         isPartly: false,
-        partCount: 0,
-        partPrice: 0,
+        partCount: "",
+        partPrice: "",
 
     }
 
     const fields = [
-        { name: "quantity", label: "Quantity", id: "quantity", type: "number" as const, placeholder: "Quantity..." },
-        { name: "name", label: "Name", id: "name", type: "text" as const, placeholder: "Name..." },
-        { name: "byWho", label: "By Who", id: "byWho", type: "text" as const, placeholder: "Who..." },
-        { name: "exchangeType", label: "Exchange", id: "exchangeType", type: "text" as const, placeholder: "Exchange..." },
+        { name: "quantity", label: "Quantity", id: "quantity", type: "text" as const, placeholder: "Quantity" },
+        { name: "byWho", label: "By Who", id: "byWho", type: "text" as const, placeholder: "Who" },
+        { name: "exchangeType", label: "Exchange", id: "exchangeType", type: "text" as const, placeholder: "Exchange" },
         { name: "date", label: "Date", id: "date", type: "date" as const, placeholder: "Date..." },
-        { name: "description", label: "Description", id: "description", type: "text" as const, placeholder: "Description..." },
+        { name: "description", label: "Description", id: "description", type: "text" as const, placeholder: "Description" },
         { name: "type", label: "Type", id: "type", type: "text" as const, placeholder: "Type..." },
         { name: "isPartly", label: "Is Partly?", id: "isPartly", type: "checkbox" as const, placeholder: "" },
-        { name: "partCount", label: "Part Count", id: "partCount", type: "number" as const, placeholder: "Part Count..." },
-        { name: "partPrice", label: "Part Price", id: "partPrice", type: "number" as const, placeholder: "Part Price..." },
+        { name: "partCount", label: "Part Count", id: "partCount", type: "text" as const, placeholder: "Part Count" },
+        { name: "price", label: "Price", id: "partPrice", type: "text" as const, placeholder: "Price" },
         { name: "isIncome", label: "Is Income?", id: "isIncome", type: "checkbox" as const, placeholder: "" },
-        { name: "file-upload", label: "Select File ", id: "file-upload", type: "file" as const, placeholder: "" },
+        { name: "file-upload", label: "Select File", id: "file-upload", type: "file" as const, placeholder: "" },
+        { name: "category", label: "Category", id: "category", type: "text" as const, as: "select" as const, options: [{ label: "Transport", value: "transport" }, { label: "Food", value: "food" }], placeholder: "Category" },
     ]
 
     return (
         <div>
-            <DynamicForm colorScheme='bg-sky-400' hoverScheme='hover:bg-sky-500' initialValues={initialValues} title='Add Transaction' onSubmit={handleSubmit} fields={fields} />
+            <DynamicForm colorScheme='bg-blue-400' hoverScheme='hover:bg-blue-500' initialValues={initialValues} title='Add Transaction' onSubmit={handleSubmit} fields={fields} />
         </div >
     )
 }
