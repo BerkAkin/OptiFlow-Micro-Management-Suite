1b2d34d3b26d2eff70ec5b849733f036cdf911e3
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-15 15:51:01 +0300

    FEAT: Added sqlserver and context

---
+++ backend/FinanceModule/src/Program.cs
@@ -0,0 +1,53 @@
+using FinanceModule.DBOperations;
+using FinanceModule.Mappings;
+using FinanceModule.Repositories;
+using FinanceModule.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+//BAÐLANTI DÝZESÝNÝ OKUYUP BAÐLANTI KURMA KODU
+var connectionString = builder.Configuration.GetConnectionString("FinanceModuleDb");
+builder.Services.AddDbContext<FinanceDBContext>(options => { options.UseSqlServer(connectionString); });
+
+builder.Services.AddScoped<TransactionService>();
+builder.Services.AddScoped<TransactionRepository>();
+
+
+builder.Services.AddAutoMapper(cfg =>
+{
+    cfg.AddProfile(new MappingProfile());
+});
+
+
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<FinanceDBContext>();
+    db.Database.Migrate();
+}
+
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
