43688077048a9ea4f46d42ff63379076db57bff9
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-15 15:56:11 +0300

    STYLE: grid system reorganized

--- client/MainClient/src/components/FinanceComponents/Transactions/Transactions.tsx
+++ client/MainClient/src/components/FinanceComponents/Transactions/Transactions.tsx
@@ -12,19 +12,19 @@ function Transactions() {
         <div className='my-6'>
             <div className='grid grid-cols-10 gap-6'>
                 <div className='grid col-span-8 gap-6'>
-                    <div className='border rounded-lg bg-white shadow-custom border-gray-200 h-[500px]'>
+                    <div className='border rounded-lg bg-white shadow-custom border-gray-200 h-[600px] transition-all duration-200 ease-out hover:scale-101 hover:-translate-y-1'>
                         <LatestTransactions />
                     </div>
                     <div className='grid grid-cols-10 gap-6 h-[500px]'>
-                        <div className='col-span-5 border bg-white rounded-lg shadow-custom border-gray-200'>
+                        <div className='col-span-5 border bg-white rounded-lg shadow-custom border-gray-200 transition-all duration-200 ease-out hover:scale-101 hover:-translate-y-1'>
                             <InstallmentPayments />
                         </div>
-                        <div className='col-span-5 border bg-white rounded-lg shadow-custom border-gray-200'>
+                        <div className='col-span-5 border bg-white rounded-lg shadow-custom border-gray-200 transition-all duration-200 ease-out hover:scale-101 hover:-translate-y-1'>
                             <RecurrentPayments />
                         </div>
                     </div>
                 </div>
-                <div className='col-span-2 h-[850px] bg-white shadow-custom border border-gray-200 rounded-lg'>
+                <div className='col-span-2 h-[600px] bg-white shadow-custom border border-gray-200 rounded-lg transition-all duration-200 ease-out hover:scale-101 hover:-translate-y-1'>
                     <AddTransaction />
                 </div>
             </div>
