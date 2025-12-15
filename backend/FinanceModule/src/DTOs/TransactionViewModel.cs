0c380c81313d78189612e17c19f584c199481084
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-15 15:52:28 +0300

    FEAT: Transactions graphics dto entity defined

---
+++ backend/FinanceModule/src/DTOs/TransactionViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace FinanceModule.DTOs
+{
+    public class TransactionViewModel
+    {
+        public string Type { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string By { get; set; }
+        public DateTime Date { get; set; }
+        public string Exchange { get; set; }
+        public int Quantity { get; set; }
+        public int Price { get; set; }
+    }
+}
