aae4745ea911066fdd522e72449af73fb13244e1
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-16 20:39:31 +0300

    REFACTOR: Removed unnecessary fields as type

--- backend/FinanceModule/src/DTOs/TransactionViewModel.cs
+++ backend/FinanceModule/src/DTOs/TransactionViewModel.cs
@@ -2,13 +2,16 @@
 {
     public class TransactionViewModel
     {
-        public string Type { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public string By { get; set; }
-        public DateTime Date { get; set; }
-        public string Exchange { get; set; }
         public int Quantity { get; set; }
-        public int Price { get; set; }
+        public string Who { get; set; }
+        public string ExchangeType { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public bool IsIncome { get; set; }
+        public bool? IsPartly { get; set; }
+        public int? PartCount { get; set; }
+        public string Category { get; set; }
+        public string InvoicePath { get; set; }
     }
 }
