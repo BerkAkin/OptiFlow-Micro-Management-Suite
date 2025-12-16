b7b36c8f271bac90528266ee7aee92b037347c1e
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-16 22:25:16 +0300

    REFACTOR: Removed unnecessary form fields and rearranged inputs

--- client/MainClient/src/components/FinanceComponents/AddTransaction/AddTransaction.tsx
+++ client/MainClient/src/components/FinanceComponents/AddTransaction/AddTransaction.tsx
@@ -6,6 +6,7 @@ function AddTransaction() {
     const mutation = useCreateTransaction();
 
     const handleSubmit = (data: any) => {
+        console.log(data);
         mutation.mutate(data, {
             onSuccess: (data) => { console.log("Data Saved YAY", data) },
             onError: (error) => { console.log("Error Na!", error) }
@@ -14,31 +15,31 @@ function AddTransaction() {
 
     const initialValues = {
         quantity: "",
-        name: "",
-        byWho: "",
+        who: "",
         exchangeType: "",
         date: new Date().toISOString().split("T")[0],
         description: "",
-        type: "",
         isIncome: false,
         isPartly: false,
         partCount: "",
-        partPrice: "",
+        price: "",
+        category: "",
+        invoicePath: "",
 
     }
 
     const fields = [
+        { name: "description", label: "Description", id: "description", type: "text" as const, as: "textarea" as const, placeholder: "Description" },
         { name: "quantity", label: "Quantity", id: "quantity", type: "text" as const, placeholder: "Quantity" },
-        { name: "who", label: "Who", id: "Who", type: "text" as const, placeholder: "Who" },
-        { name: "exchangeType", label: "Exchange", id: "exchangeType", type: "text" as const, placeholder: "Exchange" },
-        { name: "date", label: "Date", id: "date", type: "date" as const, placeholder: "Date..." },
-        { name: "description", label: "Description", id: "description", type: "text" as const, placeholder: "Description" },
-        { name: "isPartly", label: "Is Partly?", id: "isPartly", type: "checkbox" as const, placeholder: "" },
         { name: "partCount", label: "Part Count", id: "partCount", type: "text" as const, placeholder: "Part Count" },
         { name: "price", label: "Price", id: "partPrice", type: "text" as const, placeholder: "Price" },
-        { name: "isIncome", label: "Is Income?", id: "isIncome", type: "checkbox" as const, placeholder: "" },
-        { name: "invoicePath", label: "Select File", id: "invoicePath", type: "file" as const, placeholder: "" },
+        { name: "who", label: "Who", id: "Who", type: "text" as const, placeholder: "Who" },
+        { name: "exchangeType", label: "Exchange", id: "exchangeType", type: "text" as const, placeholder: "Exchange" },
         { name: "category", label: "Category", id: "category", type: "text" as const, as: "select" as const, options: [{ label: "Transport", value: "transport" }, { label: "Food", value: "food" }], placeholder: "Category" },
+        { name: "date", label: "Date", id: "date", type: "date" as const, placeholder: "Date..." },
+        { name: "invoicePath", label: "Select File", id: "invoicePath", type: "file" as const, placeholder: "" },
+        { name: "isPartly", label: "Installment?", id: "isPartly", type: "checkbox" as const, placeholder: "" },
+        { name: "isIncome", label: "Is Income?", id: "isIncome", type: "checkbox" as const, placeholder: "" },
     ]
 
     return (
