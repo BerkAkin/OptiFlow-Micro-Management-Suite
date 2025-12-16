1506ee57bab0150afb484cd4e2a1ffe2e1f4d589
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-16 20:45:00 +0300

    FEAT: Latestactivity is now returnig filterfields and maxpage info

--- client/MainClient/src/services/FinanceServices/LatestActivityService.ts
+++ client/MainClient/src/services/FinanceServices/LatestActivityService.ts
@@ -2,5 +2,20 @@ import { fetchLatestActivity } from "../../repositories/FinanceRepositories/Fina
 
 export const LatestActivityService = async (filters: any, page: number) => {
   const data = await fetchLatestActivity(filters, page);
-  return data;
+  return {
+    values: [...data.values],
+    maxPage: data.maxPage,
+    filterFields: [
+      { name: "date", type: "date" as const, placeholder: "" },
+      {
+        name: "type",
+        type: "select" as const,
+        placeholder: "",
+        options: [
+          { label: "Income", value: "1" },
+          { label: "Expense", value: "0" },
+        ],
+      },
+    ],
+  };
 };
