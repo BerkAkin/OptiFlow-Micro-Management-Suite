35e3286cb30f9a621696bad903648f04d54ef058
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-16 22:25:43 +0300

    FEAT: Now including filtering and pagination

--- client/MainClient/src/components/FinanceComponents/InstallmentPayments/InstallmentPayments.tsx
+++ client/MainClient/src/components/FinanceComponents/InstallmentPayments/InstallmentPayments.tsx
@@ -10,6 +10,9 @@ function InstallmentPayments() {
     const [page, setPage] = useState(1);
     const [filters, setFilters] = useState({})
 
+
+    const { error, isLoading, data } = useInstallment(filters, page);
+
     const handleFilter = (values: any) => {
         setPage(1)
         setFilters(values)
@@ -18,19 +21,19 @@ function InstallmentPayments() {
     const onPrev = () => {
         if (page > 1) setPage(page - 1)
     }
-
     const onNext = () => {
         if (!data) return
         if (page < data.maxPage) setPage(page + 1)
+
     }
 
-    const { error, isLoading, data } = useInstallment(filters, page);
+
 
     if (error || !data) return (<p>Error...</p>)
     if (isLoading) return (<p>Loading...</p>)
 
     return (
-        <div className='w-full h-full'>
+        <div className='w-full h-[500px]'>
             <DynamicTable onPrev={onPrev} onNext={onNext} handleFilter={handleFilter} filterFields={data.filterFields} textScheme='text-orange-400' colorScheme='bg-orange-400' data={data.values} title='Installments' />
         </div>
 
