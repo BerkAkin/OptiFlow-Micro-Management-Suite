ec16402176e8122613ce3b9c65b9ee4cb52ceaa0
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-16 20:39:36 +0300

    REFACTOR: Removed unnecessary fields as type

--- backend/FinanceModule/src/Entities/Transaction.cs
+++ backend/FinanceModule/src/Entities/Transaction.cs
@@ -6,16 +6,15 @@ namespace FinanceModule.Entities
     {
         public int Id { get; set; }
         public int Quantity { get; set; }
-        public string Name { get; set; }
-        public string ByWho { get; set; }
-        public string Exchange { get; set; }
-        public DateTime Date { get; set; } = DateTime.Now;
+        public string Who { get; set; }
+        public string ExchangeType { get; set; }
+        public DateTime Date { get; set; }
         public string Description { get; set; }
-        public string Type { get; set; }
         public bool IsPartly { get; set; }
         public int? PartCount { get; set; }
         public decimal Price { get; set; }
         public bool IsIncome { get; set; }
         public string Category { get; set; }
+        public string InvoicePath { get; set; }
     }
 }
