653443450843e7ca826376b52219074ee9068e04
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-16 22:26:38 +0300

    REFACTOR: ALL endpoints now fetching data from real sources

--- client/MainClient/src/repositories/FinanceRepositories/FinanceRepository.ts
+++ client/MainClient/src/repositories/FinanceRepositories/FinanceRepository.ts
@@ -1,46 +1,5 @@
 import { api } from "../../lib/api";
 
-const installmentData = {
-  maxPage: 2,
-  values: [
-    {
-      description: "Denemeler",
-      date: "2025.02.12",
-      parts: "2/4",
-      price: 5000,
-      detail: "/dtl/ins/1",
-    },
-  ],
-
-  filterFields: [
-    {
-      name: "description",
-      type: "text" as const,
-      placeholder: "Description...",
-    },
-  ],
-};
-
-const recurrentData = {
-  maxPage: 6,
-  values: [
-    {
-      description: "Deneme",
-      price: 123,
-      to: "Berk",
-      recurs: "25th",
-    },
-  ],
-  filterFields: [
-    {
-      name: "description",
-      placeholder: "Description...",
-      type: "text" as const,
-    },
-    { name: "to", placeholder: "To", type: "text" as const },
-  ],
-};
-
 export const fetchMonthlyData = async () => {
   try {
     const res = await api.get("/finance/MonthlySummary");
@@ -73,20 +32,20 @@ export const fetchLatestActivity = async (filters: any, page: number) => {
 };
 
 export const fetchInstallments = async (filters: any, page: number) => {
-  /*  const res  = await api.get('/api/finance/installements',{params:{...filters,page}});
-  //return res.data*/
-
-  return installmentData;
+  const res = await api.get("/finance/Installments", {
+    params: { ...filters, page },
+  });
+  return res.data;
 };
 
 export const fetchRecurrent = async (filters: any, page: number) => {
-  /* const res= await api.get('/api/finance/remainings',{params:{...filters,page}}); */
-  //return res.data
-
-  return recurrentData;
+  const res = await api.get("/finance/recurrents", {
+    params: { ...filters, page },
+  });
+  return res.data;
 };
 
 export const createTransaction = async (payload: any) => {
-  const res = await api.post("/api/finance/transactions", payload);
+  const res = await api.post("/finance/", payload);
   return res.data;
 };
