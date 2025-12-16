9d27487d655f84581e67dbc3cb8635c657eb8682
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-16 20:38:24 +0300

    FEAT: Filter dto created to filter transactions

---
+++ backend/FinanceModule/src/DTOs/FinanceFilterDto.cs
@@ -0,0 +1,9 @@
+﻿namespace FinanceModule.DTOs
+{
+    public class FinanceFilterDto
+    {
+        public string? Type { get; set; }
+        public string? Date { get; set; }
+        public int Page { get; set; }
+    }
+}
