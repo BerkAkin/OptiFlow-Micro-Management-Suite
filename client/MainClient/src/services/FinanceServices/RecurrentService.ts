967393c76d985e3b0995dd4c5f0f32f9f5a4f181
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-16 22:25:55 +0300

    FEAT: Now including filtering and pagination

--- client/MainClient/src/services/FinanceServices/RecurrentService.ts
+++ client/MainClient/src/services/FinanceServices/RecurrentService.ts
@@ -2,5 +2,16 @@ import { fetchRecurrent } from "../../repositories/FinanceRepositories/FinanceRe
 
 export const RecurrentService = async (filters: any, page: number) => {
   const data = await fetchRecurrent(filters, page);
-  return data;
+
+  return {
+    values: data.values,
+    maxPage: data.maxPage,
+    filterFields: [
+      {
+        name: "description",
+        placeholder: "Description...",
+        type: "text" as const,
+      },
+    ],
+  };
 };
