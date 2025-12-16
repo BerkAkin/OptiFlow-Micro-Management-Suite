450bf69be8ff6b2fa2cd34e5881c113b995403ca
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-16 20:42:12 +0300

    FIX: Field names and types

--- client/MainClient/src/components/FinanceComponents/AddTransaction/AddTransaction.tsx
+++ client/MainClient/src/components/FinanceComponents/AddTransaction/AddTransaction.tsx
@@ -29,16 +29,15 @@ function AddTransaction() {
 
     const fields = [
         { name: "quantity", label: "Quantity", id: "quantity", type: "text" as const, placeholder: "Quantity" },
-        { name: "byWho", label: "By Who", id: "byWho", type: "text" as const, placeholder: "Who" },
+        { name: "who", label: "Who", id: "Who", type: "text" as const, placeholder: "Who" },
         { name: "exchangeType", label: "Exchange", id: "exchangeType", type: "text" as const, placeholder: "Exchange" },
         { name: "date", label: "Date", id: "date", type: "date" as const, placeholder: "Date..." },
         { name: "description", label: "Description", id: "description", type: "text" as const, placeholder: "Description" },
-        { name: "type", label: "Type", id: "type", type: "text" as const, placeholder: "Type..." },
         { name: "isPartly", label: "Is Partly?", id: "isPartly", type: "checkbox" as const, placeholder: "" },
         { name: "partCount", label: "Part Count", id: "partCount", type: "text" as const, placeholder: "Part Count" },
         { name: "price", label: "Price", id: "partPrice", type: "text" as const, placeholder: "Price" },
         { name: "isIncome", label: "Is Income?", id: "isIncome", type: "checkbox" as const, placeholder: "" },
-        { name: "file-upload", label: "Select File", id: "file-upload", type: "file" as const, placeholder: "" },
+        { name: "invoicePath", label: "Select File", id: "invoicePath", type: "file" as const, placeholder: "" },
         { name: "category", label: "Category", id: "category", type: "text" as const, as: "select" as const, options: [{ label: "Transport", value: "transport" }, { label: "Food", value: "food" }], placeholder: "Category" },
     ]
 
