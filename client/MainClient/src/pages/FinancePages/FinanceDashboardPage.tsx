13974414000487beff6e976cbaa2f2ef58d6941f
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-16 22:26:19 +0300

    REAFACTOR: Recurretns, installments and addtransaction is now here to fit in ui

--- client/MainClient/src/pages/FinancePages/FinanceDashboardPage.tsx
+++ client/MainClient/src/pages/FinancePages/FinanceDashboardPage.tsx
@@ -3,7 +3,10 @@ import Calendar from '../../components/FinanceComponents/Calendar/Calendar';
 import BarChart from '../../components/FinanceComponents/Charts/BarChart';
 import DoughChart from '../../components/FinanceComponents/Charts/DoughChart';
 import VerticalChart from '../../components/FinanceComponents/Charts/VerticalChart';
-import Transactions from '../../components/FinanceComponents/Transactions/Transactions';
+import InstallmentPayments from '../../components/FinanceComponents/InstallmentPayments/InstallmentPayments';
+import AddTransaction from '../../components/FinanceComponents/AddTransaction/AddTransaction';
+import LatestTransactions from '../../components/FinanceComponents/LatestTransactions/LatestTransactions';
+import RecurrentPayments from '../../components/FinanceComponents/RecurrentPayments/RecurrentPayments';
 
 ChartJS.register(ArcElement, CategoryScale, Tooltip, Legend, LinearScale, BarElement, PointElement, LineElement)
 
@@ -25,8 +28,21 @@ function FinanceDashboardPage() {
                     <Calendar />
                 </div>
             </div>
-            <div>
-                <Transactions />
+            <div className='grid grid-cols-10 gap-6 my-6'>
+                <div className='col-span-8 bg-white shadow-custom rounded-lg border border-gray-200 transition-all duration-200 ease-out hover:scale-101 hover:-translate-y-1'>
+                    <LatestTransactions />
+                </div>
+                <div className='col-span-2 bg-white shadow-custom  rounded-lg border border-gray-200 transition-all duration-200 ease-out hover:scale-101 hover:-translate-y-1'>
+                    <AddTransaction />
+                </div>
+            </div>
+            <div className='grid grid-cols-10 gap-6 mb-10'>
+                <div className='col-span-5 bg-white shadow-custom rounded-lg border border-gray-200 transition-all duration-200 ease-out hover:scale-101 hover:-translate-y-1'>
+                    <InstallmentPayments />
+                </div>
+                <div className='col-span-5 bg-white shadow-custom  rounded-lg border border-gray-200 transition-all duration-200 ease-out hover:scale-101 hover:-translate-y-1'>
+                    <RecurrentPayments />
+                </div>
             </div>
 
 
