24f78f3bf0b243e6a1a064b272bb07bbc731aada
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-17 19:45:15 +0300

    FEAT: Created spinner component to show while data is loading

---
+++ client/MainClient/src/components/Spinner/Spinner.tsx
@@ -0,0 +1,12 @@
+import styles from './styles.module.css';
+
+function Spinner() {
+    return (
+        <div className='h-full w-full p-6 flex justify-center items-center flex-col'>
+            <div className={`${styles.loader} rounded-full border-3 border-gray-200 border-t-gray-400 size-32`}></div>
+            <p className={`my-4 text-gray-400`}>Yükleniyor</p>
+        </div>
+    )
+}
+
+export default Spinner
