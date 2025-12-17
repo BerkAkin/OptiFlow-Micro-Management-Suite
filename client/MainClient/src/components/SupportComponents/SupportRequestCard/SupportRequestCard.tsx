a547ba23eab954f2d6897b4bcd713ab48a597955
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-17 19:47:21 +0300

    STYLE: minor sizing changes

--- client/MainClient/src/components/SupportComponents/SupportRequestCard/SupportRequestCard.tsx
+++ client/MainClient/src/components/SupportComponents/SupportRequestCard/SupportRequestCard.tsx
@@ -25,7 +25,7 @@ function SupportRequestCard() {
     ]
 
     return (
-        <div className='absolute h-68 w-64 bg-white rounded-lg border border-gray-200 shadow-custom z-20 left-1/2 transform -translate-x-1/2 top-16'>
+        <div className='absolute h-60 w-64 bg-white rounded-lg border border-gray-200 shadow-custom z-20 left-1/2 transform -translate-x-1/2 top-16'>
             <DynamicForm colorScheme='bg-lime-600' hoverScheme='hover:bg-lime-700' initialValues={initialValues} onSubmit={handleSubmit} title='Request Support' fields={fields} />
         </div>
     )
