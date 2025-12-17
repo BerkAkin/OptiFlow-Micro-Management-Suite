7f22c9ba119cb6191022b29c3791d389d70bf8ce
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-17 19:44:57 +0300

    FEAT: Created error component to show when an error has occured on data fetching

---
+++ client/MainClient/src/components/ErrorMessage/ErrorMessage.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import styles from './styles.module.css'
+
+function ErrorMessage() {
+    return (
+        <>
+            <div className='h-full w-full'>
+                <div className='flex relative justify-center items-end h-[60%]'>
+                    <div className={`absolute h-24 w-1 rotate-[45deg] rounded-full  ${styles.line}`}></div>
+                    <div className={`absolute h-24 w-1 rotate-[-45deg] rounded-full  ${styles.line}`}></div>
+                </div>
+                <div className='h-[40%] flex justify-center'>
+                    <div className={`${styles.glowText}`}>Hata</div>
+                </div>
+
+
+            </div>
+
+        </>
+
+    )
+}
+
+export default ErrorMessage
