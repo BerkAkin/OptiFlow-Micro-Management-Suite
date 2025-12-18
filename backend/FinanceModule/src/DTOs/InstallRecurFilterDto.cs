81f331f2e2652e664674a4da8b85053476771bf4
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-18 19:11:53 +0300

    RENAMED

--- backend/FinanceModule/src/DTOs/InstallRecurFilterDto.cs
+++ backend/FinanceModule/src/DTOs/InstallRecurFilterDto.cs
@@ -1,6 +1,6 @@
 ﻿namespace FinanceModule.DTOs
 {
-    public class InstallRecurFilterDto
+    public class InstallRecurFilterDTO
     {
         public string? Description { get; set; }
         public int Page { get; set; }
