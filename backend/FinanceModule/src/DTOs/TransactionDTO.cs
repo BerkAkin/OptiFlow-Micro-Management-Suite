fbe17cd3555e71df0a6c5dea73650f9773253267
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-18 19:13:09 +0300

    FEAT: Added json ignore when values of partly and parts props are null

---
+++ backend/FinanceModule/src/DTOs/TransactionDTO.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json.Serialization;
+
+namespace FinanceModule.DTOs
+{
+    public class TransactionDTO
+    {
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public bool Income { get; set; }
+        public string Who { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public string Exchange { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public bool? Partly { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public int? Parts { get; set; }
+        public DateTime Date { get; set; }
+        public string Invoice { get; set; }
+    }
+}
