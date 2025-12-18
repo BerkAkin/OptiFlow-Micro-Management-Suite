a1a0bed91aecf8dbcaebd46fbb56015be09f64d4
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-18 19:15:25 +0300

    REFACTOR: Almost all queries are now CQRS pattern and excluded from service

--- backend/FinanceModule/src/Services/TransactionService.cs
+++ backend/FinanceModule/src/Services/TransactionService.cs
@@ -1,7 +1,6 @@
 ﻿using AutoMapper;
 using FinanceModule.DTOs;
 using FinanceModule.Entities;
-using FinanceModule.Mappings;
 using FinanceModule.Repositories;
 
 namespace FinanceModule.Services
@@ -15,47 +14,12 @@ namespace FinanceModule.Services
             _mapper = mapper;
         }
 
-        public async Task AddAsync(TransactionViewModel model)
+        public async Task AddAsync(TransactionDTO model)
         {
             Transaction transaction = _mapper.Map<Transaction>(model);
             await _repository.AddAsync(transaction);
         }
 
-        public async Task<(List<TransactionViewModel> data,int maxPage)> GetAllTransactions(FinanceFilterDto filters)
-        {
-          var (transactions,maxPage) =  await _repository.GetAllTransactions(filters);
-          var mapped = _mapper.Map<List<TransactionViewModel>>(transactions);
-            return (mapped, maxPage);
-        }
-
-        public async Task<MonthlySummaryViewModel> GetMonthlySummary()
-        {
-            return await _repository.GetMonthlySummary();
-            
-        }
-
-        public async Task<List<CategoricalTransactionSummary>> GetCategoricalSummary()
-        {
-            return await _repository.GetCategoricalSummary();
-        }
-
-        public async Task<List<CategoricalTransactionSummary>> GetMostCategorySummary()
-        {
-            return await _repository.GetMostCategorySummary();
-        }
-
-        public async Task<(List<InstallmentsViewModel> data,int maxPage)> GetInstallments(InstallRecurFilterDto filters)
-        {
-            var (installments,maxPage) = await _repository.GetInstallments(filters);
-            var mapped=  _mapper.Map<List<InstallmentsViewModel>>(installments);
-            return (mapped, maxPage);
-        }
-
-        public async Task<(List<RecurrentsViewModel>data,int maxPage)> GetRecurrents(InstallRecurFilterDto filters)
-        {
-            var (data,maxPage) = await _repository.GetRecurrents(filters);
-            var mapped = _mapper.Map<List<RecurrentsViewModel>>(data);
-            return (mapped, maxPage);
-        }
+      
     }
 }
