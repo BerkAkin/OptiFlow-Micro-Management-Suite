7ea755685d7990a98d53e0108075cd9da6c7e788
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-18 19:16:43 +0300

    REFACTOR: Changed field names to match them with backend

--- client/MainClient/src/components/FinanceComponents/AddTransaction/AddTransaction.tsx
+++ client/MainClient/src/components/FinanceComponents/AddTransaction/AddTransaction.tsx
@@ -7,39 +7,36 @@ function AddTransaction() {
 
     const handleSubmit = (data: any) => {
         console.log(data);
-        mutation.mutate(data, {
-            onSuccess: (data) => { console.log("Data Saved YAY", data) },
-            onError: (error) => { console.log("Error Na!", error) }
-        })
+        mutation.mutate(data)
     }
 
     const initialValues = {
         quantity: "",
         who: "",
-        exchangeType: "",
+        exchange: "",
         date: new Date().toISOString().split("T")[0],
         description: "",
-        isIncome: false,
-        isPartly: false,
-        partCount: "",
+        income: false,
+        partly: false,
+        parts: "",
         price: "",
         category: "",
-        invoicePath: "",
+        invoice: "",
 
     }
 
     const fields = [
         { name: "description", label: "Description", id: "description", type: "text" as const, as: "textarea" as const, placeholder: "Description" },
         { name: "quantity", label: "Quantity", id: "quantity", type: "text" as const, placeholder: "Quantity" },
-        { name: "partCount", label: "Part Count", id: "partCount", type: "text" as const, placeholder: "Part Count" },
+        { name: "parts", label: "Part Count", id: "partCount", type: "text" as const, placeholder: "Part Count" },
         { name: "price", label: "Price", id: "partPrice", type: "text" as const, placeholder: "Price" },
         { name: "who", label: "Who", id: "Who", type: "text" as const, placeholder: "Who" },
         { name: "exchangeType", label: "Exchange", id: "exchangeType", type: "text" as const, placeholder: "Exchange" },
         { name: "category", label: "Category", id: "category", type: "text" as const, as: "select" as const, options: [{ label: "Transport", value: "transport" }, { label: "Food", value: "food" }], placeholder: "Category" },
         { name: "date", label: "Date", id: "date", type: "date" as const, placeholder: "Date..." },
-        { name: "invoicePath", label: "Select File", id: "invoicePath", type: "file" as const, placeholder: "" },
-        { name: "isPartly", label: "Installment?", id: "isPartly", type: "checkbox" as const, placeholder: "" },
-        { name: "isIncome", label: "Is Income?", id: "isIncome", type: "checkbox" as const, placeholder: "" },
+        { name: "invoice", label: "Select File", id: "invoicePath", type: "file" as const, placeholder: "Dosya:" },
+        { name: "partly", label: "Installment?", id: "isPartly", type: "checkbox" as const, placeholder: "" },
+        { name: "income", label: "Is Income?", id: "isIncome", type: "checkbox" as const, placeholder: "" },
     ]
 
     return (
