e8acf1eabaf3fef0a80933e3babeab78a75d6860
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-18 19:11:40 +0300

    RENAMED

---
+++ backend/FinanceModule/src/DTOs/CategoricalTransactionSummaryDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace FinanceModule.DTOs
+{
+    public class CategoricalTransactionSummaryDTO
+    {
+        public string Category { get; set; }
+        public decimal Expense { get; set; }
+    }
+}
