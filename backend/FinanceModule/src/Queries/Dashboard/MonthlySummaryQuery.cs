caff12b46be69b20f2ae5773518cc5e4c33c2803
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-18 19:14:26 +0300

    REFACTOR: Monthly Summary data is now CQRS Pattern

---
+++ backend/FinanceModule/src/Queries/Dashboard/MonthlySummaryQuery.cs
@@ -0,0 +1,50 @@
+﻿using FinanceModule.DBOperations;
+using FinanceModule.DTOs;
+using Microsoft.EntityFrameworkCore;
+using System.Text.RegularExpressions;
+
+namespace FinanceModule.Queries.Dashboard
+{
+    public class MonthlySummaryQuery
+    {
+        private readonly FinanceDBContext _context;
+        public MonthlySummaryQuery(FinanceDBContext context)
+        {
+             _context = context;
+        }
+
+        public async Task<MonthlySummaryDTO> Execute()
+        {
+            DateTime startDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1).AddMonths(-11);
+            DateTime endDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1).AddMonths(1);
+
+
+            var query = await _context.Transactions
+                .AsNoTracking()
+                .Where(x => x.Date < endDate && x.Date >= startDate)
+                .GroupBy(x => new { x.Date.Year, x.Date.Month })
+                .OrderBy(x => x.Key.Month)
+                .Select(g => new
+                {
+                    g.Key.Month,
+                    Expense = g.Where(x=>!x.IsIncome).Sum(x=>x.Price),
+                    Income = g.Where(x => x.IsIncome).Sum(x => x.Price),
+                }).ToListAsync();
+
+            return new MonthlySummaryDTO
+            {
+                Incomes = query.Select(x => new MonthlyValueDTO
+                {
+                    Month = x.Month,
+                    Value = x.Income
+                }).ToList(),
+
+                Expenses = query.Select(x => new MonthlyValueDTO
+                {
+                    Month = x.Month,
+                    Value = x.Expense
+                }).ToList()
+            };
+        }
+    }
+}
