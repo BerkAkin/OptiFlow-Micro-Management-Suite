9dc800905818319ce5daee0cc706c18163c02f6c
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-18 22:31:44 +0300

    REFACTOR: Applied mediator pattern

--- backend/FinanceModule/src/Queries/Dashboard/TransactionsQuery.cs
+++ backend/FinanceModule/src/Queries/Dashboard/TransactionsQuery.cs
@@ -1,40 +1,42 @@
 ﻿using FinanceModule.DBOperations;
 using FinanceModule.DTOs;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 
 namespace FinanceModule.Queries.Dashboard
 {
-    public class TransactionsQuery
+    public record TransactionsQuery(FinanceFilterDTO filters): IRequest<(List<TransactionDTO> data, int maxPage)>;
+    public class TransactionsQueryHandler: IRequestHandler<TransactionsQuery, (List<TransactionDTO> data, int maxPage)>
     {
         private readonly FinanceDBContext _context;
-        public TransactionsQuery(FinanceDBContext context)
+        public TransactionsQueryHandler(FinanceDBContext context)
         {
             _context = context;
         }
 
-        public async Task<(List<TransactionDTO> data, int maxPage)> Execute(FinanceFilterDTO filters)
+        public async Task<(List<TransactionDTO> data, int maxPage)> Handle(TransactionsQuery request, CancellationToken cancellationToken)
         {
             var query = _context.Transactions.AsNoTracking().AsQueryable();
 
-            if (!string.IsNullOrEmpty(filters.Type))
+            if (!string.IsNullOrEmpty(request.filters.Type))
             {
-                bool isIncome = filters.Type == "1";
+                bool isIncome = request.filters.Type == "1";
                 query = query.Where(x => x.IsIncome == isIncome);
             }
 
 
-            if (!string.IsNullOrEmpty(filters.Date) && DateTime.TryParse(filters.Date, out DateTime parsedDate))
+            if (!string.IsNullOrEmpty(request.filters.Date) && DateTime.TryParse(request.filters.Date, out DateTime parsedDate))
             {
                 query = query.Where(x => x.Date.Date == parsedDate.Date);
             }
 
             int pageSize = 11;
-            int page = filters.Page <= 0 ? 1 : filters.Page;
+            int page = request.filters.Page <= 0 ? 1 : request.filters.Page;
             int totalCount = await query.CountAsync();
             int maxPage = (int)Math.Ceiling((double)totalCount / pageSize);
             if (maxPage == 0) maxPage = 1;
 
-            var data = await query.OrderByDescending(x => x.Date).Skip((filters.Page - 1) * pageSize).Take(pageSize).Select(x=>new TransactionDTO
+            var data = await query.OrderByDescending(x => x.Date).Skip((request.filters.Page - 1) * pageSize).Take(pageSize).Select(x=>new TransactionDTO
             {
                 Category = x.Category,
                 Date = x.Date,
