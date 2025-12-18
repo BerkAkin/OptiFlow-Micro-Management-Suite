e463c8c6993d3b7123c3826b0ed550feb53a5a37
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-18 19:12:40 +0300

    RENAMED

--- backend/FinanceModule/src/DTOs/MonthlySummaryViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace FinanceModule.DTOs
-{
-    public class MonthlySummaryViewModel
-    {
-        public List<MonthlyValueViewModel> Incomes { get; set; }
-        public List<MonthlyValueViewModel> Expenses { get; set; }
-    }
-}
