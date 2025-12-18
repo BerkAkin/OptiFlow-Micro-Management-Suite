b1b5540f69e2c857f9b90a4c17fac2ecc0de4f38
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-18 19:14:34 +0300

    REFACTOR: Most Category data is now CQRS Pattern

---
+++ backend/FinanceModule/src/Queries/Dashboard/MostCategorySummaryQuery.cs
@@ -0,0 +1,31 @@
+﻿using FinanceModule.DBOperations;
+using FinanceModule.DTOs;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinanceModule.Queries.Dashboard
+{
+    public class MostCategorySummaryQuery
+    {
+        private readonly FinanceDBContext _context;
+        public MostCategorySummaryQuery(FinanceDBContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<CategoricalTransactionSummaryDTO>> Execute()
+        {
+            return await _context.Transactions
+                .AsNoTracking()
+                .Where(x => x.Date.Month == DateTime.Now.Month && x.Date.Year == DateTime.Now.Year && !x.IsIncome)
+                .GroupBy(x => x.Category)
+                .Select(g => new CategoricalTransactionSummaryDTO
+                {
+                    Category = g.Key,
+                    Expense = g.Sum(x => x.Price)
+                })
+                .OrderByDescending(x => x.Expense)
+                .Take(2)
+                .ToListAsync();
+        }
+    }
+}
