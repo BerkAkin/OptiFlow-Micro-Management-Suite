6b6f706ef48f0f8119c8f97f77fef939cece5cb2
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-19 17:55:41 +0300

    FEAT: Main app is now wrapped with toast context provider to show toast messages

--- client/MainClient/src/main.tsx
+++ client/MainClient/src/main.tsx
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { AuthContextProvider } from './context/AuthContext';
+import { ToastContextProvider } from './context/ToastContext';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -22,9 +23,11 @@ const queryClient = new QueryClient({
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AuthContextProvider>
-      <QueryClientProvider client={queryClient}>
-        <App />
-      </QueryClientProvider>
+      <ToastContextProvider>
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
+      </ToastContextProvider>
     </AuthContextProvider>
   </StrictMode>,
 )
