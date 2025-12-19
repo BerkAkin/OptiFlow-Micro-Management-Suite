dc685f67fa4afedeb9fccadcf128b81c82588901
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-19 20:10:31 +0300

    FEAT: Added transaction validator

--- backend/FinanceModule/src/Services/TransactionService.cs
+++ backend/FinanceModule/src/Services/TransactionService.cs
@@ -2,6 +2,7 @@
 using FinanceModule.DTOs;
 using FinanceModule.Entities;
 using FinanceModule.Repositories;
+using FluentValidation;
 
 namespace FinanceModule.Services
 {
@@ -9,13 +10,16 @@ namespace FinanceModule.Services
     {
         private readonly TransactionRepository _repository;
         private readonly IMapper _mapper;
-        public TransactionService(TransactionRepository repository, IMapper mapper) {
+        private readonly IValidator<TransactionDTO> _validator;
+        public TransactionService(TransactionRepository repository, IMapper mapper, IValidator<TransactionDTO> validator) {
             _repository = repository;
             _mapper = mapper;
+            _validator = validator;
         }
 
         public async Task AddAsync(TransactionDTO model)
         {
+            await _validator.ValidateAndThrowAsync(model);
             Transaction transaction = _mapper.Map<Transaction>(model);
             await _repository.AddAsync(transaction);
         }
