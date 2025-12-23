cfe7e60689803e6d7d0ebeaf846a743d7d00cf57
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-23 16:08:53 +0300

    FEAT: Survey and related question, answer relations defined

---
+++ backend/SurveyModule/src/Infrastructure/Persistance/SurveyDbContext.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SurveyModule.Domain.Entities;
+
+namespace SurveyModule.Infrastructure.Persistance
+{
+    public class SurveyDbContext: DbContext
+    {
+        public SurveyDbContext(DbContextOptions<SurveyDbContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Survey> Surveys { get; set; }
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<Answer> Answers { get; set; }
+        public DbSet<MiniUser> MiniUsers { get; set; }
+        public DbSet<UserAnswer> UserAnswers { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Survey>()
+                .HasMany(s => s.Questions)
+                .WithOne(q => q.Survey)
+                .HasForeignKey(q => q.SurveyId);
+
+            modelBuilder.Entity<Question>()
+                .HasMany(q => q.Answers)
+                .WithOne(a => a.Question)
+                .HasForeignKey(a => a.QuestionId);
+
+            modelBuilder.Entity<MiniUser>()
+                .HasMany(u => u.UserAnswers)
+                .WithOne(ua => ua.User)
+                .HasForeignKey(ua => ua.UserId);
+
+            modelBuilder.Entity<UserAnswer>()
+                .HasOne(ua => ua.Question)
+                .WithMany()
+                .HasForeignKey(ua => ua.QuestionId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<UserAnswer>()
+                .HasOne(ua => ua.Answer)
+                .WithMany()
+                .HasForeignKey(ua => ua.AnswerId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<UserAnswer>()
+                .HasOne(ua => ua.Survey)
+                .WithMany()
+                .HasForeignKey(ua => ua.SurveyId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+
+        }
+
+    }
+}
