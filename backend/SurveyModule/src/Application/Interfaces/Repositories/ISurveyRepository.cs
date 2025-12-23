243bcb730ff43a598acb6a7c67db23de538f56e1
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-23 23:44:25 +0300

    FEAT: Added addsurvey to interface

--- backend/SurveyModule/src/Application/Interfaces/Repositories/ISurveyRepository.cs
+++ backend/SurveyModule/src/Application/Interfaces/Repositories/ISurveyRepository.cs
@@ -1,11 +1,13 @@
-﻿using SurveyModule.Application.Queries.GetSurveyDetails;
+﻿using SurveyModule.Application.DTOs;
 using SurveyModule.Application.Queries.GetSurveys;
+using SurveyModule.Domain.Entities;
 
 namespace SurveyModule.Application.Interfaces.Repositories
 {
     public interface ISurveyRepository
     {
         Task<List<GetSurveyDto>> GetSurveyList(int TenantId);
-        Task<GetSurveyDetailDto> GetSurveyDetail(int SurveyId);
+        Task<SurveyDto> GetSurveyDetail(int SurveyId);
+        Task AddSurvey(Survey survey);
     }
 }
