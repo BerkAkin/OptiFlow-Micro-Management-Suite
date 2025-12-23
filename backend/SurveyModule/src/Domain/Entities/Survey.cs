40db13a59496614d787e329367684c6ef62d69f5
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-23 16:08:15 +0300

    FEAT: Defined survey entity for holding surveys

---
+++ backend/SurveyModule/src/Domain/Entities/Survey.cs
@@ -0,0 +1,14 @@
+﻿using SurveyModule.Domain.Enums;
+
+namespace SurveyModule.Domain.Entities
+{
+    public class Survey
+    {
+        public int Id { get; set; }
+        public int CreatorId { get; set; }
+        public string Title { get; set; }
+        public SurveyStatus Status { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public ICollection<Question> Questions { get; set; } = new List<Question>();
+    }
+}
