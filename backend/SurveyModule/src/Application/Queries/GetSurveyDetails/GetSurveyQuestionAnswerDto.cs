88cb363aa7f4602ecdc91f2256e3cf759db3ab31
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-23 19:31:52 +0300

    FEAT: Survey answer dto defined to suvey adder and seeder

---
+++ backend/SurveyModule/src/Application/Queries/GetSurveyDetails/GetSurveyQuestionAnswerDto.cs
@@ -0,0 +1,8 @@
+﻿namespace SurveyModule.Application.Queries.GetSurveyDetails
+{
+    public class GetSurveyQuestionAnswerDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+    }
+}
