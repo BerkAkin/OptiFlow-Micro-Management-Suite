8d4061bd16212ff139c1abe13d243e15b7d76cba
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-23 23:44:33 +0300

    REMOVED

--- backend/SurveyModule/src/Application/Queries/GetSurveyDetails/GetSurveyDetailDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SurveyModule.Application.Queries.GetSurveyDetails
-{
-    public class GetSurveyDetailDto
-    {
-        public string Title { get; set; }
-        public ICollection<GetSurveyQuestionDto> Questions { get; set; } = new List<GetSurveyQuestionDto>();
-    }
-}
