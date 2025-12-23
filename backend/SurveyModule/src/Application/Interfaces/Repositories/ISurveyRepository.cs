b2deefc3bf10cc247e0f4398bf56454ae6ce9d8f
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-23 19:30:40 +0300

    FEAT: Interface for repository has been defined to change behaviour through dependency injection

---
+++ backend/SurveyModule/src/Application/Interfaces/Repositories/ISurveyRepository.cs
@@ -0,0 +1,11 @@
+﻿using SurveyModule.Application.Queries.GetSurveyDetails;
+using SurveyModule.Application.Queries.GetSurveys;
+
+namespace SurveyModule.Application.Interfaces.Repositories
+{
+    public interface ISurveyRepository
+    {
+        Task<List<GetSurveyDto>> GetSurveyList(int TenantId);
+        Task<GetSurveyDetailDto> GetSurveyDetail(int SurveyId);
+    }
+}
