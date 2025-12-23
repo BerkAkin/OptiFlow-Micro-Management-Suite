a7f099a7cec8cf45a8e6007fd9a04a3bfe1f59cd
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-23 19:30:50 +0300

    FEAT: Survey detail dto defined

---
+++ backend/SurveyModule/src/Application/Queries/GetSurveyDetails/GetSurveyDetailDto.cs
@@ -0,0 +1,8 @@
+﻿namespace SurveyModule.Application.Queries.GetSurveyDetails
+{
+    public class GetSurveyDetailDto
+    {
+        public string Title { get; set; }
+        public ICollection<GetSurveyQuestionDto> Questions { get; set; } = new List<GetSurveyQuestionDto>();
+    }
+}
