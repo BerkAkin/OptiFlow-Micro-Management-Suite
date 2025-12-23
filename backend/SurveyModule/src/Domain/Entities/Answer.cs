222b5e9ce3a14bb3a849f76b1a78e8d7214df7bf
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-23 16:07:31 +0300

    FEAT: Defined answer entity for answers

---
+++ backend/SurveyModule/src/Domain/Entities/Answer.cs
@@ -0,0 +1,10 @@
+﻿namespace SurveyModule.Domain.Entities
+{
+    public class Answer
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int QuestionId { get; set; }
+        public Question Question { get; set; }
+    }
+}
