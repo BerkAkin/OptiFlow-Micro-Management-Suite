2474af4efac6f79ba3d1bc0ed047db0c00fef005
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-23 16:07:43 +0300

    FEAT:  Defined temporary user entity

---
+++ backend/SurveyModule/src/Domain/Entities/MiniUser.cs
@@ -0,0 +1,12 @@
+﻿namespace SurveyModule.Domain.Entities
+{
+    public class MiniUser
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public List<UserAnswer> UserAnswers { get; set; } = new List<UserAnswer>();
+
+
+    }
+}
