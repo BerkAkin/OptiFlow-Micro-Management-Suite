79a8eca7bd1416f4535374bc1d3e652b55b10800
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-23 23:44:12 +0300

    FEAT: SurveyDTo defined for both retrieve surveydetails and posting survey

---
+++ backend/SurveyModule/src/Application/DTOs/SurveyDto.cs
@@ -0,0 +1,11 @@
+﻿using SurveyModule.Application.Queries.GetSurveyDetails;
+
+namespace SurveyModule.Application.DTOs
+{
+    public class SurveyDto
+    {
+        public int? Id { get; set; }
+        public string Title { get; set; }
+        public List<QuestionDto> Questions { get; set; } = new();
+    }
+}
