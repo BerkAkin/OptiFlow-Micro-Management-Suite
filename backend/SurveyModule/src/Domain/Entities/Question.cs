f96a5c4961d7ec9d80b18f457df3f0c874d9f358
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-23 16:08:04 +0300

    FEAT: Defined question entity for holding questions

---
+++ backend/SurveyModule/src/Domain/Entities/Question.cs
@@ -0,0 +1,12 @@
+﻿namespace SurveyModule.Domain.Entities
+{
+    public class Question
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public ICollection<Answer> Answers { get; set; } = new List<Answer>();
+
+        public int SurveyId { get; set; }
+        public Survey Survey { get; set; }
+    }
+}
