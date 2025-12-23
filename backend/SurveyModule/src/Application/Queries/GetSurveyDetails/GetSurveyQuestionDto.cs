7ac3b362db101b2e87fe9d8e1e8f7118333bebec
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-23 19:32:22 +0300

    FEAT: Survey questions dto defined to retrieve datas on detail endpoint call

---
+++ backend/SurveyModule/src/Application/Queries/GetSurveyDetails/GetSurveyQuestionDto.cs
@@ -0,0 +1,9 @@
+﻿namespace SurveyModule.Application.Queries.GetSurveyDetails
+{
+    public class GetSurveyQuestionDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public ICollection<GetSurveyQuestionAnswerDto> Answers { get; set; } = new List<GetSurveyQuestionAnswerDto>();
+    }
+}
