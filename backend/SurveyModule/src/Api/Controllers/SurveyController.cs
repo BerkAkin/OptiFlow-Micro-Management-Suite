40e413ee4d4af27f3d8daf6c4f87bb5c830644a1
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-23 23:43:14 +0300

    FEAT: Added survey Adder endpoint

--- backend/SurveyModule/src/Api/Controllers/SurveyController.cs
+++ backend/SurveyModule/src/Api/Controllers/SurveyController.cs
@@ -1,5 +1,7 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using SurveyModule.Application.Commands.AddSurvey;
+using SurveyModule.Application.DTOs;
 using SurveyModule.Application.Queries.GetSurveyDetails;
 using SurveyModule.Application.Queries.GetSurveys;
 
@@ -24,6 +26,13 @@ namespace SurveyModule.Api.Controllers
         public async Task<IActionResult> GetSurveyDetail([FromQuery] int SurveyId)
         {
             var data = await _mediator.Send(new GetSurveyDetailQuery(SurveyId));
+            return Ok(data);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddSurvey([FromBody] SurveyDto survey)
+        {
+            var data = await _mediator.Send(new AddSurveyCommand(survey));
             return Ok(data);
         }
            
