53f15e3728b5a8bafedfd06f8c25eac3979156ce
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-23 19:33:19 +0300

    FEAT: Added surveydetail repository function

---
+++ backend/SurveyModule/src/Infrastructure/Repositories/SurveysRepository.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SurveyModule.Application.Interfaces.Repositories;
+using SurveyModule.Application.Queries.GetSurveyDetails;
+using SurveyModule.Application.Queries.GetSurveys;
+using SurveyModule.Infrastructure.Persistance;
+
+namespace SurveyModule.Infrastructure.Repositories
+{
+    public class SurveysRepository: ISurveyRepository
+    {
+        private readonly SurveyDbContext _context;
+        public SurveysRepository(SurveyDbContext context)
+        {
+            _context= context;
+        }
+         
+        public async Task<List<GetSurveyDto>> GetSurveyList(int TenantId)
+        {
+            return await _context.Surveys.Where(x => x.TenantId == TenantId).Select(x => new GetSurveyDto
+            {
+                Title = x.Title,
+                TenantId = x.TenantId,  
+                Id=x.Id,
+                Date= x.CreatedAt
+
+            }).ToListAsync();
+        }
+
+        public async Task<GetSurveyDetailDto> GetSurveyDetail(int SurveyId)
+        {
+            return await _context.Surveys.Where(x => x.Id == SurveyId).Select(x => new GetSurveyDetailDto
+            {
+                Title= x.Title,
+                Questions= x.Questions.Select(q=> new GetSurveyQuestionDto
+                {
+                    Id=q.Id,
+                    Title= q.Title,
+                    Answers = q.Answers.Select(a=> new GetSurveyQuestionAnswerDto
+                    {
+                        Id=a.Id,    
+                        Title= a.Title,
+                    }).ToList()   
+                }).ToList()
+            }).SingleOrDefaultAsync();
+        }
+    }
+}
