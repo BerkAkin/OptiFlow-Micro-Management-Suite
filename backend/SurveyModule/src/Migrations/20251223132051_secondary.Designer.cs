41caa13bf61b6d8067c51149d79b541d5f708fa1
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-23 19:33:47 +0300

    FEAT: Migration reimplemented as second

--- backend/SurveyModule/src/Migrations/20251223132051_secondary.Designer.cs
+++ backend/SurveyModule/src/Migrations/20251223132051_secondary.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SurveyModule.Infrastructure.Persistance;
 
@@ -11,9 +12,11 @@ using SurveyModule.Infrastructure.Persistance;
 namespace SurveyModule.Migrations
 {
     [DbContext(typeof(SurveyDbContext))]
-    partial class SurveyDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251223132051_secondary")]
+    partial class secondary
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ backend/SurveyModule/src/Seeder/DummyDataSeeder.cs
@@ -0,0 +1,50 @@
+﻿using SurveyModule.Domain.Entities;
+using SurveyModule.Infrastructure.Persistance;
+
+namespace SurveyModule.Seeder
+{
+    public class DummyDataSeeder
+    {
+        private readonly SurveyDbContext _context;
+        public DummyDataSeeder(SurveyDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Seeder()
+        {
+           await SeedSurveys();
+        }
+        public async Task SeedSurveys()
+        {
+            if (!_context.Surveys.Any())
+            {
+                Survey survey = new Survey()
+                {
+                    TenantId = 1,
+                    CreatorId = 1,
+                    CreatedAt = DateTime.UtcNow,
+                    Status = Domain.Enums.SurveyStatus.Published,
+                    Title = "Dummy 1",
+                    Questions = new List<Question>
+                    {
+                     new Question {
+                        Title= "Dummy question 1",
+                        Answers= new List<Answer>{
+                            new Answer
+                            {
+                                Title="Dummy Answer 1"
+                            },
+                        }
+
+                     },
+                    }
+
+                };
+
+                await _context.AddAsync(survey);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
