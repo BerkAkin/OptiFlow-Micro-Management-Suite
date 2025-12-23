1286e116c1c9bca1102c595878d1f76df5035ed0
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-23 16:08:26 +0300

    FEAT: Defined useranswer entity for holding useranswers

---
+++ backend/SurveyModule/src/Domain/Entities/UserAnswer.cs
@@ -0,0 +1,17 @@
+﻿namespace SurveyModule.Domain.Entities
+{
+    public class UserAnswer
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public MiniUser User { get; set; }
+        public int SurveyId { get; set; }
+        public Survey Survey { get; set; }
+        public int QuestionId { get; set; }
+        public Question Question { get; set; }
+        public int AnswerId { get; set; }
+        public Answer Answer { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+    }
+}
