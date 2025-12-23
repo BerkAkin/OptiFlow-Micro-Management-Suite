88f3fef379fb55dcae9575032f8638914c4aea07
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-23 19:29:58 +0300

    FEAT: Get survey and get survey details implemented

---
+++ backend/SurveyModule/src/Api/Controllers/SurveyController.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using SurveyModule.Application.Queries.GetSurveyDetails;
+using SurveyModule.Application.Queries.GetSurveys;
+
+namespace SurveyModule.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SurveyController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+      public SurveyController(IMediator mediator) {
+            _mediator = mediator;
+      }
+
+        [HttpGet]
+        public async Task<IActionResult> GetSurvey([FromQuery] int TenantId) {
+            var data = await _mediator.Send(new GetSurveyQuery(TenantId));
+            return Ok(data);
+        }
+
+        [HttpGet("SurveyDetail")]
+        public async Task<IActionResult> GetSurveyDetail([FromQuery] int SurveyId)
+        {
+            var data = await _mediator.Send(new GetSurveyDetailQuery(SurveyId));
+            return Ok(data);
+        }
+           
+    }
+}
