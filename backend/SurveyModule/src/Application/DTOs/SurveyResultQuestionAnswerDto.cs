4966da4becf6d289b74de67656cf63d3e1eba50f
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-24 23:54:14 +0300

    FEAT: Survey Result Question Answer Dto defined

---
+++ backend/SurveyModule/src/Application/DTOs/SurveyResultQuestionAnswerDto.cs
@@ -0,0 +1,8 @@
+﻿namespace SurveyModule.Application.DTOs
+{
+    public class SurveyResultQuestionAnswerDto
+    {
+        public int Id { get; set; }
+        public int Percent { get; set; }
+    }
+}
