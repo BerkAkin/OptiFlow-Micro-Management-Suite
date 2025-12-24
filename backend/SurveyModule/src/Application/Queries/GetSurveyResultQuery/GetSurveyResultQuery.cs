4a22763f308a8cc52ae7bddd9f363aeb848f8a8f
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-24 23:56:14 +0300

    FEAT: Get Survey results query implemented

---
+++ backend/SurveyModule/src/Application/Queries/GetSurveyResultQuery/GetSurveyResultQuery.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using SurveyModule.Application.DTOs;
+using SurveyModule.Application.Interfaces.Repositories;
+
+namespace SurveyModule.Application.Queries.GetSurveyResult
+{
+    public record GetSurveyResultQuery(int SurveyId) : IRequest<SurveyResultDto>;
+    public class GetSurveyResultQueryHandler : IRequestHandler<GetSurveyResultQuery, SurveyResultDto>
+    {
+        private readonly ISurveyRepository _repository;
+        public GetSurveyResultQueryHandler(ISurveyRepository repository)
+        {
+            _repository = repository;
+        }
+        
+        public async Task<SurveyResultDto> Handle(GetSurveyResultQuery request,CancellationToken cancellationToken) { 
+               return await _repository.GetSurveyResult(request.SurveyId);
+        }
+    }
+}
