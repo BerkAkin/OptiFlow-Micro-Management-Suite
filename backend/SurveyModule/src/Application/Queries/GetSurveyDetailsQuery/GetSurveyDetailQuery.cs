5daf1528e8b7e2953788e6d6bfce4c82d03248e0
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-24 23:56:04 +0300

    FEAT: Get Survey detail query implemented

---
+++ backend/SurveyModule/src/Application/Queries/GetSurveyDetailsQuery/GetSurveyDetailQuery.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using SurveyModule.Application.DTOs;
+using SurveyModule.Application.Interfaces.Repositories;
+
+namespace SurveyModule.Application.Queries.GetSurveyDetailsQuery
+{
+    public record GetSurveyDetailQuery(int SurveyId) : IRequest<SurveyDto>;
+    public class GetSurveyDetailHandler: IRequestHandler<GetSurveyDetailQuery, SurveyDto>
+    {
+        private readonly ISurveyRepository _repository;
+        public GetSurveyDetailHandler(ISurveyRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<SurveyDto> Handle(GetSurveyDetailQuery request, CancellationToken cancellationToken)
+        {
+            return await _repository.GetSurveyDetail(request.SurveyId);
+        }
+    }
+}
