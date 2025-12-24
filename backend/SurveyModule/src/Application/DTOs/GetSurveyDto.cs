6e5e46aeec65715bddd3461bbd95820386bf0763
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-24 23:53:27 +0300

    FEAT: Get Survey List dto relocated

---
+++ backend/SurveyModule/src/Application/DTOs/GetSurveyDto.cs
@@ -0,0 +1,10 @@
+﻿namespace SurveyModule.Application.DTOs
+{
+    public class GetSurveyDto
+    {
+        public int Id { get; set; }
+        public int TenantId { get; set; }
+        public DateTime Date { get; set; }
+        public string Title { get; set; }
+    }
+}
