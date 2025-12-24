2e280285bac72fee5f4a02c08de31a686c41fdbe
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-24 23:53:05 +0300

    FEAT: Survey joiner command implemented

---
+++ backend/SurveyModule/src/Application/Commands/AnswerSurveyCommand/AnswerSurveyCommand.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using SurveyModule.Application.DTOs;
+using SurveyModule.Application.Interfaces.Repositories;
+using SurveyModule.Domain.Entities;
+
+namespace SurveyModule.Application.Commands.AnswerSurveyCommand
+{
+    public record AnswerSurveyCommand(UserAnswerDto UserAnswer): IRequest<Unit>;
+    public class AnswerSurveyCommandHandler : IRequestHandler<AnswerSurveyCommand,Unit>
+    {
+        private readonly ISurveyRepository _surveyRepository;
+        public AnswerSurveyCommandHandler(ISurveyRepository surveyRepository)
+        {
+            _surveyRepository = surveyRepository;
+        }
+
+        public async Task<Unit> Handle(AnswerSurveyCommand request, CancellationToken cancellationToken)
+        {
+            var entites = request.UserAnswer.Answers.Select(a => new UserAnswer
+            {
+                UserId = request.UserAnswer.UserId,
+                SurveyId = request.UserAnswer.SurveyId,
+                AnswerId = a.AnswerId,
+                QuestionId = a.QuestionId,
+                CreatedAt = DateTime.Now,
+            }).ToList();
+
+            await _surveyRepository.AnswerSurvey(entites);
+            return Unit.Value;
+        }
+    }
+}
