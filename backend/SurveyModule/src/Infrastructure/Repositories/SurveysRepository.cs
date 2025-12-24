f275c9c355c512c1a4f6edff3bd42afc1e8ad331
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-24 23:57:09 +0300

    FEAT: Added survey adder and survet answer repository definition functions

--- backend/SurveyModule/src/Infrastructure/Repositories/SurveysRepository.cs
+++ backend/SurveyModule/src/Infrastructure/Repositories/SurveysRepository.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SurveyModule.Application.DTOs;
 using SurveyModule.Application.Interfaces.Repositories;
-using SurveyModule.Application.Queries.GetSurveys;
 using SurveyModule.Domain.Entities;
 using SurveyModule.Infrastructure.Persistance;
 
@@ -17,7 +16,7 @@ namespace SurveyModule.Infrastructure.Repositories
          
         public async Task<List<GetSurveyDto>> GetSurveyList(int TenantId)
         {
-            return await _context.Surveys.Where(x => x.TenantId == TenantId).Select(x => new GetSurveyDto
+            return await _context.Surveys.AsNoTracking().Where(x => x.TenantId == TenantId).Select(x => new GetSurveyDto
             {
                 Title = x.Title,
                 TenantId = x.TenantId,  
@@ -29,7 +28,7 @@ namespace SurveyModule.Infrastructure.Repositories
 
         public async Task<SurveyDto> GetSurveyDetail(int SurveyId)
         {
-            return await _context.Surveys.Where(x => x.Id == SurveyId).Select(x => new SurveyDto
+            return await _context.Surveys.AsNoTracking().Where(x => x.Id == SurveyId).Select(x => new SurveyDto
             {
                 Id= x.Id,
                 Title= x.Title,
@@ -48,8 +47,51 @@ namespace SurveyModule.Infrastructure.Repositories
 
         public async Task AddSurvey(Survey survey)
         {
-            await _context.AddAsync(survey);
+
+            await _context.Surveys.AddAsync(survey);
             await _context.SaveChangesAsync();
         }
+
+        public async Task AnswerSurvey(List<UserAnswer> UserAnswer)
+        {
+            var userId = UserAnswer.First().UserId;
+            var surveyId = UserAnswer.First().SurveyId;
+            var alreadyAnswered = await _context.UserAnswers.AnyAsync(x => x.UserId == userId && x.SurveyId == surveyId);
+            if (alreadyAnswered)
+                throw new InvalidOperationException("Bu kullanıcı ankete daha önce katılmış.");
+
+            await _context.UserAnswers.AddRangeAsync(UserAnswer);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<SurveyResultDto> GetSurveyResult(int SurveyId)
+        {
+            var answers = await _context.UserAnswers
+                .Where(x => x.SurveyId == SurveyId)
+                .ToListAsync();
+
+            var result = new SurveyResultDto
+            {
+                Id = SurveyId,
+                Questions = answers
+                .GroupBy(x => x.QuestionId)
+                .Select(questionGroup => new SurveyResultQuestionDto
+                {
+                    Id = questionGroup.Key,
+                    Answers = questionGroup
+                    .GroupBy(x => x.AnswerId)
+                    .Select(answerGroup => new SurveyResultQuestionAnswerDto
+                    {
+                        Id = answerGroup.Key,
+                        Percent = (int)Math.Round((double)answerGroup.Count() * 100 / questionGroup.Count())
+                    }).ToList()
+                }).ToList()
+            };
+
+
+            return result;
+                
+        }
+
     }
 }
