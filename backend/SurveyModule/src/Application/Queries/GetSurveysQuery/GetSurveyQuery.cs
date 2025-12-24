fcc3adcad99e90bd19a59861efefddf9f38e25b7
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-24 23:56:18 +0300

    RELOCATED

---
+++ backend/SurveyModule/src/Application/Queries/GetSurveysQuery/GetSurveyQuery.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using SurveyModule.Application.DTOs;
+using SurveyModule.Application.Interfaces.Repositories;
+
+namespace SurveyModule.Application.Queries.GetSurveysQuery
+{
+    public record GetSurveyQuery(int TenantId) : IRequest<List<GetSurveyDto>>;
+    public class GetSurveyQueryHandler : IRequestHandler<GetSurveyQuery,List<GetSurveyDto>>
+    {
+        private readonly ISurveyRepository _repository;
+        public GetSurveyQueryHandler(ISurveyRepository repository)
+        {
+            _repository = repository;
+        }
+        public async Task<List<GetSurveyDto>> Handle(GetSurveyQuery request,CancellationToken cancellationToken) {
+            return await _repository.GetSurveyList(request.TenantId);
+        }
+    }
+}
