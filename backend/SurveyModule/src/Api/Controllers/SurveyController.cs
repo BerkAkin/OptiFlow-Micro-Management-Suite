0cf05070cba913740bfed33d152b3811968f5f3e
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-24 23:52:38 +0300

    FEAT: Added survey addition and joining survey endpoints

--- backend/SurveyModule/src/Api/Controllers/SurveyController.cs
+++ backend/SurveyModule/src/Api/Controllers/SurveyController.cs
@@ -1,9 +1,11 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using SurveyModule.Application.Commands.AddSurvey;
+using SurveyModule.Application.Commands.AnswerSurveyCommand;
 using SurveyModule.Application.DTOs;
-using SurveyModule.Application.Queries.GetSurveyDetails;
-using SurveyModule.Application.Queries.GetSurveys;
+using SurveyModule.Application.Queries.GetSurveyDetailsQuery;
+using SurveyModule.Application.Queries.GetSurveyResult;
+using SurveyModule.Application.Queries.GetSurveysQuery;
 
 namespace SurveyModule.Api.Controllers
 {
@@ -33,6 +35,20 @@ namespace SurveyModule.Api.Controllers
         public async Task<IActionResult> AddSurvey([FromBody] SurveyDto survey)
         {
             var data = await _mediator.Send(new AddSurveyCommand(survey));
+            return Ok("Survey Kaydedildi");
+        }
+
+        [HttpPost("UserAnswer")]
+        public async Task<IActionResult> UserAnswer([FromBody] UserAnswerDto UserAnswer)
+        {
+            var data = await _mediator.Send(new AnswerSurveyCommand(UserAnswer));
+            return Ok("Cevaplar Kaydedildi");
+        }
+
+        [HttpGet("SurveyResult")]
+        public async Task<IActionResult> GetSurveyResult([FromQuery] int SurveyId)
+        {
+            var data = await _mediator.Send(new GetSurveyResultQuery(SurveyId));
             return Ok(data);
         }
            
