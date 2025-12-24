b3bfa683d1d8f884919e57f6cdb1e95256461ba7
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2025-12-24 23:54:33 +0300

    FEAT: User answer Question defined

---
+++ backend/SurveyModule/src/Application/DTOs/UserAnswerQuestionDto.cs
@@ -0,0 +1,8 @@
+﻿namespace SurveyModule.Application.DTOs
+{
+    public class UserAnswerQuestionDto
+    {
+        public int QuestionId { get; set; }
+        public int AnswerId { get; set; }
+    }
+}
