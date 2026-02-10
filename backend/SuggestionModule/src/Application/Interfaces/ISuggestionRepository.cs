f67899bca6de640e04dde4f258c312441fa3c6e7
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-02-10 14:17:30 +0300

    FEAT: Interface for Suggestions reporsitory defined

---
+++ backend/SuggestionModule/src/Application/Interfaces/ISuggestionRepository.cs
@@ -0,0 +1,16 @@
+﻿using SuggestionModule.Application.DTOs;
+using SuggestionModule.Domain.Entities;
+using SuggestionModule.Domain.Enums;
+
+namespace SuggestionModule.Application.Interfaces
+{
+    public interface ISuggestionRepository
+    {
+        Task<List<SuggestionDto>> GetSuggestions(int tenantId);
+        Task<MostSuggestionsDto> GetBestSuggestions(int tenantId);
+        Task MakeVote(Vote vote);
+        Task MakeSuggestion(Suggestion suggestion);
+        Task<Suggestion?> GetByIdAsync(int id);
+        Task SaveAsync();
+    }
+}
