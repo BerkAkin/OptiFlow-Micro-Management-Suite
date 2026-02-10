042363993fea75a6f1853ded24827c9cc38b7d4e
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-02-10 14:12:39 +0300

    FEAT: Added cors policy for future fix to gateway

--- backend/Gateway/src/Program.cs
+++ backend/Gateway/src/Program.cs
@@ -1,3 +1,4 @@
+using Gateway.Handlers;
 using Microsoft.IdentityModel.Tokens;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
@@ -6,21 +7,6 @@ using System.Text;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
-builder.Services.AddOcelot(builder.Configuration);
-
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowAny",
-        policy => policy.WithOrigins("http://localhost:3000")
-                        .AllowAnyHeader()
-                        .AllowAnyMethod());
-});
-
-
 
 builder.Services.AddAuthentication("Bearer")
     .AddJwtBearer("Bearer", options =>
@@ -34,30 +20,29 @@ builder.Services.AddAuthentication("Bearer")
             ValidIssuer = "AuthModule",
             ValidAudience = "Gateway",
             IssuerSigningKey = new SymmetricSecurityKey(
-             Encoding.UTF8.GetBytes(builder.Configuration["JwtSettings:SecretKey"]))
+                Encoding.UTF8.GetBytes(builder.Configuration["JwtSettings:SecretKey"]))
         };
     });
+builder.Services.AddTransient<JwtToHeaderHandler>();
+builder.Services.AddOcelot(builder.Configuration).AddDelegatingHandler<JwtToHeaderHandler>(true);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAny",
+        policy => policy
+            .WithOrigins("http://localhost:3000")
+            .AllowAnyHeader()
+            .AllowAnyMethod());
+});
 
 var app = builder.Build();
 
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
 app.UseHttpsRedirection();
 app.UseCors("AllowAny");
-app.UseAuthorization();
+
 app.UseAuthentication();
-
-app.MapControllers();
-
-
+app.UseAuthorization();
 
 await app.UseOcelot();
 
-
 app.Run();
-
