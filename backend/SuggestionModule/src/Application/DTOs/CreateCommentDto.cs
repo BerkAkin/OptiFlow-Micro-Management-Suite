aecc42a1e90de017b44ff5f8a877fbdde5917b39
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-02-10 14:15:56 +0300

    FEAT: Entity object for comment creation on suggestions defined

---
+++ backend/SuggestionModule/src/Application/DTOs/CreateCommentDto.cs
@@ -0,0 +1,8 @@
+﻿namespace SuggestionModule.Application.DTOs
+{
+    public class CreateCommentDto
+    {
+        public int SuggestionId { get; set; }
+        public string Text { get; set; }
+    }
+}
