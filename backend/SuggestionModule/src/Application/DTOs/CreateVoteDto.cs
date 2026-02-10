73ba1660439a965b140aaf9ce367965ea2ccdaa5
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-02-10 14:16:22 +0300

    FEAT: Entity object for adding vote  on suggestion defined

---
+++ backend/SuggestionModule/src/Application/DTOs/CreateVoteDto.cs
@@ -0,0 +1,11 @@
+﻿using SuggestionModule.Domain.Enums;
+
+namespace SuggestionModule.Application.DTOs
+{
+    public class CreateVoteDto
+    {
+        public int SuggestionId { get; set; }
+        public int UserId { get; set; }
+        public VoteType VoteType { get; set; }
+    }
+}
