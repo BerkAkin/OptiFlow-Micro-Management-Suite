20b702b344e84712a35e2f3bdb3acdfa7d94914b
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-02-10 14:25:20 +0300

    FEAT: Added satisfaction abstraction

--- backend/SurveyModule/src/Application/Interfaces/Repositories/ISurveyRepository.cs
+++ backend/SurveyModule/src/Application/Interfaces/Repositories/ISurveyRepository.cs
@@ -6,9 +6,11 @@ namespace SurveyModule.Application.Interfaces.Repositories
     public interface ISurveyRepository
     {
         Task<List<GetSurveyDto>> GetSurveyList(int TenantId);
-        Task<SurveyDto> GetSurveyDetail(int SurveyId);
+        Task<SurveyDto> GetSurveyDetail(int SurveyId, int tenantId,int UserId);
         Task<SurveyResultDto> GetSurveyResult(int SurveyId);
         Task AddSurvey(Survey survey);
         Task AnswerSurvey(List<UserAnswer> UserAnswer);
+
+        Task AddSatisfaction(SatisfactionRate satisfaction);
     }
 }
