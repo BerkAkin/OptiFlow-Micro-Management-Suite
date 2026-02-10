9e6222b7af1b0fadd6b3fc9f2789ee26aa427749
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-02-10 14:17:58 +0300

    FEAT: Suggestion getter query implemented

---
+++ backend/SuggestionModule/src/Application/Queries/GetSuggestionsQuery/GetSuggestionsQuery.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using SuggestionModule.Application.DTOs;
+using SuggestionModule.Application.Interfaces;
+
+namespace SuggestionModule.Application.Queries.GetSuggestionsQuery
+{
+    public record GetSuggestionsQuery(int tenantId) : IRequest<List<SuggestionDto>>;
+    public class GetSuggestionsQueryHandler : IRequestHandler<GetSuggestionsQuery, List<SuggestionDto>> {
+
+        private readonly ISuggestionRepository _repository;
+        public GetSuggestionsQueryHandler(ISuggestionRepository repository) { 
+            _repository = repository;
+        } 
+        public async Task<List<SuggestionDto>> Handle(GetSuggestionsQuery request,CancellationToken cancellationToken){
+            var data = await _repository.GetSuggestions(request.tenantId);
+            return data;
+        }
+    }
+}
