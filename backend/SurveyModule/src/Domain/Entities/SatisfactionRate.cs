1af3c5dd2c84c65da3d5ed28a33ba5f110aad8ab
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-02-10 14:26:29 +0300

    FEAT: Satisfaction rate entity defined

---
+++ backend/SurveyModule/src/Domain/Entities/SatisfactionRate.cs
@@ -0,0 +1,13 @@
+﻿using SurveyModule.Domain.Enums;
+
+namespace SurveyModule.Domain.Entities
+{
+    public class SatisfactionRate
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int SurveyId { get; set; }
+        public Survey Survey { get; set; }
+        public SatisfactionStatus Satisfaction { get; set; }
+    }
+}
