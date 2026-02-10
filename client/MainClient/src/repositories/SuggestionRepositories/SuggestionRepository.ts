999c6f08eea6d0ead768652ec1cfb438a41e1047
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-02-10 14:39:21 +0300

    FEAT: SuggestionRepository is now using real data

--- client/MainClient/src/repositories/SuggestionRepositories/SuggestionRepository.ts
+++ client/MainClient/src/repositories/SuggestionRepositories/SuggestionRepository.ts
@@ -1,85 +1,32 @@
 import { api } from "../../lib/api";
 
-const tempSuggestionData = {
-  suggestions: [
-    {
-      id: 0,
-      status: "Approved",
-      header: "Sokak Hayvanları",
-      description:
-        "Bence soğuk havada hayvanları ofiste ayrılmış bölümün içerisine almalıyız",
-      voteCount: 1,
-      date: "14.08.2025",
-      commentCount: 2,
-      comments: [
-        {
-          commentText: "Alerjisi olan insanlar için kötü bir fikir",
-          date: "20.10.2025",
-        },
-        {
-          commentText: "Ayrı bir bölüm olacaksa olabilir",
-          date: "20.10.2025",
-        },
-      ],
-    },
-    {
-      id: 1,
-      status: "Rejected",
-      header: "Sokak Hayvanları",
-      description: "Ofis camları günde 5 saat kadar açılmalı ve öyle kalmalı",
-      voteCount: 1,
-      date: "25.12.2025",
-      commentCount: 2,
-      comments: [
-        {
-          commentText: "Havalar soğuk olmaz",
-          date: "20.10.2025",
-        },
-        {
-          commentText: "Mola saatlerinde olabilir",
-          date: "20.10.2025",
-        },
-      ],
-    },
-  ],
-};
-
-const tempShowcase = {
-  monthly: "Bence soğuk havalarda da olsa ofis günde 3 saat havalandırılmalı",
-  monthlyVotes: 4568,
-  allTimes: "Evcil hayvanları getirip buluşma günleri yapılmalı",
-  allTimesVotes: 12441,
-};
-
 export const fetchSuggestions = async () => {
-  /* const res = await api.get("/api/suggestions");
-  return res.data; */
-  return tempSuggestionData;
+  const res = await api.get("/suggestions");
+  return res.data;
 };
 
 export const suggestionSendVote = async (payload: any) => {
   console.log(payload);
-  const res = await api.post("/api/suggestion/vote", payload);
+  const res = await api.post("/suggestions/vote", payload);
   return res.data;
 };
 
 export const suggestionSendComment = async (payload: any) => {
   console.log(payload);
 
-  const res = await api.post("/api/suggestion/comment", payload);
+  const res = await api.post("/suggestions/makeComment", payload);
   return res.data;
 };
 
 export const suggestionApproveOrReject = async (payload: any) => {
   console.log(payload);
-  const res = await api.post("/api/suggestion/approveOrReject", payload);
+  const res = await api.post("/suggestion/approveOrReject", payload);
   return res.data;
 };
 
 export const fetchSuggestionShowCase = async () => {
-  /*const res = await api.get("/api/suggestion/showcasae");
-  return res.data; */
-  return tempShowcase;
+  const res = await api.get("/suggestions/GetBest");
+  return res.data;
 };
 
 export const suggestionMake = async (payload: any) => {
