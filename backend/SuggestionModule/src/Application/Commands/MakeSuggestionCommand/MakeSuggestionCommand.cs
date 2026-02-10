2b429eb5e39df97d1c90e24cd02a19c7ac8d2d5d
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-02-10 14:15:06 +0300

    FEAT: Suggestion maker command implemented

---
+++ backend/SuggestionModule/src/Application/Commands/MakeSuggestionCommand/MakeSuggestionCommand.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using SuggestionModule.Application.DTOs;
+using SuggestionModule.Application.Interfaces;
+using SuggestionModule.Domain.Entities;
+
+namespace SuggestionModule.Application.Commands.MakeSuggestionCommand
+{
+
+
+        public record MakeSuggestionCommand(CreateSuggestionDto suggestion,int tenantId,int userId) : IRequest<Unit>;
+        public class MakeSuggestionCommandHandler : IRequestHandler<MakeSuggestionCommand, Unit>
+        {
+            private readonly ISuggestionRepository _repository;
+            public MakeSuggestionCommandHandler(ISuggestionRepository repository)
+            {
+                _repository = repository;
+            }
+
+            public async Task<Unit> Handle(MakeSuggestionCommand command, CancellationToken cancellationToken)
+            {
+            Suggestion suggestion = new Suggestion(
+                command.suggestion.Title,    
+                command.suggestion.Description,
+                command.userId,
+                command.tenantId
+            );
+                await _repository.MakeSuggestion(suggestion);
+                return Unit.Value;
+            }
+        }
+    
+
+}
