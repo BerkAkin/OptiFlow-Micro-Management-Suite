bda4c5250460e40b9b171161607066bbfc5930e0
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-02-10 14:17:46 +0300

    FEAT: Best suggestion getter query implemented

---
+++ backend/SuggestionModule/src/Application/Queries/GetBestSuggestionsQuery/GetBestSuggestionsQuery.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using SuggestionModule.Application.DTOs;
+using SuggestionModule.Application.Interfaces;
+
+namespace SuggestionModule.Application.Queries.GetBestSuggestionsQuery
+{
+    public record GetBestSuggestionsQuery(int tenantId) : IRequest<MostSuggestionsDto>;
+    public class GetBestSuggestionsQueryHandler : IRequestHandler<GetBestSuggestionsQuery, MostSuggestionsDto>
+    {
+        private readonly ISuggestionRepository _suggestionRepository;
+        public GetBestSuggestionsQueryHandler(ISuggestionRepository suggestionRepository) { 
+            _suggestionRepository = suggestionRepository;
+        }
+
+        public async Task<MostSuggestionsDto> Handle(GetBestSuggestionsQuery request, CancellationToken cancellationToken) {
+            var data = await _suggestionRepository.GetBestSuggestions(request.tenantId);
+            return data;
+        }
+    }
+}
