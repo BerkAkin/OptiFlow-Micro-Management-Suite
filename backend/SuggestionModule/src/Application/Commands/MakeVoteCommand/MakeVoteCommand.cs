501594b5c92540b4bc157196a8a3285f4bd14cb8
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-02-10 14:15:13 +0300

    FEAT: Suggestion voter command implemented

---
+++ backend/SuggestionModule/src/Application/Commands/MakeVoteCommand/MakeVoteCommand.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using SuggestionModule.Application.DTOs;
+using SuggestionModule.Application.Interfaces;
+using SuggestionModule.Domain.Entities;
+
+namespace SuggestionModule.Application.Commands.MakeVoteCommand
+{
+    public record MakeVoteCommand(CreateVoteDto vote,int userId) : IRequest<Unit>;
+
+    public class MakeVoteCommandHandler : IRequestHandler<MakeVoteCommand, Unit>
+    {
+        private readonly ISuggestionRepository _repository;
+        public MakeVoteCommandHandler(ISuggestionRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<Unit> Handle(MakeVoteCommand command, CancellationToken cancellationToken) {
+
+            Vote vote = new Vote(
+                command.userId,
+                command.vote.SuggestionId,
+                command.vote.VoteType
+            );
+            await _repository.MakeVote(vote);
+            return Unit.Value;
+        }
+    }
+}
