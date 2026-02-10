b7245c9b206c94bb19e76574b705baad68a5ec49
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-02-10 14:23:26 +0300

    FEAT: Satisfaction about survey command implemented

---
+++ backend/SurveyModule/src/Application/Commands/SatisfactionCommand/SatisfactionCommand.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using SurveyModule.Application.DTOs;
+using SurveyModule.Application.Interfaces.Repositories;
+using SurveyModule.Domain.Entities;
+
+namespace SurveyModule.Application.Commands.SatisfactionCommand
+{
+    public record SatisfactionCommand(SatisfactionRateDto satisfaction,int UserId) : IRequest<Unit>;
+    public class SatisfactionCommandHandler: IRequestHandler<SatisfactionCommand,Unit>
+    {
+        private readonly ISurveyRepository _repository;
+        public SatisfactionCommandHandler(ISurveyRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<Unit> Handle(SatisfactionCommand command, CancellationToken cancellationToken)
+        {
+            var satisfaction = new SatisfactionRate
+            {
+                UserId= command.UserId,
+                Satisfaction = command.satisfaction.Satisfaction,
+                SurveyId = command.satisfaction.SurveyId,
+            };
+
+
+            await _repository.AddSatisfaction(satisfaction);
+            return Unit.Value;
+        }
+    }
+}
