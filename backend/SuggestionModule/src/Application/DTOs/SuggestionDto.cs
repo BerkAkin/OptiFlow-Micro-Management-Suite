25752f33991f03a65126befbdffc115dad0e1363
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-02-10 14:17:09 +0300

    FEAT: DTO object for getting suggestion defined

---
+++ backend/SuggestionModule/src/Application/DTOs/SuggestionDto.cs
@@ -0,0 +1,15 @@
+﻿using SuggestionModule.Domain.Enums;
+
+namespace SuggestionModule.Application.DTOs
+{
+    public class SuggestionDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public SuggestionStatus Status { get; set; }
+        public int Votes { get; set; } 
+        public ICollection<CommentDto> Comments { get; set; } = new List<CommentDto>();
+    }
+}
