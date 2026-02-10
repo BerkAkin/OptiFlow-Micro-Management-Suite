5d415c8efc0cce361b88b64d7d26f0b25ec2c70f
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-02-10 14:30:23 +0300

    Migrations refined

---
+++ backend/SurveyModule/src/Migrations/20251227191542_surveySatisfaction.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SurveyModule.Migrations
+{
+    /// <inheritdoc />
+    public partial class surveySatisfaction : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_UserAnswers_UserId",
+                table: "UserAnswers");
+
+            migrationBuilder.CreateTable(
+                name: "SatisfactionRates",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SurveyId = table.Column<int>(type: "int", nullable: false),
+                    Satisfaction = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SatisfactionRates", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SatisfactionRates_Surveys_SurveyId",
+                        column: x => x.SurveyId,
+                        principalTable: "Surveys",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserAnswers_UserId_SurveyId_QuestionId",
+                table: "UserAnswers",
+                columns: new[] { "UserId", "SurveyId", "QuestionId" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SatisfactionRates_SurveyId",
+                table: "SatisfactionRates",
+                column: "SurveyId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SatisfactionRates");
+
+            migrationBuilder.DropIndex(
+                name: "IX_UserAnswers_UserId_SurveyId_QuestionId",
+                table: "UserAnswers");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserAnswers_UserId",
+                table: "UserAnswers",
+                column: "UserId");
+        }
+    }
+}
---
+++ backend/SurveyModule/src/Migrations/20251228134800_addedcheckofduplicate.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SurveyModule.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedcheckofduplicate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "SatisfactionRates",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SatisfactionRates_UserId_SurveyId",
+                table: "SatisfactionRates",
+                columns: new[] { "UserId", "SurveyId" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_SatisfactionRates_UserId_SurveyId",
+                table: "SatisfactionRates");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "SatisfactionRates");
+        }
+    }
+}
