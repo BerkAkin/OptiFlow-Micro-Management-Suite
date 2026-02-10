86c20ab8b94be39e443a25de369800c04c50aca3
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-02-10 14:23:44 +0300

    FEAT: Added satisfactioncounts data to survey dto

--- backend/SurveyModule/src/Application/DTOs/GetSurveyDto.cs
+++ backend/SurveyModule/src/Application/DTOs/GetSurveyDto.cs
@@ -1,4 +1,6 @@
-﻿namespace SurveyModule.Application.DTOs
+﻿using SurveyModule.Domain.Enums;
+
+namespace SurveyModule.Application.DTOs
 {
     public class GetSurveyDto
     {
@@ -6,5 +8,9 @@
         public int TenantId { get; set; }
         public DateTime Date { get; set; }
         public string Title { get; set; }
+        public SurveyStatus Status { get; set; }
+
+        public int SatisfactionCount { get; set; }
+        public int DissatisfactionCount { get; set; }
     }
 }
