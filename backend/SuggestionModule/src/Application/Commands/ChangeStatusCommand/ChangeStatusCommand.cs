499866ce7ab3ead3ab0e7cbd03d7dd3b55ccb8fb
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-02-10 14:14:46 +0300

    FEAT: Change status command implemented to change status of suggestion

---
+++ backend/SuggestionModule/src/Application/Commands/ChangeStatusCommand/ChangeStatusCommand.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using SuggestionModule.Application.DTOs;
+using SuggestionModule.Application.Interfaces;
+using SuggestionModule.Domain.Entities;
+
+namespace SuggestionModule.Application.Commands.ChangeStatusCommand
+{
+    public record ChangeStatusCommand(StatusDto status) : IRequest<Unit>;
+    public class ChangeStatusCommandHandler : IRequestHandler<ChangeStatusCommand, Unit>
+    {
+        private readonly ISuggestionRepository _repository;
+        public ChangeStatusCommandHandler(ISuggestionRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<Unit> Handle(ChangeStatusCommand command, CancellationToken cancellationToken)
+        {
+            var suggestion = await _repository.GetByIdAsync(command.status.SuggestionId);
+            suggestion.ChangeStatus(command.status.Status);
+            await _repository.SaveAsync();
+            return Unit.Value;
+        }
+    }
+
+}
