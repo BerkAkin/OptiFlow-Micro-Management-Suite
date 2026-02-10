8fd11b687e2d86eb101ea9cc4d01e4686de28941
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-02-10 14:21:46 +0300

    FEAT: Added cors options to survey module

--- backend/SurveyModule/src/Api/Program.cs
+++ backend/SurveyModule/src/Api/Program.cs
@@ -1,8 +1,10 @@
 using Microsoft.EntityFrameworkCore;
 using SurveyModule.Application.Interfaces.Repositories;
+using SurveyModule.Application.Middlewares;
 using SurveyModule.Application.Queries.GetSurveysQuery;
 using SurveyModule.Infrastructure.Persistance;
 using SurveyModule.Infrastructure.Repositories;
+using SurveyModule.Infrastructure.Security;
 using SurveyModule.Seeder;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,21 +12,51 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+
+
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins("http://localhost:5173").AllowAnyHeader().AllowAnyMethod().AllowCredentials();
+    });
+});
+
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddScoped<ICurrentUserService, CurrentUserService>();
+
 builder.Services.AddDbContext<SurveyDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("SurveyModuleDb")));
+
 builder.Services.AddTransient<DummyDataSeeder>();
+
 builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(GetSurveyQuery).Assembly));
+
 builder.Services.AddScoped<ISurveyRepository,SurveysRepository>();
+
+
+
+
+
 
 var app = builder.Build();
 
+app.UseCors();
+
+
+app.UseMiddleware<CookieMiddleware>();
 
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHttpsRedirection();
+}
 
 app.UseAuthorization();
 
