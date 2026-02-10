6e52361484d9968e8249b620438bb04eaf30b04e
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-02-10 14:18:42 +0300

    FEAT: Entity for users defined for temporarily

---
+++ backend/SuggestionModule/src/Domain/Entities/MiniUser.cs
@@ -0,0 +1,12 @@
+﻿namespace SuggestionModule.Domain.Entities
+{
+    public class MiniUser
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public ICollection<Vote> Votes { get; set; } = new List<Vote>();
+        public ICollection<Suggestion> Suggestions { get; set; } = new List<Suggestion>();
+        public ICollection<Comment> Comments { get; set; } = new List<Comment>();
+    }
+}
