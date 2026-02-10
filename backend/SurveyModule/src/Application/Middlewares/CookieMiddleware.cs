965b57d3f9cd43463932e0b799ef3f7831202784
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-02-10 14:25:41 +0300

    FEAT: Middleware added to project to get tenantId from cookies

---
+++ backend/SurveyModule/src/Application/Middlewares/CookieMiddleware.cs
@@ -0,0 +1,18 @@
+﻿namespace SurveyModule.Application.Middlewares
+{
+    public class CookieMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public CookieMiddleware(RequestDelegate next) {
+            _next = next;
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            if (context.Request.Cookies.TryGetValue("TenantId", out var cookieValue))
+            {
+                context.Items["TenantId"] = cookieValue;
+            }
+            await _next(context);
+        }
+    }
+}
