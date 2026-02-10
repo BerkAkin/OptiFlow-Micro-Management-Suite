009b92a1ef0da55115ba5e0bd2afa5fd03688ff1
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-02-10 14:14:56 +0300

    FEAT: Comment adder command implemented

---
+++ backend/SuggestionModule/src/Application/Commands/MakeCommentCommand/MakeCommentCommand.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using SuggestionModule.Application.DTOs;
+using SuggestionModule.Application.Interfaces;
+using SuggestionModule.Domain.Entities;
+
+namespace SuggestionModule.Application.Commands.MakeCommentCommand
+{
+    public record MakeCommentCommand(CreateCommentDto comment,int userId): IRequest<Unit>;
+    public class MakeCommentCommandHandler : IRequestHandler<MakeCommentCommand, Unit>
+    {
+        private readonly ISuggestionRepository _repository;
+        public MakeCommentCommandHandler(ISuggestionRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<Unit> Handle(MakeCommentCommand command, CancellationToken cancellationToken)
+        {
+            var suggestion = await _repository.GetByIdAsync(command.comment.SuggestionId);
+
+            suggestion.AddComment(command.userId, command.comment.Text);
+
+            await _repository.SaveAsync();
+            return Unit.Value;
+        }
+    }
+}
