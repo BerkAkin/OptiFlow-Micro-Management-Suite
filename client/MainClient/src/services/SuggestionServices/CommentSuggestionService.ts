009eb28b8c99addd2da7926d8b96ca56940764db
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-02-10 14:39:34 +0300

    REFACTOR: Service is now getting values direct from component instead of merging them in service

--- client/MainClient/src/services/SuggestionServices/CommentSuggestionService.ts
+++ client/MainClient/src/services/SuggestionServices/CommentSuggestionService.ts
@@ -1,7 +1,6 @@
 import { suggestionSendComment } from "../../repositories/SuggestionRepositories/SuggestionRepository";
 
 export const CommentSuggestionService = async (values: any) => {
-  const { id, text } = values;
-  const data = await suggestionSendComment({ id: id, text: text });
+  const data = await suggestionSendComment(values);
   return data;
 };
