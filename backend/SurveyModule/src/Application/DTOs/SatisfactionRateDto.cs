f8740604b2c9d4ff2bb193e710a6f6c201810019
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-02-10 14:24:14 +0300

    FEAT: Created satisfactionrate dto to set get satisfaction counts on a survey

---
+++ backend/SurveyModule/src/Application/DTOs/SatisfactionRateDto.cs
@@ -0,0 +1,10 @@
+﻿using SurveyModule.Domain.Enums;
+
+namespace SurveyModule.Application.DTOs
+{
+    public class SatisfactionRateDto
+    {
+        public int SurveyId { get; set; }
+        public SatisfactionStatus Satisfaction { get; set; }
+    }
+}
