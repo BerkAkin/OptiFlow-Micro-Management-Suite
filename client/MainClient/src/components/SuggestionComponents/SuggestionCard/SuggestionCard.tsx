a0ed50f97594915944d28286f7318f602d22f81e
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-02-10 14:33:04 +0300

    Minor changes and fixes

--- client/MainClient/src/components/SuggestionComponents/SuggestionCard/SuggestionCard.tsx
+++ client/MainClient/src/components/SuggestionComponents/SuggestionCard/SuggestionCard.tsx
@@ -8,29 +8,28 @@ import { useApprove, useComment, useVote } from '../../../hooks/SuggestionHooks/
 
 interface SuggestionCardProps {
     id: number,
-    status: string,
-    header: string,
+    status: number,
+    title: string,
     date: string,
     description: string,
-    voteCount: number,
-    commentCount: number,
+    votes: number,
     comments: {
-        commentText: string,
-        date: string,
+        id: number,
+        text: string,
     }[]
 }
 
-function SuggestionCard({ id, status, header, description, voteCount, commentCount, comments, date }: SuggestionCardProps) {
+function SuggestionCard({ id, status, title, description, votes, comments, date }: SuggestionCardProps) {
 
     const voteMutation = useVote();
     const commentMutation = useComment();
     const approveOrReject = useApprove();
 
     const handleVote = (vote: number) => {
-        voteMutation.mutate({ id, vote })
+        voteMutation.mutate({ suggestionId: id, voteType: vote })
     }
     const handleComment = (values: any) => {
-        commentMutation.mutate({ ...values, id })
+        commentMutation.mutate({ ...values, suggestionId: id })
     }
     const handleApproveOrReject = (decision: boolean) => {
         approveOrReject.mutate({ id, decision })
@@ -41,7 +40,7 @@ function SuggestionCard({ id, status, header, description, voteCount, commentCou
         <div className='border border-gray-200 rounded-lg '>
             <div className='text-center py-4 flex justify-center items-center  w-full'>
                 <p className='text-gray-900'>
-                    {header}
+                    {title}
                     <button type='button' onClick={() => handleApproveOrReject(false)} className='cursor-pointer ms-2 bg-red-500 rounded-full shadow-custom w-6 text-xs text-white'>✘</button>
                     <button type='button' onClick={() => handleApproveOrReject(true)} className='cursor-pointer bg-green-500 rounded-full shadow-custom w-6 text-xs text-white mx-1 '>✔</button>
                 </p>
@@ -53,10 +52,10 @@ function SuggestionCard({ id, status, header, description, voteCount, commentCou
                 <div className=' flex items-center justify-start'>
                     <p className='text-gray-800 grid grid-cols-3 flex items-center'>
 
-                        <button type='button' onClick={() => handleVote(0)} className='cursor-pointer'>
+                        <button type='button' onClick={() => handleVote(-1)} className='cursor-pointer'>
                             <img className='hover:bg-red-300 rounded-full' src={down} width={20} alt="" />
                         </button>
-                        {voteCount}
+                        {votes}
 
                         <button type='button' onClick={() => handleVote(1)} className=' cursor-pointer text-2xl '>
                             <img className='hover:bg-green-300 rounded-full' src={up} width={20} alt="" />
@@ -65,17 +64,28 @@ function SuggestionCard({ id, status, header, description, voteCount, commentCou
                     </p>
                 </div>
                 <div className=' flex justify-center items-center'>
-                    <p className={`${status === "Approved" ? 'text-green-600' : 'text-red-500'} `}>{status}</p>
+                    {
+                        status === 1 ?
+                            (
+                                <p className={`text-green-600`}>Approved</p>
+
+                            )
+                            :
+                            (
+                                <p className={`text-red-500`}>Rejected</p>
+
+                            )
+                    }
                 </div>
                 <div className=' flex items-center justify-end'>
                     <button className='cursor-pointer' onClick={() => setCommentSection(!commentSection)}>
                         <img src={list} width={20} alt="" />
                     </button>
-                    <p className='text-gray-800 mx-1'> {commentCount}</p>
+                    <p className='text-gray-800 mx-1'> { }</p>
                 </div>
             </div>
             <div className='flex items-center  py-1 justify-center'>
-                <p className='text-sm text-gray-400'>{date}</p>
+                <p className='text-sm text-gray-400'>{date.split("T")[0]}</p>
             </div>
             {commentSection &&
                 <div className='border-t border-gray-200 h-[200px] w-full overflow-y-auto'>
@@ -90,8 +100,8 @@ function SuggestionCard({ id, status, header, description, voteCount, commentCou
                     </Formik>
                     {comments.map((item, index) => (
                         <div key={index} className="border-b border-gray-200 last:border-none py-2 px-5 text-sm text-gray-600 text-center">
-                            {index + 1}{")"} {item.commentText}
-                            <span className="text-sm text-gray-400 ps-1">{item.date}</span>
+                            {index + 1}{")"} {item.text}
+                            <span className="text-sm text-gray-400 ps-1"></span>
                         </div>
                     ))}
 
