fe130f39b1fc152f47857ddd576cde35b1d6edbc
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-02-10 14:19:38 +0300

    Vote entity defined

---
+++ backend/SuggestionModule/src/Domain/Entities/Vote.cs
@@ -0,0 +1,27 @@
+﻿using SuggestionModule.Domain.Enums;
+
+namespace SuggestionModule.Domain.Entities
+{
+    public class Vote
+    {
+        protected Vote() { }
+        public Vote(int userId,int suggestionId,VoteType voteType) { 
+            
+            UserId = userId;
+            SuggestionId = suggestionId;
+            VoteType = voteType;
+        }
+
+        public int Id { get; private set; }
+        public int UserId { get; private set; }
+        public MiniUser User { get; private set; }
+        public int SuggestionId { get; private set; }
+        public Suggestion Suggestion { get; private set; }
+        public VoteType VoteType { get; private set; }
+
+        public void ChangeVote(VoteType voteType)
+        {
+            VoteType = voteType;
+        }
+    }
+}
