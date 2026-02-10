84337de080892884aba913c1fe5b934baf8a28be
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-02-10 14:16:11 +0300

    FEAT: Entity object for creation of suggestion defined

---
+++ backend/SuggestionModule/src/Application/DTOs/CreateSuggestionDto.cs
@@ -0,0 +1,8 @@
+﻿namespace SuggestionModule.Application.DTOs
+{
+    public class CreateSuggestionDto
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
