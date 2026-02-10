f0bb41ff264f932467c41ec354e177c0e3090984
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-02-10 14:18:12 +0300

    FEAT: Comment entity defined

---
+++ backend/SuggestionModule/src/Domain/Entities/Comment.cs
@@ -0,0 +1,17 @@
+﻿namespace SuggestionModule.Domain.Entities
+{
+    public class Comment
+    {
+        protected Comment() { }
+        public int Id { get; private set; }
+        public string Text { get; private set; }
+        public int UserId { get; private set; }
+        internal int SuggestionId { get; private set; }
+        internal Suggestion Suggestion { get; private set; }
+        public MiniUser User { get; private set; }
+        internal Comment(int userId, string text) {
+            UserId = userId;
+            Text = text;
+        }
+    }
+}
