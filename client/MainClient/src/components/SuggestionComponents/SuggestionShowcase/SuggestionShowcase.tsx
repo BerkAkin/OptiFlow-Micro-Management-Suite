21e34096c2342b6c8c970b937c53d5a13a64cf0d
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-02-10 14:33:58 +0300

    REFACTOR: Component is now showing loading and error states instead of plain texts

--- client/MainClient/src/components/SuggestionComponents/SuggestionShowcase/SuggestionShowcase.tsx
+++ client/MainClient/src/components/SuggestionComponents/SuggestionShowcase/SuggestionShowcase.tsx
@@ -1,20 +1,21 @@
 import { useShowcase } from '../../../hooks/SuggestionHooks/useSuggestion'
+import ErrorMessage from '../../ErrorMessage/ErrorMessage';
+import Spinner from '../../Spinner/Spinner';
 import SuggestionShowcaseCard from '../SuggestionShowcaseCard/SuggestionShowcaseCard'
 
 function SuggestionShowcase() {
 
     const { data, isLoading, error } = useShowcase();
-    if (isLoading) return (<p>Loading...</p>)
-    if (error || !data) return (<p>Error...</p>)
-    console.log(data);
+    if (isLoading) return (<Spinner />)
+    if (error || !data) return (<ErrorMessage />)
 
     return (
         <div className='grid grid-cols-10 gap-6 '>
             <div className='col-span-5 '>
-                <SuggestionShowcaseCard isStar={true} Description={data.allTimes} VoteCount={data.allTimesVotes} />
+                <SuggestionShowcaseCard isStar={true} Description={data.best.description} VoteCount={data.best.votes} />
             </div>
             <div className='col-span-5 '>
-                <SuggestionShowcaseCard isStar={false} Description={data.monthly} VoteCount={data.monthlyVotes} />
+                <SuggestionShowcaseCard isStar={false} Description={data.thisMonth.description} VoteCount={data.thisMonth.votes} />
             </div>
         </div>
     )
