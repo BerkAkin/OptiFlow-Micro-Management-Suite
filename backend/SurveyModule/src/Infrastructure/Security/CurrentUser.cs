d28452939e6133566315e8b73abebf541a5b5a59
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-02-10 14:29:43 +0300

    FEAT: Created currentuser entity

---
+++ backend/SurveyModule/src/Infrastructure/Security/CurrentUser.cs
@@ -0,0 +1,9 @@
+﻿namespace SurveyModule.Infrastructure.Security
+{
+    public class CurrentUser
+    {
+        public int UserId { get; set; }
+        public int TenantId { get; set; }
+        public string Email { get; set; }
+    }
+}
