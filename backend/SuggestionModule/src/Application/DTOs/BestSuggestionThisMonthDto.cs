3f04bda94605efe8d82196bad556053c1a41d712
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-02-10 14:15:35 +0300

    FEAT: Entity object for best suggestions of this month defined

---
+++ backend/SuggestionModule/src/Application/DTOs/BestSuggestionThisMonthDto.cs
@@ -0,0 +1,8 @@
+﻿namespace SuggestionModule.Application.DTOs
+{
+    public class BestSuggestionThisMonthDto
+    {
+        public string Description { get; set; }
+        public int Votes { get; set; }
+    }
+}
