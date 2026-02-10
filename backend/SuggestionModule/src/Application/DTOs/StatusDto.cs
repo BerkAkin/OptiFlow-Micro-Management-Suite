dec769c495303b9e0c7e28b4e5233a44c2ccc7bf
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-02-10 14:16:51 +0300

    FEAT: Entity object for changing status of suggestion defined

---
+++ backend/SuggestionModule/src/Application/DTOs/StatusDto.cs
@@ -0,0 +1,10 @@
+﻿using SuggestionModule.Domain.Enums;
+
+namespace SuggestionModule.Application.DTOs
+{
+    public class StatusDto
+    {
+        public SuggestionStatus Status { get; set; }
+        public int SuggestionId { get; set; }
+    }
+}
