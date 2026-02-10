85e1b856f60472b1bde756b6683fa55075c8aafc
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-02-10 14:34:55 +0300

    FEAT: Added accesstoken support and check to interceptor of api definition

--- client/MainClient/src/lib/api.ts
+++ client/MainClient/src/lib/api.ts
@@ -3,4 +3,19 @@ import axios from "axios";
 export const api = axios.create({
   baseURL: "/api",
   timeout: 10000,
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("AccessToken");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
