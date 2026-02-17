23ed4bb548710c81508e81b1bee9f14b82c645e8
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-02-17 15:11:46 +0300

    FEAT: support message list getter dto defined

---
+++ backend/SupportModule/src/Application/DTOs/SupportMessageDto.cs
@@ -0,0 +1,8 @@
+﻿namespace SupportModule.Application.DTOs
+{
+    public class SupportMessageDto
+    {
+        public string Message { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
