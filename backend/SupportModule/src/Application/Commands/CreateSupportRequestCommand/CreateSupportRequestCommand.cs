5d3077638315a9d61f4b5261de073fd62b793f2b
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-02-17 15:11:26 +0300

    FEAT: Support request creator command implemented

---
+++ backend/SupportModule/src/Application/Commands/CreateSupportRequestCommand/CreateSupportRequestCommand.cs
@@ -0,0 +1,41 @@
+﻿using MediatR;
+using SupportModule.Application.DTOs;
+using SupportModule.Application.Interfaces;
+using SupportModule.Domain.Entites;
+
+namespace SupportModule.Application.Commands.CreateSupportRequestCommand
+{
+    public record CreateSupportRequestCommand(SupportMessageDto message ,int userId, int tenantId) : IRequest<Unit>;
+
+    public class CreateSupportRequestCommandHandler : IRequestHandler<CreateSupportRequestCommand , Unit>
+    {
+        private readonly ISupportRepository _repository;
+        public CreateSupportRequestCommandHandler(ISupportRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<Unit> Handle(CreateSupportRequestCommand command, CancellationToken cancellationToken)
+        {
+
+            SupportRequest spRq = new SupportRequest()
+            {
+                CreatedAt= DateTime.UtcNow,
+                UserId= command.userId,
+                TenantId=command.tenantId,
+            };
+
+            SupportMessage spMsg = new SupportMessage() { 
+                CreatedAt= DateTime.UtcNow,
+                Message= command.message.Message,
+                SenderId= command.userId,
+                SupportRequest= spRq,
+            };
+
+            await _repository.CreateSupportRequest(spMsg,spRq);
+            return Unit.Value;
+        }
+
+        
+    }
+}
