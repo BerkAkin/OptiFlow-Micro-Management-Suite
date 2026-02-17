495adf05ed407ea238132228d256c4760782743c
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-02-17 15:12:39 +0300

    FEAT: Support message entity defined

---
+++ backend/SupportModule/src/Domain/Entites/SupportMessage.cs
@@ -0,0 +1,13 @@
+﻿namespace SupportModule.Domain.Entites
+{
+    public class SupportMessage
+    {
+        public int Id { get; set; }
+        public string Message { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public int SenderId { get; set; }
+        public int? ReceiverId { get; set; }
+        public int SupportRequestId { get; set; }
+        public SupportRequest SupportRequest { get; set; }
+    }
+}
