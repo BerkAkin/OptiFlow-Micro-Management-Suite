d1f0e677d6d6c522320a84efbec2f9c67cafb690
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-02-17 15:12:08 +0300

    FEAT: Sıpport repository abstraction interface defined

---
+++ backend/SupportModule/src/Application/Interfaces/ISupportRepository.cs
@@ -0,0 +1,14 @@
+﻿
+
+using SupportModule.Application.DTOs;
+using SupportModule.Domain.Entites;
+
+namespace SupportModule.Application.Interfaces
+{
+    public interface ISupportRepository
+    {
+        Task CreateSupportRequest(SupportMessage message, SupportRequest request);
+        Task<List<SupportRequestsDto>> GetSupportRequests(int TenantId);
+        Task<List<SupportMessageDto>> GetSupportMessages(int RequestId);
+    }
+}
