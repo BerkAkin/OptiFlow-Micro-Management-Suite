baa9958fe8953e5b988df5cd957e23ba71e3c769
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-02-17 15:12:52 +0300

    FEAT: Support db context implemented

---
+++ backend/SupportModule/src/Infrastructure/Persistence/SupportDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SupportModule.Domain.Entites;
+
+namespace SupportModule.Infrastructure.Persistence
+{
+    public class SupportDbContext : DbContext
+    {
+        public SupportDbContext(DbContextOptions<SupportDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<SupportRequest> SupportRequests { get; set; }
+        public DbSet<SupportMessage> SupportMessages { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<SupportMessage>()
+                .HasOne(sm => sm.SupportRequest)
+                .WithMany(sr => sr.SupportMessages)
+                .HasForeignKey(sm => sm.SupportRequestId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
