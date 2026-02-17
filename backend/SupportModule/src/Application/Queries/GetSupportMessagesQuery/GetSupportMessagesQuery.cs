7493ce78be6ab0129623551d94f37a4eb42a3486
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-02-17 15:12:20 +0300

    FEAT: Support messages query implemented

---
+++ backend/SupportModule/src/Application/Queries/GetSupportMessagesQuery/GetSupportMessagesQuery.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using SupportModule.Application.DTOs;
+using SupportModule.Application.Interfaces;
+
+namespace SupportModule.Application.Queries.GetSupportMessagesQuery
+{
+    public record GetSupportMessagesQuery(int RequestId):IRequest<List<SupportMessageDto>>;
+
+    public class GetSupportMessagesQueryHandler : IRequestHandler<GetSupportMessagesQuery,List<SupportMessageDto>>
+    {
+       
+        private readonly ISupportRepository _supportRepository;
+        public GetSupportMessagesQueryHandler(ISupportRepository supportRepository) {
+            _supportRepository = supportRepository;
+        }
+        
+        public async Task<List<SupportMessageDto>> Handle(GetSupportMessagesQuery query, CancellationToken cancellationToken)
+        {
+            return await _supportRepository.GetSupportMessages(query.RequestId);
+        }
+    }
+}
