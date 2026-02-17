a47061cc754a781816493553fd291af2dc6c2550
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-02-17 15:12:29 +0300

    FEAT: Support request lists query implemented

---
+++ backend/SupportModule/src/Application/Queries/GetSupportRequestsQuery/GetSupportRequestsQuery.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using SupportModule.Application.DTOs;
+using SupportModule.Application.Interfaces;
+
+namespace SupportModule.Application.Queries.GetSupportRequestsQuery
+{
+    public record GetSupportRequestsQuery(int TenantId):IRequest<List<SupportRequestsDto>>;
+    
+    public class GetSupportRequestsQueryHandler: IRequestHandler<GetSupportRequestsQuery , List<SupportRequestsDto>>
+    {
+        private readonly ISupportRepository _supportRepository;
+        public GetSupportRequestsQueryHandler(ISupportRepository supportRepository)
+        {
+            _supportRepository = supportRepository;
+        }
+
+        public async Task<List<SupportRequestsDto>> Handle(GetSupportRequestsQuery query, CancellationToken cancellationToken)
+        {
+            return await _supportRepository.GetSupportRequests(query.TenantId);
+        }
+
+      
+    }
+}
