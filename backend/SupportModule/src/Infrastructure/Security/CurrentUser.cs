89a96a25d2ff45d5f3b6ad21573006bda9e9f125
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-02-17 15:13:54 +0300

    FEAT: CurrentUser entity defined

---
+++ backend/SupportModule/src/Infrastructure/Security/CurrentUser.cs
@@ -0,0 +1,8 @@
+﻿namespace SuggestionModule.Infrastructure.Security
+{
+    public class CurrentUser
+    {
+        public int UserId { get; set; }
+        public int TenantId { get; set; }
+    }
+}
