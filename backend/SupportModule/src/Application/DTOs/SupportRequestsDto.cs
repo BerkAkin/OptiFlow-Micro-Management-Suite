85708d9ffea44cd152bfc19539c9dba3a3306641
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-02-17 15:11:51 +0300

    FEAT: support request list getter dto defined

---
+++ backend/SupportModule/src/Application/DTOs/SupportRequestsDto.cs
@@ -0,0 +1,11 @@
+﻿namespace SupportModule.Application.DTOs
+{
+    public class SupportRequestsDto
+    {
+        public int Id { get; set; }
+        public int RequestId { get; set; }
+        public string UserName { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public bool IsClosed { get; set; } = false;
+    }
+}
