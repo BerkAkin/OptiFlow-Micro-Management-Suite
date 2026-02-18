169ded22237e3b054ac1860d11f199e42dfd0a9f
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-02-18 20:20:40 +0300

    FEAT: SupportRequests Entity defined

---
+++ backend/SupportModule/src/Domain/Entities/SupportRequest.cs
@@ -0,0 +1,14 @@
+﻿namespace SupportModule.Domain.Entities
+{
+    public class SupportRequest
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public MiniUser User { get; set; }
+        public int TenantId { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public bool IsClosed { get; set; } = false;
+        public ICollection<SupportMessage> SupportMessages { get; set; } = new List<SupportMessage>();
+
+    }
+}
