3e2f0552e85356f46b7aea72b19b5f1353518057
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-02-18 20:17:01 +0300

    FEAT: Deletion of employee comment command implemented

---
+++ backend/SupportModule/src/Application/Commands/DeleteEmployeeCommentCommand/DeleteEmployeeCommentCommand.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using SupportModule.Application.Interfaces;
+
+namespace SupportModule.Application.Commands.DeleteEmployeeCommentCommand
+{
+    public record DeleteEmployeeCommentCommand(int CommentId):IRequest<Unit>;
+    public class DeleteEmployeeCommentCommandHandler : IRequestHandler<DeleteEmployeeCommentCommand, Unit>
+    {
+        private readonly ISupportRepository _supportRepository;
+        public DeleteEmployeeCommentCommandHandler(ISupportRepository supportRepository)
+        {
+            _supportRepository = supportRepository;
+        }
+
+        //Silme işlemi için de entity içinden silme yapılmalı bir de id yollamak yerine doğrudan entity bulunup silinmeli. Future Development
+        public async Task<Unit> Handle(DeleteEmployeeCommentCommand command,CancellationToken cancellationToken)
+        {
+            await _supportRepository.DeleteEmployeeComment(command.CommentId);
+            return Unit.Value;
+        }
+    }
+
+
+}
