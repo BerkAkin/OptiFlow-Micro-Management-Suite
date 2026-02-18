d04b335bf5e7a5858440b5e8791c099b0afbf191
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-02-18 20:22:00 +0300

    FEAT: Added repository commands for requests, comments etc...

--- backend/SupportModule/src/Infrastructure/Repositories/SupportRepository.cs
+++ backend/SupportModule/src/Infrastructure/Repositories/SupportRepository.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SupportModule.Application.DTOs;
 using SupportModule.Application.Interfaces;
-using SupportModule.Domain.Entites;
+using SupportModule.Domain.Entities;
 using SupportModule.Infrastructure.Persistence;
 
 namespace SupportModule.Infrastructure.Repositories
@@ -23,13 +23,13 @@ namespace SupportModule.Infrastructure.Repositories
         public async Task<List<SupportRequestsDto>> GetSupportRequests(int TenantId){
             var data = await _supportDbContext.SupportRequests
                 .AsNoTracking()
-                .Where(sr => sr.TenantId == TenantId)
+                .Where(sr => sr.TenantId == TenantId).Include(sr=>sr.User)
                 .Select(sr=>new SupportRequestsDto
                 {
                     CreatedAt= sr.CreatedAt,
-                    RequestId= sr.Id,
+                    Id= sr.Id,
                     IsClosed=sr.IsClosed,
-                    UserName= sr.UserId.ToString(),
+                    UserName= sr.User.Username,
                 })
                 .OrderByDescending(sr=>sr.CreatedAt)
                 .ToListAsync();
@@ -51,6 +51,91 @@ namespace SupportModule.Infrastructure.Repositories
             return data;
         }
 
-       
+        public async Task<List<SupportRequestsDto>> GetMyRequest(int UserId)
+        {
+            var data = await _supportDbContext.SupportRequests
+                .AsNoTracking()
+                .Where(sr => sr.UserId == UserId)
+                .Select(sr => new SupportRequestsDto
+                {
+                    Id= sr.Id,
+                    IsClosed = sr.IsClosed,
+                    CreatedAt = sr.CreatedAt,
+                    UserName= sr.User.Username,
+                })
+                .OrderByDescending(sr=>sr.CreatedAt)
+                .ToListAsync();
+            return data;
+        }
+
+        public async Task<List<MonthltRequestsCountDto>> GetMonthlyRequestCountsQuery(int tenantId)
+        {
+            var year = DateTime.UtcNow.Year;
+
+            var data = await _supportDbContext.SupportRequests
+                .AsNoTracking()
+                .Where(sr => sr.CreatedAt.Year == year)
+                .GroupBy(sr => sr.CreatedAt.Month)
+                .Select(g => new MonthltRequestsCountDto
+                {
+                    Month = g.Key,
+                    Count = g.Count()
+                })
+                .OrderBy(x => x.Month)
+                .ToListAsync();
+
+            return data;
+        }
+
+        public async Task<List<EmployeeDto>> GetEmployeeListQuery(int tenantId)
+        {
+            var data = await _supportDbContext.Users
+                .AsNoTracking()
+                .Where(u => u.TenantId == tenantId)
+                .Select(u => new EmployeeDto
+                {
+                      UserId= u.Id,
+                      Username= u.Username
+                })
+                .ToListAsync();
+            return data;
+                
+        }
+
+
+        public async Task CreateEmployeeComment(UserComment comment)
+        {
+            await _supportDbContext.UserComments.AddAsync(comment);
+            await _supportDbContext.SaveChangesAsync();
+        }
+
+        public async Task<List<UserCommentDto>> GetEmployeeComments(int UserId)
+        {
+            var data = await _supportDbContext.UserComments
+                .AsNoTracking()
+                .Where(u => u.UserId == UserId)
+                .Select(uc=>new UserCommentDto
+                {
+                    UserId= uc.UserId,
+                    Comment= uc.Comment
+                })
+                .ToListAsync();
+            return data;    
+        }
+
+        
+        public async Task DeleteEmployeeComment(int CommentId)
+        {
+            var data = await _supportDbContext.UserComments.FindAsync(CommentId);
+            if (data is null)
+                throw new Exception("Yorum bulunamadı");
+
+            _supportDbContext.UserComments.Remove(data);
+            await _supportDbContext.SaveChangesAsync();
+
+
+
+        }
+
     }
 }
