ca57f38a697ec656a82eee23801acaaed8566502
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-02-18 20:18:55 +0300

    FEAT: Employee ceomment getter mediator query defined

---
+++ backend/SupportModule/src/Application/Queries/GetEmployeeCommentsQuery/GetEmployeeCommentsQuery.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using SupportModule.Application.DTOs;
+using SupportModule.Application.Interfaces;
+
+namespace SupportModule.Application.Queries.GetEmployeeCommentsQuery
+{
+    public record GetEmployeeCommentsQuery(int UserId): IRequest<List<UserCommentDto>>{}
+    public class GetEmploteeCommentsQueryHandler: IRequestHandler<GetEmployeeCommentsQuery,List<UserCommentDto>>{
+        private readonly ISupportRepository _supportRepository;
+        public GetEmploteeCommentsQueryHandler(ISupportRepository supportRepository)
+        {
+            _supportRepository = supportRepository;
+        }
+        public async Task<List<UserCommentDto>> Handle(GetEmployeeCommentsQuery query,CancellationToken cancellationToken)
+        {
+            var data = await _supportRepository.GetEmployeeComments(query.UserId);
+            return data;
+        }
+    }
+}
