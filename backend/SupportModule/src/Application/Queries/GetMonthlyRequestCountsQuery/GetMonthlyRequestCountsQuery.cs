507a303bb72114ec29560b24e989d52ee1be4568
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-02-18 20:19:11 +0300

    FEAT: Monthly requests getter mediator query defined

---
+++ backend/SupportModule/src/Application/Queries/GetMonthlyRequestCountsQuery/GetMonthlyRequestCountsQuery.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using SupportModule.Application.DTOs;
+using SupportModule.Application.Interfaces;
+
+namespace SupportModule.Application.Queries.GetMonthlyRequestCountsQuery
+{
+    public record GetMonthlyRequestCountsQuery(int tenantId):IRequest<List<MonthltRequestsCountDto>>;
+    
+    public class GetMonthlyRequestCountsQueryHandler: IRequestHandler<GetMonthlyRequestCountsQuery, List<MonthltRequestsCountDto>>
+    {
+        private readonly ISupportRepository _supportRepository;
+        public GetMonthlyRequestCountsQueryHandler(ISupportRepository supportRepository)
+        {
+            _supportRepository = supportRepository;
+        }
+
+        public async Task<List<MonthltRequestsCountDto>> Handle(GetMonthlyRequestCountsQuery query, CancellationToken token)
+        {
+            var data = await _supportRepository.GetMonthlyRequestCountsQuery(query.tenantId);
+            return data;
+        }
+    }
+}
