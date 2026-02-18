247437d85e03b0cf6ad2aebebbfd71bd4cb527da
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-02-18 20:17:47 +0300

    REFACTOR: Deleted unnecessart dto fields

--- backend/SupportModule/src/Application/DTOs/SupportRequestsDto.cs
+++ backend/SupportModule/src/Application/DTOs/SupportRequestsDto.cs
@@ -3,9 +3,9 @@
     public class SupportRequestsDto
     {
         public int Id { get; set; }
-        public int RequestId { get; set; }
         public string UserName { get; set; }
         public DateTime CreatedAt { get; set; }
-        public bool IsClosed { get; set; } = false;
+        public bool IsClosed { get; set; }
+
     }
 }
