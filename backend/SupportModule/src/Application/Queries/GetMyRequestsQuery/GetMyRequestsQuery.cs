ec3b7251f087799195fbacf0a04a078ddc183ba8
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-02-18 20:19:41 +0300

    FEAT: Own requests getter mediator query defined

---
+++ backend/SupportModule/src/Application/Queries/GetMyRequestsQuery/GetMyRequestsQuery.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using SupportModule.Application.DTOs;
+using SupportModule.Application.Interfaces;
+
+namespace SupportModule.Application.Queries.GetMyRequestsQuery
+{
+    public record GetMyRequestsQuery(int UserId) : IRequest<List<SupportRequestsDto>>;
+    public class GetMyRequestsQueryHandler : IRequestHandler<GetMyRequestsQuery,List<SupportRequestsDto>>
+    {
+        private readonly ISupportRepository _supportRepository;
+        public GetMyRequestsQueryHandler(ISupportRepository supportRepository)
+        {
+            _supportRepository = supportRepository;
+        }
+
+        public async Task<List<SupportRequestsDto>> Handle(GetMyRequestsQuery query,CancellationToken cancellationToken) 
+        {
+            var data = await  _supportRepository.GetMyRequest(query.UserId);
+            return data;
+        }
+    }
+}
