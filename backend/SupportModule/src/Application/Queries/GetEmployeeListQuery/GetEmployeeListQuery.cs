f808244a91d9f5d3b8c27a8bccf6440f950391f1
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-02-18 20:19:01 +0300

    FEAT: Employee list getter mediator query defined

---
+++ backend/SupportModule/src/Application/Queries/GetEmployeeListQuery/GetEmployeeListQuery.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using SupportModule.Application.DTOs;
+using SupportModule.Application.Interfaces;
+
+namespace SupportModule.Application.Queries.GetEmployeeListQuery
+{
+    public record GetEmployeeListQuery(int tenantId):IRequest<List<EmployeeDto>>;
+        public class GetEmployeeListQueryHandler : IRequestHandler<GetEmployeeListQuery, List<EmployeeDto>>
+    {
+        private readonly ISupportRepository _supportRepository;
+        public GetEmployeeListQueryHandler(ISupportRepository supportRepository)
+        {
+            _supportRepository = supportRepository;
+        }
+        public async Task<List<EmployeeDto>> Handle(GetEmployeeListQuery query,CancellationToken cancellationToken)
+        {
+            var data = await _supportRepository.GetEmployeeListQuery(query.tenantId);
+            return data;
+        }
+    }
+}
