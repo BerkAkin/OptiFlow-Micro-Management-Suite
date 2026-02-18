89d12c9313ffcad5b2620b70ae2bffb640bbda58
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-02-18 20:11:45 +0300

    FEAT: Creator of employee comment mediator command implemented

---
+++ backend/SupportModule/src/Application/Commands/CreateEmployeeCommentCommand/CreateEmployeeCommentCommand.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using SupportModule.Application.DTOs;
+using SupportModule.Application.Interfaces;
+using SupportModule.Domain.Entities;
+
+namespace SupportModule.Application.Commands.CreateEmployeeCommentCommand
+{
+    public record CreateEmployeeCommentCommand(UserCommentDto comment): IRequest<Unit>;
+
+    public class CreateEmployeeCommentCommandHandler: IRequestHandler<CreateEmployeeCommentCommand,Unit> {
+        private readonly ISupportRepository _supportRepository;
+        public CreateEmployeeCommentCommandHandler(ISupportRepository supportRepository)
+        {
+            _supportRepository = supportRepository;
+        }
+        //employee listin alındığı zamanda employee entity içindeki comments bağlantısı içinde değişiklik yapılarak optimize edilmeli entityler bucket of proptan çıkartılmalı. Future optimization
+        public async Task<Unit> Handle(CreateEmployeeCommentCommand command, CancellationToken cancellationToken) {
+
+            UserComment userComment = new UserComment()
+            {
+                Comment= command.comment.Comment,
+                UserId= command.comment.UserId
+            };
+
+            await _supportRepository.CreateEmployeeComment(userComment);
+            return Unit.Value;
+        }
+    }
+}
