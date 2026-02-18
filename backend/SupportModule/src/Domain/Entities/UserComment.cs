f02f98a9e955d790923b10bf35905d69ec588289
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-02-18 20:20:46 +0300

    FEAT: UserComment Entity defined

---
+++ backend/SupportModule/src/Domain/Entities/UserComment.cs
@@ -0,0 +1,11 @@
+﻿namespace SupportModule.Domain.Entities
+{
+    public class UserComment
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public MiniUser User { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string Comment { get; set; }
+    }
+}
