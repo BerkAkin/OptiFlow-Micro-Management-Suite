034aa290797286a0a7c76936db9724838e26bd2f
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-02-18 20:20:17 +0300

    FEAT: Miniuser entity defined as temporarily until rabbitmq gets alive on project

---
+++ backend/SupportModule/src/Domain/Entities/MiniUser.cs
@@ -0,0 +1,12 @@
+﻿namespace SupportModule.Domain.Entities
+{
+    public class MiniUser
+    {
+        public int Id { get; set; }
+        public int TenantId { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public List<SupportRequest> SupportRequest { get; set; }
+        public List<UserComment> UserComments { get; set; }
+    }
+}
