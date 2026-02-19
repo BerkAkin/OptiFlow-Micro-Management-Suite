8b7988ae60f0711719f7587bbca54edfd408ea6c
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-02-19 23:36:34 +0300

    FEAT: UserId getter for signalR Websocket implemented

---
+++ backend/SupportModule/src/Infrastructure/Hubs/HeaderUserIdProvider.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SupportModule.Infrastructure.Hubs
+{
+    public class HeaderUserIdProvider : IUserIdProvider
+    {
+        public string? GetUserId(HubConnectionContext connection)
+        {
+                var context = connection.GetHttpContext();
+                var userId= context.Request.Headers["X-User-Id"].ToString();
+                return userId;
+        }
+    }
+}
