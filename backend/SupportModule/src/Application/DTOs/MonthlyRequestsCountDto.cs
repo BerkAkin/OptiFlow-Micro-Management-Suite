eeccf52a58779defedb002a8cfc732f594324d3e
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-02-19 23:34:57 +0300

    FIX: Typo

---
+++ backend/SupportModule/src/Application/DTOs/MonthlyRequestsCountDto.cs
@@ -0,0 +1,8 @@
+﻿namespace SupportModule.Application.DTOs
+{
+    public class MonthlyRequestsCountDto
+    {
+        public int Month { get; set; }
+        public int Count { get; set; }
+    }
+}
