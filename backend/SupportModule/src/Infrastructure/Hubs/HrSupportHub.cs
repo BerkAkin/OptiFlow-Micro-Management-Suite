d925b7b45bbfaf439b04f7d96e7426177ecc9477
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-02-19 23:36:48 +0300

    FEAT: Hub for signalR created

---
+++ backend/SupportModule/src/Infrastructure/Hubs/HrSupportHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SupportModule.Infrastructure.Hubs
+{
+    public class HrSupportHub : Hub
+    {
+        public async Task SendToUser(string receiverUserId, string message)
+        {
+                var senderId = Context.UserIdentifier ?? "BilinmeyenGonderici";            
+                await Clients.User(receiverUserId).SendAsync("ReceiveMessage", senderId, message);
+        }
+    }
+}
