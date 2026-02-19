e78eb447123cb079cb5690ea4d65bf99bf2555c7
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-02-19 23:35:11 +0300

    FEAT: Added category info

--- backend/SupportModule/src/Application/DTOs/SupportRequestsDto.cs
+++ backend/SupportModule/src/Application/DTOs/SupportRequestsDto.cs
@@ -1,9 +1,12 @@
-﻿namespace SupportModule.Application.DTOs
+﻿using SupportModule.Domain.Enums;
+
+namespace SupportModule.Application.DTOs
 {
     public class SupportRequestsDto
     {
         public int Id { get; set; }
         public string UserName { get; set; }
+        public SupportCategories Category { get; set; }
         public DateTime CreatedAt { get; set; }
         public bool IsClosed { get; set; }
 
