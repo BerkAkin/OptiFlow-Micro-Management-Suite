efa298638a60678a275b63d57f5f8eb9fd195a78
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-02-20 01:03:07 +0300

    FEAT: Created middleware for setting data from jwt to headers globally. Made this change for both websocket and traditional http requests

---
+++ backend/Gateway/src/Middlewares/WebSocketAuthMiddleware.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json.Linq;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace Gateway.Middlewares
+{
+    public class WebSocketAuthMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public WebSocketAuthMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+
+            string token = null;
+            if (context.Request.Headers.TryGetValue("Authorization", out var authHeader))
+            {
+                token = authHeader.ToString().Replace("Bearer ", "");
+            }
+            else if (context.Request.Query.TryGetValue("access_token", out var queryToken))
+            {
+                token = queryToken;
+            }
+
+            if (!string.IsNullOrEmpty(token))
+            {
+                var handler = new JwtSecurityTokenHandler();
+                if (handler.CanReadToken(token))
+                {
+                    var jwtToken = handler.ReadJwtToken(token);
+
+                    var userId = jwtToken.Claims.FirstOrDefault(c => c.Type == "userId" || c.Type == JwtRegisteredClaimNames.Sub )?.Value;
+                    var tenantId = jwtToken.Claims.FirstOrDefault(c => c.Type == "tenantId")?.Value;
+
+                    if (!string.IsNullOrEmpty(userId))
+                        context.Request.Headers["X-User-Id"] = userId;
+
+                    if (!string.IsNullOrEmpty(tenantId))
+                        context.Request.Headers["X-Tenant-Id"] = tenantId;
+                }
+            }
+
+            await _next(context);
+        }
+    }
+}
