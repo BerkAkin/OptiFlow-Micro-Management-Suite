ebfa8e0541417b49f5b119adaabc558697d484fe
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-02-19 23:35:09 +0300

    FEAT: Added category info

--- backend/SupportModule/src/Application/DTOs/SupportMessageDto.cs
+++ backend/SupportModule/src/Application/DTOs/SupportMessageDto.cs
@@ -1,8 +1,12 @@
-﻿namespace SupportModule.Application.DTOs
+﻿
+using SupportModule.Domain.Enums;
+
+namespace SupportModule.Application.DTOs
 {
     public class SupportMessageDto
     {
         public string Message { get; set; }
+        public SupportCategories Category { get; set; }
         public DateTime CreatedAt { get; set; }
     }
 }
