b97ce02b9139f42d3b0909172d0da9464ff4e612
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-02-19 23:36:03 +0300

    FEAT: Enum of categories defined for showing with request

---
+++ backend/SupportModule/src/Domain/Enums/SupportCategories.cs
@@ -0,0 +1,12 @@
+﻿namespace SupportModule.Domain.Enums
+{
+    public enum SupportCategories
+    {
+        Environment = 0,
+        Colleagues = 1,
+        Workload = 3,
+        Mood = 4,
+        System = 5,
+        General=6,
+    }
+}
