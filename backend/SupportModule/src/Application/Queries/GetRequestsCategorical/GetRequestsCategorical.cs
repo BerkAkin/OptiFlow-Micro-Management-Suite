344481d238388ba6e601860ab4a36ded806100c8
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-02-20 21:23:01 +0300

    FEAT: Added categorical data retriever query and its handler

---
+++ backend/SupportModule/src/Application/Queries/GetRequestsCategorical/GetRequestsCategorical.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using SupportModule.Application.DTOs;
+using SupportModule.Application.Interfaces;
+
+namespace SupportModule.Application.Queries.GetRequestsCategorical
+{
+    public record GetRequestsCategoricalQuery(int TenantId): IRequest<List<CategoricalRequestDto>>;
+    public class GetRequestsCategoricalHandler : IRequestHandler<GetRequestsCategoricalQuery, List<CategoricalRequestDto>>
+    {
+        private readonly ISupportRepository _supportRepository;
+        public GetRequestsCategoricalHandler(ISupportRepository supportRepository) { 
+
+            _supportRepository = supportRepository;
+        }
+
+        public async Task<List<CategoricalRequestDto>> Handle(GetRequestsCategoricalQuery request, CancellationToken cancellationToken)
+        {
+            var data = await _supportRepository.GetRequestsCategorical(request.TenantId);
+            return data;
+        }
+    }
+}
