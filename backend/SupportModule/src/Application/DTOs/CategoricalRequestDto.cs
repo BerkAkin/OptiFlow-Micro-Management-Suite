9948f79a53951c45835c626af9f1e9dea8cb5da5
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-02-20 21:22:23 +0300

    FEAT: Dto for categorical data retrieve operation defined

---
+++ backend/SupportModule/src/Application/DTOs/CategoricalRequestDto.cs
@@ -0,0 +1,11 @@
+﻿using SupportModule.Domain.Enums;
+
+namespace SupportModule.Application.DTOs
+{
+    public class CategoricalRequestDto
+    {
+        public int Count { get; set; }
+        public SupportCategories Category { get; set; }
+        public string CategoryName => Category.ToString();
+    }
+}
