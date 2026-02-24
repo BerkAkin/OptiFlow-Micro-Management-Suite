0816ae274395e09bc0d10fdad701c5107c0883b1
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-02-24 22:37:20 +0300

    REFACTOR: Command doesn't use repository anymore. Accessing directly to dbcontext to prevent using repository which is the same layer as dbcontext

---
+++ backend/SupportModule/src/Application/Commands/SendMessageCommand/SendMessageCommand.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using SupportModule.Application.DTOs;
+using SupportModule.Domain.Entities;
+using SupportModule.Infrastructure.Persistence;
+
+namespace SupportModule.Application.Commands.SendMessageCommand
+{
+    public record SendMessageCommand(SendMessageDto message, int UserId) : IRequest<Unit>;
+    public class SendMessageCommandHandler : IRequestHandler<SendMessageCommand,Unit>
+    {
+        private readonly SupportDbContext _dbContext;
+        public SendMessageCommandHandler(SupportDbContext dbContext) {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Unit> Handle(SendMessageCommand command, CancellationToken cancellationToken) {
+
+            SupportRequest request = await _dbContext.SupportRequests
+                .FirstOrDefaultAsync(sr => sr.Id == command.message.RequestId, cancellationToken);
+
+            if (request == null)
+                throw new Exception("Request not found");
+
+            request.AddMessage(command.message.Message, command.UserId);
+
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        
+        }
+    }
+}
