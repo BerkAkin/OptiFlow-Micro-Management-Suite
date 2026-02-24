fc746c4c64d40c3b552b3727c993cf7110fffe3a
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-02-24 22:37:42 +0300

    REFACTOR: Command doesn't use repository anymore. Accessing directly to dbcontext to prevent using repository which is the same layer as dbcontext

--- backend/SupportModule/src/Application/Queries/GetMyRequestsQuery/GetMyRequestsQuery.cs
+++ backend/SupportModule/src/Application/Queries/GetMyRequestsQuery/GetMyRequestsQuery.cs
@@ -1,22 +1,34 @@
 ﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
 using SupportModule.Application.DTOs;
-using SupportModule.Application.Interfaces;
+using SupportModule.Infrastructure.Persistence;
 
 namespace SupportModule.Application.Queries.GetMyRequestsQuery
 {
     public record GetMyRequestsQuery(int UserId) : IRequest<List<SupportRequestsDto>>;
     public class GetMyRequestsQueryHandler : IRequestHandler<GetMyRequestsQuery,List<SupportRequestsDto>>
     {
-        private readonly ISupportRepository _supportRepository;
-        public GetMyRequestsQueryHandler(ISupportRepository supportRepository)
+        private readonly SupportDbContext _dbContext;
+        public GetMyRequestsQueryHandler(SupportDbContext dbContext)
         {
-            _supportRepository = supportRepository;
+            _dbContext = dbContext;
         }
 
         public async Task<List<SupportRequestsDto>> Handle(GetMyRequestsQuery query,CancellationToken cancellationToken) 
         {
-            var data = await  _supportRepository.GetMyRequest(query.UserId);
-            return data;
+            return await _dbContext.SupportRequests
+                .AsNoTracking()
+                .Where(sr => sr.UserId == query.UserId)
+                .OrderByDescending(sr => sr.CreatedAt)
+                .Select(sr => new SupportRequestsDto
+                {
+                    Id = sr.Id,
+                    IsClosed = sr.IsClosed,
+                    CreatedAt = sr.CreatedAt,
+                    UserName = sr.User.Username,
+                    Category = sr.Category,
+                })
+                .ToListAsync(cancellationToken);
         }
     }
 }
