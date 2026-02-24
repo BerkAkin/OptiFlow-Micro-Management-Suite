a47b3ab45d10ae00918ca8e6766533a29e9da29e
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-02-24 22:37:36 +0300

    REFACTOR: Query doesn't use repository anymore. Accessing directly to dbcontext to prevent using repository which is the same layer as dbcontext

--- backend/SupportModule/src/Application/Queries/GetEmployeeCommentsQuery/GetEmployeeCommentsQuery.cs
+++ backend/SupportModule/src/Application/Queries/GetEmployeeCommentsQuery/GetEmployeeCommentsQuery.cs
@@ -1,20 +1,29 @@
 ﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
 using SupportModule.Application.DTOs;
-using SupportModule.Application.Interfaces;
+using SupportModule.Infrastructure.Persistence;
 
 namespace SupportModule.Application.Queries.GetEmployeeCommentsQuery
 {
     public record GetEmployeeCommentsQuery(int UserId): IRequest<List<UserCommentDto>>{}
     public class GetEmploteeCommentsQueryHandler: IRequestHandler<GetEmployeeCommentsQuery,List<UserCommentDto>>{
-        private readonly ISupportRepository _supportRepository;
-        public GetEmploteeCommentsQueryHandler(ISupportRepository supportRepository)
+
+        private readonly SupportDbContext _dbContext;
+        public GetEmploteeCommentsQueryHandler(SupportDbContext dbContext)
         {
-            _supportRepository = supportRepository;
+            _dbContext = dbContext;
         }
         public async Task<List<UserCommentDto>> Handle(GetEmployeeCommentsQuery query,CancellationToken cancellationToken)
         {
-            var data = await _supportRepository.GetEmployeeComments(query.UserId);
-            return data;
+            return await _dbContext.UserComments
+              .AsNoTracking()
+              .Where(u => u.UserId == query.UserId)
+              .Select(uc => new UserCommentDto
+              {
+                  UserId = uc.UserId,
+                  Comment = uc.Comment
+              })
+              .ToListAsync(cancellationToken);
         }
     }
 }
