50d877649f1adc29dc2d9f802ec621633f6c1457
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-02-24 22:39:05 +0300

    FEAT: Defined to use it while sending messages

---
+++ backend/SupportModule/src/Application/DTOs/SendMessageDto.cs
@@ -0,0 +1,8 @@
+﻿namespace SupportModule.Application.DTOs
+{
+    public class SendMessageDto
+    {
+        public string Message { get; set; }
+        public int RequestId  { get; set; }
+    }
+}
