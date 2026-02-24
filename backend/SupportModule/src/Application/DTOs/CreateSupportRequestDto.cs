bcba3ad16fae3458406dd4bb640e08fb77861894
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-02-24 22:38:35 +0300

    FEAT: Defined createsupportrequestdto to use it when creating new support requests

---
+++ backend/SupportModule/src/Application/DTOs/CreateSupportRequestDto.cs
@@ -0,0 +1,10 @@
+﻿using SupportModule.Domain.Enums;
+
+namespace SupportModule.Application.DTOs
+{
+    public class CreateSupportRequestDto
+    {
+        public string Content { get; set; }
+        public SupportCategories Category { get; set; }
+    }
+}
