af8d5481c5723b1756361bef178bd195b75d9851
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-02-24 22:42:30 +0300

    FEAT: Defined data seeder to seed data on project deployment

---
+++ backend/SupportModule/src/Infrastructure/Seeders/DataSeeder.cs
@@ -0,0 +1,31 @@
+﻿using SupportModule.Domain.Entities;
+using SupportModule.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using SupportModule.Domain.Enums;
+
+namespace SupportModule.Infrastructure.Seeders
+{
+    public static class DataSeeder
+    {
+        public static async Task SeedAsync(SupportDbContext context)
+        {
+            if (!await context.Users.AnyAsync())
+            {
+                MiniUser user1 = new MiniUser("berk", "berk@mail.com", tenantId: 1);
+                await context.Users.AddAsync(user1);
+                await context.SaveChangesAsync();
+
+                SupportRequest request1 = new SupportRequest(user1.Id, tenantId: 1,SupportCategories.General);
+                await context.SupportRequests.AddAsync(request1);
+                await context.SaveChangesAsync();
+
+                UserComment comment1 = new UserComment("Hello from berk", user1.Id);
+                await context.UserComments.AddAsync(comment1);
+                await context.SaveChangesAsync();
+
+                request1.AddMessage("İstek 1 için yeni mesaj", user1.Id);
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+}
