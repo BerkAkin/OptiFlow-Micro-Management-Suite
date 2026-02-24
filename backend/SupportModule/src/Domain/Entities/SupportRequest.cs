2a0c958cacc9d1d371fdd34bfd96d894bdc2b828
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-02-24 22:41:51 +0300

    REFACTOR: Updated entity from anemic entity model to ddd entity model. Added constructor. Added message adder function, close and open request functions.

--- backend/SupportModule/src/Domain/Entities/SupportRequest.cs
+++ backend/SupportModule/src/Domain/Entities/SupportRequest.cs
@@ -4,14 +4,49 @@ namespace SupportModule.Domain.Entities
 {
     public class SupportRequest
     {
-        public int Id { get; set; }
-        public int UserId { get; set; }
+        public int Id { get; private set; }
+        public int UserId { get; private set; }
         public MiniUser User { get; set; }
-        public int TenantId { get; set; }
-        public SupportCategories Category { get; set; }
-        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-        public bool IsClosed { get; set; } = false;
-        public ICollection<SupportMessage> SupportMessages { get; set; } = new List<SupportMessage>();
+        public int TenantId { get; private set; }
+        public SupportCategories Category { get; private set; }
+        public DateTime CreatedAt { get; private set; } = DateTime.UtcNow;
+        public bool IsClosed { get; private set; } = false;
+
+        private readonly List<SupportMessage> _messages = new();
+        public IReadOnlyCollection<SupportMessage> Messages => _messages;
+        private SupportRequest() { }
+        public SupportRequest(int userId, int tenantId, SupportCategories category)
+        {
+            UserId = userId;
+            TenantId = tenantId;
+            Category = category;
+            CreatedAt = DateTime.UtcNow;
+            IsClosed = false;
+        }
+        public void AddMessage(string message, int senderId)
+        {
+           if (IsClosed)
+              throw new Exception("Cannot add message to closed request");
+           if (string.IsNullOrWhiteSpace(message))
+              throw new Exception("Message cannot be empty");
+
+            _messages.Add(new SupportMessage(message,senderId));
+
+        }
+
+        public void CloseRequest()
+        {
+            if (IsClosed) throw new Exception("Request is already closed");
+
+            this.IsClosed = true;
+
+        }
+        public void ReopenRequest()
+        {
+            if (!IsClosed) throw new Exception("Request is already opened");
+
+            this.IsClosed = false;
+        }
 
     }
 }
