748cfc4e89cd912e66d03efb381f655a44a4f42e
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-02-24 22:40:30 +0300

    REFACTOR: Updated entity from anemic entity model to ddd entity model. Added constructor and comment addition function

--- backend/SupportModule/src/Domain/Entities/MiniUser.cs
+++ backend/SupportModule/src/Domain/Entities/MiniUser.cs
@@ -2,11 +2,37 @@
 {
     public class MiniUser
     {
-        public int Id { get; set; }
-        public int TenantId { get; set; }
-        public string Username { get; set; }
-        public string Email { get; set; }
-        public List<SupportRequest> SupportRequest { get; set; }
-        public List<UserComment> UserComments { get; set; }
+        public int Id { get; private set; }
+        public int TenantId { get; private set; }
+        public string Username { get; private set; }
+        public string Email { get; private set; }
+
+        private readonly List<SupportRequest> _supportRequests = new();
+        public IReadOnlyCollection<SupportRequest> SupportRequests => _supportRequests;
+
+        private readonly List<UserComment> _userComments = new();
+        public IReadOnlyCollection<UserComment> UserComments => _userComments;
+
+        public MiniUser(string username, string email, int tenantId)
+        {
+            if (string.IsNullOrWhiteSpace(username))
+                throw new ArgumentException("Username cannot be empty");
+            if (string.IsNullOrWhiteSpace(email))
+                throw new ArgumentException("Email cannot be empty");
+
+            Username = username;
+            Email = email;
+            TenantId = tenantId;
+        }
+
+        public void AddComment(string comment)
+        {
+            if (string.IsNullOrWhiteSpace(comment))
+                throw new Exception("Comment cannot be empty");
+
+            _userComments.Add(new UserComment(comment, this.Id));
+        }
+
+       
     }
 }
