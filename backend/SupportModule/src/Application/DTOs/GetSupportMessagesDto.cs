43de4a6aa890d8366b931e41bc35bd868c0dae9e
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-02-24 22:38:50 +0300

    FEAT: Defined for using when getting support messages

---
+++ backend/SupportModule/src/Application/DTOs/GetSupportMessagesDto.cs
@@ -0,0 +1,9 @@
+﻿namespace SupportModule.Application.DTOs
+{
+    public class GetSupportMessagesDto
+    {
+        public string Content { get; set; }
+        public int SenderId { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
