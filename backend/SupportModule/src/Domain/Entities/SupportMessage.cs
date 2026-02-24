314046e43419fc065211fe584988498b49d19f50
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-02-24 22:40:45 +0300

    REFACTOR: Updated entity from anemic entity model to ddd entity model. Added constructor

--- backend/SupportModule/src/Domain/Entities/SupportMessage.cs
+++ backend/SupportModule/src/Domain/Entities/SupportMessage.cs
@@ -2,12 +2,21 @@
 {
     public class SupportMessage
     {
-        public int Id { get; set; }
-        public string Message { get; set; }
-        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-        public int SenderId { get; set; }
-        public int? ReceiverId { get; set; }
-        public int SupportRequestId { get; set; }
-        public SupportRequest SupportRequest { get; set; }
+        public int Id { get; private set; }
+        public string Content { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public int SenderId { get; private set; }
+        private SupportMessage() { }
+        public SupportMessage(string content, int senderId)
+        {
+            if (string.IsNullOrWhiteSpace(content))
+                throw new ArgumentException("Message cannot be empty");
+
+            Content = content;
+            SenderId = senderId;
+            CreatedAt = DateTime.UtcNow;
+        }
+        public int SupportRequestId { get;private set; }
+        public SupportRequest SupportRequest { get;private set; }
     }
 }
