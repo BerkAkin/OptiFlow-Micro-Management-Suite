177a5adddf244782034b116ba90f07446d884d3d
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-02-25 20:47:10 +0300

    REFACTOR: Added comments

--- client/MainClient/src/pages/MoodPages/MoodPage.tsx
+++ client/MainClient/src/pages/MoodPages/MoodPage.tsx
@@ -1,6 +1,7 @@
 import MoodRecorder from '../../components/MoodComponents/MoodRecorder/MoodRecorder'
 import PreviousMoods from '../../components/MoodComponents/PreviousMoods/PreviousMoods'
 import TodaysMoods from '../../components/MoodComponents/TodaysMoods/TodaysMoods'
+import UserComments from '../../components/MoodComponents/UserComments/UserComments'
 
 function MoodPage() {
     return (
@@ -12,8 +13,13 @@ function MoodPage() {
                 <div className='bg-white h-[500px] col-span-7 shadow-custom border border-gray-200 rounded-lg'>
                     <TodaysMoods />
                 </div>
-                <div className='bg-white h-[350px] col-span-3 shadow-custom border border-gray-200 rounded-lg '>
-                    <PreviousMoods />
+                <div className='col-span-3'>
+                    <div className='bg-white h-[244px] shadow-custom border border-gray-200 rounded-lg '>
+                        <PreviousMoods />
+                    </div>
+                    <div className='bg-white h-[244px] my-2 shadow-custom border border-gray-200 rounded-lg '>
+                        <UserComments />
+                    </div>
                 </div>
 
             </div>
