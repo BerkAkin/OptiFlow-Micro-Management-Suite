260700af9f13ad2dc1ffd4c78d92cc20c5bc9650
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-02-25 22:32:10 +0300

    FEAT: Categorical data is now real data

--- client/MainClient/src/repositories/SupportRepositories/SupportRepository.ts
+++ client/MainClient/src/repositories/SupportRepositories/SupportRepository.ts
@@ -1,20 +1,5 @@
 import { api } from "../../lib/api";
 
-const tempSupportMonthly = [
-  { month: "January", value: 245 },
-  { month: "February", value: 173 },
-  { month: "March", value: 836 },
-  { month: "April", value: 567 },
-];
-
-const tempSupportCategorical = [
-  { category: "Employees", value: 53 },
-  { category: "Stress", value: 183 },
-  { category: "Environment", value: 128 },
-  { category: "Family", value: 21 },
-  { category: "Friends", value: 33 },
-];
-
 const tempRequests = [
   {
     id: 0,
@@ -60,35 +45,34 @@ export const fetchMonthlySupport = async () => {
 };
 
 export const fetchCategoricalSupport = async () => {
-  /*   const res = await api.get("/api/support/categorical");
-  return res.data; */
-  return tempSupportCategorical;
+  const res = await api.get("/support/GetSupportRequestsCategorical");
+  return res.data;
 };
 
 export const fetchSupportRequests = async (tenantId: number) => {
-  /*   const res = await api.get("/api/support/employees");
+  /*   const res = await api.get("/support/employees");
   return res.data; */
   return tempRequests;
 };
 
 export const fetchSupportMessages = async (requestId: number) => {
-  /*   const res = await api.get("/api/support/employees");
+  /*   const res = await api.get("/support/employees");
   return res.data; */
   return tempMessages;
 };
 
 export const fetchEmployeeComments = async (id: string) => {
-  /*   const res = await api.get(`/api/support/employeeComments/${id}`);
+  /*   const res = await api.get(`/support/employeeComments/${id}`);
   return res.data; */
   return tempEmployeeComments;
 };
 
 export const rateOthers = async (payload: any) => {
-  const res = await api.post("/api/support/rateOthers", payload);
+  const res = await api.post("/support/rateOthers", payload);
   return res.data;
 };
 
 export const requestSupport = async (payload: any) => {
-  const res = await api.post("/api/support/request", payload);
+  const res = await api.post("/support/request", payload);
   return res.data;
 };
