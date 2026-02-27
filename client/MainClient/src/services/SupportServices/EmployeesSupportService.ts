e7d6db1c9a315de35e9ebb16224ea5d4e606f338
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-02-27 18:44:51 +0300

    REMOVED Rate employees from support module

--- client/MainClient/src/services/SupportServices/EmployeesSupportService.ts
+++
@@ -1,6 +0,0 @@
-import { fetchSupportRequests } from "../../repositories/SupportRepositories/SupportRepository";
-
-export const SupportRequestsService = async (tenantId: number) => {
-  const data = await fetchSupportRequests(tenantId);
-  return data;
-};
--- client/MainClient/src/services/SupportServices/RateOthersService.ts
+++
@@ -1,6 +0,0 @@
-import { rateOthers } from "../../repositories/SupportRepositories/SupportRepository";
-
-export const RateOthersService = async (payload: any) => {
-  const data = await rateOthers(payload);
-  return data;
-};
