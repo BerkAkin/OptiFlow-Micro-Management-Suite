4ae645fa963d2f72f9b2280ebdf4dc1391119cc5
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-02-27 18:48:31 +0300

    FEAT: Added category field for requesting support card

--- client/MainClient/src/components/SupportComponents/SupportRequestCard/SupportRequestCard.tsx
+++ client/MainClient/src/components/SupportComponents/SupportRequestCard/SupportRequestCard.tsx
@@ -6,18 +6,33 @@ function SupportRequestCard() {
     const mutation = useRequestSupport();
 
     const handleSubmit = (values: any) => {
-        mutation.mutate(values)
+        const newValues = {
+            ...values,
+            category: Number(values.category)
+        }
+        mutation.mutate(newValues)
     }
     const initialValues = {
         message: ""
     }
 
     const fields = [
-        { name: "message", label: "Message", id: "message", placeholder: "Message...", type: "text" as const, as: "textarea" as const }
+        {
+            name: "category", label: "Category", id: "category", placeholder: "Category...", type: "text" as const, as: "select" as const,
+            options: [
+                { label: "Environment", value: 1 },
+                { label: "Colleagues", value: 2 },
+                { label: "Workload", value: 3 },
+                { label: "Mood", value: 4 },
+                { label: "System", value: 5 },
+                { label: "General", value: 6 },
+            ],
+        },
+        { name: "message", label: "Message", id: "message", placeholder: "Message...", type: "text" as const, as: "textarea" as const },
     ]
 
     return (
-        <div className='absolute h-60 w-64 bg-white rounded-lg border border-gray-200 shadow-custom z-20 left-1/2 transform -translate-x-1/2 top-16'>
+        <div className='absolute h-72 w-64 bg-white rounded-lg border border-gray-200 shadow-custom z-20 left-1/2 transform -translate-x-1/2 top-16'>
             <DynamicForm colorScheme='bg-lime-600' hoverScheme='hover:bg-lime-700' initialValues={initialValues} onSubmit={handleSubmit} title='Request Support' fields={fields} />
         </div>
     )
