1ea03c67883a674efcd5697f12acea417ccc12f4
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-02-27 18:44:31 +0300

    STYLE: Minor improvements on visuals

--- client/MainClient/src/components/SupportComponents/SupportRequests/SupportRequests.tsx
+++ client/MainClient/src/components/SupportComponents/SupportRequests/SupportRequests.tsx
@@ -1,13 +1,15 @@
 import { Link, Outlet } from 'react-router'
 import { useSupportRequests } from '../../../hooks/SupportHooks/UseSupport';
+import Spinner from '../../Spinner/Spinner';
+import ErrorMessage from '../../ErrorMessage/ErrorMessage';
 
 
 function SupportRequests() {
 
-    const { data, isLoading, error } = useSupportRequests(1);
+    const { data, isLoading, error } = useSupportRequests();
 
-    if (isLoading) return (<p>Loading...</p>)
-    if (error || !data) return (<p>Error...</p>)
+    if (isLoading) return <Spinner />
+    if (error || !data) return <ErrorMessage />
 
     return (
         <div className='grid grid-cols-12 gap-6'>
@@ -17,18 +19,19 @@ function SupportRequests() {
                 </div>
                 <div className='overflow-y-auto overflow-x-hidden h-[400px] px-1 py-5'>
                     {
-                        data.map((item) => (
+                        data.map((item: any) => (
 
-                            <Link to={`request\\${item.id}\\messages`} className='cursor-pointer'>
+                            <Link key={item.Id} to={`request\\${item.id}\\messages`} className='cursor-pointer'>
                                 <div className='grid grid-cols-8 h-[25%] border rounded-md shadow-sm border-gray-200 my-2 mx-2 hover:bg-gray-50'>
-                                    <div className='col-span-8 flex items-center justify-start px-2 text-start flex items-center'>
+                                    <div className='col-span-8 flex items-center justify-between px-2 text-start flex items-center'>
                                         <p className='text-gray-600'><span className='font-bold'>Category: </span>{item.category}</p>
+                                        <p className='text-gray-400'>{item.isClosed ? "Closed" : "Open"}</p>
                                     </div>
                                     <div className='col-span-8 flex items-center justify-start px-2 text-start flex items-center'>
-                                        <p className='text-gray-500'>{item.name}</p>
+                                        <p className='text-gray-500'>Employee: {item.userName}</p>
                                     </div>
                                     <div className='col-span-8 flex items-center justify-end px-2 text-start flex items-center'>
-                                        <p className='text-gray-400'>{item.date}</p>
+                                        <p className='text-gray-400'>{item.createdAt}</p>
                                     </div>
 
                                 </div>
