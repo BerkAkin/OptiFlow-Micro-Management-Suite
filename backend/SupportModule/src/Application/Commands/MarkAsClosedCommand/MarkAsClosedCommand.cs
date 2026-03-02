bbe12792f8d706ae5baab98329162e46fab66a64
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-03-02 21:45:29 +0300

    FEAT: implemented mark as read operation for support requests

---
+++ backend/SupportModule/src/Application/Commands/MarkAsClosedCommand/MarkAsClosedCommand.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using SupportModule.Domain.Entities;
+using SupportModule.Infrastructure.Persistence;
+
+namespace SupportModule.Application.Commands.MarkAsClosedCommand
+{
+    public record MarkAsClosedCommand(int RequestId,int TenantId) : IRequest<Unit>; 
+    public class MarkAsClosedCommandHandler: IRequestHandler<MarkAsClosedCommand, Unit>  {
+        private readonly SupportDbContext _context;
+        public MarkAsClosedCommandHandler(SupportDbContext context) {
+            _context = context;
+        }
+        public async Task<Unit> Handle(MarkAsClosedCommand command, CancellationToken cancellationToken) {
+
+            SupportRequest data = await _context.SupportRequests
+                .FirstOrDefaultAsync(x => x.TenantId == command.TenantId && x.Id == command.RequestId,cancellationToken);
+            if (data == null)
+                throw new Exception("Request is not available");
+
+            data.CloseRequest();
+
+            await _context.SaveChangesAsync();
+            return Unit.Value;
+        }
+    }
+}
