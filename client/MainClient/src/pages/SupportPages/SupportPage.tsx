0a69e4d6b1ef289914c9f2fca973cdc3b2ed83c5
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-03-03 22:13:25 +0300

    FEAT: Added live chat starter button

--- client/MainClient/src/pages/SupportPages/SupportPage.tsx
+++ client/MainClient/src/pages/SupportPages/SupportPage.tsx
@@ -1,6 +1,8 @@
 import SupportRequests from '../../components/SupportComponents/SupportRequests/SupportRequests'
 import SupportBarChart from '../../components/SupportComponents/SupportCharts/SupportBarChart'
 import SupportDoughChart from '../../components/SupportComponents/SupportCharts/SupportDoughChart'
+import SupportChat from '../../components/SupportComponents/SupportChat/SupportChat'
+import SupportLiveChatButton from '../../components/SupportComponents/SupportLiveChatButton/SupportLiveChatButton'
 
 function SupportPage() {
     return (
@@ -17,9 +19,12 @@ function SupportPage() {
             <div className='grid grid-cols-12 my-6 gap-6'>
                 <div className='col-span-12 h-[600px]'>
                     <SupportRequests />
+
+                </div>
+                <div className='absolute right-20 bottom-20'>
+                    <SupportLiveChatButton />
                 </div>
             </div>
-
 
         </div>
     )
