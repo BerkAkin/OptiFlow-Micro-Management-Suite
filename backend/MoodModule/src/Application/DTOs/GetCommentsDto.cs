41b94d8f34c8da7bf5c354e123b1aae57b2dd3c3
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-03-05 18:53:00 +0300

    FEAT: Defined user based comment getter dto

---
+++ backend/MoodModule/src/Application/DTOs/GetCommentsDto.cs
@@ -0,0 +1,9 @@
+﻿namespace MoodModule.Application.DTOs
+{
+    public class GetCommentsDto
+    {
+        public string Content { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+    }
+}
