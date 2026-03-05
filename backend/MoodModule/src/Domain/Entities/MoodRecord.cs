36741377caa95fa78f4ccc916934a3d72dbd3cfe
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-03-05 18:55:36 +0300

    FEAT: Defined mood entity with ddd model

---
+++ backend/MoodModule/src/Domain/Entities/MoodRecord.cs
@@ -0,0 +1,25 @@
+﻿using MoodModule.Domain.Enums;
+
+namespace MoodModule.Domain.Entities
+{
+    public class MoodRecord
+    {
+        public int Id { get; private set; }
+        public int MoodId { get; private set; }
+        public List<TagsEnum> Tags { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public int TenantId { get; private set; }
+        public int UserId { get; private set; }
+        public MiniUser User { get; private set; }
+
+        private MoodRecord() { }
+        public MoodRecord(int UserId,int tenantId,List<TagsEnum> Tags, int MoodId)
+        {
+            this.Tags = Tags;
+            this.MoodId = MoodId;
+            this.UserId = UserId;
+            TenantId = tenantId;
+            CreatedAt = DateTime.UtcNow;
+        }
+    }
+}
