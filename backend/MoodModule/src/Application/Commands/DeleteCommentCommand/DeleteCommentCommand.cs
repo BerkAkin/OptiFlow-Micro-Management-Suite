549512b3e504507476a095934b3f41c9f69a2948
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-03-05 18:52:05 +0300

    FEAT: Delete command of comments implemented

---
+++ backend/MoodModule/src/Application/Commands/DeleteCommentCommand/DeleteCommentCommand.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using MoodModule.Application.DTOs;
+using MoodModule.Domain.Entities;
+using MoodModule.Infrastructure.Persistence;
+
+namespace MoodModule.Application.Commands.DeleteCommentCommand
+{
+
+    public record DeleteCommentCommand(DeleteCommentDto Comment, int TenantId) : IRequest<Unit>;
+    public class DeleteCommentCommandHandler : IRequestHandler<DeleteCommentCommand, Unit>
+    {
+        private readonly MoodDbContext _dbContext;
+        public DeleteCommentCommandHandler(MoodDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Unit> Handle(DeleteCommentCommand command, CancellationToken cancellationToken)
+        {
+
+            MiniUser user = await _dbContext.Users
+                .Include(u => u.Comments)
+                .SingleOrDefaultAsync(u => u.Id == command.Comment.UserId && u.TenantId == command.TenantId, cancellationToken);
+            if (user is null)
+            {
+                throw new Exception("User does not exists");
+            }
+            user.RemoveComment(command.Comment.CommentId);
+
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
