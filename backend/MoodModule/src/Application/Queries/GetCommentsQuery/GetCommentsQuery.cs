af53328570cf902adc50b5dd5bf7b8f4eb179c8f
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-03-05 18:54:01 +0300

    FEAT: Implemented user based comment getter query

---
+++ backend/MoodModule/src/Application/Queries/GetCommentsQuery/GetCommentsQuery.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using MoodModule.Application.DTOs;
+using MoodModule.Infrastructure.Persistence;
+
+namespace MoodModule.Application.Queries.GetCommentsQuery
+{
+    public record GetCommentsQuery(int UserId) : IRequest<List<GetCommentsDto>>;
+    public class GetCommentsQueryHandler : IRequestHandler<GetCommentsQuery, List<GetCommentsDto>>
+    {
+        private readonly MoodDbContext _dbContext;
+        public GetCommentsQueryHandler(MoodDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<List<GetCommentsDto>> Handle(GetCommentsQuery query, CancellationToken cancellationToken)
+        {
+            return await _dbContext.Comments
+               .AsNoTracking()
+               .Where(c => c.UserId == query.UserId)
+               .OrderByDescending(c => c.CreatedAt)
+               .Select(c => new GetCommentsDto
+               {
+                   Content = c.Content,
+                   CreatedAt = c.CreatedAt,
+               })
+               .ToListAsync();
+        }
+    }
+}
