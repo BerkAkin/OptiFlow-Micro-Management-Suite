d910a9327a5167062cf7d5ad6f95bfbe8d8c290f
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-03-05 18:53:47 +0300

    FEAT: Implemented user's comment getter query

---
+++ backend/MoodModule/src/Application/Queries/GetAllCommentsQuery/GetAllCommentsQuery.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using MoodModule.Application.DTOs;
+using MoodModule.Infrastructure.Persistence;
+
+namespace MoodModule.Application.Queries.GetAllCommentsQuery
+{
+    public record GetAllCommentsQuery(int UserId):IRequest<List<GetAllCommentsDto>>;
+    public class GetAllCommentsQueryHandler : IRequestHandler<GetAllCommentsQuery, List<GetAllCommentsDto>>
+    {
+        private readonly MoodDbContext _dbContext;
+        public GetAllCommentsQueryHandler(MoodDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<List<GetAllCommentsDto>> Handle(GetAllCommentsQuery query, CancellationToken cancellationToken)
+        {
+            return await _dbContext.Comments
+                .AsNoTracking()
+                .Where(c => c.UserId == query.UserId)
+                .OrderByDescending(c => c.CreatedAt)
+                .Select(c => new GetAllCommentsDto { 
+                    Id=c.Id,
+                    Content=c.Content,
+                    CreatedAt=c.CreatedAt,
+                })
+                .ToListAsync();
+        }
+    }
+}
