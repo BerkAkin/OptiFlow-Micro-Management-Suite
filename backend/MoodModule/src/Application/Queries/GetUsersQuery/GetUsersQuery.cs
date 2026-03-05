3a6b3d5e50134a81487efac0cbfa5b76a22f936e
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-03-05 18:54:42 +0300

    FEAT: Implemented user getter query

---
+++ backend/MoodModule/src/Application/Queries/GetUsersQuery/GetUsersQuery.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using MoodModule.Application.DTOs;
+using MoodModule.Infrastructure.Persistence;
+
+namespace MoodModule.Application.Queries.GetUsersQuery
+{
+    public record GetUsersQuery(int TenantId) : IRequest<List<GetUsersDto>>;
+    public class GetUsersQueryHandler : IRequestHandler<GetUsersQuery, List<GetUsersDto>>
+    {
+        private readonly MoodDbContext _dbContext;
+        public GetUsersQueryHandler(MoodDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<List<GetUsersDto>> Handle(GetUsersQuery query, CancellationToken cancellationToken)
+        {
+            return await _dbContext.Users
+                .AsNoTracking()
+                .Where(u => u.TenantId == query.TenantId)
+                .Select(u=> new GetUsersDto
+                {
+                    UserId = u.Id,
+                    Employee= u.Username,
+                })
+                .ToListAsync();
+        }
+    }
+}
