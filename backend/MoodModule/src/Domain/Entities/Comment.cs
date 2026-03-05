efec8c181155866ab2928c95542b106bb7a55f32
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-03-05 18:55:00 +0300

    FEAT: Defined comment entity

---
+++ backend/MoodModule/src/Domain/Entities/Comment.cs
@@ -0,0 +1,21 @@
+﻿namespace MoodModule.Domain.Entities
+{
+    public class Comment
+    {
+        public int Id { get; private set; }
+        public string Content { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public MiniUser User { get; private set; }
+        public int UserId { get; private set; }
+        public int TenantId { get; private set; }
+
+        private Comment() { }
+        public Comment(int UserId, int TenantId, string Content) {
+
+            this.Content = Content;
+            this.UserId = UserId;
+            this.TenantId= TenantId;
+            CreatedAt = DateTime.UtcNow;
+        }
+    }
+}
