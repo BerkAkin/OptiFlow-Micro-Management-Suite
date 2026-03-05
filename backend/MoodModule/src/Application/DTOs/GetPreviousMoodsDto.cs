8bc1a3c7ad34ae82bbb2862e78c9d947e619bc21
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-03-05 18:53:21 +0300

    FEAT: Defined all previous 5 daily getter dto

---
+++ backend/MoodModule/src/Application/DTOs/GetPreviousMoodsDto.cs
@@ -0,0 +1,11 @@
+﻿using MoodModule.Domain.Enums;
+
+namespace MoodModule.Application.DTOs
+{
+    public class GetPreviousMoodsDto
+    {
+            public int CurrentMood { get; set; }
+            public DateTime CreatedAt { get; set; }
+        
+    }
+}
