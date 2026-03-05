32d65a91a85347f2cfe5db628b16f26735029cb7
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-03-05 18:51:50 +0300

    FEAT: Recorder of daily moods command implemented

---
+++ backend/MoodModule/src/Application/Commands/AddMoodRecordCommand/AddMoodRecordCommand.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using MoodModule.Application.DTOs;
+using MoodModule.Domain.Entities;
+using MoodModule.Infrastructure.Persistence;
+
+namespace MoodModule.Application.Commands.AddMoodRecordCommand
+{
+    public record AddMoodRecordCommand(AddMoodRecordDto mood,int UserId,int TenantId): IRequest<Unit>;
+    public class AddMoodRecordCommandHandler : IRequestHandler<AddMoodRecordCommand, Unit>
+    {
+        private readonly MoodDbContext _dbContext;
+        public AddMoodRecordCommandHandler(MoodDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Unit> Handle(AddMoodRecordCommand command,CancellationToken cancellationToken) {
+            MiniUser user = await _dbContext.Users
+                 .SingleOrDefaultAsync(u => u.Id == command.UserId && u.TenantId == command.TenantId,cancellationToken);
+            if (user == null) 
+                throw new Exception("User is not exists");
+
+            user.AddMoodRecord(command.mood.MoodId, command.mood.Tags);
+            await _dbContext.SaveChangesAsync();
+            return Unit.Value;
+        }
+    }
+}
