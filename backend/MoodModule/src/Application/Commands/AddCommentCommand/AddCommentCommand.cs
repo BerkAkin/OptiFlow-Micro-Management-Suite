11cf66d1ab0be7f68a7de8ab0ba712c139d430a4
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-03-05 18:51:27 +0300

    FEAT: Comment adder command implemented to user

---
+++ backend/MoodModule/src/Application/Commands/AddCommentCommand/AddCommentCommand.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using MoodModule.Application.DTOs;
+using MoodModule.Domain.Entities;
+using MoodModule.Infrastructure.Persistence;
+
+namespace MoodModule.Application.Commands.AddCommentCommand
+{
+    public record AddCommentCommand(AddCommentDto comment,int TenantId):IRequest<Unit> ;
+    public class AddCommentCommandHandler: IRequestHandler<AddCommentCommand, Unit>
+    {
+        private readonly MoodDbContext _dbContext;
+        public AddCommentCommandHandler(MoodDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Unit> Handle(AddCommentCommand command,CancellationToken cancellationToken) {
+
+            MiniUser user = await _dbContext.Users.SingleOrDefaultAsync(u=>u.Id==command.comment.UserId && u.TenantId== command.TenantId, cancellationToken);
+            if (user is null) {
+                throw new Exception("User does not exists");
+            }
+            user.AddComment(command.comment.Content);
+
+            await _dbContext.SaveChangesAsync();
+
+            return Unit.Value;
+        }
+    }
+}
