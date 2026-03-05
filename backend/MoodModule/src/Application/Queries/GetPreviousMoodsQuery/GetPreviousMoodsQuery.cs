88e219a4d7cf42303af2c205ae94b1940192938a
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-03-05 18:54:52 +0300

    FEAT: Implemented 5 days of mood getter query

---
+++ backend/MoodModule/src/Application/Queries/GetPreviousMoodsQuery/GetPreviousMoodsQuery.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using MoodModule.Application.DTOs;
+using MoodModule.Infrastructure.Persistence;
+
+namespace MoodModule.Application.Queries.GetPreviousMoodsQuery
+{
+    public record GetPreviousMoodsQuery(int TenantId,int UserId):IRequest<List<GetPreviousMoodsDto>>;
+    public class GetPreviousMoodsQueryHandler : IRequestHandler<GetPreviousMoodsQuery, List<GetPreviousMoodsDto>>
+    {
+        private readonly MoodDbContext _dbContext;
+        public GetPreviousMoodsQueryHandler(MoodDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<List<GetPreviousMoodsDto>> Handle(GetPreviousMoodsQuery query,CancellationToken cancellationToken)
+        {
+            List<GetPreviousMoodsDto> data = await _dbContext.Moods
+                .AsNoTracking()
+                .Where(m => m.UserId == query.UserId && m.TenantId == query.TenantId)
+                .OrderByDescending(m => m.CreatedAt)
+                .Take(5)
+                .Select(m => new GetPreviousMoodsDto
+                {
+                      CreatedAt = m.CreatedAt,
+                      CurrentMood=m.MoodId
+
+                }).ToListAsync(cancellationToken);
+            return data;
+        }
+    }
+}
