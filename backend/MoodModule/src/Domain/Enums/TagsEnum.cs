86d2871b55db02d59b3afafec3c14fd4efa269ed
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-03-05 18:56:22 +0300

    FEAT: Tags enum defined for classifying tags via their ids

---
+++ backend/MoodModule/src/Domain/Enums/TagsEnum.cs
@@ -0,0 +1,20 @@
+﻿namespace MoodModule.Domain.Enums
+{
+    public enum TagsEnum
+    {
+        Work=1,
+        Friends=2,
+        Ergonomy=3,
+        Workload=4,
+        Health=5,
+        Sickness=6,
+        Psychology=7,
+        Family=8,
+        Sleep=9,
+        Tiredness=10,
+        Food=11,
+        Time=12,
+        Enviornment=13,
+        Weather=14,
+    }
+}
