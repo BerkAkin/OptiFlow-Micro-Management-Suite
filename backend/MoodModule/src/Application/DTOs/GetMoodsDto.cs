fa1115b1ae416242e02aaea61e71367200f965d4
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-03-05 18:53:10 +0300

    FEAT: Defined moods getter dto

---
+++ backend/MoodModule/src/Application/DTOs/GetMoodsDto.cs
@@ -0,0 +1,13 @@
+﻿using MoodModule.Domain.Enums;
+
+namespace MoodModule.Application.DTOs
+{
+    public class GetMoodsDto
+    {
+        public string Employee { get; set; }
+        public MoodEnum CurrentMood { get; set; }
+        public string Mood => CurrentMood.ToString();
+        public List<TagsEnum> CurrentTags { get; set; }
+        public DateTime CreatedAt { get; set; } 
+    }
+}
