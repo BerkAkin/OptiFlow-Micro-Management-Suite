ce47dce4b876c2b5c0f4a5e92f19c6f473910bfb
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-03-09 22:18:46 +0300

    REFACTOR: Get previous mood is now enum typed

--- backend/MoodModule/src/Application/DTOs/GetPreviousMoodsDto.cs
+++ backend/MoodModule/src/Application/DTOs/GetPreviousMoodsDto.cs
@@ -4,7 +4,7 @@ namespace MoodModule.Application.DTOs
 {
     public class GetPreviousMoodsDto
     {
-            public int CurrentMood { get; set; }
+            public MoodEnum CurrentMood { get; set; }
             public DateTime CreatedAt { get; set; }
         
     }
