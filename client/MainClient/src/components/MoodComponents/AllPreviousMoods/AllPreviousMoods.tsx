4876d8e191a9385854e2629bda2d33f70932367b
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-03-09 22:21:25 +0300

    RENAMED

---
+++ client/MainClient/src/components/MoodComponents/AllPreviousMoods/AllPreviousMoods.tsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import { UseAllPreviousMoods } from '../../../hooks/MoodHooks/UseMood'
+import DynamicTable from '../../DynamicTable/DynamicTable'
+import Spinner from '../../Spinner/Spinner';
+import ErrorMessage from '../../ErrorMessage/ErrorMessage';
+
+function AllPreviousMoods() {
+
+
+    const [page, setPage] = useState(1);
+    const [filters, setFilters] = useState({});
+    const { data, isLoading, error } = UseAllPreviousMoods(filters, page);
+
+    if (isLoading) return <Spinner />
+    if (error || !data) return <ErrorMessage />
+
+    console.log(data);
+    const onPrev = () => {
+        if (page > 1) setPage(page - 1)
+    }
+
+    const onNext = () => {
+        if (!data) return
+        if (page < data.maxPage) setPage(page + 1)
+    }
+
+    const handleFilter = (values: any) => {
+        setPage(1)
+        setFilters(values)
+    }
+
+    return (
+        <div className=' w-full h-full '>
+            <DynamicTable onPrev={onPrev} onNext={onNext} handleFilter={handleFilter} filterFields={data.filterFields} data={data.values} title='Previous Moods' />
+        </div>
+    )
+}
+
+export default AllPreviousMoods
