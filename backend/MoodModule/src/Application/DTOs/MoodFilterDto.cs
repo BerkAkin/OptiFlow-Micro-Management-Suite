7ad3c90ea69413dd8700d0153653688e4a2b0e2c
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-03-09 22:18:54 +0300

    FEAT: Added filtering dto

---
+++ backend/MoodModule/src/Application/DTOs/MoodFilterDto.cs
@@ -0,0 +1,12 @@
+﻿using MoodModule.Domain.Enums;
+
+namespace MoodModule.Application.DTOs
+{
+    public class MoodFilterDto
+    {
+        public string? Mood { get; set; }
+        public string? Date { get; set; }
+        public int Page { get; set; }
+
+    }
+}
