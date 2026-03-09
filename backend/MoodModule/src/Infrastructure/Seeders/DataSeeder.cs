a23202b0c1f55fd4555a4074e652a9ab2ee534e8
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-03-09 22:20:38 +0300

    FEAT: Added dummy data seeder for moods

---
+++ backend/MoodModule/src/Infrastructure/Seeders/DataSeeder.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoodModule.Domain.Entities;
+using MoodModule.Domain.Enums;
+using MoodModule.Infrastructure.Persistence;
+
+
+namespace MoodModule.Infrastructure.Seeders
+{
+    public static class DataSeeder
+    {
+        public static async Task SeedAsync(MoodDbContext context)
+        {
+            if (!await context.Users.AnyAsync())
+            {
+                MiniUser user1 = new MiniUser("berk", "berk@mail.com", 1,1);
+                user1.AddMoodRecord(4, [1,2,3,4]);
+                user1.AddComment("Kullanıcı 1'e ait deneme yorumu");
+                await context.Users.AddAsync(user1);
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+}
