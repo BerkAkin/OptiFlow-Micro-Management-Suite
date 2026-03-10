f7a883ab68737bdc96b9b8838df9b9ee338e4d25
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-03-10 22:36:24 +0300

    RENAMED AND RELOCATED

--- client/MainClient/src/services/MoodServices/RateOthersService.ts
+++
@@ -1,6 +0,0 @@
-import { rateOthers } from "../../repositories/MoodRepositories/MoodRepository";
-
-export const RateOthersService = async (payload: any) => {
-  const data = await rateOthers(payload);
-  return data;
-};
--- client/MainClient/src/services/SupportServices/EmployeeCommentsSupportService.ts
+++
@@ -1,6 +0,0 @@
-import { fetchEmployeeComments } from "../../repositories/SupportRepositories/SupportRepository";
-
-export const EmployeeCommentsSupportService = async (id: string) => {
-  const data = await fetchEmployeeComments(id);
-  return data;
-};
