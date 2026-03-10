33fae8b09c7cfa8aadba8821f6f7075757a18696
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-03-10 22:32:20 +0300

    REFACTOR: Date prop is now renamed as date instead of createdat

--- backend/MoodModule/src/Application/DTOs/GetAllCommentsDto.cs
+++ backend/MoodModule/src/Application/DTOs/GetAllCommentsDto.cs
@@ -4,6 +4,6 @@
     {
         public int Id { get; set; }
         public string Content { get; set; }
-        public DateTime CreatedAt { get; set; }
+        public DateTime Date { get; set; }
     }
 }
