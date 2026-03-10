3fdd5e0d110961de2f07810fd1078d0c3f0c5a17
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-03-10 22:37:14 +0300

    STYLE: Refined alignment of components

--- client/MainClient/src/pages/MoodPages/MoodPage.tsx
+++ client/MainClient/src/pages/MoodPages/MoodPage.tsx
@@ -1,7 +1,8 @@
 import MoodRecorder from '../../components/MoodComponents/MoodRecorder/MoodRecorder'
 import PreviousMoods from '../../components/MoodComponents/PreviousMoods/PreviousMoods'
 import AllPreviousMoods from '../../components/MoodComponents/AllPreviousMoods/AllPreviousMoods'
-import UserComments from '../../components/MoodComponents/UserComments/UserComments'
+import MyComments from '../../components/MoodComponents/MyComments/MyComments'
+import CommentOnEmployees from '../../components/MoodComponents/CommentOnEmployees/CommentOnEmployees'
 
 function MoodPage() {
     return (
@@ -10,15 +11,18 @@ function MoodPage() {
                 <MoodRecorder />
             </div>
             <div className='grid grid-cols-10 my-6 gap-6'>
-                <div className='bg-white h-[500px] col-span-7 shadow-custom border border-gray-200 rounded-lg'>
+                <div className='bg-white  col-span-7 shadow-custom border border-gray-200 rounded-lg'>
                     <AllPreviousMoods />
                 </div>
-                <div className='col-span-3'>
-                    <div className='bg-white h-[244px] shadow-custom border border-gray-200 rounded-lg '>
+                <div className='col-span-3 '>
+                    <div className='bg-white  shadow-custom border border-gray-200 rounded-lg '>
                         <PreviousMoods />
                     </div>
-                    <div className='bg-white h-[244px] my-2 shadow-custom border border-gray-200 rounded-lg '>
-                        <UserComments />
+                    <div className='bg-white h-[250px] my-2 shadow-custom border border-gray-200 rounded-lg '>
+                        <MyComments />
+                    </div>
+                    <div className='bg-white h-[500px] shadow-custom border border-gray-200 rounded-lg '>
+                        <CommentOnEmployees />
                     </div>
                 </div>
 
