bdaa825488879bbe05a913e318670113f7fcbde1
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-03-24 21:11:04 +0300

    STYLE & FEAT: Minor changes on data fields on user info section. User info is now real data

--- client/MainClient/src/pages/ProfilePages/ProfilePage.tsx
+++ client/MainClient/src/pages/ProfilePages/ProfilePage.tsx
@@ -1,10 +1,11 @@
-import React, { useState } from 'react'
-import MySuggestions from '../../components/ProfileComponents/MySuggestions/MySuggestions'
+import { useState } from 'react'
 import MyDayOffs from '../../components/ProfileComponents/MyDayOffs/MyDayOffs'
 import Settings from '../../components/ProfileComponents/Settings/Settings'
+import { useAuthContext } from '../../context/AuthContext'
 
 function ProfilePage() {
     const [activeTab, setActiveTab] = useState(0)
+    const { userInfo } = useAuthContext();
 
     return (
         <div className='container mx-auto my-10'>
@@ -14,34 +15,23 @@ function ProfilePage() {
                 </div>
 
                 <div className='col-span-8 bg-white rounded-lg shadow-custom border border-gray-200 grid grid-cols-4 gap-6'>
-                    <div className='col-span-1 flex items-center justify-center'>
-                        <p className='text-gray-600 text-5xl '>BERK AKIN</p>
+                    <div className='col-span-1  flex-col flex items-center justify-center'>
+                        <p className='text-gray-600 text-5xl capitalize'>{userInfo.username}</p>
+                        <p className='text-gray-400 text-lg'>{userInfo.email}</p>
                     </div>
-                    <div className='col-span-3 grid grid-cols-2'>
-                        <div className='grid'>
-                            <div className='flex items-center'> <p className='text-gray-600 text-lg'>Company: <span className='text-gray-400'>MicroUsers</span></p></div>
-                            <div className='flex items-center'> <p className='text-gray-600 text-lg'>Department: <span className='text-gray-400'>Administration</span></p></div>
-                            <div className='flex items-center'> <p className='text-gray-600 text-lg'>Role: <span className='text-gray-400'>SuperUser</span></p></div>
+                    <div className='col-span-3 grid'>
+                        <div className='flex items-center capitalize'>
+                            <p className='text-gray-600 text-lg'>Company: <span className='text-gray-400'>{userInfo.company}</span></p>
                         </div>
-                        <div className='grid'>
-
-                            <div className='flex items-center'> <p className='text-gray-600 text-lg'>Email: <span className='text-gray-400'>berkakin@mail.com</span></p></div>
-                            <div className='flex items-center'> <p className='text-gray-600 text-lg'>Change Password:<span className='text-gray-400'>*****</span></p></div>
-                            <div className='flex items-center'></div>
+                        <div className='flex items-center capitalize'>
+                            <p className='text-gray-600 text-lg'>Role: <span className='text-gray-400'>{userInfo.department}</span></p>
                         </div>
                     </div>
                 </div>
 
 
-                <div className='col-span-1 bg-white rounded-lg shadow-custom border border-gray-200 '>
-                    <div className='h-[50%] text-start flex justify-start items-center '>
-                        <p className={`text-lg font-semibold text-slate-800 font-rubik px-4`}>Last Online</p>
-                    </div>
-
-                    <div className='h-[50%] flex items-start justify-center'>
-                        <p className={`text-2xl text-gray-600 font-rubik`}>25.12.2025</p>
-                    </div>
-
+                <div className='col-span-1 bg-white rounded-lg shadow-custom border border-gray-200 hover:scale-[105%] hover:border-sky-400 transition-all '>
+                    <button className='text-gray-600 text-lg size-full cursor-pointer'>Change Password</button>
                 </div>
             </div>
 
@@ -50,16 +40,12 @@ function ProfilePage() {
                     <div>
                         <button className={`cursor-pointer p-1 text-gray-700 text-lg rounded-t-lg h-full w-full ${activeTab === 0 ? "bg-sky-400 text-white" : "hover:bg-gray-200"}`} onClick={() => setActiveTab(0)} >Days Off</button>
                     </div>
-                    <div >
-                        <button className={`cursor-pointer p-1 text-gray-700 text-lg h-full w-full ${activeTab === 1 ? "bg-indigo-400 text-white" : "hover:bg-gray-200"}`} onClick={() => setActiveTab(1)} >Suggestions</button>
-                    </div>
                     <div>
                         <button className={`cursor-pointer p-1 text-gray-700 text-lg h-full rounded-b-lg w-full ${activeTab === 2 ? "bg-orange-400 text-white" : "hover:bg-gray-200"}`} onClick={() => setActiveTab(2)} >Settings</button>
                     </div>
                 </div>
                 <div className=' h-[500px] col-span-9'>
                     {activeTab === 0 && <div> <MyDayOffs /></div>}
-                    {activeTab === 1 && <div><MySuggestions /></div>}
                     {activeTab === 2 && <div><Settings /></div>}
 
                 </div>
