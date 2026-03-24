312898c82374647fe62b77c0fde2c52b6cc78b41
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-03-24 21:11:50 +0300

    REFACTOR: Changed the behaviour of localstorage jwt token setter functions

--- client/MainClient/src/services/AuthServices/LoginService.ts
+++ client/MainClient/src/services/AuthServices/LoginService.ts
@@ -1,6 +1,5 @@
 import { login } from "../../repositories/AuthRepositories/AuthRepository";
 
 export const loginService = async (payload: any) => {
-  const data = await login(payload);
-  localStorage.setItem("AccessToken", data);
+  return await login(payload);
 };
