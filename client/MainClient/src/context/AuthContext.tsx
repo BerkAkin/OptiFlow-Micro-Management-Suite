ad7648d81d5ad2b1fab31bd44c45750f697a1017
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-03-24 21:08:23 +0300

    REFACTOR: Changed the behaviour of login and logout system. Login is now setting userinfo data decoded from accesstoken jwt and logout is now a function which removes userdata, isauth and localstorage jwt data

--- client/MainClient/src/context/AuthContext.tsx
+++ client/MainClient/src/context/AuthContext.tsx
@@ -1,12 +1,41 @@
-import { createContext, useContext, useState } from "react";
+import { jwtDecode } from "jwt-decode";
+import { createContext, useContext, useEffect, useState } from "react";
+
+interface UserInfoShape {
+    username: string,
+    email: string,
+    company: string,
+    department: string,
+    permissions: string[]
+}
 
 const AuthContext = createContext<any>(null);
 
 export const AuthContextProvider = ({ children }: any) => {
 
     const [isAuth, setIsAuth] = useState<boolean>(false);
-    const values = { isAuth, setIsAuth }
+    const [userInfo, setUserInfo] = useState<UserInfoShape | null>(null);
 
+    const handleLoginState = (token: string) => {
+        try {
+            const decoded = jwtDecode<UserInfoShape>(token);
+            setUserInfo({ username: decoded.username, email: decoded.email, company: decoded.company, department: decoded.department, permissions: decoded.permissions });
+            setIsAuth(true);
+            localStorage.setItem("AccessToken", token);
+        } catch (error) {
+            setIsAuth(false);
+            console.error("Anahtar açma hatası");
+        }
+    }
+
+    const handleLogoutState = () => {
+        setIsAuth(false);
+        setUserInfo(null);
+        localStorage.removeItem("AccessToken");
+    }
+
+
+    const values = { isAuth, handleLogoutState, userInfo, handleLoginState }
     return (
         <AuthContext.Provider value={values}>
             {children}
