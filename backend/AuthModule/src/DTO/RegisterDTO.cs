21cec22b4847a8f536fe9c826c015596baee3045
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-03-24 21:05:22 +0300

    REFACTOR: Removed departmentId field

--- backend/AuthModule/src/DTO/RegisterDTO.cs
+++ backend/AuthModule/src/DTO/RegisterDTO.cs
@@ -6,9 +6,11 @@
         public string Surname { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-        public string Tenant { get; set; }
         public string PhoneNum { get; set; }
-        public int DepartmentId { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        public string Tenant { get; set; }
+
         public string Street { get; set; }
         public string Street2 { get; set; }
         public string ApartmentNum { get; set; }
@@ -16,7 +18,7 @@
         public string Province { get; set; }
         public string District { get; set; }
         public string FullAddress { get; set; }
-        public DateTime BirthDate { get; set; }
+
         public List<string> SelectedModuleIds { get; set; } 
     }
 }
