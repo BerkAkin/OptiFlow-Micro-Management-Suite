5d2fe0984a8aeb8fd29105030434033b9ab0d961
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-03-25 22:15:44 +0300

    FEAT: Added function definition for showing user's own suggestions

--- backend/SuggestionModule/src/Application/Interfaces/ISuggestionRepository.cs
+++ backend/SuggestionModule/src/Application/Interfaces/ISuggestionRepository.cs
@@ -1,12 +1,12 @@
 ﻿using SuggestionModule.Application.DTOs;
 using SuggestionModule.Domain.Entities;
-using SuggestionModule.Domain.Enums;
 
 namespace SuggestionModule.Application.Interfaces
 {
     public interface ISuggestionRepository
     {
         Task<List<SuggestionDto>> GetSuggestions(int tenantId);
+        Task<List<SuggestionDto>> GetMySuggestions(int tenantId,int userId);
         Task<MostSuggestionsDto> GetBestSuggestions(int tenantId);
         Task MakeVote(Vote vote);
         Task MakeSuggestion(Suggestion suggestion);
