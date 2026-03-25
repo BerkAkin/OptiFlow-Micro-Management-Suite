01074ca850bd591f552627e55be2c564639bd586
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-03-25 22:19:05 +0300

    FEAT: Components are now protected with role based access

--- client/MainClient/src/pages/SupportPages/SupportPage.tsx
+++ client/MainClient/src/pages/SupportPages/SupportPage.tsx
@@ -1,21 +1,22 @@
 import SupportRequests from '../../components/SupportComponents/SupportRequests/SupportRequests'
 import SupportBarChart from '../../components/SupportComponents/SupportCharts/SupportBarChart'
 import SupportDoughChart from '../../components/SupportComponents/SupportCharts/SupportDoughChart'
-import SupportChat from '../../components/SupportComponents/SupportChat/SupportChat'
 import SupportLiveChatButton from '../../components/SupportComponents/SupportLiveChatButton/SupportLiveChatButton'
+import RoleBasedGuard from '../../components/RoleBasedGuard/RoleBasedGuard'
 
 function SupportPage() {
     return (
         <div className='container my-10 mx-auto'>
-
-            <div className='grid grid-cols-12 my-6 gap-6'>
-                <div className='bg-white h-[400px] col-span-3 shadow-custom border border-gray-200 rounded-lg'>
-                    <SupportDoughChart />
+            <RoleBasedGuard allowedDepartments={["HR", 'Manager']}>
+                <div className='grid grid-cols-12 my-6 gap-6'>
+                    <div className='bg-white h-[400px] col-span-3 shadow-custom border border-gray-200 rounded-lg'>
+                        <SupportDoughChart />
+                    </div>
+                    <div className='bg-white h-[400px] col-span-9 shadow-custom border border-gray-200 rounded-lg '>
+                        <SupportBarChart />
+                    </div>
                 </div>
-                <div className='bg-white h-[400px] col-span-9 shadow-custom border border-gray-200 rounded-lg '>
-                    <SupportBarChart />
-                </div>
-            </div>
+            </RoleBasedGuard>
             <div className='grid grid-cols-12 my-6 gap-6'>
                 <div className='col-span-12 h-[600px]'>
                     <SupportRequests />
