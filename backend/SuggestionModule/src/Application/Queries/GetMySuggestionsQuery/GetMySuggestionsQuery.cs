7619b54ba3ed2e36c80885502a3518fe26e01f72
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-03-25 22:16:01 +0300

    TODO: Implemented function definition for showing user's own suggestions

---
+++ backend/SuggestionModule/src/Application/Queries/GetMySuggestionsQuery/GetMySuggestionsQuery.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using SuggestionModule.Application.DTOs;
+using SuggestionModule.Application.Interfaces;
+
+namespace SuggestionModule.Application.Queries.GetSuggestionsQuery
+{
+    public record GetMySuggestionsQuery(int tenantId,int userId) : IRequest<List<SuggestionDto>>;
+    public class GetMySuggestionsQueryHandler : IRequestHandler<GetMySuggestionsQuery, List<SuggestionDto>>
+    {
+
+        private readonly ISuggestionRepository _repository;
+        public GetMySuggestionsQueryHandler(ISuggestionRepository repository)
+        {
+            _repository = repository;
+        }
+        public async Task<List<SuggestionDto>> Handle(GetMySuggestionsQuery request, CancellationToken cancellationToken)
+        {
+            var data = await _repository.GetMySuggestions(request.tenantId,request.userId);
+            return data;
+        }
+    }
+}
