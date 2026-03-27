50d17662aa8303853b7e23f3fff07fd7260956cf
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-03-27 22:52:33 +0300

    FEAT: Getter dto of off day defined

---
+++ backend/SupportModule/src/Application/DTOs/DayOffDto.cs
@@ -0,0 +1,15 @@
+﻿namespace SupportModule.Application.DTOs
+{
+    public class DayOffDto
+    {
+        public int Id { get; set; }
+        public int TenantId { get; set; }
+        public int UserId { get; set; }
+        public string Username { get; set; } 
+        public string Topic { get; set; }
+        public string Description { get; set; }
+        public int Days { get; set; }
+        public DateTime StartingDate { get; set; }
+        public int Status { get; set; }
+    }
+}
