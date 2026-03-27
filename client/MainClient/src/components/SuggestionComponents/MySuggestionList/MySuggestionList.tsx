a986951958ae461f76551b3992a499ee01ca633e
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-03-27 22:59:39 +0300

    STYLE & FEAT: SuggestionList is now functional and shows users's own suggestions and styled

--- client/MainClient/src/components/SuggestionComponents/MySuggestionList/MySuggestionList.tsx
+++ client/MainClient/src/components/SuggestionComponents/MySuggestionList/MySuggestionList.tsx
@@ -1,16 +1,27 @@
-import React from 'react'
+import { useMySuggestions } from '../../../hooks/SuggestionHooks/useSuggestion';
+import Spinner from '../../Spinner/Spinner';
+import ErrorMessage from '../../ErrorMessage/ErrorMessage';
+import SuggestionCard from '../SuggestionCard/SuggestionCard';
 
 function MySuggestionList() {
+
+    const { data, isLoading, error } = useMySuggestions();
+    if (isLoading) return (<Spinner />)
+    if (error || !data) return (<ErrorMessage />)
+
     return (
         <div className='h-[650px] bg-white rounded-lg shadow-custom border border-gray-200'>
-            <div className='h-[10%] text-start flex justify-start'>
+            <div className='h-[10%] text-start flex justify-start border-b border-slate-100'>
                 <p className={`text-xl font-semibold text-slate-800 font-rubik ps-6 py-4`}>My Suggestions</p>
             </div>
 
-            <div className='h-[90%] overflow-y-auto my-6 grid gap-6 px-6 grid-cols-12'>
-
+            <div className='h-[80%] overflow-y-auto grid gap-6 px-3 overflow-x-hidden grid-cols-12'>
+                {data.map((item: any, index: number) => (
+                    <div className='col-span-12 my-4' key={index}>
+                        <SuggestionCard {...item} />
+                    </div>
+                ))}
             </div>
-
         </div>
     )
 }
