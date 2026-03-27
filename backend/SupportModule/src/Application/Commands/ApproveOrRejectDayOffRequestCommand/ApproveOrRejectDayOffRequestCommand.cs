257eebf4a2c2f1c29ed2317e9603b250e955fb7e
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-03-27 22:50:20 +0300

    FEAT: Command for approving or rejecting day off request implemented with mediatr pattern

---
+++ backend/SupportModule/src/Application/Commands/ApproveOrRejectDayOffRequestCommand/ApproveOrRejectDayOffRequestCommand.cs
@@ -0,0 +1,40 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using SupportModule.Application.DTOs;
+using SupportModule.Domain.Entities;
+using SupportModule.Infrastructure.Persistence;
+
+namespace SupportModule.Application.Commands.ApproveOrRejectDayOffRequestCommand
+{
+
+
+    public record ApproveOrRejectDayOffRequestCommand(ApproveOrRejectDto dto, int currentTenant) : IRequest<Unit>;
+    public class ApproveOrRejectDayOffRequestCommandHandler : IRequestHandler<ApproveOrRejectDayOffRequestCommand, Unit>
+    {
+        private readonly SupportDbContext _dbContext;
+
+        public ApproveOrRejectDayOffRequestCommandHandler(SupportDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Unit> Handle(ApproveOrRejectDayOffRequestCommand command, CancellationToken cancellationToken)
+        {
+            DayOff data = await _dbContext.DayOffs.SingleOrDefaultAsync(d=>d.Id == command.dto.requestId && d.TenantId==command.currentTenant, cancellationToken);
+
+            if (data is null)
+            {
+                throw new Exception("Request does not exist");
+            }
+
+            data.ChangeStatus(command.dto.status);
+            await _dbContext.SaveChangesAsync();
+
+            return Unit.Value;
+
+        }
+
+
+    }
+
+}
