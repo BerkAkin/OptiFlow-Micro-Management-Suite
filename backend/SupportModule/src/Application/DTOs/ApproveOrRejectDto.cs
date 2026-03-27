540bad11faa9551a76b332544dbf2a53bab0bcd0
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-03-27 22:52:13 +0300

    FEAT: Approve or reject dto defined

---
+++ backend/SupportModule/src/Application/DTOs/ApproveOrRejectDto.cs
@@ -0,0 +1,8 @@
+﻿namespace SupportModule.Application.DTOs
+{
+    public class ApproveOrRejectDto
+    {
+        public int requestId { get; set; }
+        public int status { get; set; }
+    }
+}
