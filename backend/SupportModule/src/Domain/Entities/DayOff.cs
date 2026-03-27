6fbab28bee60330d4b0c002487ff4bd4249f89e5
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-03-27 22:52:52 +0300

    FEAT: Day off entity defined within the rules of DDD

---
+++ backend/SupportModule/src/Domain/Entities/DayOff.cs
@@ -0,0 +1,31 @@
+﻿namespace SupportModule.Domain.Entities
+{
+    public class DayOff
+    {
+        public int Id { get; private set; }
+        public int TenantId { get; private set; }
+        public int UserId { get; private set; }
+        public MiniUser User { get; private set; }
+        public string Topic { get; private set; }
+        public string Description { get; private set; }
+        public int Days { get; private set; }
+        public DateTime StartingDate { get; private set; }
+        public int Status { get; private set; } = 0;
+
+        private DayOff() { }
+        public DayOff(int tenantId,string topic, string description, int days, DateTime startingDate, int status=0) {
+            TenantId = tenantId;
+            Topic = topic;
+            Description = description;
+            Days = days;
+            StartingDate = startingDate;
+            Status = status;
+        }
+
+        public void ChangeStatus(int status)
+        {
+           Status = status;
+        }
+
+    }
+}
