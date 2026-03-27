d6190a2f7bd7c903cc6932e22a8743f0b05d4f6c
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-03-27 23:05:11 +0300

    STYLE: Added animations to components

--- client/MainClient/src/pages/MoodPages/MoodPage.tsx
+++ client/MainClient/src/pages/MoodPages/MoodPage.tsx
@@ -7,29 +7,29 @@ import RoleBasedGuard from '../../components/RoleBasedGuard/RoleBasedGuard'
 
 function MoodPage() {
     return (
-        <div className='container my-10 mx-auto'>
+        <div className='container my-10 mx-auto '>
             <RoleBasedGuard allowedDepartments={['Employee', 'Finance Accountant', 'Manager']}>
                 <div>
                     <MoodRecorder />
                 </div>
             </RoleBasedGuard>
-            <div className='grid grid-cols-10 my-6 gap-6'>
-                <div className='bg-white h-[700px] col-span-7 shadow-custom border border-gray-200 rounded-lg'>
+            <div className='grid grid-cols-10 my-6 gap-6 '>
+                <div className='bg-white h-[700px] col-span-7 shadow-custom border border-gray-200 rounded-lg transition-all duration-200 ease-out hover:scale-101 hover:-translate-y-1'>
                     <AllPreviousMoods />
                 </div>
                 <div className='col-span-3 '>
                     <RoleBasedGuard allowedDepartments={['Employee', 'Finance Accountant', 'Manager']}>
-                        <div className='bg-white mb-2 shadow-custom border border-gray-200 rounded-lg '>
+                        <div className='bg-white mb-2 shadow-custom border border-gray-200 rounded-lg transition-all duration-200 ease-out hover:scale-101 hover:-translate-y-1'>
                             <PreviousMoods />
                         </div>
                     </RoleBasedGuard>
                     <RoleBasedGuard allowedDepartments={['Employee', 'Finance Accountant', 'Manager']}>
-                        <div className='bg-white h-[250px] shadow-custom border border-gray-200 rounded-lg '>
+                        <div className='bg-white h-[250px] shadow-custom border border-gray-200 rounded-lg transition-all duration-200 ease-out hover:scale-101 hover:-translate-y-1'>
                             <MyComments />
                         </div>
                     </RoleBasedGuard>
                     <RoleBasedGuard allowedDepartments={["HR"]}>
-                        <div className='bg-white h-[480px] shadow-custom border border-gray-200 rounded-lg '>
+                        <div className='bg-white h-[480px] shadow-custom border border-gray-200 rounded-lg transition-all duration-200 ease-out hover:scale-101 hover:-translate-y-1'>
                             <CommentOnEmployees />
                         </div>
                     </RoleBasedGuard>
