7b5ccf535bfdb92c1e34980b7c4222e110ff3425
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-03-27 22:56:12 +0300

    STYLE: Color scheme changed to sky/blue

--- client/MainClient/src/components/SupportComponents/SupportMessages/SupportMessages.tsx
+++ client/MainClient/src/components/SupportComponents/SupportMessages/SupportMessages.tsx
@@ -52,25 +52,25 @@ function SupportMessages() {
     if (error || !data) return <ErrorMessage />;
 
     return (
-        <div className='border border-gray-200 bg-white rounded-lg shadow-custom h-[460px] w-full'>
+        <div className='h-full'>
             <div className='h-[10%] text-start flex justify-start border-b border-gray-200 pb-2'>
                 <p className={`text-xl font-semibold text-slate-800 font-rubik ps-4 py-4 `}>Messages</p>
             </div>
 
-            <div className='overflow-y-auto h-[70%] px-3 py-4  mb-4'>
+            <div className='overflow-y-auto h-[70%] px-3 py-4 mb-4'>
                 {messages.map((item, index) =>
                 (
                     item.isMine
                         ?
                         <div className='flex justify-end'>
-                            <div key={index} className='border bg-lime-500 w-[25%] rounded-md border-gray-200 my-2 p-3'>
+                            <div key={index} className='border bg-sky-400 w-[25%] rounded-md border-gray-200 my-2 p-3'>
                                 <p className='text-white'>{item.message}</p>
                                 <p className='text-end text-xs text-gray-200'>{item.createdAt}</p>
                             </div>
                         </div>
                         :
                         <div className='flex justify-start'>
-                            <div key={index} className='border bg-lime-600 w-[25%] rounded-md border-gray-200 my-2 p-3'>
+                            <div key={index} className='border bg-sky-600 w-[25%] rounded-md border-gray-200 my-2 p-3'>
                                 <p className='text-white'>{item.message}</p>
                                 <p className='text-end text-xs text-gray-200'>{item.createdAt}</p>
                             </div>
@@ -86,7 +86,7 @@ function SupportMessages() {
                                 <Form>
                                     <div className='flex px-4'>
                                         <Field id="message" name="message" className="border border-gray-200 rounded px-4 mx-2 outline-none w-[95%]" placeholder="Message..." />
-                                        <button type="submit" className={`bg-lime-600 text-white w-[5%] py-2 flex items-center justify-center rounded-sm transition-all hover:scale-[1.03] hover:bg-lime-500 active:scale-[0.98]`}>
+                                        <button type="submit" className={`bg-sky-600 text-white w-[5%] py-2 flex items-center justify-center rounded-sm transition-all hover:scale-[1.03] hover:bg-sky-500 active:scale-[0.98]`}>
                                             <img src={send} alt="Send" width={25} />
                                         </button>
                                     </div>
