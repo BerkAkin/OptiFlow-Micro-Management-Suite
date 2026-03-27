17c5ba32b08f4b9be24fa5de493b139a67993145
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-03-27 22:57:24 +0300

    STYLE: Added top rated and monthly most rated suggestions headers and info texts

--- client/MainClient/src/components/SuggestionComponents/SuggestionShowcaseCard/SuggestionShowcaseCard.tsx
+++ client/MainClient/src/components/SuggestionComponents/SuggestionShowcaseCard/SuggestionShowcaseCard.tsx
@@ -9,15 +9,26 @@ interface suggestionShowcaseProps {
 
 function SuggestionShowcaseCard({ isStar, Description, VoteCount }: suggestionShowcaseProps) {
     return (
-        <div className='bg-white rounded-lg h-[100px] shadow-custom border border-gray-200 grid grid-cols-10'>
-            <div className='col-span-2 flex items-center justify-center'>
+        <div className='bg-white rounded-lg h-[100px] shadow-custom border border-gray-200 grid grid-cols-10 transition-all duration-200 ease-out hover:scale-101 hover:-translate-y-1'>
+            <div className='col-span-2 flex items-center justify-center border-e border-amber-200'>
                 <p className={`${isStar == true ? "text-6xl" : "text-7xl pb-2"} text-amber-500`}>{isStar == true ? <img src={star} width={60} /> : <img src={crown} width={60} />}</p>
             </div>
-            <div className='col-span-6'>
-                <p className='text-gray-700 text-lg h-full w-full pt-6 px-6'>{Description}</p>
+            <div className='col-span-7'>
+                <div className='h-[40px] flex items-center justify-start'>
+                    <p className='ps-4 w-full '>{isStar ? "This Month's Best" : "All Time Best"}</p>
+                </div>
+                <div className='h-[60px] flex justify-start items-start'>
+                    <p className='text-gray-700 text-sm py-2 px-4'>{Description}</p>
+                </div>
             </div>
-            <div className='col-span-2 flex justify-center items-center'>
-                <p className='text-4xl text-gray-600'>{VoteCount}</p>
+            <div className='col-span-1'>
+                <div className='h-[40px] flex items-center justify-start'>
+                    <p className='ps-4 w-full '>Votes</p>
+                </div>
+                <div className='h-[60px] flex justify-center items-start'>
+                    <p className='text-3xl text-gray-600'>{VoteCount}</p>
+                </div>
+
             </div>
         </div>
     )
