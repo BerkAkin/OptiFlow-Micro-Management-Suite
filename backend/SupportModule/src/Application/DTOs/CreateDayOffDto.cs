7ac5fd112de2eb7b1051efc93ad1e9d0892f4dcd
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-03-27 22:52:26 +0300

    FEAT: Creator dto of off day defined

---
+++ backend/SupportModule/src/Application/DTOs/CreateDayOffDto.cs
@@ -0,0 +1,10 @@
+﻿namespace SupportModule.Application.DTOs
+{
+    public class CreateDayOffDto
+    {
+            public string Topic { get; set; }
+            public string Description { get; set; }
+            public int Days { get; set; }
+            public DateTime StartingDate { get; set; }
+    }
+}
