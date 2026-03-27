cccd3c33266db3fc07755b8069530e707b749f4f
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-03-27 22:51:11 +0300

    FEAT: Query for users's day off requests list implemented with mediatr pattern

---
+++ backend/SupportModule/src/Application/Queries/GetMyDayOffListQuery/GetMyDayOffListQuery.cs
@@ -0,0 +1,38 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using SupportModule.Application.DTOs;
+using SupportModule.Infrastructure.Persistence;
+
+namespace SupportModule.Application.Queries.GetMyDayOffListQuery
+{
+    public record GetMyDayOffListQuery(int currentTenant,int currentUser) : IRequest<List<DayOffDto>>;
+    public class GetMyDayOffListQueryHandler : IRequestHandler<GetMyDayOffListQuery, List<DayOffDto>>
+    {
+        private readonly SupportDbContext _dbContext;
+
+        public GetMyDayOffListQueryHandler(SupportDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<List<DayOffDto>> Handle(GetMyDayOffListQuery query, CancellationToken cancellationToken)
+        {
+            return await _dbContext.DayOffs.AsNoTracking()
+                .Where(d => d.TenantId == query.currentTenant && d.UserId==query.currentUser)
+                .Select(d => new DayOffDto
+                {
+                    Topic = d.Topic,
+                    Description = d.Description,
+                    StartingDate = d.StartingDate,
+                    Days = d.Days,
+                    Status = d.Status,
+                })
+                .ToListAsync();
+
+        }
+
+
+    }
+
+
+}
