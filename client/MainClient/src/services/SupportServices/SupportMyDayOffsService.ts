dd4f2ca2a61d1e7264eb8570229b33d0d0c4f408
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-10 14:16:36 +0300

    FEAT: Added data filtering and pagination for user's day off list

--- client/MainClient/src/services/SupportServices/SupportMyDayOffsService.ts
+++ client/MainClient/src/services/SupportServices/SupportMyDayOffsService.ts
@@ -1,6 +1,36 @@
 import { fetchMyDayOffs } from "../../repositories/SupportRepositories/SupportRepository";
 
-export const SupportMyDayOffsService = async () => {
-  const data = await fetchMyDayOffs();
-  return data;
+export const SupportMyDayOffsService = async (filters: any, page: any) => {
+  const data = await fetchMyDayOffs(filters, page);
+
+  const cleanedData = data.values.map(
+    ({
+      id,
+      username,
+      userId,
+      tenantId,
+      startingDate,
+      status,
+      ...rest
+    }: any) => ({
+      ...rest,
+      date: startingDate,
+      status: status == 1 ? "Accepted" : status == 2 ? "Rejected" : "Pending",
+    }),
+  );
+
+  const newData = {
+    maxPage: data.maxPage,
+    filterFields: [
+      { name: "date", type: "date" as const },
+      {
+        name: "topic",
+        type: "text" as const,
+        placeholder: "Topic...",
+      },
+    ],
+    data: cleanedData,
+  };
+  console.log("Filteli sayfalı", newData);
+  return newData;
 };
