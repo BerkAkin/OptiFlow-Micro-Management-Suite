2f80acd2084ce93c18a01a3bd3504569c69d07ae
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-10 20:06:15 +0300

    FEAT: Defined notification types

---
+++ backend/NotificationService/src/Models/Enums/NotificationType.cs
@@ -0,0 +1,15 @@
+﻿namespace NotificationService.Models.Enums
+{
+    public enum NotificationType
+    {
+        SurveyCreated = 1,
+        SurveyTimeout = 2,
+        SuggestionCreated=3,
+        SuggestionStatusChanged=4,
+        SupportRequestNewMessage=5,
+        SupportDayOffRequest=6, 
+        SupportDayOffStatusChanged=7,
+        MoodNewComment=8,
+        ProfilePasswordChanged=9,
+    }
+}
