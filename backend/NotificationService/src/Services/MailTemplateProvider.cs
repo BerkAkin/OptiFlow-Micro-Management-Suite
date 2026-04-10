4eb95d8e6bf35007630f12ca3d2740d58f7c99ff
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-10 20:06:41 +0300

    FEAT: Implemented  for selection right notification from enum

---
+++ backend/NotificationService/src/Services/MailTemplateProvider.cs
@@ -0,0 +1,24 @@
+﻿using NotificationService.Models.Enums;
+
+namespace NotificationService.Services
+{
+    public class MailTemplateProvider
+    {
+        public (string Subject, string FileName) GetTemplateDetails(NotificationType type)
+        {
+            return type switch
+            {
+                NotificationType.SurveyCreated => ("Yeni Bir Anket Var!", "SurveyCreatedTemplate.html"),
+                NotificationType.SurveyTimeout => ("Anket Süresi Doldu!", "SurveyTimeoutTemplate.html"),
+                NotificationType.SuggestionCreated => ("Yeni Bir Öneri Var!", "SuggestionCreatedTemplate.html"),
+                NotificationType.SuggestionStatusChanged => ("Önerinizin Durumu Güncellendi!", "SuggestionStatusChangedTemplate.html"),
+                NotificationType.MoodNewComment => ("Yeni Bir Çalışan Yorumunuz Var!", "MoodNewCommentTemplate.html"),
+                NotificationType.ProfilePasswordChanged => ("Hesap Parola Değişikliği!", "ProfilePasswordChangedTemplate.html"),
+                NotificationType.SupportDayOffStatusChanged => ("İzin İsteğiniz Hakkında Güncelleme!", "SupportDayOffStatusChangedTemplate.html"),
+                NotificationType.SupportDayOffRequest => ("Yeni Bir Çalışan İzin İsteği Var!", "SupportDayOffRequestTemplate.html"),
+                NotificationType.SupportRequestNewMessage => ("Destek Talebiniz Güncellendi!", "SupportRequestNewMessageTemplate.html"),
+                _ => ("Bilgilendirme", "DefaultTemplate.html")
+            };
+        }
+    }
+}
