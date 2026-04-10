37ae35dfde0bfc5ada436b88fe7264d88f67c3a8
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-10 14:16:59 +0300

    FEAT: Filter props defined for filtering and pagination of user

---
+++ backend/SupportModule/src/Application/DTOs/MyDaysOffFilterDto.cs
@@ -0,0 +1,9 @@
+﻿namespace SupportModule.Application.DTOs
+{
+    public class MyDaysOffFilterDto
+    {
+        public string? Date { get; set; }
+        public string? Topic { get; set; }
+        public int Page { get; set; }
+    }
+}
