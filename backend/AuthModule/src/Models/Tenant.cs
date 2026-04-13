69b86786d83f3b03513986bdd97f2e767ff9b7c4
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-13 13:53:48 +0300

    FEAT: Added compnay info to tenants

--- backend/AuthModule/src/Models/Tenant.cs
+++ backend/AuthModule/src/Models/Tenant.cs
@@ -7,6 +7,14 @@ namespace AuthModule.Models
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Address { get; set; }
+        public string PhoneNum { get; set; }
+        public string? FaxNum { get; set; }
+        public string MailAddress { get; set; }
+        public string TaxOffice { get; set; }
+        public string TaxNumber { get; set; }
+        public string MersisNum { get; set; }
+        public string TradeRegistryNum { get; set; }
         public List<TenantModule> TenantModules { get; set; }
     }
 }
