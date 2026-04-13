9ad1a10521ccd7f3aad50532d463335fc9b7440c
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-13 13:58:47 +0300

    FEAT: dto of company info for invoice has been defined

---
+++ backend/FinanceModule/src/Entities/MiniTenantInfo.cs
@@ -0,0 +1,17 @@
+﻿namespace FinanceModule.Entities
+{
+    public class MiniTenantInfo
+    {
+        public int Id { get; set; }
+        public int TenantId { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string PhoneNum { get; set; }
+        public string? FaxNum { get; set; }
+        public string MailAddress { get; set; }
+        public string TaxOffice { get; set; }
+        public string TaxNumber { get; set; }
+        public string MersisNum { get; set; }
+        public string TradeRegistryNum { get; set; }
+    }
+}
