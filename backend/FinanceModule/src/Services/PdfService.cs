08aefe79376cbf03feaa35e2ec3cf528df4796b7
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-13 14:00:32 +0300

    FEAT: PDFService implemented. This service uses PuppeteerSharp to create pdf on docker environment with using minimal chromium installation.

---
+++ backend/FinanceModule/src/Services/PdfService.cs
@@ -0,0 +1,41 @@
+﻿using PuppeteerSharp;
+using PuppeteerSharp.Media;
+
+namespace FinanceModule.Services
+{
+
+    public interface IPdfService
+    {
+        Task<byte[]> CreatePdf(string htmlContent);
+    }
+
+    public class PdfService : IPdfService
+    {
+        public async Task<byte[]> CreatePdf(string htmlContent)
+        {
+            var browserFetcher = new BrowserFetcher();
+            await browserFetcher.DownloadAsync();
+
+            var launchOptions = new LaunchOptions
+            {
+                Headless = true,
+                Args = new[] {
+                "--no-sandbox",
+                "--disable-setuid-sandbox",
+                "--disable-dev-shm-usage"
+            }
+            };
+
+            await using var browser = await Puppeteer.LaunchAsync(launchOptions);
+            await using var page = await browser.NewPageAsync();
+            await page.SetContentAsync(htmlContent);
+
+            return await page.PdfDataAsync(new PdfOptions
+            {
+                Format = PaperFormat.A4,
+                PrintBackground = true,
+                MarginOptions = new MarginOptions { Top = "20px", Bottom = "20px" }
+            });
+        }
+    }
+}
