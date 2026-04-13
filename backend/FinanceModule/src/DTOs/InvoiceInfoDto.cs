ca493126c2228c07a8d69cb560cbc3b98b96ed43
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-13 13:58:07 +0300

    FEAT: Invoice dto defined

---
+++ backend/FinanceModule/src/DTOs/InvoiceInfoDto.cs
@@ -0,0 +1,17 @@
+﻿namespace FinanceModule.DTOs
+{
+    public class InvoiceDto
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Address { get; set; }
+        public string PhoneNum { get; set; }
+        public string PersonSerialNum { get; set; }
+        public string Email { get; set; }
+        public string InvoiceDate { get; set; }
+        public List<InvoiceProductDto> Products { get; set; }
+        public decimal SubTotal => Products.Sum(p => p.Price * p.Quantity);
+        public decimal TaxRate { get; set; } = 10;
+        public decimal GrandTotal => SubTotal + (SubTotal * TaxRate / 100);
+    }
+}
