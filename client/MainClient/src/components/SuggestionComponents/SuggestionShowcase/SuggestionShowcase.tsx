61c312142b61dce216dda12cba63baf5f26b318c
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-15 20:36:57 +0300

    FIX: crash that caused page to return blank when there was no data to pass has been fixed

--- client/MainClient/src/components/SuggestionComponents/SuggestionShowcase/SuggestionShowcase.tsx
+++ client/MainClient/src/components/SuggestionComponents/SuggestionShowcase/SuggestionShowcase.tsx
@@ -11,11 +11,19 @@ function SuggestionShowcase() {
 
     return (
         <div className='grid grid-cols-10 gap-6 '>
-            <div className='col-span-5 '>
-                <SuggestionShowcaseCard isStar={true} Description={data.best.description} VoteCount={data.best.votes} />
+            <div className='col-span-5'>
+                <SuggestionShowcaseCard
+                    isStar={true}
+                    Description={data?.best?.description || "Best of this month has not been elected"}
+                    VoteCount={data?.best?.votes || 0}
+                />
             </div>
-            <div className='col-span-5 '>
-                <SuggestionShowcaseCard isStar={false} Description={data.thisMonth.description} VoteCount={data.thisMonth.votes} />
+            <div className='col-span-5'>
+                <SuggestionShowcaseCard
+                    isStar={false}
+                    Description={data?.thisMonth?.description || "No suggestions yet"}
+                    VoteCount={data?.thisMonth?.votes || 0}
+                />
             </div>
         </div>
     )
