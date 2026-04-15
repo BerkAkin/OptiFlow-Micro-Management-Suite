c91c1dcc1bc8ff77a31e20b4d056d65cd2764c1f
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-15 20:35:47 +0300

    FEAT: Notification preference command implemented

---
+++ backend/NotificationService/src/Services/UpdatePreferenceCommand.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using NotificationService.Data;
+
+namespace NotificationService.Services
+{
+    public record UpdatePreferenceCommand(int currentUserId) : IRequest<Unit>;
+
+    public class UpdatePreferenceCommandHandler : IRequestHandler<UpdatePreferenceCommand, Unit>
+    {
+        private readonly AppDbContext _context;
+
+        public UpdatePreferenceCommandHandler(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Unit> Handle(UpdatePreferenceCommand request, CancellationToken cancellationToken)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(x=>x.UserId == request.currentUserId);
+
+            if (user == null)
+            {
+                throw new Exception("User does not exist");
+            }
+            user.NotificationPreference = !user.NotificationPreference;
+            await _context.SaveChangesAsync(cancellationToken);
+            return Unit.Value;
+
+        }
+    }
+}
