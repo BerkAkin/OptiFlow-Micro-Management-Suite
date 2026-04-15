1037ccabf84ef6a4decc953d9dca17ddbbbeade0
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-15 20:36:22 +0300

    FEAT: Added notificatin info and changer features to notification checkbox

--- client/MainClient/src/components/ProfileComponents/Settings/Settings.tsx
+++ client/MainClient/src/components/ProfileComponents/Settings/Settings.tsx
@@ -1,22 +1,33 @@
 import React from 'react'
+import { useChangeEmailPreference, useEmailPreference } from '../../../hooks/ProfileHooks/UseProfile';
 
 function Settings() {
+    const { data: isEnabled, isLoading } = useEmailPreference();
+    const mutation = useChangeEmailPreference();
+
     return (
         <div className='grid grid-cols-12'>
-            <div className='col-span-12 bg-white rounded-lg shadow-custom border border-gray-200'>
-                <div className='my-1'>
-                    <div className='my-3'>
-                        <div className='ps-3 my-2'>
-                            <input name='mailFinancial' className='cursor-pointer size-4' type='checkbox'></input>
-                            <label htmlFor='mailFinancial' className='px-2 text-md text-gray-700'>Notify me about updates</label>
-                            {/* onchange ile güncelleme yapılacak burada toggle a çevrilip */}
-                        </div>
+            <div className='col-span-12 bg-white rounded-lg shadow-custom border border-gray-200 p-4'>
+                <div>
+                    <div className='flex items-center'>
+                        <input
+                            id="mailFinancial"
+                            type="checkbox"
+                            className='w-4 h-4 cursor-pointer disabled:opacity-50'
+                            checked={!!isEnabled}
+                            onChange={() => mutation.mutate()}
+                            disabled={isLoading || mutation.isPending}
+                        />
+                        <label
+                            htmlFor='mailFinancial'
+                            className='px-2 text-sm text-gray-700 cursor-pointer select-none'
+                        >
+                            {mutation.isPending ? 'Updating Status...' : 'Email Notifications'}
+                        </label>
                     </div>
                 </div>
-
             </div>
-
-        </div >
+        </div>
     )
 }
 
