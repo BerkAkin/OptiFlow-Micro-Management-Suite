123e14aee122bd874f58aec5405594dced29a930
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-17 23:21:27 +0300

    FEAT: Defined interface for storageservice

---
+++ backend/AuthModule/src/Interfaces/IStorageService.cs
@@ -0,0 +1,10 @@
+﻿
+namespace AuthModule.Interfaces
+{
+    public interface IStorageService
+    {
+        Task<string> UploadFileAsync(IFormFile file);
+        Task DeleteFileAsync(string fileName);
+        Task<Stream> GetFileStreamAsync(string fileName, string bucketName);
+    }
+}
