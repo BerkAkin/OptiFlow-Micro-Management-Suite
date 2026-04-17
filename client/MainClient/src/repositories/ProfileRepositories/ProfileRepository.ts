6c71962c45e9c0eb9335630a3ec4abb61ec6771d
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-17 23:24:55 +0300

    FEAT: Added changeProfilePicture and GetProfilePicture repository callers

--- client/MainClient/src/repositories/ProfileRepositories/ProfileRepository.ts
+++ client/MainClient/src/repositories/ProfileRepositories/ProfileRepository.ts
@@ -9,3 +9,17 @@ export const changeEmailPreference = async () => {
   const res = await api.post("/notification/emailPreference");
   return res.data;
 };
+
+export const changeProfilePicture = async (formData: FormData) => {
+  const res = await api.post("/auth/upload-avatar", formData, {
+    headers: { "Content-Type": "multipart/form-data" },
+  });
+  return res.data;
+};
+
+export const getProfilePicture = async (fileName: string) => {
+  const res = await api.get(`/auth/profile-picture/${fileName}`, {
+    responseType: "blob",
+  });
+  return res.data;
+};
