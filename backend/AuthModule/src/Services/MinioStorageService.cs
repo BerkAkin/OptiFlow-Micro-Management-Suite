dba6e6a6b5684a17777abfef812ce8ddd56324f8
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-17 23:22:22 +0300

    FEAT: Implemented minio storage service to upload dleete and get  files

---
+++ backend/AuthModule/src/Services/MinioStorageService.cs
@@ -0,0 +1,55 @@
+﻿using AuthModule.Interfaces;
+using Minio;
+using Minio.DataModel.Args;
+namespace AuthModule.Services
+{
+    public class MinioStorageService :IStorageService
+    {
+        private readonly IMinioClient _minioClient;
+        private readonly string _bucketName = "user-profiles";
+        public MinioStorageService(IMinioClient minioClient) {
+            _minioClient = minioClient;
+        }
+
+        public async Task<string> UploadFileAsync(IFormFile file)
+        {
+            var fileName = $"{Guid.NewGuid()}{Path.GetExtension(file.FileName)}";
+            using var stream = file.OpenReadStream() ;
+
+            var putObjectArgs = new PutObjectArgs()
+                .WithBucket(_bucketName)
+                .WithObject(fileName)
+                .WithStreamData(stream)
+                .WithObjectSize(stream.Length)
+                .WithContentType(file.ContentType);
+
+            await _minioClient.PutObjectAsync(putObjectArgs);
+            return fileName;   
+        }
+
+        public async Task DeleteFileAsync(string fileName)
+        {
+            var removeObjectArgs = new RemoveObjectArgs()
+                .WithBucket(_bucketName)
+                .WithObject(fileName);
+
+            await _minioClient.RemoveObjectAsync(removeObjectArgs);
+        }
+
+        public async Task<Stream> GetFileStreamAsync(string fileName, string bucketName)
+        {
+            var memoryStream = new MemoryStream();
+            var args = new GetObjectArgs()
+                .WithBucket(bucketName)
+                .WithObject(fileName)
+                .WithCallbackStream(async (stream) =>
+                {
+                    await stream.CopyToAsync(memoryStream);
+                });
+
+            await _minioClient.GetObjectAsync(args);
+            memoryStream.Position = 0;
+            return memoryStream;
+        }
+    }
+}
