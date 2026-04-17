d596e26daaba284048aa171cacfce2ac8f443fc2
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-17 23:21:48 +0300

    FEAT: Added storageservice and current user definitions and added endpoints for image upload and get ops

--- backend/AuthModule/src/Controllers/AuthController.cs
+++ backend/AuthModule/src/Controllers/AuthController.cs
@@ -1,8 +1,13 @@
 using AuthModule.Data;
 using AuthModule.DTO;
+using AuthModule.Interfaces;
 using AuthModule.Services;
+using MediatR;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components.Forms;
 using Microsoft.AspNetCore.Mvc;
+using ProjectMicro.Shared.Interfaces;
+using ProjectMicro.Shared.Services;
 
 
 namespace AuthModule.Controllers
@@ -16,13 +21,22 @@ namespace AuthModule.Controllers
 
         private readonly AuthDBContext _context;
         private readonly AuthService _authService;
+        private readonly IStorageService _storageService;
+        private readonly IMediator _mediator;
+        private readonly ICurrentUserService _currentUserService;
 
 
-
-        public AuthController(AuthDBContext context, AuthService authService)
+        public AuthController(AuthDBContext context, 
+            AuthService authService, 
+            IStorageService storageService ,
+            IMediator mediator, 
+            ICurrentUserService currentUserService)
         {
             _context = context;
             _authService = authService;
+            _storageService = storageService;
+            _mediator = mediator;
+            _currentUserService = currentUserService;
         }
 
 
@@ -55,8 +69,29 @@ namespace AuthModule.Controllers
             var newAccessToken = await _authService.RefreshToken();
             return Ok(newAccessToken);
         }
-       
 
+
+        [HttpPost("upload-avatar")]
+        public async Task<IActionResult> UploadAvatar(IFormFile file)
+        {
+            int currentUser = _currentUserService.User.UserId;
+            if (file == null || file.Length == 0) return BadRequest("Dosya boþ.");
+            var savedFileName = await _storageService.UploadFileAsync(file);
+
+            var result = await _mediator.Send(new UpdateProfilePictureCommand(currentUser, savedFileName));
+            return Ok(new { FileName = savedFileName });
+        }
+
+        [HttpGet("profile-picture/{fileName}")]
+        public async Task<IActionResult> GetProfilePicture(string fileName)
+        {
+            try
+            {
+                var stream = await _storageService.GetFileStreamAsync(fileName, "user-profiles");
+                return File(stream, "image/jpeg");
+            }
+            catch { return NotFound(); }
+        }
 
     }
 }
