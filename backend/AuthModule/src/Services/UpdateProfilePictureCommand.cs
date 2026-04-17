18e48443cbf34203459f8866d2b382c507f62521
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-17 23:23:10 +0300

    FEAT: Implemented user profile picture uploader mediator configed handler command

---
+++ backend/AuthModule/src/Services/UpdateProfilePictureCommand.cs
@@ -0,0 +1,41 @@
+﻿using AuthModule.Data;
+using AuthModule.Interfaces;
+using MediatR;
+
+namespace AuthModule.Services
+{
+    public record UpdateProfilePictureCommand(int UserId,string fileName) : IRequest<Unit>;
+
+    public class UpdateUserProfilePictureCommandHandler : IRequestHandler<UpdateProfilePictureCommand, Unit>
+    {
+        private AuthDBContext _dbContext;
+        private readonly IStorageService _storageService;
+        public UpdateUserProfilePictureCommandHandler(AuthDBContext dbContext, IStorageService storageService)
+        {
+            _dbContext = dbContext;
+            _storageService = storageService;
+        }
+
+        public async Task<Unit> Handle(UpdateProfilePictureCommand request, CancellationToken cancellationToken)
+        {
+            var user = await _dbContext.Users.FindAsync(request.UserId);
+            if (user == null)
+                throw new Exception("User doesn't exist");
+
+            if (!string.IsNullOrEmpty(user.ProfilePicture))
+            {
+                try
+                {
+                    await _storageService.DeleteFileAsync(user.ProfilePicture);
+                }
+                catch (Exception ex)
+                {
+                }
+            }
+            user.ProfilePicture = request.fileName;
+
+            await _dbContext.SaveChangesAsync();
+            return Unit.Value;
+        }
+    }
+}
