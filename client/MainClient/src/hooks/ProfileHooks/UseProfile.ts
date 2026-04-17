0a68bd9a928b7da6d6da6cf3b7c055128534b7db
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-17 23:24:42 +0300

    FEAT: Added changeProfilePicture and GetProfilePicture hooks

--- client/MainClient/src/hooks/ProfileHooks/UseProfile.ts
+++ client/MainClient/src/hooks/ProfileHooks/UseProfile.ts
@@ -1,6 +1,8 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { GetEmailPreferenceService } from "../../services/ProfileServices/GetEmailPreferenceService";
 import { ChangeEmailPreferenceService } from "../../services/ProfileServices/ChangeEmailPreferenceService";
+import { changeProfilePictureService } from "../../services/ProfileServices/ChangeProfilePictureService";
+import { getProfilePictureService } from "../../services/ProfileServices/GetProfilePictureService";
 
 export const useEmailPreference = () => {
   return useQuery({
@@ -20,5 +22,29 @@ export const useChangeEmailPreference = () => {
     onError: (error: any) => {
       console.log("Error", error);
     },
+  });
+};
+
+export const useChangeProfilePicture = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: (formData: FormData) => changeProfilePictureService(formData),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["profilePicture"] });
+    },
+    onError: (err) => console.log("Hata:", err),
+  });
+};
+
+export const useProfilePicture = (fileName: string | null) => {
+  return useQuery({
+    queryKey: ["profilePicture", fileName],
+    queryFn: async () => {
+      if (!fileName) return null;
+      const blob = await getProfilePictureService(fileName);
+      return URL.createObjectURL(blob);
+    },
+    enabled: !!fileName,
   });
 };
