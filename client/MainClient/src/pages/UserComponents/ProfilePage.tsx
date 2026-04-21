6963ddf567e951afcafe4b88990764febf1dd262
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-21 17:24:30 +0300

    RENAMED

--- client/MainClient/src/pages/UserComponents/ProfilePage.tsx
+++ client/MainClient/src/pages/UserComponents/ProfilePage.tsx
@@ -1,17 +1,20 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import MyDayOffs from '../../components/ProfileComponents/MyDayOffs/MyDayOffs'
 import Settings from '../../components/ProfileComponents/Settings/Settings';
 import { useAuthContext } from '../../context/AuthContext'
 import { useChangeProfilePicture, useProfilePicture } from '../../hooks/ProfileHooks/UseProfile';
+import UpdatePasswordPopUp from '../../components/UpdatePasswordPopUp/UpdatePasswordPopUp';
 
 function ProfilePage() {
+    const [isChange, setIsChange] = useState<boolean>(false);
     const { userInfo } = useAuthContext();
     const { data: profileImgUrl, isLoading } = useProfilePicture(userInfo.profilePicture);
+    const { mutate: changeImage, isPending: isPendingPicture } = useChangeProfilePicture();
     const fileInputRef = useRef<HTMLInputElement>(null);
-    const { mutate: changeImage, isPending } = useChangeProfilePicture();
 
     const handleImageClick = () => fileInputRef.current?.click();
-    console.log("Profil Resmi Adı:", userInfo.profilePicture);
+    const handleChangePassword = () => setIsChange(prev => !prev);
+
     const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const file = event.target.files?.[0];
         if (!file) return;
@@ -28,7 +31,7 @@ function ProfilePage() {
                 <div className='col-span-1 flex justify-center items-center'>
                     <div
                         onClick={handleImageClick}
-                        className={`bg-white rounded-full shadow-custom flex justify-center items-center border border-gray-200 size-20 cursor-pointer overflow-hidden relative hover:border-sky-400 transition-all ${isPending ? 'opacity-50 pointer-events-none' : ''}`}
+                        className={`bg-white rounded-full shadow-custom flex justify-center items-center border border-gray-200 size-20 cursor-pointer overflow-hidden relative hover:border-sky-400 transition-all ${isPendingPicture ? 'opacity-50 pointer-events-none' : ''}`}
                     >
                         <div className={`...`}>
                             {isLoading ? (
@@ -41,7 +44,7 @@ function ProfilePage() {
                                 />
                             )}
                         </div>
-                        {isPending && (
+                        {isPendingPicture && (
                             <div className="absolute inset-0 flex items-center justify-center bg-white/50">
                                 <div className="size-4 border-2 border-sky-500 border-t-transparent animate-spin rounded-full"></div>
                             </div>
@@ -63,7 +66,7 @@ function ProfilePage() {
                 </div>
 
                 <div className='col-span-1 bg-white rounded-lg shadow-custom border border-gray-200 hover:scale-[105%] hover:border-sky-400 transition-all '>
-                    <button className='text-gray-600 text-lg size-full cursor-pointer'>Change Password</button>
+                    <button type='button' onClick={handleChangePassword} className='text-gray-600 text-lg size-full cursor-pointer'>Change Password</button>
                 </div>
             </div>
 
@@ -75,6 +78,10 @@ function ProfilePage() {
                     <MyDayOffs />
                 </div>
             </div>
+
+            {
+                isChange && <UpdatePasswordPopUp handleChange={handleChangePassword} />
+            }
         </div>
     );
 }
