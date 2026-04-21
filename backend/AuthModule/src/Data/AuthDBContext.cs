1f78b2e86031ff8402d11bec5ec9be3c71a2b67a
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-21 16:07:05 +0300

    FEAT: Added password reset tokens table and set configs

--- backend/AuthModule/src/Data/AuthDBContext.cs
+++ backend/AuthModule/src/Data/AuthDBContext.cs
@@ -14,6 +14,7 @@ namespace AuthModule.Data
         public DbSet<Department> Departments { get; set; }
         public DbSet<Module> Modules { get; set; }
         public DbSet<Tenant> Tenants { get; set; }
+        public DbSet<PasswordToken> PasswordTokens { get; set; }
 
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -23,7 +24,29 @@ namespace AuthModule.Data
             modelBuilder.Entity<User>()
                 .HasOne(u => u.RefreshToken)
                 .WithOne(rt => rt.User)
-                .HasForeignKey<RefreshToken>(rt => rt.UserId);
+                .HasForeignKey<RefreshToken>(rt => rt.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<User>()
+                .HasOne(u => u.PasswordToken)
+                .WithOne(rt => rt.User)
+                .HasForeignKey<PasswordToken>(pt => pt.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.HasIndex(u => u.Email).IsUnique();
+                entity.Property(u => u.Email).IsRequired().HasMaxLength(150);
+            });
+
+
+
+            modelBuilder.Entity<PasswordToken>()
+                .HasOne(pt => pt.User)
+                .WithOne(u => u.PasswordToken);
+
+            modelBuilder.Entity<PasswordToken>()
+                .HasIndex(pt => pt.ResetToken);
 
         }
 
