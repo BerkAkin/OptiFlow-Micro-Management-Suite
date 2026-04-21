51ee8d7bc49b47e7fdae16cfa0107f7db5449545
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-21 17:22:21 +0300

    FIX: Userpp is now setting to claim with check of existence

--- backend/AuthModule/src/Services/TokenCreateService.cs
+++ backend/AuthModule/src/Services/TokenCreateService.cs
@@ -31,7 +31,7 @@ namespace AuthModule.Services
                 new Claim("tenantId", user.TenantId.ToString()),
                 new Claim("department",user.Department.Name.ToString()),
                 new Claim("departmentId",user.DepartmentId.ToString()),
-                new Claim("profilePicture",user.ProfilePicture.ToString()),
+                new Claim("profilePicture",user.ProfilePicture ?? ""),
             };
 
             foreach (var module in user.Tenant.TenantModules.Select(m => m.Module.Name))
