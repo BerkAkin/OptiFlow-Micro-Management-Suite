829e999476084f71f8402be64f37d9fe1e4b3205
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-21 16:07:12 +0300

    FEAT: Password reset dto defined

---
+++ backend/AuthModule/src/DTO/PasswordResetDto.cs
@@ -0,0 +1,10 @@
+﻿namespace AuthModule.DTO
+{
+    public class PasswordResetDto
+    {
+        public string Email { get; set; }
+        public string ResetToken { get; set; }
+        public string Password { get; set; }
+        public string PasswordAgain { get; set; }
+    }
+}
