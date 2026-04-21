abe2b5cdf099c894e52ead1bc86a1fc27eefb504
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-21 16:07:28 +0300

    FEAT: Password update dto defined

---
+++ backend/AuthModule/src/DTO/PasswordUpdateDto.cs
@@ -0,0 +1,9 @@
+﻿namespace AuthModule.DTO
+{
+    public class PasswordUpdateDto
+    {
+        public string OldPassword { get; set; }
+        public string Password { get; set; }
+        public string PasswordAgain { get; set; }
+    }
+}
