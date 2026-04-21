b01db6d7a0b9eca4f403223ec8526b1d221d1d1f
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-21 16:10:30 +0300

    FEAT: Added password reseter and password reset requester hooks

--- client/MainClient/src/hooks/AuthHooks/useAuth.ts
+++ client/MainClient/src/hooks/AuthHooks/useAuth.ts
@@ -1,8 +1,34 @@
 import { useMutation } from "@tanstack/react-query";
 import { loginService } from "../../services/AuthServices/LoginService";
+import { resetPasswordService } from "../../services/AuthServices/ResetPasswordService";
+import { resetPasswordRequestService } from "../../services/AuthServices/ResetPasswordRequestService";
 
 export const useLogin = () => {
   return useMutation({
     mutationFn: loginService,
+  });
+};
+
+export const useResetPassword = () => {
+  return useMutation({
+    mutationFn: (data: any) => resetPasswordService(data),
+    onError: (error: any) => {
+      console.log(error);
+    },
+    onSuccess: () => {
+      console.log("Password Has Been Refreshed Succesfully");
+    },
+  });
+};
+
+export const useResetPasswordRequest = () => {
+  return useMutation({
+    mutationFn: (data: any) => resetPasswordRequestService(data),
+    onError: (error: any) => {
+      console.log(error);
+    },
+    onSuccess: () => {
+      console.log("Password Reset Request Mail Has Been Sent");
+    },
   });
 };
