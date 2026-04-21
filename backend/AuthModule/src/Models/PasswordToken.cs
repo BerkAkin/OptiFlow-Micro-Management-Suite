f7eaf1cc1b1a4696c9d20610993fd0e0ed77d8f6
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-21 16:07:56 +0300

    FEAT: Password token entity model defined

---
+++ backend/AuthModule/src/Models/PasswordToken.cs
@@ -0,0 +1,11 @@
+﻿namespace AuthModule.Models
+{
+    public class PasswordToken
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public string? ResetToken { get; set; }
+        public DateTime? ExpireDate { get; set; }
+    }
+}
