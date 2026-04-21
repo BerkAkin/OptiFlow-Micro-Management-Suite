e0684123b7486583436fb273c68598752c2f27d0
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-21 16:09:02 +0300

    FEAT: Password resetor command implemented

---
+++ backend/AuthModule/src/Services/PasswordResetCommand.cs
@@ -0,0 +1,50 @@
+﻿using AuthModule.Data;
+using AuthModule.DTO;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthModule.Services
+{
+    public record PasswordResetCommand(PasswordResetDto dto) : IRequest<Unit>;
+
+    public class PasswordResetCommandHandler : IRequestHandler<PasswordResetCommand, Unit>
+    {
+        private readonly AuthDBContext _context;
+        public PasswordResetCommandHandler(AuthDBContext context)
+        {
+            _context = context;
+        }
+        public async Task<Unit> Handle(PasswordResetCommand query, CancellationToken cancellationToken)
+        {
+
+            var user = await _context.Users
+               .Include(u => u.PasswordToken)
+               .SingleOrDefaultAsync(u => u.Email == query.dto.Email);
+
+            if (user == null || user.PasswordToken == null || user.PasswordToken.ResetToken != query.dto.ResetToken)
+            {
+                throw new Exception("Invalid token or email.");
+            }
+
+            if (query.dto.Password != query.dto.PasswordAgain)
+            {
+                throw new Exception("Confirmation password and new password does not match");
+            }
+
+            if (!user.PasswordToken.ExpireDate.HasValue || user.PasswordToken.ExpireDate < DateTime.UtcNow)
+            {
+                throw new Exception("Token has expired or is invalid.");
+            }
+
+            user.PasswordHash = BCrypt.Net.BCrypt.HashPassword(query.dto.Password);
+            user.DateUpdate = DateTime.UtcNow;
+
+            user.PasswordToken.ResetToken = null;
+            user.PasswordToken.ExpireDate = null;
+
+            await _context.SaveChangesAsync();
+
+            return Unit.Value;
+        }
+    }
+}
