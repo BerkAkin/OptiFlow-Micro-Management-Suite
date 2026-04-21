202c3a825ad1503e3e1b463c7e86d8dbd2b7864b
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-21 17:23:40 +0300

    FEAT: change password hook implemented

--- client/MainClient/src/hooks/AuthHooks/useAuth.ts
+++ client/MainClient/src/hooks/AuthHooks/useAuth.ts
@@ -2,6 +2,7 @@ import { useMutation } from "@tanstack/react-query";
 import { loginService } from "../../services/AuthServices/LoginService";
 import { resetPasswordService } from "../../services/AuthServices/ResetPasswordService";
 import { resetPasswordRequestService } from "../../services/AuthServices/ResetPasswordRequestService";
+import { changePasswordService } from "../../services/AuthServices/ChangePasswordService";
 
 export const useLogin = () => {
   return useMutation({
@@ -29,6 +30,18 @@ export const useResetPasswordRequest = () => {
     },
     onSuccess: () => {
       console.log("Password Reset Request Mail Has Been Sent");
+    },
+  });
+};
+
+export const useChangePassword = () => {
+  return useMutation({
+    mutationFn: (data: any) => changePasswordService(data),
+    onError: (error: any) => {
+      console.log("Backend Mesajı:", error.response?.data);
+    },
+    onSuccess: () => {
+      console.log("Password Changed Succesfully");
     },
   });
 };
