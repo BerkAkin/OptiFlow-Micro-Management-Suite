9a662b73fe05dbbb2f79f026cb4167e4638af09d
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-21 16:10:40 +0300

    FEAT: Added password reseter and password reset requester endpoint callers

--- client/MainClient/src/repositories/AuthRepositories/AuthRepository.ts
+++ client/MainClient/src/repositories/AuthRepositories/AuthRepository.ts
@@ -4,3 +4,14 @@ export const login = async (payload: any) => {
   const res = await api.post("/auth/login", payload);
   return res.data;
 };
+
+export const resetPassword = async (payload: any) => {
+  const res = await api.post("/auth/PasswordReset", payload);
+  return res.data;
+};
+
+export const resetPasswordRequest = async (payload: any) => {
+  console.log("latest", payload);
+  const res = await api.post("/auth/PasswordResetRequester", payload);
+  return res.data;
+};
