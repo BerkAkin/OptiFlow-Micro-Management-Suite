f406e50e8ea7c59f1b46795819814563b41fbda7
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-23 01:36:11 +0300

    STYLE: Refined

--- client/MainClient/src/components/ErrorMessage/ErrorMessage.tsx
+++ client/MainClient/src/components/ErrorMessage/ErrorMessage.tsx
@@ -3,22 +3,22 @@ import styles from './styles.module.css'
 
 function ErrorMessage() {
     return (
-        <>
-            <div className='h-full w-full'>
-                <div className='flex relative justify-center items-end h-[60%]'>
-                    <div className={`absolute h-24 w-1 rotate-[45deg] rounded-full  ${styles.line}`}></div>
-                    <div className={`absolute h-24 w-1 rotate-[-45deg] rounded-full  ${styles.line}`}></div>
-                </div>
-                <div className='h-[40%] flex justify-center'>
-                    <div className={`${styles.glowText}`}>Error</div>
-                </div>
-
-
+        <div className='h-full min-h-[300px] w-full flex flex-col justify-center items-center bg-transparent'>
+            <div className='relative flex justify-center items-center h-32 w-32 mb-8'>
+                <div className={`absolute h-20 w-1.5 rotate-[45deg] rounded-full ${styles.line}`}></div>
+                <div className={`absolute h-20 w-1.5 rotate-[-45deg] rounded-full ${styles.line}`}></div>
             </div>
 
-        </>
-
+            <div className='flex flex-col items-center gap-2'>
+                <h3 className={`${styles.glowText} text-2xl font-bold tracking-widest uppercase`}>
+                    Error
+                </h3>
+                <p className="text-gray-400 text-sm font-medium px-6 text-center max-w-xs">
+                    There was a problem loading the data. Please check your connection.
+                </p>
+            </div>
+        </div>
     )
 }
 
-export default ErrorMessage
+export default ErrorMessage;
