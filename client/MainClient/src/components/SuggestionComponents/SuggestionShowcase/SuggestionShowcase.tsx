ca4809e186da4fae874b4b40b426e8cebec598e1
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-23 01:37:13 +0300

    STYLE: Refined

--- client/MainClient/src/components/SuggestionComponents/SuggestionShowcase/SuggestionShowcase.tsx
+++ client/MainClient/src/components/SuggestionComponents/SuggestionShowcase/SuggestionShowcase.tsx
@@ -4,27 +4,30 @@ import Spinner from '../../Spinner/Spinner';
 import SuggestionShowcaseCard from '../SuggestionShowcaseCard/SuggestionShowcaseCard'
 
 function SuggestionShowcase() {
-
     const { data, isLoading, error } = useShowcase();
-    if (isLoading) return (<Spinner />)
-    if (error || !data) return (<ErrorMessage />)
+
+    if (isLoading) return <Spinner />
+    if (error || !data) return <ErrorMessage />
 
     return (
-        <div className='grid grid-cols-10 gap-6 '>
-            <div className='col-span-5'>
+        <div className='grid grid-cols-2 gap-6 w-full'>
+
+            <div className='w-full'>
                 <SuggestionShowcaseCard
                     isStar={true}
                     Description={data?.best?.description || "Best of this month has not been elected"}
                     VoteCount={data?.best?.votes || 0}
                 />
             </div>
-            <div className='col-span-5'>
+
+            <div className='w-full'>
                 <SuggestionShowcaseCard
                     isStar={false}
                     Description={data?.thisMonth?.description || "No suggestions yet"}
                     VoteCount={data?.thisMonth?.votes || 0}
                 />
             </div>
+
         </div>
     )
 }
