ea58b94f35daaa41ea540647799bd981454265ba
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-23 17:33:02 +0300

    STYLE: Minor changes on coloring and typography

--- client/MainClient/src/pages/SurveyAndInsightsPages/SurveyBuilder.tsx
+++ client/MainClient/src/pages/SurveyAndInsightsPages/SurveyBuilder.tsx
@@ -73,21 +73,21 @@ function SurveyBuilder() {
     return (
         <div className='container my-10 mx-auto border border-gray-200 shadow-custom rounded-xl overflow-hidden'>
             <div className='bg-white border-b border-gray-200 p-8 text-center'>
-                <h1 className='text-4xl font-bold text-gray-800 font-rubik tracking-tight'>Build New Survey</h1>
-                <p className='text-gray-400 mt-2 italic'>Start by giving a name to your survey...</p>
+                <h1 className='text-3xl font-bold text-gray-800 font-rubik tracking-tight'>Build New Survey</h1>
+                <p className='text-gray-400 mt-2'>Start by giving a name to your survey...</p>
             </div>
 
             <div className='p-8'>
-                <div className='flex gap-3 bg-white p-4 rounded-xl border border-gray-200 mb-8'>
+                <div className='flex gap-3 bg-white p-2 rounded-lg border border-gray-200 mb-8'>
                     <input
                         value={surveyName}
                         onChange={(e) => setSurveyName(e.target.value)}
-                        className='flex-1 ps-4 py-3 text-lg border-b-2 border-transparent transition-all outline-none text-gray-700'
+                        className='flex-1 ps-4 py-3 text-md border-b-2 border-transparent transition outline-none text-gray-700'
                         placeholder='Type survey title here...'
                     />
                     <button
                         onClick={() => handleSurveyName(surveyName)}
-                        className='bg-blue-600 hover:bg-blue-700 p-3 rounded-lg transition-all transform active:scale-95 cursor-pointer'
+                        className='bg-blue-600 hover:bg-blue-700 p-3 rounded-lg transition active:scale-95 cursor-pointer'
                     >
                         <img src={add} alt="Add" className="brightness-0 invert w-6 h-6" />
                     </button>
@@ -95,10 +95,10 @@ function SurveyBuilder() {
 
                 {survey.title && (
                     <div className="space-y-6">
-                        <div className='bg-white flex flex-col flex-row items-center justify-between gap-4 p-6 rounded-xl border border-gray-200'>
-                            <h2 className='text-2xl font-semibold text-slate-700 ps-4'>
+                        <div className='bg-white flex flex-col flex-row items-center justify-between gap-4 p-2 rounded-lg border border-gray-200'>
+                            <h3 className='text-xl font-semibold text-slate-700 ps-4'>
                                 {survey.title} Survey
-                            </h2>
+                            </h3>
                             <div className='flex flex-1 max-w-md gap-2'>
                                 <input
                                     value={question}
@@ -108,7 +108,7 @@ function SurveyBuilder() {
                                 />
                                 <button
                                     onClick={() => handleAddQuestion(question)}
-                                    className='bg-green-500 hover:bg-green-600 p-2 rounded-lg transition-all transform active:scale-95 cursor-pointer'
+                                    className='bg-green-500 hover:bg-green-600 p-2 rounded-lg transition active:scale-95 cursor-pointer'
                                 >
                                     <img src={add} alt="Add" className="brightness-0 invert w-6 h-6" />
                                 </button>
@@ -117,22 +117,25 @@ function SurveyBuilder() {
 
                         <div className='space-y-6'>
                             {survey.questions.map((question, qIndex) => (
-                                <div key={qIndex} className='bg-white rounded-xl p-6 border border-gray-200 transition-all'>
+                                <div key={qIndex} className='bg-white rounded-xl p-4 border border-gray-200 transition'>
                                     <div className='flex justify-between items-start mb-4'>
-                                        <h3 className='text-xl font-medium text-gray-800 flex items-center'>
-                                            <span className='text-slate-700  px-2 mr-3'>{qIndex + 1})</span>
-                                            {question.title}
-                                        </h3>
-                                        <button onClick={() => deleteQuestion(qIndex)} className='p-2 hover:bg-red-200 rounded-full transition-colors cursor-pointer'>
-                                            <img src={trash} alt="Delete" className="w-5 h-5 transition-opacity" />
+                                        <h2 className='text-lg font-medium text-gray-800 flex items-center'>
+                                            <span className='text-slate-700 px-2'>Question {qIndex + 1})</span>
+                                            <span className='text-slate-500'>{question.title}</span>
+                                        </h2>
+                                        <button onClick={() => deleteQuestion(qIndex)} className='p-2 hover:bg-red-200 rounded-full transition cursor-pointer'>
+                                            <img src={trash} alt="Delete" className="w-5 h-5 transition" />
                                         </button>
                                     </div>
 
                                     <div className='space-y-2 mb-6 ml-10'>
                                         {question.answers.map((answer, aIndex) => (
                                             <div key={aIndex} className='flex items-center border border-gray-200 rounded-lg px-4 py-2 w-fit min-w-[200px]'>
-                                                <p className="flex-1 text-gray-600">{answer.title}</p>
-                                                <button onClick={() => deleteAnswer(qIndex, aIndex)} className='ml-4 transition-opacity p-1 hover:bg-red-200 rounded-full cursor-pointer'>
+                                                <h2 className="text-sm flex-1 text-gray-600">
+                                                    Ans {aIndex + 1})
+                                                    <span className='text-slate-500'> {answer.title}</span>
+                                                </h2>
+                                                <button onClick={() => deleteAnswer(qIndex, aIndex)} className='ml-4 transition p-1 hover:bg-red-200 rounded-full cursor-pointer'>
                                                     <img src={trash} alt="Delete" className="w-4 h-4" />
                                                 </button>
                                             </div>
@@ -143,10 +146,10 @@ function SurveyBuilder() {
                                         <input
                                             value={answer}
                                             onChange={(e) => setAnswer(e.target.value)}
-                                            className='text-sm border-b border-gray-200 outline-none py-1 w-48 transition-colors'
+                                            className='text-sm border-b border-gray-200 outline-none py-1 w-48 transition'
                                             placeholder='Add an option...'
                                         />
-                                        <button onClick={() => handleAddAnswer(answer, qIndex)} className='hover:bg-green-100 rounded-full transition-colors cursor-pointer'>
+                                        <button onClick={() => handleAddAnswer(answer, qIndex)} className='hover:bg-green-100 rounded-full transition cursor-pointer'>
                                             <img src={add} alt="Add" className="w-5 h-5" />
                                         </button>
                                     </div>
