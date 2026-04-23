5020f366944b344b3f8f71380243b34b2e0cd94c
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-23 17:32:18 +0300

    STYLE: Minor changes on coloring and typography

--- client/MainClient/src/components/Spinner/Spinner.tsx
+++ client/MainClient/src/components/Spinner/Spinner.tsx
@@ -4,23 +4,21 @@ function Spinner() {
     return (
         <div className='h-full w-full p-10 flex flex-col justify-center items-center'>
             <div className="relative">
-                {/* Arka plan halkası (Opsiyonel: Daha derinlikli bir görünüm için) */}
                 <div className="absolute inset-0 rounded-full border-4 border-gray-100 size-24"></div>
 
-                {/* Hareketli halka */}
                 <div className={`
                     ${styles.loader} 
                     rounded-full 
                     border-4 
                     border-transparent 
-                    border-t-indigo-500 
-                    border-r-indigo-500/30 
+                    border-t-blue-500 
+                    border-r-blue-500/30 
                     size-24
                 `}></div>
             </div>
 
             <p className="mt-6 text-sm font-medium text-gray-400 tracking-widest uppercase animate-pulse">
-                Yükleniyor
+                Loading
             </p>
         </div>
     );
