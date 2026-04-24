6e13cd1b8d84befee6a997973f8fc5799ed652d3
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-24 20:32:37 +0300

    FEAT: Employe list dto defined

---
+++ backend/AuthModule/src/DTO/EmployeesDto.cs
@@ -0,0 +1,17 @@
+﻿using AuthModule.Enums;
+
+namespace AuthModule.DTO
+{
+    public class EmployeesDto
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Department { get; set; }
+        public string Address { get; set; }
+        public IsActiveEnum IsActive { get; set; }
+
+    }
+}
