4df6dc0298c463956fe0223e741cbd6dad318d3f
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-24 20:32:14 +0300

    FEAT: Employe adder dto defined

---
+++ backend/AuthModule/src/DTO/AddNewEmployeeDto.cs
@@ -0,0 +1,19 @@
+﻿namespace AuthModule.DTO
+{
+    public class AddNewEmployeeDto
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string PhoneNum { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Street { get; set; }
+        public string Street2 { get; set; }
+        public string ApartmentNum { get; set; }
+        public string DoorNumber { get; set; }
+        public string Province { get; set; }
+        public string District { get; set; }
+        public string FullAddress { get; set; }
+        public int DepartmentId { get; set; }
+    }
+}
