3abb26dd4b43934f774f175567c3fc5ceaa178f4
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-24 20:43:49 +0300

    STYLE: Added info bar on top of page

--- client/MainClient/src/pages/SuggestionPages/SuggestionsPage.tsx
+++ client/MainClient/src/pages/SuggestionPages/SuggestionsPage.tsx
@@ -6,16 +6,27 @@ import SuggestionShowcase from '../../components/SuggestionComponents/Suggestion
 
 function SuggestIdeas() {
     return (
-        <div className='container mx-auto my-10 grid grid-cols-10 flex justify-center gap-6'>
-            <div className='col-span-10'>
-                <SuggestionShowcase />
+        <div className='container mx-auto py-8 px-4'>
+            <div className='flex justify-between mb-6 mx-2 border-b border-gray-200 pb-2'>
+                <div>
+                    <h6 className='text-2xl font-bold text-slate-800 tracking-tight font-rubik'>
+                        Tider
+                        <span className='text-blue-600'> Feedback</span>
+                        <span className='text-slate-400 text-sm ms-3 font-normal'>Share your thoughts to shape the flow</span>
+                    </h6>
+                </div>
             </div>
-            <div className='col-span-8 '>
-                <SuggestionList />
-            </div>
-            <div className='col-span-2'>
-                <MySuggestionList />
-            </div>
+            <div className='grid grid-cols-10 flex justify-center gap-6'>
+                <div className='col-span-10'>
+                    <SuggestionShowcase />
+                </div>
+                <div className='col-span-8 '>
+                    <SuggestionList />
+                </div>
+                <div className='col-span-2'>
+                    <MySuggestionList />
+                </div>
+            </div >
         </div >
     )
 }
