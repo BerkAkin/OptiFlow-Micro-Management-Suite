8041ba86f4c14377a2112315f8c9bebbeadabd51
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-24 20:32:26 +0300

    FEAT: Employe details getter dto defined

---
+++ backend/AuthModule/src/DTO/EmployeeDetailsDto.cs
@@ -0,0 +1,11 @@
+﻿using AuthModule.Enums;
+
+namespace AuthModule.DTO
+{
+    public class EmployeeDetailsDto
+    {
+        public string Email { get; set; }
+        public int Department { get; set; }
+        public IsActiveEnum IsActive { get; set; }
+    }
+}
