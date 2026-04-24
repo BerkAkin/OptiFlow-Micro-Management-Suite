82c463c446fb24fb0d1978e9531b663e549c0e0b
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-24 20:33:06 +0300

    FEAT: Updater for employees dto defined

---
+++ backend/AuthModule/src/DTO/UpdateEmployeeDetailsDto.cs
@@ -0,0 +1,9 @@
+﻿namespace AuthModule.DTO
+{
+    public class UpdateEmployeeDetailsDto
+    {
+        public string Department { get; set; }
+        public string IsActive { get; set; }
+        public string Email { get; set; }
+    }
+}
