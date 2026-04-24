8dd4104922b5a93f87f76ae321d33dbf4f5e8606
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-24 20:34:17 +0300

    FEAT: Getter of new employee details query implemented

---
+++ backend/AuthModule/src/Services/GetEmployeeDetailsQuery.cs
@@ -0,0 +1,32 @@
+﻿using AuthModule.Data;
+using AuthModule.DTO;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthModule.Services
+{
+    public record GetEmployeeDetailsQuery(string email,int currentTenant) : IRequest<EmployeeDetailsDto>;
+    public class GetEmployeeDetailsQueryHandler : IRequestHandler<GetEmployeeDetailsQuery,EmployeeDetailsDto>
+    {
+        private readonly AuthDBContext _context;
+        public GetEmployeeDetailsQueryHandler(AuthDBContext context)
+        {
+            _context = context;
+        }
+        
+        public async Task<EmployeeDetailsDto> Handle(GetEmployeeDetailsQuery query, CancellationToken cancellationToken)
+        {
+            var data = await _context.Users
+               .AsNoTracking()
+               .Where(u => u.Email == query.email && u.TenantId == query.currentTenant)
+               .Select(x => new EmployeeDetailsDto
+               {
+                   Email=x.Email,
+                   Department = x.DepartmentId,
+                   IsActive= x.IsActive,
+               }).FirstOrDefaultAsync(cancellationToken);
+
+            return data;
+        }
+    }
+}
