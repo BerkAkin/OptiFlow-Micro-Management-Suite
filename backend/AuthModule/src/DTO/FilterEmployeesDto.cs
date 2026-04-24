7d203974608b00bf2aacb73283c412bfcddc1225
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-24 20:32:46 +0300

    FEAT: Filter for employees list dto defined

---
+++ backend/AuthModule/src/DTO/FilterEmployeesDto.cs
@@ -0,0 +1,9 @@
+﻿namespace AuthModule.DTO
+{
+    public class FilterEmployeesDto
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public int Page { get; set; }
+    }
+}
