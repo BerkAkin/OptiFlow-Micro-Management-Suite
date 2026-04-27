df0983760eea09b054ca1599858843044edd2cd0
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-27 13:12:56 +0300

    REFACTOR: Command is now simpilfied and applying DDD, CQRS, Mediator and Clean Arch principles

---
+++ backend/AuthModule/src/Application/Commands/ResetPasswordCommand/PasswordResetCommand.cs
@@ -0,0 +1,46 @@
+﻿using AuthModule.Application.DTOs;
+using AuthModule.Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthModule.Application.Commands.ResetPasswordCommand
+{
+    public record PasswordResetCommand(PasswordResetDto dto) : IRequest<Unit>;
+
+    public class PasswordResetCommandHandler : IRequestHandler<PasswordResetCommand, Unit>
+    {
+        private readonly AuthDBContext _context;
+        public PasswordResetCommandHandler(AuthDBContext context)
+        {
+            _context = context;
+        }
+        public async Task<Unit> Handle(PasswordResetCommand query, CancellationToken cancellationToken)
+        {
+            var user = await _context.Users
+               .Include(u => u.PasswordToken)
+               .SingleOrDefaultAsync(u => u.Email == query.dto.Email, cancellationToken);
+
+            if (user == null || user.PasswordToken == null || user.PasswordToken.ResetToken != query.dto.ResetToken)
+            {
+                throw new Exception("Geçersiz e-posta veya kod.");
+            }
+
+            if (query.dto.Password != query.dto.PasswordAgain)
+            {
+                throw new Exception("Yeni şifreler birbiriyle uyuşmuyor.");
+            }
+
+            if (!user.PasswordToken.Expires.HasValue || user.PasswordToken.Expires < DateTime.UtcNow)
+            {
+                throw new Exception("Şifre sıfırlama kodunun süresi dolmuş.");
+            }
+
+            string hashedPassword = BCrypt.Net.BCrypt.HashPassword(query.dto.Password);
+            user.ResetPassword(hashedPassword);
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
