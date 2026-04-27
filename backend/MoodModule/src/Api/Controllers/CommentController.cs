a453b3aeb70b65d49d42697e764cadedd75ce426
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-27 13:51:29 +0300

    FEAT: Seperated from main controller and simplified endpoints

---
+++ backend/MoodModule/src/Api/Controllers/CommentController.cs
@@ -0,0 +1,69 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using MoodModule.Application.Commands.AddCommentCommand;
+using MoodModule.Application.Commands.DeleteCommentCommand;
+using MoodModule.Application.DTOs;
+using MoodModule.Application.Queries.GetAllCommentsQuery;
+using MoodModule.Application.Queries.GetCommentsQuery;
+using MoodModule.Application.Queries.GetUsersQuery;
+using ProjectMicro.Shared.Interfaces;
+
+namespace MoodModule.Api.Controllers
+{
+    [Route("api/comments")]
+    [ApiController]
+    public class CommentController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        private readonly ICurrentUserService _currentUserService;
+
+        public CommentController(IMediator mediator, ICurrentUserService currentUserService)
+        {
+            _mediator = mediator;
+            _currentUserService = currentUserService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] AddCommentDto comment)
+        {
+            int currentTenant = _currentUserService.User.TenantId;
+            var data = await _mediator.Send(new AddCommentCommand(comment, currentTenant));
+            return Ok("Comment saved successfully");
+        }
+
+        [HttpGet("me")]
+        public async Task<IActionResult> GetMine()
+        {
+            int currentUser = _currentUserService.User.UserId;
+            var data = await _mediator.Send(new GetCommentsQuery(currentUser));
+            return Ok(data);
+
+        }
+       
+        [HttpGet("employees")]
+        public async Task<IActionResult> GetEmployees()
+        {
+            int currentTenant = _currentUserService.User.TenantId;
+            var data = await _mediator.Send(new GetUsersQuery(currentTenant));
+            return Ok(data);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll([FromQuery] int UserId)
+        {
+            int currentTenant = _currentUserService.User.TenantId;
+            var data = await _mediator.Send(new GetAllCommentsQuery(UserId));
+            return Ok(data);
+        }
+
+        [HttpDelete("{commentId}")]
+        public async Task<IActionResult> Delete(int commentId, [FromQuery] int userId)
+        {
+            int currentTenant = _currentUserService.User.TenantId;
+            var data = await _mediator.Send(new DeleteCommentCommand(commentId, userId, currentTenant));
+            return Ok("Comment deleted successfully");
+        }
+        
+
+    }
+}
