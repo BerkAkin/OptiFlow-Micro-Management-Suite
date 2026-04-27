c13a4c30686b10f792c39ed091502f96525d3752
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-27 13:50:42 +0300

    REFACTOR: Refined according to new entity model relations

---
+++ backend/AuthModule/src/Infrastructure/Seeders/DbSeeder.cs
@@ -0,0 +1,37 @@
+﻿using AuthModule.Domain.Entities;
+using AuthModule.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+public static class DbSeeder
+{
+    public static async Task Seed(AuthDBContext context)
+    {
+        if (!await context.Modules.AnyAsync())
+        {
+            var modules = new List<Module>
+            {
+                new Module("Finance"),
+                new Module("Surveys"),
+                new Module("Suggestions"),
+                new Module("Support"),
+                new Module("Mood")
+            };
+            await context.Modules.AddRangeAsync(modules);
+        }
+
+        if (!await context.Departments.AnyAsync())
+        {
+
+            var departments = new List<Department>
+            {
+                new Department("Standart Employee"),
+                new Department("Finance Accountant"), 
+                new Department("Human Resources"),    
+                new Department("Company Manager")     
+            };
+            await context.Departments.AddRangeAsync(departments);
+        }
+
+        await context.SaveChangesAsync();
+    }
+}
