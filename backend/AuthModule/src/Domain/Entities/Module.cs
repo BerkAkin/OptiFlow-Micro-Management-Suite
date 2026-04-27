5a98cf9031d66a2a7d968856ca8621ed34ec8a2d
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-27 13:48:12 +0300

    REFACTOR: Now applies DDD principles

---
+++ backend/AuthModule/src/Domain/Entities/Module.cs
@@ -0,0 +1,19 @@
+﻿namespace AuthModule.Domain.Entities
+{
+    public class Module
+    {
+        public int Id { get;  private set; }
+        public string Name { get;  private set; }
+        public ICollection<TenantModule> TenantModules { get; private set; }
+
+        private Module() { }
+
+        public Module(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentException("Module name cannot be null.");
+
+            Name = name;
+        }
+    }
+}
