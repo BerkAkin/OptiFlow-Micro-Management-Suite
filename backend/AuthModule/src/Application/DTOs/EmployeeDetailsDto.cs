ce3b11c0eddb75fefeb737b66d164e6ef904ff4a
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-27 13:45:00 +0300

    REFACTOR: Relocated

---
+++ backend/AuthModule/src/Application/DTOs/EmployeeDetailsDto.cs
@@ -0,0 +1,12 @@
+﻿using AuthModule.Domain.Enums;
+
+namespace AuthModule.Application.DTOs
+{
+    public class EmployeeDetailsDto
+    {
+        public string Email { get; set; }
+        public string Department { get; set; }
+        public int DepartmentId { get; set; }
+        public IsActiveEnum IsActive { get; set; }
+    }
+}
