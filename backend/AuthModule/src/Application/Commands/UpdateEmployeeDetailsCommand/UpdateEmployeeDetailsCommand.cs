0b0de68f9abf80e77c28c3066d495d9282af6d8b
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-27 13:12:58 +0300

    REFACTOR: Command is now simpilfied and applying DDD, CQRS, Mediator and Clean Arch principles

---
+++ backend/AuthModule/src/Application/Commands/UpdateEmployeeDetailsCommand/UpdateEmployeeDetailsCommand.cs
@@ -0,0 +1,41 @@
+﻿using AuthModule.Application.DTOs;
+using AuthModule.Domain.Enums;
+using AuthModule.Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthModule.Application.Commands.UpdateEmployeeDetailsCommand
+{
+        public record UpdateEmployeeCommand(UpdateEmployeeDetailsDto dto) : IRequest<Unit>;
+        public class UpdateEmployeeCommandHandler : IRequestHandler<UpdateEmployeeCommand, Unit>
+        {
+            private readonly AuthDBContext _context;
+            public UpdateEmployeeCommandHandler(AuthDBContext context)
+            {
+                _context = context;
+            }
+
+        public async Task<Unit> Handle(UpdateEmployeeCommand command, CancellationToken cancellationToken)
+        {
+            var user = await _context.Users
+                .FirstOrDefaultAsync(x => x.Email == command.dto.Email, cancellationToken);
+
+            if (user is null)
+                throw new Exception("Kullanıcı bulunamadı.");
+
+            user.UpdateDepartment(command.dto.DepartmentId);
+           
+
+            if (Enum.TryParse<IsActiveEnum>(command.dto.IsActive, true, out var isActiveResult))
+            {
+                user.UpdateStatus(isActiveResult);
+            }
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+    
+
+}
