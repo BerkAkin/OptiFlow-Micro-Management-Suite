0956bc20310413c31117a4171f68c53c5d112ed6
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-27 13:12:45 +0300

    REFACTOR: Command is now simpilfied and applying DDD, CQRS, Mediator and Clean Arch principles

---
+++ backend/AuthModule/src/Application/Commands/ChangeProfilePictureCommand/UpdateProfilePictureCommand.cs
@@ -0,0 +1,41 @@
+﻿using AuthModule.Application.Interfaces;
+using AuthModule.Infrastructure.Persistence;
+using MediatR;
+
+namespace AuthModule.Application.Commands.ChangeProfilePictureCommand
+{
+    public record UpdateProfilePictureCommand(int UserId,string fileName) : IRequest<Unit>;
+
+    public class UpdateUserProfilePictureCommandHandler : IRequestHandler<UpdateProfilePictureCommand, Unit>
+    {
+        private AuthDBContext _dbContext;
+        private readonly IStorageService _storageService;
+        public UpdateUserProfilePictureCommandHandler(AuthDBContext dbContext, IStorageService storageService)
+        {
+            _dbContext = dbContext;
+            _storageService = storageService;
+        }
+
+        public async Task<Unit> Handle(UpdateProfilePictureCommand request, CancellationToken cancellationToken)
+        {
+            var user = await _dbContext.Users.FindAsync(request.UserId);
+            if (user == null)
+                throw new Exception("User doesn't exist");
+
+            if (!string.IsNullOrEmpty(user.ProfilePicture))
+            {
+                try
+                {
+                    await _storageService.DeleteFileAsync(user.ProfilePicture);
+                }
+                catch (Exception ex)
+                {
+                }
+            }
+            user.UpdateProfilePicture(request.fileName);
+
+            await _dbContext.SaveChangesAsync();
+            return Unit.Value;
+        }
+    }
+}
