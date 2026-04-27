a44731ae5fe37cbbc33327b2ab7b93a25eb69b30
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-27 13:12:53 +0300

    REFACTOR: Command is now simpilfied and applying DDD, CQRS, Mediator and Clean Arch principles

---
+++ backend/AuthModule/src/Application/Commands/RequestResetPasswordCommand/PasswordRequestResetCommand.cs
@@ -0,0 +1,35 @@
+﻿using AuthModule.Application.DTOs;
+using AuthModule.Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthModule.Application.Commands.RequestResetPasswordCommand
+{
+    public record PasswordRequestResetCommand(PasswordResetRequestDto passwordResetRequest):IRequest<Unit>;
+
+    public class PasswordRequestResetCommandHandler : IRequestHandler<PasswordRequestResetCommand, Unit>
+    {
+        private readonly AuthDBContext _context;
+        public PasswordRequestResetCommandHandler(AuthDBContext context)
+        {
+            _context = context;
+        }
+        public async Task<Unit> Handle(PasswordRequestResetCommand query, CancellationToken cancellationToken)
+        {
+            var user = await _context.Users
+                .Include(u => u.PasswordToken)
+                .SingleOrDefaultAsync(u => u.Email == query.passwordResetRequest.Email);
+
+            if (user is null)
+                throw new Exception("Bu e-posta adresine kayıtlı kullanıcı bulunamadı.");
+
+            string resetToken = Guid.NewGuid().ToString();
+
+            user.AddPasswordToken(resetToken);
+
+            await _context.SaveChangesAsync();
+
+            return Unit.Value;
+        }
+    }
+}
