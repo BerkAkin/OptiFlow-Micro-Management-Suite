1ea2ac1e1991667e0a6d65a30f33722d7c680586
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-27 13:48:09 +0300

    REFACTOR: Now applies DDD principles

---
+++ backend/AuthModule/src/Domain/Entities/Department.cs
@@ -0,0 +1,20 @@
+﻿
+namespace AuthModule.Domain.Entities
+{
+    public class Department
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        private Department(){}
+
+        public Department(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentException("Departmant name cannot be null");
+            Name = name;
+        }
+
+
+        
+    }
+}
