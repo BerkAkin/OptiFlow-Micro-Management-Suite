e3fafb83d2ecb5e0f51f49f79070c9c26d62fad8
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-27 13:45:16 +0300

    REFACTOR: Relocated

---
+++ backend/AuthModule/src/Application/DTOs/PasswordUpdateDto.cs
@@ -0,0 +1,9 @@
+﻿namespace AuthModule.Application.DTOs
+{
+    public class PasswordUpdateDto
+    {
+        public string CurrentPassword { get; set; }
+        public string NewPassword { get; set; }
+        public string NewPasswordAgain { get; set; }
+    }
+}
