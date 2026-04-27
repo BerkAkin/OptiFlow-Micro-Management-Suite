549dfa66ecb04a49351df5847a78b9a5b41b6c61
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-27 13:45:05 +0300

    REFACTOR: Relocated

---
+++ backend/AuthModule/src/Application/DTOs/FilterEmployeesDto.cs
@@ -0,0 +1,9 @@
+﻿namespace AuthModule.Application.DTOs
+{
+    public class FilterEmployeesDto
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public int Page { get; set; }
+    }
+}
