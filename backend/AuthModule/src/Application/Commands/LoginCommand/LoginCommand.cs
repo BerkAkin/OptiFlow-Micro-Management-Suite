8bfdf726f90a9a314f2b32d0af5e25d0660d161e
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-27 13:12:47 +0300

    REFACTOR: Command is now simpilfied and applying DDD, CQRS, Mediator and Clean Arch principles

---
+++ backend/AuthModule/src/Application/Commands/LoginCommand/LoginCommand.cs
@@ -0,0 +1,56 @@
+﻿using AuthModule.Application.DTOs;
+using AuthModule.Application.Services;
+using AuthModule.Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthModule.Application.Commands.LoginCommand
+{
+    public record LoginCommand(LoginDTO dto) : IRequest<string>;
+    public class LoginCommandHandler : IRequestHandler<LoginCommand ,string>
+    {
+        private readonly AuthDBContext _context;
+        private readonly TokenService _tokenCreateService;
+        private IHttpContextAccessor _contextAccessor;
+        public LoginCommandHandler(AuthDBContext context, TokenService tokenCreateService,IHttpContextAccessor contextAccessor)
+        {
+            _context = context;
+            _tokenCreateService = tokenCreateService;
+            _contextAccessor = contextAccessor;
+        }
+
+        public async Task<string> Handle(LoginCommand command, CancellationToken cancellationToken)
+        {
+            var user = await _context.Users.Include(u => u.RefreshToken).Include(u => u.Department)
+            .Include(u => u.Tenant).ThenInclude(t => t.TenantModules).ThenInclude(tm => tm.Module)
+            .FirstOrDefaultAsync(u => u.Email == command.dto.Email);
+
+            if (user is null)
+                throw new Exception("Kullanıcı bulunamadı");
+            bool passwordValid = BCrypt.Net.BCrypt.Verify(command.dto.Password, user.PasswordHash);
+            if (!passwordValid)
+                throw new Exception("Email veya şifre hatalı");
+
+
+            string accessToken = _tokenCreateService.GenerateAccessToken(user);
+
+
+            var refreshToken = _tokenCreateService.GenerateRefreshToken();
+            user.AddRefreshToken(refreshToken);
+            await _context.SaveChangesAsync();
+
+
+
+            _contextAccessor.HttpContext.Response.Cookies.Append("refreshToken", refreshToken, new CookieOptions
+            {
+                HttpOnly = true,
+                Secure = true,
+                SameSite = SameSiteMode.Strict,
+                Expires = DateTimeOffset.UtcNow.AddDays(7)
+            });
+
+
+            return accessToken;
+        }
+    }
+}
