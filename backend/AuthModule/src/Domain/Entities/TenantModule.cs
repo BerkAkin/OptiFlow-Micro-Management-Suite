40c3804494aec758b19e64f070d1b27631db148c
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-27 13:48:22 +0300

    REFACTOR: Now applies DDD principles

---
+++ backend/AuthModule/src/Domain/Entities/TenantModule.cs
@@ -0,0 +1,20 @@
+﻿
+namespace AuthModule.Domain.Entities
+{
+    public class TenantModule
+    {
+        public int TenantId { get; private set; }
+        public Tenant Tenant { get; private set; }
+
+        public int ModuleId { get; private set; }
+        public Module Module { get; private set; }
+
+        private TenantModule() { } 
+
+        public TenantModule(int tenantId, int moduleId)
+        {
+            TenantId = tenantId;
+            ModuleId = moduleId;
+        }
+    }
+}
