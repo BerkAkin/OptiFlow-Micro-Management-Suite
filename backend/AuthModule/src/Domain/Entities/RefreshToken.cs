00f81bc3bea097ab77d12e1a1d764fff1eae7753
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-27 13:48:17 +0300

    REFACTOR: Now applies DDD principles

---
+++ backend/AuthModule/src/Domain/Entities/RefreshToken.cs
@@ -0,0 +1,31 @@
+﻿
+namespace AuthModule.Domain.Entities
+{
+    public class RefreshToken
+    {
+        public int Id { get; private set; }
+        public string Token { get; private set; }
+        public DateTime Expires { get; private set; }
+        public DateTime Created { get; private set; }
+        public int UserId { get; private set; }
+        public User User { get; private set; }
+
+        private RefreshToken() { }
+
+        public RefreshToken(string token, int userId)
+        {
+            Token = token;
+            Expires = DateTime.UtcNow.AddDays(7);
+            UserId = userId;
+            Created = DateTime.UtcNow;
+        }
+
+        public void UpdateToken(string newToken)
+        {
+            Token = newToken;
+            Created = DateTime.UtcNow;
+            Expires = DateTime.UtcNow.AddDays(7);
+        }
+
+    }
+}
