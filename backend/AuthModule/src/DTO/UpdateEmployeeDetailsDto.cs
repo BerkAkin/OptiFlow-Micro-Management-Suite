4af7b8029a801647d279e42aae6c78e92102da9a
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-27 13:49:00 +0300

    REFACTOR: Relocated

--- backend/AuthModule/src/DTO/UpdateEmployeeDetailsDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AuthModule.DTO
-{
-    public class UpdateEmployeeDetailsDto
-    {
-        public string Department { get; set; }
-        public string IsActive { get; set; }
-        public string Email { get; set; }
-    }
-}
