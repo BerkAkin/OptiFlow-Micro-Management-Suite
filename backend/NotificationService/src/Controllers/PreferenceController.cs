d6d4db8f8aa5b0bd985d1ac7ee6caa46b7552831
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-27 13:51:36 +0300

    FEAT: Seperated from main controller and simplified endpoints

---
+++ backend/NotificationService/src/Controllers/PreferenceController.cs
@@ -0,0 +1,38 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using NotificationService.Services;
+using ProjectMicro.Shared.Interfaces;
+
+namespace NotificationService.Controllers
+{
+    [Route("api/preferences")]
+    [ApiController]
+    public class PreferenceController : ControllerBase
+    {
+        private readonly ICurrentUserService _currentUserService;
+        private readonly IMediator _mediator;
+
+        public PreferenceController(ICurrentUserService currentUserService, IMediator mediator)
+        {
+
+            _currentUserService = currentUserService;
+            _mediator = mediator;
+        }
+
+        [HttpPatch]
+        public async Task<IActionResult> Update()
+        {
+            int currentUser = _currentUserService.User.UserId;
+            await _mediator.Send(new UpdatePreferenceCommand(currentUser));
+            return Ok("Kullanıcı Bildirim İzni Başarıyla Değiştirildi");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            int currentUser = _currentUserService.User.UserId;
+            var isEnabled = await _mediator.Send(new GetPreferenceQuery(currentUser));
+            return Ok(isEnabled);
+        }
+    }
+}
