2ea195f21b1b56ae1dfe2076c081c6b975e0347e
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-27 13:12:42 +0300

    REFACTOR: Command is now simpilfied and applying DDD, CQRS, Mediator and Clean Arch principles

---
+++ backend/AuthModule/src/Application/Commands/ChangePasswordCommand/UpdatePasswordCommand.cs
@@ -0,0 +1,38 @@
+﻿using AuthModule.Application.DTOs;
+using AuthModule.Infrastructure.Persistence;
+using MediatR;
+
+namespace AuthModule.Application.Commands.ChangePasswordCommand
+{
+    public record UpdatePasswordCommand(int currentUser,PasswordUpdateDto dto) :IRequest<Unit>;
+    public class UpdatePasswordCommandHandler : IRequestHandler<UpdatePasswordCommand,Unit>
+    {
+        private readonly AuthDBContext _context;
+        public UpdatePasswordCommandHandler(AuthDBContext context)
+        {
+            _context = context;
+        }
+        public async Task<Unit> Handle(UpdatePasswordCommand command, CancellationToken cancellationToken)
+        {
+            var user = await _context.Users.FindAsync(command.currentUser,cancellationToken);
+            if (user == null) {
+                throw new Exception("User does not exist"); 
+            }
+
+            bool isPasswordValid = BCrypt.Net.BCrypt.Verify(command.dto.CurrentPassword, user.PasswordHash);
+            if (!isPasswordValid)
+            {
+                throw new Exception("Current Password Doesn't Match With Old Password");
+            }
+            if (command.dto.NewPassword != command.dto.NewPasswordAgain)
+            {
+                throw new Exception("New Password Doesn't Match With Set Again Password");
+            }
+
+            string newPassword = BCrypt.Net.BCrypt.HashPassword(command.dto.NewPassword);
+            user.UpdatePassword(newPassword);
+            await _context.SaveChangesAsync(cancellationToken);
+            return Unit.Value;
+        }
+    }
+}
