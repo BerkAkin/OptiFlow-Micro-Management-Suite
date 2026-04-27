e07b21603e2bb6c9fdf128b15f935e0683e54af9
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-27 13:54:19 +0300

    REMOVE: Unnecessary id field

--- backend/SurveyModule/src/Application/DTOs/SatisfactionRateDto.cs
+++ backend/SurveyModule/src/Application/DTOs/SatisfactionRateDto.cs
@@ -4,7 +4,6 @@ namespace SurveyModule.Application.DTOs
 {
     public class SatisfactionRateDto
     {
-        public int SurveyId { get; set; }
         public SatisfactionStatus Satisfaction { get; set; }
     }
 }
