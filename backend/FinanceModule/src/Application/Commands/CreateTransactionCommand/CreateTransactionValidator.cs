86672bc1c18964f219b11fc3c00b3fd26d091e28
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-29 13:50:02 +0300

    RELOCATED

---
+++ backend/FinanceModule/src/Application/Commands/CreateTransactionCommand/CreateTransactionValidator.cs
@@ -0,0 +1,20 @@
+﻿using FinanceModule.DTOs;
+using FluentValidation;
+
+namespace FinanceModule.Validators
+{
+    public class CreateTransactionValidator : AbstractValidator<TransactionDTO>
+    {
+        public CreateTransactionValidator() {
+            RuleFor(x => x.Category).NotEmpty();
+            RuleFor(x => x.Description).NotEmpty().MaximumLength(100).MinimumLength(5);
+            RuleFor(x => x.Who).NotEmpty().MaximumLength(30).MinimumLength(1);
+            RuleFor(x => x.Quantity).NotEmpty().InclusiveBetween(1,100000);
+            RuleFor(x => x.Parts).NotEmpty();
+            RuleFor(x=>x.Price).NotEmpty().GreaterThan(0);
+            RuleFor(x=>x.Exchange).NotEmpty();
+            RuleFor(x => x.Date).NotEmpty();
+            
+        }
+    }
+}
