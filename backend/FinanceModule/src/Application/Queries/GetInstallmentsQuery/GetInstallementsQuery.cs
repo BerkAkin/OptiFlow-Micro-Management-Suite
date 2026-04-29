49891579c2519b59b680c10c52fc231ca82591d8
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-29 13:48:16 +0300

    REFACTOR: Now applies DDD, Clean arch, CQRS and Mediator principles

---
+++ backend/FinanceModule/src/Application/Queries/GetInstallmentsQuery/GetInstallementsQuery.cs
@@ -0,0 +1,53 @@
+﻿using FinanceModule.DBOperations;
+using FinanceModule.DTOs;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinanceModule.Queries.Dashboard
+{
+
+    public record InstallmentsQuery(InstallRecurFilterDTO filters,int currentTenant) : IRequest<(List<InstallmentsDTO> data, int maxPage)>;
+
+    public class InstallementsQueryHandler : IRequestHandler<InstallmentsQuery, (List<InstallmentsDTO> data, int maxPage)>
+    {
+
+        private readonly FinanceDBContext _context;
+
+        public InstallementsQueryHandler(FinanceDBContext context) {
+            _context = context;
+        }
+
+        public async Task<(List<InstallmentsDTO>data,int maxPage)> Handle(InstallmentsQuery request, CancellationToken cancellationToken)
+        {
+           const int pageSize = 10;
+
+            var query = _context.Transactions
+                .AsNoTracking()
+                .Where(x => x.TenantSummaryId == request.currentTenant && x.IsPartly && !x.IsIncome);
+
+            if (!string.IsNullOrEmpty(request.filters.Description))
+            {
+                query = query.Where(x => x.Description.Contains(request.filters.Description));
+            }
+
+            int totalCount = await query.CountAsync();
+            int maxPage = (int)Math.Ceiling((double)totalCount / pageSize);
+
+            var data = await query
+                .OrderByDescending(o => o.Date)
+                .Skip((request.filters.Page - 1) * pageSize)
+                .Take(pageSize)
+                .Select(x=> new InstallmentsDTO
+                {
+                    Date = x.Date,
+                    Description = x.Description,
+                    Parts = (int)x.PartCount,
+                    Price = (int)x.Price
+
+                })
+                .ToListAsync(cancellationToken);
+
+            return (data,maxPage);
+        }
+    }
+}
