bc295743355dfe551b95fa660e4f8e97344de7f0
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-29 13:49:26 +0300

    RELOCATED

---
+++ backend/FinanceModule/src/Application/DTOs/MonthlySummaryDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace FinanceModule.DTOs
+{
+    public class MonthlyValueDTO
+    {
+        public int Month { get; set; }
+        public decimal Value { get; set; }
+    }
+    public class MonthlySummaryDTO
+    {
+        public List<MonthlyValueDTO> Incomes { get; set; }
+        public List<MonthlyValueDTO> Expenses { get; set; }
+    }
+}
