b273ee756ab1d0c179288e850b8f511219c23fd0
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-29 13:49:17 +0300

    REFACTOR: Now applies DDD, Clean arch, CQRS and Mediator principles

---
+++ backend/FinanceModule/src/Domain/Entities/InvoiceProducts.cs
@@ -0,0 +1,27 @@
+﻿using FinanceModule.Entities;
+
+namespace FinanceModule.Domain.Entities
+{
+    public class InvoiceProducts
+    {
+        public int Id { get; private set; }
+        public string Category { get; private set; }
+        public string Description { get; private set; }
+        public int Quantity { get; private set; }
+        public decimal Price { get; private set; }
+        public Invoice Invoice { get; private set; }
+        public int InvoiceId { get; private set; }
+        private InvoiceProducts() { }
+
+        public InvoiceProducts(string category, string description, int quantity, decimal price)
+        {
+            this.Category = category;
+            this.Description = description;
+            this.Quantity = quantity;
+            this.Price = price;
+        }
+    }
+
+
+
+}
