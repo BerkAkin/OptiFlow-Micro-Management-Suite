7cb0ba9bb27ce4792b419c17439714a124d4ca92
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-29 13:48:13 +0300

    REFACTOR: Now applies DDD, Clean arch, CQRS and Mediator principles

---
+++ backend/FinanceModule/src/Application/Queries/GetMostQuery/GetMostQuery.cs
@@ -0,0 +1,33 @@
+﻿using FinanceModule.DBOperations;
+using FinanceModule.DTOs;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinanceModule.Queries.Dashboard
+{
+    public record GetMostQuery(int currentTenant):IRequest<List<CategoricalTransactionSummaryDTO>>;
+    public class MostCategorySummaryQueryHandler:IRequestHandler<GetMostQuery, List<CategoricalTransactionSummaryDTO>>
+    {
+        private readonly FinanceDBContext _context;
+        public MostCategorySummaryQueryHandler(FinanceDBContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<CategoricalTransactionSummaryDTO>> Handle(GetMostQuery request, CancellationToken cancellationToken)
+        {
+            return await _context.Transactions
+                .AsNoTracking()
+                .Where(x => x.TenantSummaryId == request.currentTenant && x.Date.Month == DateTime.Now.Month && x.Date.Year == DateTime.Now.Year && !x.IsIncome)
+                .GroupBy(group => group.Category)
+                .Select(group => new CategoricalTransactionSummaryDTO
+                {
+                    Category = group.Key,
+                    Expense = group.Sum(x => x.Price)
+                })
+                .OrderByDescending(x => x.Expense)
+                .Take(2)
+                .ToListAsync(cancellationToken);
+        }
+    }
+}
