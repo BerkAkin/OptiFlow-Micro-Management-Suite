741bdbde88c8a7d8fde162bdc8ad750d7b2a1906
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-29 13:50:38 +0300

    FEAT: Is now having options for invoice entity

---
+++ backend/FinanceModule/src/Infrastructure/Persistence/FinanceDBContext.cs
@@ -0,0 +1,45 @@
+﻿using FinanceModule.Domain.Entities;
+using FinanceModule.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinanceModule.DBOperations
+{
+    public class FinanceDBContext: DbContext
+    {
+        public FinanceDBContext(DbContextOptions<FinanceDBContext> options): base(options) { }
+
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<Invoice> Invoices { get; set; } 
+        public DbSet<TenantSummary> Tenants { get; set; }
+        public DbSet<InvoiceProducts> InvoiceProducts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<TenantSummary>()
+                .Property(ts => ts.Id)
+                .ValueGeneratedNever();
+
+            modelBuilder.Entity<TenantSummary>()
+                .HasMany(ts => ts.Invoices)
+                .WithOne(i => i.TenantSummary)
+                .HasForeignKey(i => i.TenantSummaryId);
+
+            modelBuilder.Entity<TenantSummary>()
+                .HasMany(ts => ts.Transactions)
+                .WithOne(t => t.TenantSummary)
+                .HasForeignKey(t => t.TenantSummaryId);
+
+            modelBuilder.Entity<Invoice>()
+                .HasMany(i => i.Products)
+                .WithOne(p => p.Invoice)
+                .HasForeignKey(p => p.InvoiceId);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
