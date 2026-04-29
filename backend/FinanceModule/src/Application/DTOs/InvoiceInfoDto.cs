b948d5d2bd44fc493fb4cbe1f7ff9ab43d81f6e5
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-29 13:49:33 +0300

    RELOCATED

---
+++ backend/FinanceModule/src/Application/DTOs/InvoiceInfoDto.cs
@@ -0,0 +1,19 @@
+﻿namespace FinanceModule.DTOs
+{
+    public class InvoiceDto
+    {
+
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Address { get; set; }
+        public string PhoneNum { get; set; }
+        public string PersonSerialNum { get; set; }
+        public string Email { get; set; }
+        public DateTime OrderDate { get; set; }
+        public List<InvoiceProductDto> Products { get; set; }
+
+        public decimal SubTotal => Products.Sum(x=>x.Quantity*x.Price);
+        public decimal GrandTotal => SubTotal + ((SubTotal*20)/100) ;
+
+    }
+}
