2ef6c87dc4754e6c305bf372254db396bdba7c5f
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-29 13:48:06 +0300

    REFACTOR: Now applies DDD, Clean arch, CQRS and Mediator principles

---
+++ backend/FinanceModule/src/Application/Queries/GetMonthlyQuery/GetMonthlyQuery.cs
@@ -0,0 +1,51 @@
+﻿using FinanceModule.DBOperations;
+using FinanceModule.DTOs;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinanceModule.Queries.Dashboard
+{
+    public record GetMonthlyQuery(int currentTenant):IRequest<MonthlySummaryDTO>;
+    public class MonthlySummaryQueryHandler: IRequestHandler<GetMonthlyQuery,MonthlySummaryDTO>
+    {
+        private readonly FinanceDBContext _context;
+        public MonthlySummaryQueryHandler(FinanceDBContext context)
+        {
+             _context = context;
+        }
+
+        public async Task<MonthlySummaryDTO> Handle(GetMonthlyQuery request, CancellationToken cancellationToken)
+        {
+            DateTime startDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1).AddMonths(-11);
+            DateTime endDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1).AddMonths(1);
+
+
+            var query = await _context.Transactions
+                .AsNoTracking()
+                .Where(x=>x.TenantSummaryId==request.currentTenant && x.Date < endDate && x.Date >= startDate)
+                .GroupBy(x => new { x.Date.Year, x.Date.Month })
+                .OrderBy(group => group.Key.Month)
+                .Select(group => new
+                {
+                    group.Key.Month,
+                    Expense = group.Sum(x => !x.IsIncome ? x.Price : 0),
+                    Income = group.Sum(x => x.IsIncome ? x.Price : 0)
+                }).ToListAsync(cancellationToken);
+
+            return new MonthlySummaryDTO
+            {
+                Incomes = query.Select(x => new MonthlyValueDTO
+                {
+                    Month = x.Month,
+                    Value = x.Income
+                }).ToList(),
+
+                Expenses = query.Select(x => new MonthlyValueDTO
+                {
+                    Month = x.Month,
+                    Value = x.Expense
+                }).ToList()
+            };
+        }
+    }
+}
