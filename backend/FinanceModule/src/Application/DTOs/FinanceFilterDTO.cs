e8999efdb1bd84b69e549600fc93bb66ba55b9ce
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-29 13:49:35 +0300

    RELOCATED

---
+++ backend/FinanceModule/src/Application/DTOs/FinanceFilterDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace FinanceModule.DTOs
+{
+    public class FinanceFilterDTO
+    {
+        public string? Type { get; set; }
+        public string? Date { get; set; }
+        public int Page { get; set; }
+    }
+}
