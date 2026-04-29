e156f8dcd26d6a466363cd6b7d0558b33d6375bd
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-29 13:49:42 +0300

    RELOCATED

---
+++ backend/FinanceModule/src/Application/DTOs/TransactionDTO.cs
@@ -0,0 +1,18 @@
+﻿
+namespace FinanceModule.DTOs
+{
+    public class TransactionDTO
+    {
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public bool Income { get; set; }
+        public string Who { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public string Exchange { get; set; }
+        public bool Partly { get; set; }
+        public int Parts { get; set; }
+        public DateTime Date { get; set; }
+        public string? Invoice { get; set; }
+    }
+}
