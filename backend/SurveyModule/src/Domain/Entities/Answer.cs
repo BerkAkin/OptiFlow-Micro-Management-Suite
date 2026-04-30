e747fb15ff2c00a2d98b703367aaacf99bf57d95
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-30 18:35:53 +0300

    REFACTOR: Is now applying DDD, Clean Arch, CQRS, Mediator principles

--- backend/SurveyModule/src/Domain/Entities/Answer.cs
+++ backend/SurveyModule/src/Domain/Entities/Answer.cs
@@ -2,9 +2,18 @@
 {
     public class Answer
     {
-        public int Id { get; set; }
-        public string Title { get; set; }
-        public int QuestionId { get; set; }
-        public Question Question { get; set; }
+        public int Id { get; private set; }
+        public string Text { get; private set; }
+        public int QuestionId { get; private set; }
+        public Question Question { get; private set; }
+
+        private Answer() { }
+
+        public Answer(string text, int questionId)
+        {
+            this.Text = text;
+            this.QuestionId = questionId;
+        }
+
     }
 }
