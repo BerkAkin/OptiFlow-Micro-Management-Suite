071b853b864733fc93d1994bb5338e597658dcd8
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-30 18:35:22 +0300

    REFACTOR: Project is now applying DDD, Clean Arch, CQRS, Mediator principles

---
+++ backend/SurveyModule/src/Api/Controllers/AnswersController.cs
@@ -0,0 +1,39 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using ProjectMicro.Shared.Interfaces;
+using SurveyModule.Application.Commands.AnswerSurveyCommand;
+using SurveyModule.Application.DTOs;
+using SurveyModule.Application.Queries.GetSurveyResult;
+
+namespace SurveyModule.Api.Controllers
+{
+    [Route("api/answers")]
+    [ApiController]
+    public class AnswersController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        private readonly ICurrentUserService _currentUser;
+        public AnswersController(IMediator mediator, ICurrentUserService currentUser)
+        {
+            _mediator = mediator;
+            _currentUser = currentUser;
+        }
+
+        [HttpPost("user-answer")]
+        public async Task<IActionResult> Create([FromBody] UserAnswerDto UserAnswer)
+        {
+            int userId = _currentUser.User.UserId;
+            int tenantId = _currentUser.User.TenantId;
+            var data = await _mediator.Send(new CreateAnswersCommand(tenantId, userId, UserAnswer));
+            return Ok("Your answer saved succesfully");
+        }
+
+        [HttpGet("{id}/results")]
+        public async Task<IActionResult> GetAll(int id)
+        {
+            var data = await _mediator.Send(new GetSurveyResultQuery(id));
+            return Ok(data);
+        }
+
+    }
+}
