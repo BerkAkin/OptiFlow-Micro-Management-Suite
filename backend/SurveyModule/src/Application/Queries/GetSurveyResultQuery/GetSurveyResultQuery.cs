d716a1d7e7d31fc0a53135b5cfbaf5465b647f90
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-30 18:35:51 +0300

    REFACTOR: Is now applying DDD, Clean Arch, CQRS, Mediator principles

--- backend/SurveyModule/src/Application/Queries/GetSurveyResultQuery/GetSurveyResultQuery.cs
+++ backend/SurveyModule/src/Application/Queries/GetSurveyResultQuery/GetSurveyResultQuery.cs
@@ -1,20 +1,43 @@
 ﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
 using SurveyModule.Application.DTOs;
-using SurveyModule.Application.Interfaces.Repositories;
+using SurveyModule.Infrastructure.Persistance;
 
 namespace SurveyModule.Application.Queries.GetSurveyResult
 {
     public record GetSurveyResultQuery(int SurveyId) : IRequest<SurveyResultDto>;
     public class GetSurveyResultQueryHandler : IRequestHandler<GetSurveyResultQuery, SurveyResultDto>
     {
-        private readonly ISurveyRepository _repository;
-        public GetSurveyResultQueryHandler(ISurveyRepository repository)
+        private readonly SurveyDbContext _context;
+        public GetSurveyResultQueryHandler(SurveyDbContext context)
         {
-            _repository = repository;
+            _context = context;
         }
-        
-        public async Task<SurveyResultDto> Handle(GetSurveyResultQuery request,CancellationToken cancellationToken) { 
-               return await _repository.GetSurveyResult(request.SurveyId);
+
+        public async Task<SurveyResultDto> Handle(GetSurveyResultQuery request, CancellationToken cancellationToken)
+        {
+            var answers = await _context.Surveys
+                .AsNoTracking()
+                .Where(x => x.Id == request.SurveyId)
+                .Select(s => new SurveyResultDto
+                {
+                    Name = s.Title,
+                    Questions = s.Questions.Select(q => new SurveyResultQuestionDto
+                    {
+                        Title = q.Text,
+                        Answers = q.Answers.Select(a => new SurveyResultQuestionAnswerDto
+                        {
+                            Title = q.Text,
+                            Count = _context.UserAnswers.Count(ua => ua.AnswerId == a.Id),
+                        }).ToList(),
+                    }).ToList(),
+                }).SingleOrDefaultAsync(cancellationToken);
+
+            if (answers == null)
+                throw new KeyNotFoundException("No survey has been found.");
+
+            return answers;
+
         }
     }
 }
