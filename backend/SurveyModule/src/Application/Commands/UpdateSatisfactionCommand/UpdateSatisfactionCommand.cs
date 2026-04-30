30f16c3e3256001b0e9e6ac5320de917c90825f6
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-30 18:49:29 +0300

    FEAT: Added updater command of satisfaction rate of a survey

---
+++ backend/SurveyModule/src/Application/Commands/UpdateSatisfactionCommand/UpdateSatisfactionCommand.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using SurveyModule.Infrastructure.Persistance;
+
+namespace SurveyModule.Application.Commands.UpdateSatisfactionCommand
+{
+
+    public record UpdateSatisfactionCommand(int surveyId, int currentTenant) : IRequest<Unit>;
+    public class UpdateSatisfactionCommandHandler : IRequestHandler<UpdateSatisfactionCommand, Unit>
+    {
+        private readonly SurveyDbContext _context;
+        public UpdateSatisfactionCommandHandler(SurveyDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Unit> Handle(UpdateSatisfactionCommand request, CancellationToken cancellationToken)
+        {
+
+
+            var survey = await _context.Surveys.FirstOrDefaultAsync(s => s.Id == request.surveyId && s.TenantId == request.currentTenant, cancellationToken);
+
+            if (survey is null)
+                throw new KeyNotFoundException("No survey has been found");
+
+            survey.IncreaseSatisfaction();
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+
+}
