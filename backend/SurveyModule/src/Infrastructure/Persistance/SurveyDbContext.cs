449fd2cdd48445bd9d862d41926cef577c7df93c
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-30 18:36:17 +0300

    REFACTOR: Is now applying DDD, Clean Arch, CQRS, Mediator principles

--- backend/SurveyModule/src/Infrastructure/Persistance/SurveyDbContext.cs
+++ backend/SurveyModule/src/Infrastructure/Persistance/SurveyDbContext.cs
@@ -3,38 +3,54 @@ using SurveyModule.Domain.Entities;
 
 namespace SurveyModule.Infrastructure.Persistance
 {
-    public class SurveyDbContext: DbContext
+    public class SurveyDbContext : DbContext
     {
-        public SurveyDbContext(DbContextOptions<SurveyDbContext> options): base(options)
+        public SurveyDbContext(DbContextOptions<SurveyDbContext> options) : base(options)
         {
 
         }
 
+        public DbSet<Tenant> Tenants { get; set; }
+        public DbSet<User> Users { get; set; }
         public DbSet<Survey> Surveys { get; set; }
         public DbSet<Question> Questions { get; set; }
         public DbSet<Answer> Answers { get; set; }
-        public DbSet<MiniUser> MiniUsers { get; set; }
         public DbSet<UserAnswer> UserAnswers { get; set; }
-        public DbSet<SatisfactionRate> SatisfactionRates { get; set; }
 
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+
+            modelBuilder.Entity<Tenant>()
+                .HasMany(t => t.Users)
+                .WithOne(u => u.Tenant)
+                .HasForeignKey(u => u.TenantId);
+
+            modelBuilder.Entity<Tenant>()
+                .HasKey(x => x.Id);
+
+            modelBuilder.Entity<Tenant>()
+                .Property(x => x.Id)
+                .ValueGeneratedNever();
+
+            modelBuilder.Entity<Tenant>()
+                .HasMany(t => t.Survey)
+                .WithOne(s => s.Tenant)
+                .HasForeignKey(s => s.TenantId);
+
+
+
             modelBuilder.Entity<Survey>()
                 .HasMany(s => s.Questions)
                 .WithOne(q => q.Survey)
                 .HasForeignKey(q => q.SurveyId);
 
+
+
             modelBuilder.Entity<Question>()
                 .HasMany(q => q.Answers)
                 .WithOne(a => a.Question)
                 .HasForeignKey(a => a.QuestionId);
-
-            modelBuilder.Entity<MiniUser>()
-                .HasMany(u => u.UserAnswers)
-                .WithOne(ua => ua.User)
-                .HasForeignKey(ua => ua.UserId);      
-
 
             modelBuilder.Entity<UserAnswer>()
                 .HasIndex(x => new { x.UserId, x.SurveyId, x.QuestionId })
@@ -59,9 +75,9 @@ namespace SurveyModule.Infrastructure.Persistance
                 .HasForeignKey(ua => ua.SurveyId)
                 .OnDelete(DeleteBehavior.Restrict);
 
-            modelBuilder.Entity<SatisfactionRate>()
-                .HasIndex(x => new { x.UserId, x.SurveyId })
-                .IsUnique();
+
+
+
 
         }
 
