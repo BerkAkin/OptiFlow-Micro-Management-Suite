170532520b1cd08cbcd40a53d696ea1f0bd2d128
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-30 18:35:20 +0300

    REFACTOR: Project is now applying DDD, Clean Arch, CQRS, Mediator principles

--- backend/SurveyModule/src/Api/Program.cs
+++ backend/SurveyModule/src/Api/Program.cs
@@ -1,11 +1,8 @@
 using Microsoft.EntityFrameworkCore;
-using SurveyModule.Application.Interfaces.Repositories;
-using SurveyModule.Application.Queries.GetSurveysQuery;
-using SurveyModule.Infrastructure.Persistance;
-using SurveyModule.Infrastructure.Repositories;
 using ProjectMicro.Shared.Interfaces;
 using ProjectMicro.Shared.Services;
-using SurveyModule.Seeder;
+using SurveyModule.Application.Queries.GetSurveysQuery;
+using SurveyModule.Infrastructure.Persistance;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -30,22 +27,12 @@ builder.Services.AddScoped<ICurrentUserService, CurrentUserService>();
 
 builder.Services.AddDbContext<SurveyDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("SurveyModuleDb")));
 
-builder.Services.AddTransient<DummyDataSeeder>();
-
 builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(GetSurveyQuery).Assembly));
-
-builder.Services.AddScoped<ISurveyRepository,SurveysRepository>();
-
-
-
-
 
 
 var app = builder.Build();
 
 app.UseCors();
-
-
 
 if (app.Environment.IsDevelopment())
 {
@@ -61,10 +48,5 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-using (var scope = app.Services.CreateScope())
-{
-    var seeder = scope.ServiceProvider.GetRequiredService<DummyDataSeeder>();
-    await  seeder.Seeder();
-}
 
 app.Run();
