549ffbec146e0629836ba7d7da03ce5b58bd220d
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-30 18:35:39 +0300

    REFACTOR: Is now applying DDD, Clean Arch, CQRS, Mediator principles

---
+++ backend/SurveyModule/src/Application/Commands/CreateSurveyCommand/CreateSurveyCommand.cs
@@ -0,0 +1,39 @@
+﻿using MediatR;
+using SurveyModule.Application.DTOs;
+using SurveyModule.Infrastructure.Persistance;
+
+namespace SurveyModule.Application.Commands.AddSurvey
+{
+    public record CreateSurveyCommand(SurveyDto survey, int tenantId) : IRequest<Unit>;
+    public class CreateSurveyCommandHandler : IRequestHandler<CreateSurveyCommand, Unit>
+    {
+        private readonly SurveyDbContext _context;
+        public CreateSurveyCommandHandler(SurveyDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Unit> Handle(CreateSurveyCommand command, CancellationToken cancellationToken)
+        {
+
+            var tenant = await _context.Tenants.FindAsync(command.tenantId, cancellationToken);
+            if (tenant == null)
+                throw new Exception("Company does not exist");
+
+            var newSurvey = tenant.AddSurvey(command.survey.Title);
+
+            foreach (var question in command.survey.Questions)
+            {
+                var newQuestion = newSurvey.AddQuestion(question.Title);
+                foreach (var answer in question.Answers)
+                {
+                    newQuestion.AddAnswer(answer.Title);
+                }
+            }
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
