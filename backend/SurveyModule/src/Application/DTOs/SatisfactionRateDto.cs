d8dac3bc7e5f845e8b83001f1779a671cda466d2
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-30 18:35:47 +0300

    REFACTOR: Is now applying DDD, Clean Arch, CQRS, Mediator principles

--- backend/SurveyModule/src/Application/DTOs/SatisfactionRateDto.cs
+++
@@ -1,9 +0,0 @@
-﻿using SurveyModule.Domain.Enums;
-
-namespace SurveyModule.Application.DTOs
-{
-    public class SatisfactionRateDto
-    {
-        public SatisfactionStatus Satisfaction { get; set; }
-    }
-}
