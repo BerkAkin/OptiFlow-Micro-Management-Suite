5b95eb48cfa2e12260174be3cd015c9a1cbb14f1
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-30 18:35:55 +0300

    REFACTOR: Is now applying DDD, Clean Arch, CQRS, Mediator principles

--- backend/SurveyModule/src/Application/Queries/GetSurveysQuery/GetSurveyQuery.cs
+++ backend/SurveyModule/src/Application/Queries/GetSurveysQuery/GetSurveyQuery.cs
@@ -1,19 +1,33 @@
 ﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
 using SurveyModule.Application.DTOs;
-using SurveyModule.Application.Interfaces.Repositories;
+using SurveyModule.Infrastructure.Persistance;
 
 namespace SurveyModule.Application.Queries.GetSurveysQuery
 {
     public record GetSurveyQuery(int TenantId) : IRequest<List<GetSurveyDto>>;
-    public class GetSurveyQueryHandler : IRequestHandler<GetSurveyQuery,List<GetSurveyDto>>
+    public class GetSurveyQueryHandler : IRequestHandler<GetSurveyQuery, List<GetSurveyDto>>
     {
-        private readonly ISurveyRepository _repository;
-        public GetSurveyQueryHandler(ISurveyRepository repository)
+        private readonly SurveyDbContext _context;
+        public GetSurveyQueryHandler(SurveyDbContext context)
         {
-            _repository = repository;
+            _context = context;
         }
-        public async Task<List<GetSurveyDto>> Handle(GetSurveyQuery request,CancellationToken cancellationToken) {
-            return await _repository.GetSurveyList(request.TenantId);
+        public async Task<List<GetSurveyDto>> Handle(GetSurveyQuery request, CancellationToken cancellationToken)
+        {
+            return await _context.Surveys
+                .AsNoTracking()
+                .Where(x => x.TenantId == request.TenantId)
+                .Select(x => new GetSurveyDto
+                {
+                    Title = x.Title,
+                    TenantId = x.TenantId,
+                    Id = x.Id,
+                    Date = x.CreatedAt,
+                    Status = x.Status,
+                    SatisfactionCount = x.SatisfactionRate,
+                    ParticipateCount = x.ParticipationRate,
+                }).ToListAsync();
         }
     }
 }
