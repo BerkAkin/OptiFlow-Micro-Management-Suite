0875606aa028e62be38da8f719e943c6a85ef15c
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-30 18:35:45 +0300

    REFACTOR: Is now applying DDD, Clean Arch, CQRS, Mediator principles

--- backend/SurveyModule/src/Application/Queries/GetSurveyDetailsQuery/GetSurveyDetailQuery.cs
+++ backend/SurveyModule/src/Application/Queries/GetSurveyDetailsQuery/GetSurveyDetailQuery.cs
@@ -1,21 +1,48 @@
 ﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
 using SurveyModule.Application.DTOs;
-using SurveyModule.Application.Interfaces.Repositories;
+using SurveyModule.Infrastructure.Persistance;
 
 namespace SurveyModule.Application.Queries.GetSurveyDetailsQuery
 {
-    public record GetSurveyDetailQuery(int SurveyId,int tenantId, int UserId) : IRequest<SurveyDto>;
-    public class GetSurveyDetailHandler: IRequestHandler<GetSurveyDetailQuery, SurveyDto>
+    public record GetSurveyDetailQuery(int SurveyId, int tenantId, int UserId) : IRequest<SurveyDto>;
+    public class GetSurveyDetailHandler : IRequestHandler<GetSurveyDetailQuery, SurveyDto>
     {
-        private readonly ISurveyRepository _repository;
-        public GetSurveyDetailHandler(ISurveyRepository repository)
+        private readonly SurveyDbContext _context;
+        public GetSurveyDetailHandler(SurveyDbContext context)
         {
-            _repository = repository;
+            _context = context;
         }
 
         public async Task<SurveyDto> Handle(GetSurveyDetailQuery request, CancellationToken cancellationToken)
         {
-            return await _repository.GetSurveyDetail(request.SurveyId, request.tenantId,request.UserId);
+
+            var result = await _context.Surveys
+                .AsNoTracking()
+                .Where(x => x.Id == request.SurveyId && x.TenantId == request.tenantId)
+                .Select(x => new SurveyDto
+                {
+                    Id = x.Id,
+                    Title = x.Title,
+                    Questions = x.Questions.Select(q => new QuestionDto
+                    {
+                        Id = q.Id,
+                        Title = q.Text,
+                        Answers = q.Answers.Select(a => new AnswerDto
+                        {
+                            Id = a.Id,
+                            Title = a.Text,
+                        }).ToList()
+                    }).ToList()
+                }).SingleOrDefaultAsync(cancellationToken);
+
+            if (result == null)
+            {
+                throw new KeyNotFoundException("No survey found");
+            }
+
+            return result;
+
         }
     }
 }
