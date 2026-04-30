0027f8d6a89a64ca7409d35279888f6b3f8edc92
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-30 18:35:38 +0300

    REFACTOR: Is now applying DDD, Clean Arch, CQRS, Mediator principles

---
+++ backend/SurveyModule/src/Application/Commands/CreateAnswersCommand/CreateAnswersCommand.cs
@@ -0,0 +1,42 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using SurveyModule.Application.DTOs;
+using SurveyModule.Domain.Entities;
+using SurveyModule.Infrastructure.Persistance;
+
+namespace SurveyModule.Application.Commands.AnswerSurveyCommand
+{
+    public record CreateAnswersCommand(int currentTenant, int currentUser, UserAnswerDto userAnswers) : IRequest<Unit>;
+    public class CreateAnswersCommandHandler : IRequestHandler<CreateAnswersCommand, Unit>
+    {
+        private readonly SurveyDbContext _context;
+        public CreateAnswersCommandHandler(SurveyDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Unit> Handle(CreateAnswersCommand request, CancellationToken cancellationToken)
+        {
+            bool alreadyAnswered = await _context.UserAnswers
+                .AnyAsync(x => x.SurveyId == request.userAnswers.SurveyId &&
+                          x.UserId == request.currentUser, cancellationToken);
+
+            if (alreadyAnswered)
+                throw new InvalidOperationException("You've already took the survey");
+
+            var userAnswers = request.userAnswers.Answers
+                .Select(ans => new UserAnswer(
+                request.currentTenant,
+                request.currentUser,
+                request.userAnswers.SurveyId,
+                ans.QuestionId,
+                ans.AnswerId
+            )).ToList();
+
+            await _context.UserAnswers.AddRangeAsync(userAnswers, cancellationToken);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
