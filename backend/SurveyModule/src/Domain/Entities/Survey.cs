8661e123db900a4a961ba0e16eb95252c1ac0720
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-04-30 18:36:07 +0300

    REFACTOR: Is now applying DDD, Clean Arch, CQRS, Mediator principles

--- backend/SurveyModule/src/Domain/Entities/Survey.cs
+++ backend/SurveyModule/src/Domain/Entities/Survey.cs
@@ -4,13 +4,49 @@ namespace SurveyModule.Domain.Entities
 {
     public class Survey
     {
-        public int Id { get; set; }
-        public int CreatorId { get; set; }
-        public int TenantId { get; set; }
-        public string Title { get; set; }
-        public SurveyStatus Status { get; set; } = SurveyStatus.Published;
-        public DateTime CreatedAt { get; set; } = DateTime.Now;
-        public ICollection<Question> Questions { get; set; } = new List<Question>();
-        public ICollection<SatisfactionRate> SatisfactionRate {get;set;} = new List<SatisfactionRate>();
+        public int Id { get; private set; }
+        public string Title { get; private set; }
+
+        public int ParticipationRate { get; set; } = 0;
+        public int SatisfactionRate { get; set; } = 0;
+        public SurveyStatus Status { get; private set; }
+        public DateTime CreatedAt { get; private set; } = DateTime.UtcNow;
+
+        public Tenant Tenant { get; private set; }
+        public int TenantId { get; private set; }
+
+
+        private readonly List<Question> _questions = new();
+        public IReadOnlyCollection<Question> Questions => _questions;
+
+
+        private Survey() { }
+
+        public Survey(int tenantId, string title)
+        {
+            this.TenantId = tenantId;
+            this.Title = title;
+            this.Status = SurveyStatus.Published;
+        }
+
+        public Question AddQuestion(string text)
+        {
+            Question newQuestion = new Question(text, this.Id);
+            _questions.Add(newQuestion);
+            return newQuestion;
+        }
+
+        public void IncreaseSatisfaction()
+        {
+            this.SatisfactionRate++;
+            this.ParticipationRate++;
+
+        }
+        public void ReduceSatisfaction()
+        {
+            this.SatisfactionRate--;
+            this.ParticipationRate++;
+
+        }
     }
 }
