d9e18831fd2bc1eff9b9575ab8fa4d8d3da5ec31
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-05-15 16:06:17 +0300

    REFACTOR: Renamed miniuser entity to user for data consistency

---
+++ backend/SuggestionModule/src/Domain/Entities/User.cs
@@ -0,0 +1,30 @@
+﻿namespace SuggestionModule.Domain.Entities
+{
+    public class User
+    {
+        public int Id { get; private set; }
+        public string Fullname { get; private set; }
+
+        public int TenantId { get; private set; }
+        public Tenant Tenant { get; private set; }
+
+
+
+        private readonly List<Suggestion> _suggestions = new();
+        public IReadOnlyCollection<Suggestion> Suggestions => _suggestions;
+
+        private readonly List<Vote> _votes = new();
+        public IReadOnlyCollection<Vote> Votes => _votes;
+
+        private readonly List<Comment> _comments = new();
+        public IReadOnlyCollection<Comment> Comments => _comments;
+
+        private User() { }
+        public User(int id, string fullname, int tenantId)
+        {
+            this.Id = id;
+            this.Fullname = fullname;
+            this.TenantId = tenantId;
+        }
+    }
+}
