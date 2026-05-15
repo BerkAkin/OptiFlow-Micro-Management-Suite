cb336be40c67e212ddc62db7cb8f272e12e36cc1
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-05-15 16:18:58 +0300

    FEAT: Added tenant seed data

--- backend/AuthModule/src/Infrastructure/Seeders/DbSeeder.cs
+++ backend/AuthModule/src/Infrastructure/Seeders/DbSeeder.cs
@@ -8,7 +8,7 @@ public static class DbSeeder
     {
         if (!await context.Modules.AnyAsync())
         {
-            var modules = new List<Module>
+            List<Module> modules = new List<Module>
             {
                 new Module("Finance"),
                 new Module("Surveys"),
@@ -22,14 +22,35 @@ public static class DbSeeder
         if (!await context.Departments.AnyAsync())
         {
 
-            var departments = new List<Department>
+            List<Department> departments = new List<Department>
             {
                 new Department("Standart Employee"),
-                new Department("Finance Accountant"), 
-                new Department("Human Resources"),    
-                new Department("Company Manager")     
+                new Department("Finance Accountant"),
+                new Department("Human Resources"),
+                new Department("Company Manager")
             };
             await context.Departments.AddRangeAsync(departments);
+        }
+
+        if (!await context.Tenants.AnyAsync())
+        {
+
+            Tenant tenant = new Tenant(
+                 "Company 1", "Company Address", "11111111111", "222222222", "company@mail.com",
+                 "Company Tax Office", "3333333333", "44444444444", "5555555555");
+
+            tenant.AssignModule(1);
+            tenant.AssignModule(2);
+            tenant.AssignModule(3);
+            tenant.AssignModule(4);
+            tenant.AssignModule(5);
+            string hashPass = BCrypt.Net.BCrypt.HashPassword("123");
+
+            tenant.AddUser(
+                "User Firstname", "User Lastname", "user@mail.com", hashPass, "111111111", DateTime.UtcNow, "street 1",
+                "street 2", "apartment", "door", "province", "district", "Full Address", 4);
+
+            await context.Tenants.AddRangeAsync(tenant);
         }
 
         await context.SaveChangesAsync();
