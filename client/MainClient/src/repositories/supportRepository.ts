a58544a9a6405675bcccc53734667a62178f463a
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-05-15 16:43:08 +0300

    REFACTOR: Endpoint call urls updated according to backend's new endpoint namings

--- client/MainClient/src/repositories/supportRepository.ts
+++ client/MainClient/src/repositories/supportRepository.ts
@@ -1,67 +1,69 @@
 import { api } from "../lib/api";
 
 export const fetchMonthlySupport = async () => {
-  const res = await api.get("/support/GetMonthlySupportRequestsCount");
+  const res = await api.get("/supports/stats/monthly-count");
   return res.data;
 };
 
 export const fetchCategoricalSupport = async () => {
-  const res = await api.get("/support/GetSupportRequestsCategorical");
+  const res = await api.get("/supports/stats/categorical");
   return res.data;
 };
 
 export const requestSupport = async (payload: any) => {
-  const res = await api.post("/support/createSupportRequest", payload);
+  const res = await api.post("/supports", payload);
   return res.data;
 };
 
 export const fetchSupportRequests = async () => {
-  const res = await api.get("/support/GetSupportRequests");
+  const res = await api.get("/supports");
   return res.data;
 };
 
 export const fetchSupportMessages = async (requestId: number) => {
-  const res = await api.get("/support/GetSupportRequestMessages", {
-    params: { requestId: requestId },
-  });
+  const res = await api.get(`/supports/${requestId}/messages`);
   return res.data;
 };
 
 export const sendMessage = async (payload: any) => {
-  const res = await api.post("support/sendMessage", payload);
+  const res = await api.post(
+    `/supports/${payload.requestId}/messages`,
+    payload,
+  );
   return res.data;
 };
 
-export const markAsClosed = async (id: number) => {
-  const res = await api.get("support/MarkAsClosed", {
-    params: { requestId: id },
-  });
+export const markAsClosed = async (requestId: number) => {
+  const res = await api.patch(`/supports/${requestId}/close`);
   return res.data;
 };
 
 export const fetchUserList = async () => {
-  const res = await api.get("support/GetUserList");
+  const res = await api.get("/supports/available-employees");
   return res.data;
 };
 
 export const fetchDayoffs = async () => {
-  const res = await api.get("support/Dayoffs");
+  const res = await api.get("/leaves");
   return res.data;
 };
 
 export const fetchMyDayoffs = async (filters: any, page: number) => {
-  const res = await api.get("support/MyDayoffs", {
+  const res = await api.get("/leaves/me", {
     params: { ...filters, page },
   });
   return res.data;
 };
 
 export const requestDayoff = async (payload: any) => {
-  const res = await api.post("support/Dayoff", payload);
+  const res = await api.post("/leaves", payload);
   return res.data;
 };
 
 export const approveOrRejectDayoffRequest = async (payload: any) => {
-  const res = await api.put("support/ApproveOrRejectDayoff", payload);
+  console.log("payload", payload);
+  const res = await api.patch(`/leaves/${payload.requestId}/status`, {
+    status: payload.status,
+  });
   return res.data;
 };
