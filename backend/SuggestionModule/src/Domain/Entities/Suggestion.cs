7399df5450765bcd8c3dd0cf35f870a31c18da4a
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-05-15 16:34:40 +0300

    REFACTOR: Miniuser definition changed to user

--- backend/SuggestionModule/src/Domain/Entities/Suggestion.cs
+++ backend/SuggestionModule/src/Domain/Entities/Suggestion.cs
@@ -4,47 +4,54 @@ namespace SuggestionModule.Domain.Entities
 {
     public class Suggestion
     {
-        protected Suggestion() { }
-
-        public Suggestion(
-        string title,
-        string description,
-        int userId,
-        int tenantId)
-        {
-            Title = title;
-            Description = description;
-            UserId = userId;
-            TenantId = tenantId;
-            Date = DateTime.UtcNow;
-            Status = SuggestionStatus.Pending;
-        }
 
         public int Id { get; private set; }
         public string Title { get; private set; }
         public string Description { get; private set; }
+
         public int TenantId { get; private set; }
+        public Tenant Tenant { get; private set; }
+
         public int UserId { get; private set; }
-        public MiniUser User { get; private set; }
+        public User User { get; private set; }
+
         public DateTime Date { get; private set; }
         public SuggestionStatus Status { get; private set; }
 
         private readonly List<Comment> _comments = new();
         public IReadOnlyCollection<Comment> Comments => _comments;
-        public ICollection<Vote> Votes { get; set; } = new List<Vote>();
+
+        private readonly List<Vote> _votes = new();
+        public IReadOnlyCollection<Vote> Votes => _votes;
+
+        private Suggestion() { }
+
+        public Suggestion(string title, string description, int tenantId, int userId)
+        {
+            this.Title = title;
+            this.Description = description;
+            this.UserId = userId;
+            this.TenantId = tenantId;
+            this.Date = DateTime.UtcNow;
+            this.Status = SuggestionStatus.Pending;
+        }
 
         public void ChangeStatus(SuggestionStatus newStatus)
         {
             if (Status == newStatus)
                 return;
-            Status = newStatus;
+            this.Status = newStatus;
         }
 
-         public void AddComment(int userId, string text)
-         {
-            var comment = new Comment(userId, text);
-            _comments.Add(comment);
-         }
+        public void AddComment(int userId, string text)
+        {
+            _comments.Add(new Comment(this.Id, userId, text));
+        }
+
+        public void AddVote(int userId, VoteType voteType)
+        {
+            _votes.Add(new Vote(userId, this.Id, voteType));
+        }
 
     }
 }
