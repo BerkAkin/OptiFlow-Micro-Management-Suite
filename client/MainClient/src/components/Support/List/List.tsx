94e516ae4578455e6bd812b040501103e21edc12
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-05-15 16:39:51 +0300

    FIX: Manager keyword changed with company manager on role guard HOC

--- client/MainClient/src/components/Support/List/List.tsx
+++ client/MainClient/src/components/Support/List/List.tsx
@@ -40,10 +40,10 @@ export function List() {
 
 
                                         {!item.isClosed && (
-                                            <RoleBasedGuard allowedDepartments={["HR"]}>
+                                            <RoleBasedGuard allowedDepartments={["HR", "Company Manager"]}>
                                                 <button
                                                     onClick={(e) => handleMarkAsClosed(e, item.id)}
-                                                    className='opacity-0 group-hover:opacity-100 transition-opacity bg-rose-50 text-rose-600 text-[10px] font-bold px-2 py-1 rounded hover:bg-rose-600 hover:text-white border border-rose-100'
+                                                    className='group-hover:opacity-100 transition-opacity bg-rose-50 text-rose-600 text-[10px] font-bold px-2 py-1 rounded hover:bg-rose-600 hover:text-white border border-rose-100'
                                                 >
                                                     Close Ticket
                                                 </button>
