57764e6f92a7b09d464c65a624a5072d716d5a71
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-05-15 16:37:00 +0300

    REFACTOR: Repository call removed and linq query is now located in command

--- backend/SuggestionModule/src/Application/Commands/ChangeStatusCommand/ChangeStatusCommand.cs
+++ backend/SuggestionModule/src/Application/Commands/ChangeStatusCommand/ChangeStatusCommand.cs
@@ -1,24 +1,45 @@
 ﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
 using SuggestionModule.Application.DTOs;
-using SuggestionModule.Application.Interfaces;
-using SuggestionModule.Domain.Entities;
+using SuggestionModule.Domain.Enums;
+using SuggestionModule.Infrastructure.Persistence;
 
 namespace SuggestionModule.Application.Commands.ChangeStatusCommand
 {
     public record ChangeStatusCommand(int suggestionId, StatusDto status) : IRequest<Unit>;
     public class ChangeStatusCommandHandler : IRequestHandler<ChangeStatusCommand, Unit>
     {
-        private readonly ISuggestionRepository _repository;
-        public ChangeStatusCommandHandler(ISuggestionRepository repository)
+        private readonly SuggestionDbContext _context;
+        public ChangeStatusCommandHandler(SuggestionDbContext context)
         {
-            _repository = repository;
+            _context = context;
         }
 
         public async Task<Unit> Handle(ChangeStatusCommand command, CancellationToken cancellationToken)
         {
-            var suggestion = await _repository.GetByIdAsync(command.suggestionId);
-            suggestion.ChangeStatus(command.status.Status);
-            await _repository.SaveAsync();
+            var suggestion = await _context.Suggestions
+                .FirstOrDefaultAsync(x => x.Id == command.suggestionId, cancellationToken);
+
+            if (suggestion == null)
+            {
+                throw new KeyNotFoundException("Suggestion does not exist");
+            }
+
+
+            if (!Enum.TryParse<SuggestionStatus>(command.status.Status.ToString(), out var newStatus))
+            {
+                throw new ArgumentException("Invalid status value");
+            }
+
+            if (suggestion.Status == newStatus)
+            {
+                return Unit.Value;
+            }
+
+            suggestion.ChangeStatus(newStatus);
+
+            await _context.SaveChangesAsync(cancellationToken);
+
             return Unit.Value;
         }
     }
