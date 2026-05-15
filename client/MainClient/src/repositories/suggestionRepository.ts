f526b71087d1264425148d472f5dc47f07930ce5
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-05-15 16:43:06 +0300

    REFACTOR: Endpoint call urls updated according to backend's new endpoint namings

--- client/MainClient/src/repositories/suggestionRepository.ts
+++ client/MainClient/src/repositories/suggestionRepository.ts
@@ -6,36 +6,42 @@ export const fetchSuggestions = async () => {
 };
 
 export const fetchMySuggestions = async () => {
-  const res = await api.get("/suggestions/MySuggestions");
+  const res = await api.get("/suggestions/me");
   return res.data;
 };
 
 export const suggestionSendVote = async (payload: any) => {
   console.log(payload);
-  const res = await api.post("/suggestions/vote", payload);
+  const res = await api.post(
+    `/suggestions/${payload.suggestionId}/vote`,
+    payload,
+  );
   return res.data;
 };
 
 export const suggestionSendComment = async (payload: any) => {
-  console.log(payload);
-
-  const res = await api.post("/suggestions/makeComment", payload);
+  const res = await api.post(
+    `/suggestions/comments/${payload.suggestionId}`,
+    payload,
+  );
   return res.data;
 };
 
 export const suggestionApproveOrReject = async (payload: any) => {
-  console.log(payload);
-  const res = await api.post("/suggestion/approveOrReject", payload);
+  console.log("ssssss", payload);
+  const res = await api.patch(`/suggestions/${payload.id}/status`, {
+    status: payload.decision,
+  });
   return res.data;
 };
 
 export const fetchSuggestionShowCase = async () => {
-  const res = await api.get("/suggestions/GetBest");
+  const res = await api.get("/suggestions/best");
   return res.data;
 };
 
 export const suggestionMake = async (payload: any) => {
   console.log(payload);
-  const res = await api.post("/api/suggestion/", payload);
+  const res = await api.post("/suggestions", payload);
   return res.data;
 };
