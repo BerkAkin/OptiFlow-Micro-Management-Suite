0d194febc3e2591ff4c26673c23a849c5fd7a8bc
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-05-15 16:07:04 +0300

    Removec

--- backend/SuggestionModule/src/Application/DTOs/BestSuggestionThisMonthDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SuggestionModule.Application.DTOs
-{
-    public class BestSuggestionThisMonthDto
-    {
-        public string Description { get; set; }
-        public int Votes { get; set; }
-    }
-}
