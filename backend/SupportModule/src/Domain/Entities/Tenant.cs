45bb0b2e57df05346bd1eb2c0948f6d3d4a6289a
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-05-15 16:00:39 +0300

    REFACTOR: Location changed and now applies clean arch and DDD principles

---
+++ backend/SupportModule/src/Domain/Entities/Tenant.cs
@@ -0,0 +1,32 @@
+﻿namespace SupportModule.Domain.Entities
+{
+    public class Tenant
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public bool IsActive { get; private set; }
+
+
+        private readonly List<User> _users = new();
+        public IReadOnlyCollection<User> Users => _users;
+
+
+
+        private Tenant() { }
+
+        public Tenant(int id, string name)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.IsActive = true;
+        }
+
+        public User AddUser(string fullname, string email, int id,int departmentId)
+        {
+            User usr = new User(fullname,email,id,this.Id,departmentId);
+            this._users.Add(usr);
+            return usr;
+        }
+
+    }
+}
