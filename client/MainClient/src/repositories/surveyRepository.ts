b1d1edbc95190d2f2f2dcb9c07c3fed956ebde43
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-05-15 16:43:10 +0300

    REFACTOR: Endpoint call urls updated according to backend's new endpoint namings

--- client/MainClient/src/repositories/surveyRepository.ts
+++ client/MainClient/src/repositories/surveyRepository.ts
@@ -1,34 +1,37 @@
 import { api } from "../lib/api";
 
 export const fetchSurveys = async () => {
-  const res = await api.get("/survey");
+  const res = await api.get("/surveys");
   return res.data;
 };
 
 export const fetchSurveyDetails = async (id: string) => {
-  const res = await api.get(`/survey/SurveyDetail/`, { params: { id: id } });
+  const res = await api.get(`/surveys/${id}`);
   return res.data;
 };
 
 export const sendSurveyAnswers = async (payload: any) => {
   console.log(payload);
-  const res = await api.post("/survey/UserAnswer", payload);
+  const res = await api.post("/answers/user-answer", payload);
   return res.data;
 };
 
 export const fetchSurveyResult = async (id: number) => {
-  const res = await api.get(`survey/SurveyResult`, {
+  const res = await api.get(`/answers/${id}/results`, {
     params: { id: id },
   });
   return res.data;
 };
 
 export const sendSurvey = async (payload: any) => {
-  const res = await api.post(`/survey`, payload);
+  const res = await api.post(`/surveys`, payload);
   return res.data;
 };
 
 export const SendSatisfaction = async (payload: any) => {
-  const res = await api.post("/survey/satisfaction", payload);
+  const res = await api.post(
+    `surveys/${payload.surveyId}/increase-satisfaction`,
+    payload,
+  );
   return res.data;
 };
