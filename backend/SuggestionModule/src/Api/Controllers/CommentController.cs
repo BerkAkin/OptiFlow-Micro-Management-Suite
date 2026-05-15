f3d0537dbae42e60996854f61ae80e12bc9b0fd3
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-05-15 16:38:56 +0300

    FEAT: Controller for comments implemented

---
+++ backend/SuggestionModule/src/Api/Controllers/CommentController.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using ProjectMicro.Shared.Interfaces;
+using SuggestionModule.Application.Commands.MakeCommentCommand;
+using SuggestionModule.Application.DTOs;
+
+namespace SuggestionModule.Api.Controllers
+{
+    [Route("api/suggestions/comments")]
+    [ApiController]
+    public class CommentController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        private readonly ICurrentUserService _currentUserService;
+        public CommentController(IMediator mediator, ICurrentUserService currentUserService)
+        {
+            _mediator = mediator;
+            _currentUserService = currentUserService;
+        }
+
+        [HttpPost("{id}")]
+        public async Task<IActionResult> CreateComment(int id, [FromBody] CreateCommentDto comment)
+        {
+            int userId = _currentUserService.User.UserId;
+            await _mediator.Send(new MakeCommentCommand(id, comment, userId));
+            return Ok();
+        }
+
+    }
+}
