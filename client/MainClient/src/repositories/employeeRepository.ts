90d3a0326e912a54b8f521d06dac41de43d7936e
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-05-15 16:42:56 +0300

    REFACTOR: Endpoint call urls updated according to backend's new endpoint namings

--- client/MainClient/src/repositories/employeeRepository.ts
+++ client/MainClient/src/repositories/employeeRepository.ts
@@ -1,27 +1,23 @@
 import { api } from "../lib/api";
 
 export const fetchEmployees = async (filters: any, page: any) => {
-  const res = await api.get("/auth/AllUsers", {
+  const res = await api.get("/employees", {
     params: { ...filters, page },
   });
   return res.data;
 };
 
 export const addNewEmployee = async (payload: any) => {
-  const res = await api.post("/auth/AddNewUser", payload);
+  const res = await api.post("/employees", payload);
   return res.data;
 };
 
 export const getEmployeeDetails = async (email: string) => {
-  const res = await api.get("/auth/GetEmployeeDetails", {
-    params: {
-      email,
-    },
-  });
+  const res = await api.get(`/employees/${email}`);
   return res.data;
 };
 
 export const updateEmployeeDetails = async (payload: any) => {
-  const res = await api.put("/auth/UpdateEmployeeDetails", payload);
+  const res = await api.patch("/employees", payload);
   return res.data;
 };
