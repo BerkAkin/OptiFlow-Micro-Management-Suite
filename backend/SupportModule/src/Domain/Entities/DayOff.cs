4213cb88251d4519259dbc704d727a67e44fccd5
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-05-15 16:34:47 +0300

    REFACTOR: Miniuser definition changed to user

--- backend/SupportModule/src/Domain/Entities/DayOff.cs
+++ backend/SupportModule/src/Domain/Entities/DayOff.cs
@@ -5,7 +5,7 @@
         public int Id { get; private set; }
         public int TenantId { get; private set; }
         public int UserId { get; private set; }
-        public MiniUser User { get; private set; }
+        public User User { get; private set; }
         public string Topic { get; private set; }
         public string Description { get; private set; }
         public int Days { get; private set; }
@@ -13,7 +13,8 @@
         public int Status { get; private set; } = 0;
 
         private DayOff() { }
-        public DayOff(int tenantId,string topic, string description, int days, DateTime startingDate, int status=0) {
+        public DayOff(int tenantId, string topic, string description, int days, DateTime startingDate, int status = 0)
+        {
             TenantId = tenantId;
             Topic = topic;
             Description = description;
@@ -24,7 +25,7 @@
 
         public void ChangeStatus(int status)
         {
-           Status = status;
+            Status = status;
         }
 
     }
