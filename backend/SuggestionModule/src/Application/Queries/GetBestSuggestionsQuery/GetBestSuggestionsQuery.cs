e77320d521945ea8270c52aad4528334a4533583
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-05-15 16:36:50 +0300

    REFACTOR: Repository call removed and linq query is now located in query

--- backend/SuggestionModule/src/Application/Queries/GetBestSuggestionsQuery/GetBestSuggestionsQuery.cs
+++ backend/SuggestionModule/src/Application/Queries/GetBestSuggestionsQuery/GetBestSuggestionsQuery.cs
@@ -1,20 +1,55 @@
 ﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
 using SuggestionModule.Application.DTOs;
-using SuggestionModule.Application.Interfaces;
+using SuggestionModule.Infrastructure.Persistence;
 
 namespace SuggestionModule.Application.Queries.GetBestSuggestionsQuery
 {
     public record GetBestSuggestionsQuery(int tenantId) : IRequest<MostSuggestionsDto>;
     public class GetBestSuggestionsQueryHandler : IRequestHandler<GetBestSuggestionsQuery, MostSuggestionsDto>
     {
-        private readonly ISuggestionRepository _suggestionRepository;
-        public GetBestSuggestionsQueryHandler(ISuggestionRepository suggestionRepository) { 
-            _suggestionRepository = suggestionRepository;
+        private readonly SuggestionDbContext _context;
+        public GetBestSuggestionsQueryHandler(SuggestionDbContext context)
+        {
+            _context = context;
         }
 
-        public async Task<MostSuggestionsDto> Handle(GetBestSuggestionsQuery request, CancellationToken cancellationToken) {
-            var data = await _suggestionRepository.GetBestSuggestions(request.tenantId);
-            return data;
+        public async Task<MostSuggestionsDto> Handle(GetBestSuggestionsQuery request, CancellationToken cancellationToken)
+        {
+            var now = DateTime.UtcNow;
+            var startOfMonth = new DateTime(now.Year, now.Month, 1);
+            var startOfNextMonth = startOfMonth.AddMonths(1);
+
+
+            var best = await _context.Suggestions
+                .AsNoTracking()
+                .Where(x => x.TenantId == request.tenantId)
+                .Select(x => new BestSuggestionAllTimes
+                {
+                    Description = x.Description,
+                    Votes = x.Votes.Count()
+                })
+                .OrderByDescending(x => x.Votes)
+                .FirstOrDefaultAsync();
+
+            var month = await _context.Suggestions
+                .AsNoTracking()
+                .Where(x => x.TenantId == request.tenantId && x.Date >= startOfMonth && x.Date < startOfNextMonth)
+                .Select(x => new BestSuggestionAllTimes
+                {
+                    Description = x.Description,
+                    Votes = x.Votes.Count()
+                })
+                .OrderByDescending(x => x.Votes)
+                .FirstOrDefaultAsync();
+
+
+            return new MostSuggestionsDto
+            {
+                Best = best,
+                Month = month,
+            };
+
         }
     }
 }
