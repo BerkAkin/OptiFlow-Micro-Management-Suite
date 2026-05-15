35caec21a1712ba94a479ba7d4b0ad1d13591b2b
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-05-15 16:00:13 +0300

    REFACTOR: Location changed and now applies clean arch and DDD principles

---
+++ backend/SupportModule/src/Domain/Entities/User.cs
@@ -0,0 +1,39 @@
+﻿namespace SupportModule.Domain.Entities
+{
+    public class User
+    {
+        public int Id { get; private set; }
+        public int TenantId { get; private set; }
+        public Tenant Tenant { get; private set; }
+        public string Username { get; private set; }
+        public int DepartmentId { get; private set; }
+        public string Email { get; private set; }
+
+        private readonly List<SupportRequest> _supportRequests = new();
+        public IReadOnlyCollection<SupportRequest> SupportRequests => _supportRequests;
+
+        private readonly List<DayOff> _dayOffs = new();
+        public IReadOnlyCollection<DayOff> DayOffs => _dayOffs;
+
+        private User() { }
+        public User(string username, string email, int id, int tenantId, int departmentId)
+        {
+            if (string.IsNullOrWhiteSpace(username))
+                throw new ArgumentException("Username cannot be empty");
+            if (string.IsNullOrWhiteSpace(email))
+                throw new ArgumentException("Email cannot be empty");
+
+            Id = id;
+            Username = username;
+            Email = email;
+            TenantId = tenantId;
+            DepartmentId = departmentId;
+        }
+
+        public void AddDayOff(string Topic, string Description, int Days, DateTime StartingDate)
+        {
+            _dayOffs.Add(new DayOff(this.TenantId, Topic, Description, Days, StartingDate));
+        }
+
+    }
+}
