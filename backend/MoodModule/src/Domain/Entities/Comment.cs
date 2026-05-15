16042c505354af40b52c7a51bdbfe8e40d405c04
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-05-15 16:05:58 +0300

    REFACTOR: Renamed miniuser entity to user for data consistency

--- backend/MoodModule/src/Domain/Entities/Comment.cs
+++ backend/MoodModule/src/Domain/Entities/Comment.cs
@@ -5,16 +5,17 @@
         public int Id { get; private set; }
         public string Content { get; private set; }
         public DateTime CreatedAt { get; private set; }
-        public MiniUser User { get; private set; }
+        public User User { get; private set; }
         public int UserId { get; private set; }
         public int TenantId { get; private set; }
 
         private Comment() { }
-        public Comment(int UserId, int TenantId, string Content) {
+        public Comment(int UserId, int TenantId, string Content)
+        {
 
             this.Content = Content;
             this.UserId = UserId;
-            this.TenantId= TenantId;
+            this.TenantId = TenantId;
             CreatedAt = DateTime.UtcNow;
         }
     }
