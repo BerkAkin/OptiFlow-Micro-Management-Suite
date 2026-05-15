b32f5c99f6f58c29234185df79ebed734078aa06
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-05-15 16:43:02 +0300

    REFACTOR: Endpoint call urls updated according to backend's new endpoint namings

--- client/MainClient/src/repositories/profileRepository.ts
+++ client/MainClient/src/repositories/profileRepository.ts
@@ -1,24 +1,24 @@
 import { api } from "../lib/api";
 
 export const fetchEmailPreference = async () => {
-  const res = await api.get("/notification/emailPreference");
+  const res = await api.get("/preferences");
   return res.data;
 };
 
 export const changeEmailPreference = async () => {
-  const res = await api.post("/notification/emailPreference");
+  const res = await api.patch("/preferences");
   return res.data;
 };
 
 export const changeProfilePicture = async (formData: FormData) => {
-  const res = await api.post("/auth/upload-avatar", formData, {
+  const res = await api.post("/users/me/avatar", formData, {
     headers: { "Content-Type": "multipart/form-data" },
   });
   return res.data;
 };
 
 export const getProfilePicture = async (fileName: string) => {
-  const res = await api.get(`/auth/profile-picture/${fileName}`, {
+  const res = await api.get(`/users/me/avatar/${fileName}`, {
     responseType: "blob",
   });
   return res.data;
