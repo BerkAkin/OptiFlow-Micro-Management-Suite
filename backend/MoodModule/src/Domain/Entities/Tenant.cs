3b83d1ab1d9958a9866216d4216352d0d00e9eb1
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-05-15 16:38:43 +0300

    FEAT: Tenant entity defined

---
+++ backend/MoodModule/src/Domain/Entities/Tenant.cs
@@ -0,0 +1,30 @@
+﻿namespace MoodModule.Domain.Entities
+{
+    public class Tenant
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public bool IsActive { get; private set; }
+
+
+        private readonly List<User> _users = new();
+        public IReadOnlyCollection<User> Users => _users;
+
+
+        private Tenant() { }
+
+        public Tenant(int id, string name)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.IsActive = true;
+        }
+
+        public User AddUser(int id, string fullname, string email)
+        {
+            User usr = new User(this.Id, id, fullname, email);
+            this._users.Add(usr);
+            return usr;
+        }
+    }
+}
