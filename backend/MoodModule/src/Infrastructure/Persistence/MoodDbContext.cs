4af0e51266bd38a95691afe85540be403fe1b493
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-05-15 16:20:57 +0300

    REFACTOR: Added preferences for user tables and tenant info

--- backend/MoodModule/src/Infrastructure/Persistence/MoodDbContext.cs
+++ backend/MoodModule/src/Infrastructure/Persistence/MoodDbContext.cs
@@ -4,17 +4,38 @@ using MoodModule.Domain.Enums;
 
 namespace MoodModule.Infrastructure.Persistence
 {
-    public class MoodDbContext: DbContext
+    public class MoodDbContext : DbContext
     {
         public MoodDbContext(DbContextOptions<MoodDbContext> options) : base(options) { }
-        public DbSet<MiniUser> Users { get; set; }
+        public DbSet<Tenant> Tenants { get; set; }
+        public DbSet<User> Users { get; set; }
         public DbSet<MoodRecord> Moods { get; set; }
         public DbSet<Comment> Comments { get; set; }
- 
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
 
             base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Tenant>()
+                .HasMany(t => t.Users)
+                .WithOne(u => u.Tenant)
+                .HasForeignKey(u => u.TenantId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Tenant>()
+                .HasKey(u => u.Id);
+
+            modelBuilder.Entity<Tenant>()
+                .Property(x => x.Id)
+                .ValueGeneratedNever();
+
+            modelBuilder.Entity<User>()
+                .Property(x => x.Id)
+                .ValueGeneratedNever();
+
+            modelBuilder.Entity<User>()
+                .HasKey(u => u.Id);
 
             modelBuilder.Entity<MoodRecord>()
                 .Property(m => m.Tags)
@@ -22,15 +43,18 @@ namespace MoodModule.Infrastructure.Persistence
                 .Select(Enum.Parse<TagsEnum>)
                 .ToList());
 
-            modelBuilder.Entity<MiniUser>()
-                .HasMany(mu => mu.MoodRecords)
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.MoodRecords)
                 .WithOne(m => m.User)
+                .HasForeignKey(m => m.UserId)
                 .OnDelete(DeleteBehavior.Cascade);
 
-            modelBuilder.Entity<Comment>()
-                .HasOne(c=>c.User)
-                .WithMany(mu=>mu.Comments)
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.Comments)
+                .WithOne(c => c.User)
+                .HasForeignKey(c => c.UserId)
                 .OnDelete(DeleteBehavior.Cascade);
+
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
