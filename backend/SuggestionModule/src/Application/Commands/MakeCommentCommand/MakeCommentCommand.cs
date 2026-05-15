f84689fa8d176dca1f13b5c634d9281af389beed
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-05-15 16:37:12 +0300

    REFACTOR: Repository call removed and linq query is now located in command

--- backend/SuggestionModule/src/Application/Commands/MakeCommentCommand/MakeCommentCommand.cs
+++ backend/SuggestionModule/src/Application/Commands/MakeCommentCommand/MakeCommentCommand.cs
@@ -1,26 +1,35 @@
 ﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
 using SuggestionModule.Application.DTOs;
-using SuggestionModule.Application.Interfaces;
-using SuggestionModule.Domain.Entities;
+using SuggestionModule.Infrastructure.Persistence;
 
 namespace SuggestionModule.Application.Commands.MakeCommentCommand
 {
-    public record MakeCommentCommand(int suggestionId, CreateCommentDto comment,int userId): IRequest<Unit>;
+    public record MakeCommentCommand(int suggestionId, CreateCommentDto comment, int userId) : IRequest<Unit>;
     public class MakeCommentCommandHandler : IRequestHandler<MakeCommentCommand, Unit>
     {
-        private readonly ISuggestionRepository _repository;
-        public MakeCommentCommandHandler(ISuggestionRepository repository)
+        private readonly SuggestionDbContext _context;
+        public MakeCommentCommandHandler(SuggestionDbContext context)
         {
-            _repository = repository;
+            _context = context;
         }
 
         public async Task<Unit> Handle(MakeCommentCommand command, CancellationToken cancellationToken)
         {
-            var suggestion = await _repository.GetByIdAsync(command.suggestionId);
+            if (string.IsNullOrWhiteSpace(command.comment.Text))
+                throw new ArgumentException("Comment cannot be empty");
+
+            var suggestion = await _context.Suggestions
+                .FirstOrDefaultAsync(s => s.Id == command.suggestionId, cancellationToken);
+
+            if (suggestion == null)
+            {
+                throw new KeyNotFoundException("Suggestion does not exist");
+            }
 
             suggestion.AddComment(command.userId, command.comment.Text);
 
-            await _repository.SaveAsync();
+            await _context.SaveChangesAsync(cancellationToken);
             return Unit.Value;
         }
     }
