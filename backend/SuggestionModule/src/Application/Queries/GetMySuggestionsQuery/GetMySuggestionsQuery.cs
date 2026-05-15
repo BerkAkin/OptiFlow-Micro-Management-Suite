4be21a90c06fea7001791cac101e84aca28d788f
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-05-15 16:36:09 +0300

    FEAT: Added getter of user'S own suggestions

--- backend/SuggestionModule/src/Application/Queries/GetMySuggestionsQuery/GetMySuggestionsQuery.cs
+++ backend/SuggestionModule/src/Application/Queries/GetMySuggestionsQuery/GetMySuggestionsQuery.cs
@@ -1,21 +1,42 @@
 ﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
 using SuggestionModule.Application.DTOs;
-using SuggestionModule.Application.Interfaces;
+using SuggestionModule.Infrastructure.Persistence;
 
 namespace SuggestionModule.Application.Queries.GetSuggestionsQuery
 {
-    public record GetMySuggestionsQuery(int tenantId,int userId) : IRequest<List<SuggestionDto>>;
+    public record GetMySuggestionsQuery(int tenantId, int userId) : IRequest<List<SuggestionDto>>;
     public class GetMySuggestionsQueryHandler : IRequestHandler<GetMySuggestionsQuery, List<SuggestionDto>>
     {
 
-        private readonly ISuggestionRepository _repository;
-        public GetMySuggestionsQueryHandler(ISuggestionRepository repository)
+        private readonly SuggestionDbContext _context;
+        public GetMySuggestionsQueryHandler(SuggestionDbContext context)
         {
-            _repository = repository;
+            _context = context;
         }
         public async Task<List<SuggestionDto>> Handle(GetMySuggestionsQuery request, CancellationToken cancellationToken)
         {
-            var data = await _repository.GetMySuggestions(request.tenantId,request.userId);
+            var data = await _context.Suggestions
+                .AsNoTracking()
+                .Where(x => x.TenantId == request.tenantId && x.UserId == request.userId)
+                .Select(suggestion => new SuggestionDto
+                {
+                    Id = suggestion.Id,
+                    Description = suggestion.Description,
+                    Title = suggestion.Title,
+                    Status = suggestion.Status,
+                    Date = suggestion.Date,
+                    Votes = suggestion.Votes.Sum(v => (int)v.VoteType),
+                    Comments = suggestion.Comments
+                        .Select(comment => new CommentDto
+                        {
+                            Id = comment.Id,
+                            Text = comment.Text,
+                        })
+                        .ToList(),
+                })
+                .ToListAsync();
+
             return data;
         }
     }
