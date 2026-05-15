c9751de9f4000b070444bd69b188fb1de316067b
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-05-15 16:36:54 +0300

    REFACTOR: Repository call removed and linq query is now located in query

--- backend/SuggestionModule/src/Application/Queries/GetSuggestionsQuery/GetSuggestionsQuery.cs
+++ backend/SuggestionModule/src/Application/Queries/GetSuggestionsQuery/GetSuggestionsQuery.cs
@@ -1,18 +1,43 @@
 ﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
 using SuggestionModule.Application.DTOs;
-using SuggestionModule.Application.Interfaces;
+using SuggestionModule.Infrastructure.Persistence;
 
 namespace SuggestionModule.Application.Queries.GetSuggestionsQuery
 {
     public record GetSuggestionsQuery(int tenantId) : IRequest<List<SuggestionDto>>;
-    public class GetSuggestionsQueryHandler : IRequestHandler<GetSuggestionsQuery, List<SuggestionDto>> {
+    public class GetSuggestionsQueryHandler : IRequestHandler<GetSuggestionsQuery, List<SuggestionDto>>
+    {
 
-        private readonly ISuggestionRepository _repository;
-        public GetSuggestionsQueryHandler(ISuggestionRepository repository) { 
-            _repository = repository;
-        } 
-        public async Task<List<SuggestionDto>> Handle(GetSuggestionsQuery request,CancellationToken cancellationToken){
-            var data = await _repository.GetSuggestions(request.tenantId);
+        private readonly SuggestionDbContext _context;
+        public GetSuggestionsQueryHandler(SuggestionDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<SuggestionDto>> Handle(GetSuggestionsQuery request, CancellationToken cancellationToken)
+        {
+
+            var data = await _context.Suggestions
+                .AsNoTracking()
+                .Where(x => x.TenantId == request.tenantId)
+                .Select(suggestion => new SuggestionDto
+                {
+                    Id = suggestion.Id,
+                    Description = suggestion.Description,
+                    Title = suggestion.Title,
+                    Status = suggestion.Status,
+                    Date = suggestion.Date,
+                    Votes = suggestion.Votes.Sum(v => (int)v.VoteType),
+                    Comments = suggestion.Comments
+                    .Select(comment => new CommentDto
+                    {
+                        Id = comment.Id,
+                        Text = comment.Text,
+                    })
+                    .ToList(),
+                })
+                .ToListAsync();
+
             return data;
         }
     }
