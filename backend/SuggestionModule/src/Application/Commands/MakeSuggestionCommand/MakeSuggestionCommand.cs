9c575c341f26cf24f1b0ab82f8097fbbaf753bc0
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-05-15 16:37:09 +0300

    REFACTOR: Repository call removed and linq query is now located in command

--- backend/SuggestionModule/src/Application/Commands/MakeSuggestionCommand/MakeSuggestionCommand.cs
+++ backend/SuggestionModule/src/Application/Commands/MakeSuggestionCommand/MakeSuggestionCommand.cs
@@ -1,33 +1,35 @@
 ﻿using MediatR;
 using SuggestionModule.Application.DTOs;
-using SuggestionModule.Application.Interfaces;
-using SuggestionModule.Domain.Entities;
+using SuggestionModule.Infrastructure.Persistence;
 
 namespace SuggestionModule.Application.Commands.MakeSuggestionCommand
 {
 
 
-        public record MakeSuggestionCommand(CreateSuggestionDto suggestion,int tenantId,int userId) : IRequest<Unit>;
-        public class MakeSuggestionCommandHandler : IRequestHandler<MakeSuggestionCommand, Unit>
+    public record MakeSuggestionCommand(CreateSuggestionDto suggestion, int tenantId, int userId) : IRequest<Unit>;
+    public class MakeSuggestionCommandHandler : IRequestHandler<MakeSuggestionCommand, Unit>
+    {
+        private readonly SuggestionDbContext _context;
+        public MakeSuggestionCommandHandler(SuggestionDbContext context)
         {
-            private readonly ISuggestionRepository _repository;
-            public MakeSuggestionCommandHandler(ISuggestionRepository repository)
-            {
-                _repository = repository;
-            }
-
-            public async Task<Unit> Handle(MakeSuggestionCommand command, CancellationToken cancellationToken)
-            {
-            Suggestion suggestion = new Suggestion(
-                command.suggestion.Title,    
-                command.suggestion.Description,
-                command.userId,
-                command.tenantId
-            );
-                await _repository.MakeSuggestion(suggestion);
-                return Unit.Value;
-            }
+            _context = context;
         }
-    
+
+        public async Task<Unit> Handle(MakeSuggestionCommand command, CancellationToken cancellationToken)
+        {
+
+            var tenant = await _context.Tenants.FindAsync(command.tenantId, cancellationToken);
+
+            if (tenant is null)
+                throw new KeyNotFoundException("Company does not exist");
+
+            tenant.AddSuggestion(command.suggestion.Title, command.suggestion.Description, command.userId);
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+
 
 }
