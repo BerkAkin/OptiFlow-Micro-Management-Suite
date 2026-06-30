96c2d0447ff281a236c15b539cd1e951c3b97619
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-06-30 19:41:03 +0300

    FEAT: Date data is now formatted

--- client/MainClient/src/services/Mood/listCommentsService.ts
+++ client/MainClient/src/services/Mood/listCommentsService.ts
@@ -4,7 +4,13 @@ export const listCommentsService = async (userId: string) => {
   const data = await fetchEmployeeComments(Number(userId));
   const newData = data.map((item: any) => ({
     ...item,
-    date: item.date.split("T")[0].split("-").reverse().join("."),
+    date: new Date(item.date).toLocaleDateString("tr-TR", {
+      day: "2-digit",
+      month: "2-digit",
+      year: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
+    }),
   }));
   return newData;
 };
