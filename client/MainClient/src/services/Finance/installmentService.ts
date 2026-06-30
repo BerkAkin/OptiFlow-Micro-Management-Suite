2859c536a9d79c71256b506aeee2cbf0e2c8e3d5
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-06-30 19:40:56 +0300

    FEAT: Date data is now formatted

--- client/MainClient/src/services/Finance/installmentService.ts
+++ client/MainClient/src/services/Finance/installmentService.ts
@@ -4,7 +4,16 @@ export const installmentService = async (filters: any, page: number) => {
   const data = await fetchInstallments(filters, page);
 
   return {
-    values: data.values,
+    values: data.values.map((item: any) => ({
+      ...item,
+      date: new Date(item.date).toLocaleDateString("tr-TR", {
+        day: "2-digit",
+        month: "2-digit",
+        year: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
+      }),
+    })),
     maxPage: data.maxPage,
     filterFields: [
       {
