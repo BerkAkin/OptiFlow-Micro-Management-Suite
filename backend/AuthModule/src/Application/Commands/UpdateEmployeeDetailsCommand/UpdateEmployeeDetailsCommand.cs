1cfc03ef69ca9f2f3e833a11db7bb16144cb637f
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-06-30 17:36:47 +0300

    FEAT: Added event publishing mechanism

--- backend/AuthModule/src/Application/Commands/UpdateEmployeeDetailsCommand/UpdateEmployeeDetailsCommand.cs
+++ backend/AuthModule/src/Application/Commands/UpdateEmployeeDetailsCommand/UpdateEmployeeDetailsCommand.cs
@@ -1,41 +1,45 @@
 ﻿using AuthModule.Application.DTOs;
-using AuthModule.Domain.Enums;
+using AuthModule.Application.Services;
 using AuthModule.Infrastructure.Persistence;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using ProjectMicro.Shared.Enums;
 
 namespace AuthModule.Application.Commands.UpdateEmployeeDetailsCommand
 {
-        public record UpdateEmployeeCommand(UpdateEmployeeDetailsDto dto) : IRequest<Unit>;
-        public class UpdateEmployeeCommandHandler : IRequestHandler<UpdateEmployeeCommand, Unit>
+    public record UpdateEmployeeCommand(UpdateEmployeeDetailsDto dto) : IRequest<Unit>;
+    public class UpdateEmployeeCommandHandler : IRequestHandler<UpdateEmployeeCommand, Unit>
+    {
+        private readonly AuthDBContext _context;
+        private readonly UserUpdatedEventPublisher _publisher;
+        public UpdateEmployeeCommandHandler(AuthDBContext context, UserUpdatedEventPublisher publisher)
         {
-            private readonly AuthDBContext _context;
-            public UpdateEmployeeCommandHandler(AuthDBContext context)
-            {
-                _context = context;
-            }
+            _context = context;
+            _publisher = publisher;
+        }
 
         public async Task<Unit> Handle(UpdateEmployeeCommand command, CancellationToken cancellationToken)
         {
             var user = await _context.Users
+                .IgnoreQueryFilters()
                 .FirstOrDefaultAsync(x => x.Email == command.dto.Email, cancellationToken);
 
             if (user is null)
                 throw new Exception("Kullanıcı bulunamadı.");
 
             user.UpdateDepartment(command.dto.DepartmentId);
-           
-
-            if (Enum.TryParse<IsActiveEnum>(command.dto.IsActive, true, out var isActiveResult))
+            if (Enum.TryParse<IsActiveEnum>(command.dto.IsActive, out var isActiveResult))
             {
                 user.UpdateStatus(isActiveResult);
             }
 
             await _context.SaveChangesAsync(cancellationToken);
 
+            await _publisher.PublishEvents(user);
+
             return Unit.Value;
         }
     }
-    
+
 
 }
