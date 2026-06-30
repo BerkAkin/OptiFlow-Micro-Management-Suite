be8b0f649099bffdf4ff19e1b1047200801a5ea3
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-06-30 17:52:47 +0300

    Text formatting

--- backend/AuthModule/src/Application/Queries/GetEmployeeDetailsQuery/GetEmployeeDetailsQuery.cs
+++ backend/AuthModule/src/Application/Queries/GetEmployeeDetailsQuery/GetEmployeeDetailsQuery.cs
@@ -5,26 +5,27 @@ using Microsoft.EntityFrameworkCore;
 
 namespace AuthModule.Application.Queries.GetEmployeeDetailsQuery
 {
-    public record GetEmployeeDetailsQuery(string email,int currentTenant) : IRequest<EmployeeDetailsDto>;
-    public class GetEmployeeDetailsQueryHandler : IRequestHandler<GetEmployeeDetailsQuery,EmployeeDetailsDto>
+    public record GetEmployeeDetailsQuery(string email, int currentTenant) : IRequest<EmployeeDetailsDto>;
+    public class GetEmployeeDetailsQueryHandler : IRequestHandler<GetEmployeeDetailsQuery, EmployeeDetailsDto>
     {
         private readonly AuthDBContext _context;
         public GetEmployeeDetailsQueryHandler(AuthDBContext context)
         {
             _context = context;
         }
-        
+
         public async Task<EmployeeDetailsDto> Handle(GetEmployeeDetailsQuery query, CancellationToken cancellationToken)
         {
             var data = await _context.Users
                .AsNoTracking()
+               .IgnoreQueryFilters()
                .Where(u => u.Email == query.email && u.TenantId == query.currentTenant)
                .Select(x => new EmployeeDetailsDto
                {
-                   Email=x.Email,
+                   Email = x.Email,
                    Department = x.Department.Name,
                    DepartmentId = x.DepartmentId,
-                   IsActive= x.IsActive,
+                   IsActive = x.IsActive,
                }).FirstOrDefaultAsync(cancellationToken);
 
             return data;
