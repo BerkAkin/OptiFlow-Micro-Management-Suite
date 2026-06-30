8f83ba191dc2730b62e107b45641af084b1d8069
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-06-30 18:04:16 +0300

    FEAT: Added moodusercreated confied rabbitmq masstransit options

--- backend/MoodModule/src/Api/Program.cs
+++ backend/MoodModule/src/Api/Program.cs
@@ -1,5 +1,7 @@
+using MassTransit;
 using Microsoft.EntityFrameworkCore;
 using MoodModule.Application.Queries.GetMoodsQuery;
+using MoodModule.Infrastructure.Messaging;
 using MoodModule.Infrastructure.Persistence;
 using MoodModule.Infrastructure.Seeders;
 using ProjectMicro.Shared.Interfaces;
@@ -8,9 +10,12 @@ using ProjectMicro.Shared.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
 builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
@@ -18,11 +23,44 @@ builder.Services.AddCors(options =>
         policy.WithOrigins("http://localhost:5173").AllowAnyHeader().AllowAnyMethod().AllowCredentials();
     });
 });
+
+
 builder.Services.AddDbContext<MoodDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("MoodModuleDb")));
 builder.Services.AddHttpContextAccessor();
+
+
+
 builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(GetMoodsQuery).Assembly));
 builder.Services.AddScoped<ICurrentUserService, CurrentUserService>();
+
+
+var rabbitMqHost = builder.Configuration["RabbitMq:Host"] ?? "localhost";
+var userName = builder.Configuration["RabbitMq:UserName"] ?? "guest";
+var password = builder.Configuration["RabbitMq:Password"] ?? "guest";
+
+builder.Services.AddMassTransit(x =>
+{
+    x.AddConsumer<MoodUserCreatedConsumer>();
+    x.AddConsumer<MoodUserUpdatedConsumer>();
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.Host(rabbitMqHost, "/", h =>
+        {
+            h.Username(userName);
+            h.Password(password);
+        });
+
+        cfg.ConfigureEndpoints(context);
+    });
+});
+
+
+
 var app = builder.Build();
+
+
+
+
 
 
 app.UseCors();
