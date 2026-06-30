8e2fee625fb67e19dc4aaf06f15f16787bc52d85
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-06-30 17:57:34 +0300

    Migrations refined

--- backend/AuthModule/src/Migrations/20260518145508_acfUt.Designer.cs
+++ backend/AuthModule/src/Migrations/20260518145508_acfUt.Designer.cs
@@ -4,6 +4,7 @@ using AuthModule.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AuthModule.Migrations
 {
     [DbContext(typeof(AuthDBContext))]
-    partial class AuthDBContextModelSnapshot : ModelSnapshot
+    [Migration("20260518145508_acfUt")]
+    partial class acfUt
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ backend/AuthModule/src/Migrations/20260518145508_acfUt.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuthModule.Migrations
+{
+    /// <inheritdoc />
+    public partial class acfUt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "IsActive",
+                table: "Tenants",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Tenants");
+        }
+    }
+}
