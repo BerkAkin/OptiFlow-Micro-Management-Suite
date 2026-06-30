5d53b313c3652ec2a4ae6a57a26adab5f8a3845d
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-06-30 19:37:52 +0300

    FIX: useDetails hook's query key is now applies unique email constraints

--- client/MainClient/src/hooks/useEmployee.ts
+++ client/MainClient/src/hooks/useEmployee.ts
@@ -25,16 +25,18 @@ export const useAddEmployee = () => {
 
 export const useDetails = (email: string) => {
   return useQuery({
-    queryKey: ["employeeDetails"],
+    queryKey: ["employeeDetails", email],
     queryFn: () => employeeDetailsService(email),
   });
 };
 
 export const useUpdate = () => {
+  const queryClient = useQueryClient();
   return useMutation({
     mutationFn: (payload: any) => updateEmployeeDetailsService(payload),
     onSuccess: () => {
       console.log("Employee updated succesfully");
+      queryClient.invalidateQueries({ queryKey: ["allEmployees"] });
     },
     onError: (error: any) => {
       console.log("Error", error);
