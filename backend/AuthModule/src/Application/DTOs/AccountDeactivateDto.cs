01f3dbc4dda83f8b16333cbb8428802d22ba50b5
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-06-30 17:36:59 +0300

    FEAT: Dto for account deactivation defined

---
+++ backend/AuthModule/src/Application/DTOs/AccountDeactivateDto.cs
@@ -0,0 +1,8 @@
+﻿namespace AuthModule.Application.DTOs
+{
+    public class AccountDeactivateDto
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
