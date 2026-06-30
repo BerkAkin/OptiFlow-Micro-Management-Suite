1090ba592124d12a3ea62b0891e3dfce0ab58bc5
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-06-30 19:41:00 +0300

    FEAT: Date data is now formatted

--- client/MainClient/src/services/Finance/transactionsService.ts
+++ client/MainClient/src/services/Finance/transactionsService.ts
@@ -3,7 +3,16 @@ import { fetchLatestActivity } from "../../repositories";
 export const transactionsService = async (filters: any, page: number) => {
   const data = await fetchLatestActivity(filters, page);
   return {
-    values: [...data.values],
+    values: data.values.map((item: any) => ({
+      ...item,
+      date: new Date(item.date).toLocaleDateString("tr-TR", {
+        day: "2-digit",
+        month: "2-digit",
+        year: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
+      }),
+    })),
     maxPage: data.maxPage,
     filterFields: [
       { name: "date", type: "date" as const, placeholder: "" },
