3b81d1acb68f30f4e4185ab3ea7b00df8bca7d92
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-06-30 17:35:49 +0300

    FEAT: User status changer command implemented. Added an event to update user on other modules

---
+++ backend/AuthModule/src/Application/Commands/ChangeAccountStatusCommand/ChangeAccountStatusCommand.cs
@@ -0,0 +1,45 @@
+﻿using AuthModule.Application.DTOs;
+using AuthModule.Application.Services;
+using AuthModule.Domain.Entities;
+using AuthModule.Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using ProjectMicro.Shared.Enums;
+
+namespace AuthModule.Application.Commands.ChangeAccountStatusCommand
+{
+    public record ChangeAccountStatusCommand(int tenantId, int userId, AccountDeactivateDto dto) : IRequest<Unit>;
+    public class ChangeAccountStatusCommandHandler : IRequestHandler<ChangeAccountStatusCommand, Unit>
+    {
+        private readonly AuthDBContext _context;
+        private readonly UserUpdatedEventPublisher _eventPublisher;
+        public ChangeAccountStatusCommandHandler(AuthDBContext context, UserUpdatedEventPublisher eventPublisher)
+        {
+            _context = context;
+            _eventPublisher = eventPublisher;
+        }
+
+        public async Task<Unit> Handle(ChangeAccountStatusCommand command, CancellationToken cancellationToken)
+        {
+            User user = await _context.Users
+                .IgnoreQueryFilters()
+                .SingleOrDefaultAsync(u => u.Email == command.dto.Email
+                && u.Id == command.userId
+                && u.TenantId == command.tenantId, cancellationToken);
+
+            if (user is null)
+                throw new Exception("User does not exist");
+
+            bool passwordValid = BCrypt.Net.BCrypt.Verify(command.dto.Password, user.PasswordHash);
+            if (!passwordValid)
+                throw new Exception("Wrong email or password");
+
+            user.UpdateStatus(IsActiveEnum.Inactive);
+
+            await _context.SaveChangesAsync(cancellationToken);
+            await _eventPublisher.PublishEvents(user);
+
+            return Unit.Value;
+        }
+    }
+}
