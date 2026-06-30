f3003ce0dec39765a286796d830179afda1aeb2d
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-06-30 17:52:35 +0300

    FEAT: Added eventpublisher service to publish usercreatedevent

--- backend/AuthModule/src/Application/Commands/RegisterCommand/RegisterCommand.cs
+++ backend/AuthModule/src/Application/Commands/RegisterCommand/RegisterCommand.cs
@@ -1,4 +1,5 @@
 ﻿using AuthModule.Application.DTOs;
+using AuthModule.Application.Services;
 using AuthModule.Domain.Entities;
 using AuthModule.Infrastructure.Persistence;
 using MediatR;
@@ -7,51 +8,59 @@ using Microsoft.EntityFrameworkCore;
 namespace AuthModule.Application.Commands.RegisterCommand
 {
     public record RegisterCommand(RegisterDTO dto) : IRequest<Unit>;
-    public class RegisterCommandHandler : IRequestHandler<RegisterCommand,Unit>
+    public class RegisterCommandHandler : IRequestHandler<RegisterCommand, Unit>
     {
         private readonly AuthDBContext _context;
-        public RegisterCommandHandler(AuthDBContext context)
+        private readonly UserCreatedEventPublisher _eventPublisher;
+
+        public RegisterCommandHandler(AuthDBContext context, UserCreatedEventPublisher eventPublisher)
         {
             _context = context;
+            _eventPublisher = eventPublisher;
         }
 
         public async Task<Unit> Handle(RegisterCommand command, CancellationToken cancellationToken)
         {
-            var userExists = await _context.Users.FirstOrDefaultAsync(u => u.Email == command.dto.Email);
+            var userExists = await _context.Users
+                .IgnoreQueryFilters()
+                .FirstOrDefaultAsync(u => u.Email == command.dto.Email);
             if (userExists is not null)
-                throw new Exception("Email zaten kayıtlı");
+                throw new Exception("Email already registered");
 
             var tenantExists = await _context.Tenants.FirstOrDefaultAsync(t => t.Name == command.dto.TenantName);
 
-            if (tenantExists is null)
-            {
-                Tenant newTenant = new Tenant(
-                    command.dto.TenantName,command.dto.TenantAddress,command.dto.TenantPhoneNum,
-                    command.dto.TenantFaxNum, command.dto.TenantEmail,command.dto.TenantTaxOffice,
-                    command.dto.TenantTaxNumber,command.dto.TenantMersisNum,command.dto.TenantTradeRegistryNum
-                );
-
-                foreach (int moduleId in command.dto.SelectedModuleIds)
-                    newTenant.AssignModule(Convert.ToInt32(moduleId));
+            if (tenantExists is not null)
+                throw new Exception("Company already exists");
 
 
+            Tenant newTenant = new Tenant(
+                                    command.dto.TenantName, command.dto.TenantAddress, command.dto.TenantPhoneNum,
+                                    command.dto.TenantFaxNum, command.dto.TenantEmail, command.dto.TenantTaxOffice,
+                                    command.dto.TenantTaxNumber, command.dto.TenantMersisNum, command.dto.TenantTradeRegistryNum
+                                    );
 
-                newTenant.AddUser(
-                    command.dto.Firstname,command.dto.Lastname,command.dto.Email,
-                    BCrypt.Net.BCrypt.HashPassword(command.dto.Password),command.dto.PhoneNum,
-                    command.dto.BirthDate,command.dto.Street,command.dto.Street2,
-                    command.dto.ApartmentNum,command.dto.DoorNumber,command.dto.Province, 
-                    command.dto.District,command.dto.FullAddress,4
-                );
 
-                await _context.Tenants.AddAsync(newTenant);
-            }
-            else
-            {
-                throw new Exception("Firma ismi mevcut");
-            }
+            foreach (int moduleId in command.dto.SelectedModuleIds)
+                newTenant.AssignModule(Convert.ToInt32(moduleId));
 
-            await _context.SaveChangesAsync();
+
+
+            newTenant.AddUser(
+                                command.dto.Firstname, command.dto.Lastname, command.dto.Email,
+                                BCrypt.Net.BCrypt.HashPassword(command.dto.Password), command.dto.PhoneNum,
+                                command.dto.BirthDate, command.dto.Street, command.dto.Street2,
+                                command.dto.ApartmentNum, command.dto.DoorNumber, command.dto.Province,
+                                command.dto.District, command.dto.FullAddress, 4
+                                );
+
+            await _context.Tenants.AddAsync(newTenant);
+            await _context.SaveChangesAsync(cancellationToken);
+
+
+            var createdUser = newTenant.Users.First();
+
+            await _eventPublisher.PublishEvents(newTenant, createdUser, command.dto.SelectedModuleIds);
+
             return Unit.Value;
         }
     }
