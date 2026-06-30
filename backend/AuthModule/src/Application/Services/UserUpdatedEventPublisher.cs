fd3032e0ce227222fdc4b71cfb41db89f974ebdb
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-06-30 17:57:03 +0300

    FEAT: UserUpdated Event Publisher Service implemented

---
+++ backend/AuthModule/src/Application/Services/UserUpdatedEventPublisher.cs
@@ -0,0 +1,30 @@
+﻿using AuthModule.Domain.Entities;
+using MassTransit;
+using ProjectMicro.Shared.Events;
+
+namespace AuthModule.Application.Services
+{
+    public class UserUpdatedEventPublisher
+    {
+        private readonly IPublishEndpoint _publishEndpoint;
+        public UserUpdatedEventPublisher(IPublishEndpoint publishEndpoint)
+        {
+            _publishEndpoint = publishEndpoint;
+        }
+
+        public async Task PublishEvents(User user)
+        {
+            UserUpdatedEvent evnt = new UserUpdatedEvent()
+            {
+                UserId = user.Id,
+                TenantId = user.TenantId,
+                IsActive = user.IsActive,
+                DepartmentId = user.DepartmentId,
+                Email = user.Email,
+                Username = user.Firstname + " " + user.Lastname,
+            };
+
+            await _publishEndpoint.Publish<UserUpdatedEvent>(evnt);
+        }
+    }
+}
